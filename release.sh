5ef104e9982c3dfef050e71901ac35672da0eaf1
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2006-12-16 05:26:08 +0000

    fixed a bug
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1344 71c3de6d-444a-0410-be80-ed276b4c234a

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@
 id=$(show-pom-version target/checkout/pom.xml)
 #./publish-javadoc.sh
 javanettasks uploadFile hudson /releases/$id                "`date +"%Y/%m/%d"` release" Stable target/checkout/war/target/hudson.war
-javanettasks uploadFile hudson /releases/source-bundles/$id "`date +"%Y/%m/%d"` release" Stable target/checkout/target/hudson-1.66-src.zip
+javanettasks uploadFile hudson /releases/source-bundles/$id "`date +"%Y/%m/%d"` release" Stable target/checkout/target/hudson-$id-src.zip
 javanettasks announce hudson "Hudson $id released" << EOF
 See <a href="https://hudson.dev.java.net/changelog.html">the changelog</a> for details.
 EOF
