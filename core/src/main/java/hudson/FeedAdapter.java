258d5d490625f65bb683c3c2d453c0dee33cf6c3
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2006-12-17 18:01:17 +0000

    doc improvement.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1359 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/FeedAdapter.java
+++ core/src/main/java/hudson/FeedAdapter.java
@@ -8,8 +8,26 @@ import java.util.Calendar;
  * @author Kohsuke Kawaguchi
  */
 public interface FeedAdapter<E> {
+    /**
+     * Gets the human readable title of the entry.
+     * In RSS readers, this is usually displayed like an e-mail subject.
+     */
     String getEntryTitle(E entry);
+
+    /**
+     * Gets the URL that represents this entry.
+     * Relative to context root of the Hudson.
+     */
     String getEntryUrl(E entry);
+
+    /**
+     * Unique ID of each entry.
+     * RSS readers use this to determine what feeds are new and what are not.
+     */
     String getEntryID(E entry);
+
+    /**
+     * Timestamp of the last change in ithis entry.
+     */
     Calendar getEntryTimestamp(E entry);
 }
