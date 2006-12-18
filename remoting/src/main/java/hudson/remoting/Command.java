160627c655fcb8b56e30f97a7fe5150a42e393fe
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2006-12-18 17:27:37 +0000

    doc improvement.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1370 71c3de6d-444a-0410-be80-ed276b4c234a

--- remoting/src/main/java/hudson/remoting/Command.java
+++ remoting/src/main/java/hudson/remoting/Command.java
@@ -3,9 +3,12 @@ package hudson.remoting;
 import java.io.Serializable;
 
 /**
- * One-way command to be sent over to the remote system and executed.
- *
+ * One-way command to be sent over to the remote system and executed there.
  * This is layer 0, the lower most layer.
+ *
+ * <p>
+ * At this level, remoting of class files are not provided, so both {@link Channel}s
+ * need to have the definition of {@link Command}-implementation.
  * 
  * @author Kohsuke Kawaguchi
  */
