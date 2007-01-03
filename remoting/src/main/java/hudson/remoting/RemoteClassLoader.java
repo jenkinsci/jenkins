31ff88935774bc687f9bb1433dee8fe2c5b3a5fa
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-01-03 05:57:34 +0000

    implemented resource support.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1598 71c3de6d-444a-0410-be80-ed276b4c234a

--- remoting/src/main/java/hudson/remoting/RemoteClassLoader.java
+++ remoting/src/main/java/hudson/remoting/RemoteClassLoader.java
@@ -1,8 +1,17 @@
 package hudson.remoting;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Loads class files from the other peer through {@link Channel}.
@@ -11,6 +20,9 @@ import java.io.InputStream;
  */
 final class RemoteClassLoader extends ClassLoader {
     private final IClassLoader proxy;
+
+    private final Map<String,URL> resourceMap = new HashMap<String,URL>();
+    private final Map<String,Vector<URL>> resourcesMap = new HashMap<String,Vector<URL>>();
 
     public RemoteClassLoader(ClassLoader parent, IClassLoader proxy) {
         super(parent);
@@ -22,11 +34,58 @@ final class RemoteClassLoader extends ClassLoader {
         return defineClass(name, bytes, 0, bytes.length);
     }
 
+    protected URL findResource(String name) {
+        if(resourceMap.containsKey(name))
+            return resourceMap.get(name);
+
+        try {
+            byte[] image = proxy.getResource(name);
+            if(image==null) {
+                resourceMap.put(name,null);
+                return null;
+            }
+    
+            URL url = makeResource(name, image);
+            resourceMap.put(name,url);
+            return url;
+        } catch (IOException e) {
+            throw new Error("Unable to load resource "+name,e);
+        }
+    }
+
+    protected Enumeration<URL> findResources(String name) throws IOException {
+        Vector<URL> urls = resourcesMap.get(name);
+        if(urls!=null)
+            return urls.elements();
+
+        byte[][] images = proxy.getResources(name);
+
+        urls = new Vector<URL>();
+        for( byte[] image: images )
+            urls.add(makeResource(name,image));
+        resourcesMap.put(name,urls);
+
+        return urls.elements();
+    }
+
+    private URL makeResource(String name, byte[] image) throws IOException {
+        int idx = name.lastIndexOf('/');
+        File f = File.createTempFile("hudson-remoting","."+name.substring(idx+1));
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(image);
+        fos.close();
+        f.deleteOnExit();
+
+        return f.toURL();
+    }
+
     /**
      * Remoting interface.
      */
     /*package*/ static interface IClassLoader {
         byte[] fetch(String className) throws ClassNotFoundException;
+        byte[] getResource(String name) throws IOException;
+        byte[][] getResources(String name) throws IOException;
     }
 
     public static IClassLoader export(ClassLoader cl, Channel local) {
@@ -52,16 +111,40 @@ final class RemoteClassLoader extends ClassLoader {
             if(in==null)
                 throw new ClassNotFoundException();
 
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
             try {
-                byte[] buf = new byte[8192];
-                int len;
-                while((len=in.read(buf))>0)
-                baos.write(buf,0,len);
+                return readFully(in);
             } catch (IOException e) {
                 throw new ClassNotFoundException();
             }
+        }
+
+
+        public byte[] getResource(String name) throws IOException {
+            InputStream in = cl.getResourceAsStream(name);
+            if(in==null)   return null;
+
+            return readFully(in);
+        }
+
+        public byte[][] getResources(String name) throws IOException {
+            List<byte[]> images = new ArrayList<byte[]>();
+
+            Enumeration<URL> e = cl.getResources(name);
+            while(e.hasMoreElements()) {
+                images.add(readFully(e.nextElement().openStream()));
+            }
+
+            return images.toArray(new byte[images.size()][]);
+        }
+
+        private byte[] readFully(InputStream in) throws IOException {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            byte[] buf = new byte[8192];
+            int len;
+            while((len=in.read(buf))>0)
+                baos.write(buf,0,len);
+            in.close();
 
             return baos.toByteArray();
         }
