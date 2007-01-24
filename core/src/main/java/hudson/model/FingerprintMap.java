89a61fed3156b437002b09c9a3cb788fafd85666
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-01-24 01:26:07 +0000

    redesigned the implementation for higher concurrency.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1878 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/FingerprintMap.java
+++ core/src/main/java/hudson/model/FingerprintMap.java
@@ -5,8 +5,7 @@ import hudson.Util;
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Cache of {@link Fingerprint}s.
@@ -19,7 +18,41 @@ import java.util.Map;
  * @author Kohsuke Kawaguchi
  */
 public final class FingerprintMap {
-    private final Map<String,WeakReference<Fingerprint>> core = new HashMap<String, WeakReference<Fingerprint>>();
+    /**
+     * The value is either {@code WeakReference<Fingerprint>} or {@link Loading}.
+     *
+     * If it's {@link WeakReference}, that represents the currently available value.
+     * If it's {@link Loading}, then that indicates the fingerprint is being loaded.
+     * The thread can wait on this object to be notified when the loading completes.
+     */
+    private final ConcurrentHashMap<String,Object> core = new ConcurrentHashMap<String,Object>();
+
+    private static class Loading {
+        private Fingerprint value;
+        private boolean set;
+
+        public synchronized void set(Fingerprint value) {
+            this.set = true;
+            this.value = value;
+            notifyAll();
+        }
+
+        /**
+         * Blocks until the value is {@link #set(Fingerprint)} by another thread
+         * and returns the value.
+         */
+        public synchronized Fingerprint get() {
+            try {
+                while(!set)
+                    wait();
+                return value;
+            } catch (InterruptedException e) {
+                // assume the loading failed, but make sure we process interruption properly later
+                Thread.currentThread().interrupt();
+                return null;
+            }
+        }
+    }
 
     /**
      * Returns true if there's some data in the fingerprint database.
@@ -34,11 +67,11 @@ public final class FingerprintMap {
      *      will have this build as the owner. Otherwise null, to indicate
      *      an owner-less build.
      */
-    public synchronized Fingerprint getOrCreate(AbstractBuild build, String fileName, byte[] md5sum) throws IOException {
+    public Fingerprint getOrCreate(AbstractBuild build, String fileName, byte[] md5sum) throws IOException {
         return getOrCreate(build,fileName, Util.toHexString(md5sum));
     }
 
-    public synchronized Fingerprint getOrCreate(AbstractBuild build, String fileName, String md5sum) throws IOException {
+    public Fingerprint getOrCreate(AbstractBuild build, String fileName, String md5sum) throws IOException {
         assert build!=null;
         assert fileName!=null;
         Fingerprint fp = get(md5sum);
@@ -46,7 +79,9 @@ public final class FingerprintMap {
             return fp;  // found it.
 
         // not found. need to create one.
-        // creates a new one
+        // creates a new one.
+        // since it's nearly impossible for two different files to have the same md5 sum,
+        // this part is not synchronized.
         fp = new Fingerprint(build,fileName,toByteArray(md5sum));
 
         core.put(md5sum,new WeakReference<Fingerprint>(fp));
@@ -59,17 +94,42 @@ public final class FingerprintMap {
             return null;    // illegal input
         md5sum = md5sum.toLowerCase();
 
-        WeakReference<Fingerprint> wfp = core.get(md5sum);
-        if(wfp!=null) {
-            Fingerprint fp = wfp.get();
+        while(true) {
+            Object value = core.get(md5sum);
+
+            if(value instanceof WeakReference) {
+                WeakReference<Fingerprint> wfp = (WeakReference<Fingerprint>) value;
+                Fingerprint fp = wfp.get();
+                if(fp!=null)
+                    return fp;  // found it
+            }
+            if(value instanceof Loading) {
+                // another thread is loading it. get the value from there.
+                return ((Loading)value).get();
+            }
+
+            // the fingerprint doesn't seem to be loaded thus far, so let's load it now.
+            // the care needs to be taken that other threads might be trying to do the same.
+            Loading l = new Loading();
+            if(!core.replace(md5sum,value,l)) {
+                // the value has changed since then. another thread is attempting to do the same.
+                // go back to square 1 and try it again.
+                continue;
+            }
+
+            Fingerprint fp = Fingerprint.load(toByteArray(md5sum));
+            // let other threads know that the value is available now
+            l.set(fp);
+
+            // the map needs to be updated to reflect the result of loading
             if(fp!=null)
-                return fp;  // found it
+                core.put(md5sum,new WeakReference<Fingerprint>(fp));
+            else
+                core.put(md5sum,null);
+
+            return fp;
         }
 
-        Fingerprint fp = Fingerprint.load(toByteArray(md5sum));
-        if(fp!=null)
-            core.put(md5sum,new WeakReference<Fingerprint>(fp));
-        return fp;
     }
 
     private byte[] toByteArray(String md5sum) {
