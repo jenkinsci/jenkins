70034a5a6ae844334e193b3a9c7dfe9b1b5a1e6e
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-01-25 13:35:47 +0000

    added form validation for the timer trigger.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1912 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/triggers/TimerTrigger.java
+++ core/src/main/java/hudson/triggers/TimerTrigger.java
@@ -1,8 +1,15 @@
 package hudson.triggers;
 
 import antlr.ANTLRException;
+import static hudson.Util.fixNull;
 import hudson.model.Descriptor;
+import hudson.scheduler.CronTabList;
+import hudson.util.FormFieldValidator;
 import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 /**
  * {@link Trigger} that runs a job periodically.
@@ -22,13 +29,35 @@ public class TimerTrigger extends Trigger {
         return DESCRIPTOR;
     }
 
-    public static final Descriptor<Trigger> DESCRIPTOR = new Descriptor<Trigger>(TimerTrigger.class) {
+    public static final Descriptor<Trigger> DESCRIPTOR = new DescriptorImpl();
+
+    public static class DescriptorImpl extends Descriptor<Trigger> {
+        public DescriptorImpl() {
+            super(TimerTrigger.class);
+        }
+
         public String getDisplayName() {
             return "Build periodically";
         }
 
         public String getHelpFile() {
             return "/help/project-config/timer.html";
+        }
+
+        /**
+         * Performs syntax check.
+         */
+        public void doCheck(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+            new FormFieldValidator(req,rsp,true) {
+                protected void check() throws IOException, ServletException {
+                    try {
+                        CronTabList.create(fixNull(request.getParameter("value")));
+                        ok();
+                    } catch (ANTLRException e) {
+                        error(e.getMessage());
+                    }
+                }
+            }.process();
         }
 
         public Trigger newInstance(StaplerRequest req) throws FormException {
@@ -38,7 +67,5 @@ public class TimerTrigger extends Trigger {
                 throw new FormException(e.toString(),e,"timer_spec");
             }
         }
-    };
-
-
+    }
 }
