99e5bde6095c8960d3db9f2200a40337dbcc973e
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-01-26 14:11:06 +0000

    should accept Throwable, not just Exception.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@1942 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/IOException2.java
+++ core/src/main/java/hudson/util/IOException2.java
@@ -8,14 +8,14 @@ import java.io.IOException;
  * @author Kohsuke Kawaguchi
  */
 public class IOException2 extends IOException  {
-    private final Exception cause;
+    private final Throwable cause;
 
-    public IOException2(Exception cause) {
+    public IOException2(Throwable cause) {
         super(cause.getMessage());
         this.cause = cause;
     }
 
-    public IOException2(String s, Exception cause) {
+    public IOException2(String s, Throwable cause) {
         super(s);
         this.cause = cause;
     }
