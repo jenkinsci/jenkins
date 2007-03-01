234097273a024bf664879cbb6ad59cfae27f3bc6
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-03-01 05:33:18 +0000

    updated the test to use the listener.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@2290 71c3de6d-444a-0410-be80-ed276b4c234a

--- maven-agent/src/test/java/hudson/maven/agent/RunCommand.java
+++ maven-agent/src/test/java/hudson/maven/agent/RunCommand.java
@@ -1,6 +1,12 @@
 package hudson.maven.agent;
 
 import hudson.remoting.Callable;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.plugin.MojoExecution;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
+
+import java.io.IOException;
 
 /**
  * Starts Maven CLI. Remotely executed.
@@ -16,6 +22,17 @@ public class RunCommand implements Callable {
 
     public Object call() throws Throwable {
         // return Main.class.getClassLoader().toString();
+
+        PluginManagerInterceptor.setListener(new PluginManagerListener() {
+            public void preExecute(MavenProject project, MojoExecution exec, PlexusConfiguration mergedConfig, ExpressionEvaluator eval) throws IOException, InterruptedException, AbortException {
+                System.out.println("***** "+exec.getMojoDescriptor().getGoal());
+            }
+
+            public void postExecute(MavenProject project, MojoExecution exec, PlexusConfiguration mergedConfig, ExpressionEvaluator eval) throws IOException, InterruptedException, AbortException {
+                System.out.println("==== "+exec.getMojoDescriptor().getGoal());
+            }
+        });
+
         return Main.launch(args);
     }
 }
