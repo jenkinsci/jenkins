c9cf5985d48c97230e25dad1843d70225cdf48c7
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-03-10 06:07:10 +0000

    added a simple GUI console for launching hudson.war via JNLP.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@2390 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ war/src/launcher/java/JNLPMain.java
@@ -0,0 +1,36 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * Launches <tt>hudson.war</tt> from JNLP.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class JNLPMain {
+    public static void main(String[] args) throws Exception {
+        // don't know if this is really necessary, but jnlp-agent benefited from this,
+        // so I'm doing it here, too.
+        System.setSecurityManager(null);
+
+        // launch GUI to display output
+        setUILookAndFeel();
+        new MainDialog().setVisible(true);
+
+        Main.main(args);
+    }
+
+    /**
+     * Sets to the platform native look and feel.
+     *
+     * see http://javaalmanac.com/egs/javax.swing/LookFeelNative.html
+     */
+    public static void setUILookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (InstantiationException e) {
+        } catch (ClassNotFoundException e) {
+        } catch (UnsupportedLookAndFeelException e) {
+        } catch (IllegalAccessException e) {
+        }
+    }
+}
