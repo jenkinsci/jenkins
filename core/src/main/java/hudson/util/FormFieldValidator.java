2b9f31079de6bde63ff182943d3831bc29e2ed25
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-03-25 01:22:02 +0000

    added a convenience base class for form validator.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@2626 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/FormFieldValidator.java
+++ core/src/main/java/hudson/util/FormFieldValidator.java
@@ -1,16 +1,20 @@
 package hudson.util;
 
-import hudson.Util;
 import hudson.FilePath;
+import hudson.Util;
 import static hudson.Util.fixEmpty;
-import hudson.model.Hudson;
 import hudson.model.AbstractProject;
+import hudson.model.Hudson;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletException;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -66,6 +70,74 @@ public abstract class FormFieldValidator {
         } else {
             response.setContentType("text/html;charset=UTF-8");
             response.getWriter().print("<div class=error>"+message+"</div>");
+        }
+    }
+
+    /**
+     * Convenient base class for checking the validity of URLs 
+     */
+    public static abstract class URLCheck extends FormFieldValidator {
+
+        public URLCheck(StaplerRequest request, StaplerResponse response) {
+            // can be used to check the existence of any file in file system
+            // or other HTTP URLs inside firewall, so limit this to the admin.
+            super(request, response, true);
+        }
+
+        /**
+         * Opens the given URL and reads text content from it.
+         * This method honors Content-type header.
+         */
+        protected BufferedReader open(URL url) throws IOException {
+            // use HTTP content type to find out the charset.
+            URLConnection con = url.openConnection();
+            return new BufferedReader(
+                new InputStreamReader(con.getInputStream(),getCharset(con)));
+        }
+
+        /**
+         * Finds the string literal from the given reader.
+         * @return
+         *      true if found, false otherwise.
+         */
+        protected boolean findText(BufferedReader in, String literal) throws IOException {
+            String line;
+            while((line=in.readLine())!=null)
+                if(line.indexOf(literal)!=-1)
+                    return true;
+            return false;
+        }
+
+        /**
+         * Calls the {@link #error(String)} method with a reasonable error message.
+         * Use this method when the {@link #open(URL)} or {@link #findText(BufferedReader, String)} fails.
+         *
+         * @param url
+         *      Pass in the URL that was connected. Used for error diagnosis.
+         */
+        protected void handleIOException(String url, IOException e) throws IOException, ServletException {
+            // any invalid URL comes here
+            if(e.getMessage().equals(url))
+                // Sun JRE (and probably others too) often return just the URL in the error.
+                error("Unable to connect "+url);
+            else
+                error(e.getMessage());
+        }
+
+        /**
+         * Figures out the charset from the content-type header.
+         */
+        private String getCharset(URLConnection con) {
+            for( String t : con.getContentType().split(";") ) {
+                t = t.trim().toLowerCase();
+                if(t.startsWith("charset="))
+                    return t.substring(8);
+            }
+            // couldn't find it. HTML spec says default is US-ASCII,
+            // but UTF-8 is a better choice since
+            // (1) it's compatible with US-ASCII
+            // (2) a well-written web applications tend to use UTF-8
+            return "UTF-8";
         }
     }
 
