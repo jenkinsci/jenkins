d4850ef00c433cd2bd0552dc749e7295a733623e
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-03-25 22:42:51 +0000

    touching.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@2656 71c3de6d-444a-0410-be80-ed276b4c234a

--- remoting/src/main/java/hudson/remoting/package-info.java
+++ remoting/src/main/java/hudson/remoting/package-info.java
@@ -1,6 +1,7 @@
 /**
  * Remoting infrastructure for Hudson.
  *
+ * <p>
  * Code in this package is used for running a part of a program in slaves.
  * If you are new to this package, start from {@link Channel}.
  */
