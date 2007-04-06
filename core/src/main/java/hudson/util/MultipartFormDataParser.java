652793d06f4d8c2a8efad658180c2468c2648a5a
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-04-06 14:26:46 +0000

    added wrapper code to handle multipart/form-data becaues commons-fileupload is really tedious to use.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@2872 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ core/src/main/java/hudson/util/MultipartFormDataParser.java
@@ -0,0 +1,42 @@
+package hudson.util;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Wraps commons file-upload and handles a "multipart/form-data" form submisison
+ * (that often includes file upload.)
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class MultipartFormDataParser {
+    private final ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+    private final Map<String,FileItem> byName = new HashMap<String,FileItem>();
+
+    public MultipartFormDataParser(HttpServletRequest request) throws ServletException {
+        try {
+            for( FileItem fi : (List<FileItem>)upload.parseRequest(request) )
+                byName.put(fi.getFieldName(),fi);
+        } catch (FileUploadException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    public String get(String key) {
+        FileItem fi = byName.get(key);
+        if(fi==null)    return null;
+        return fi.getString();
+    }
+
+    public FileItem getFileItem(String key) {
+        return byName.get(key);
+    }
+}
