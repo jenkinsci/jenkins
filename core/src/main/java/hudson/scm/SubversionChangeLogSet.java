efd2e996dbcd8eb4656d48105508e3103942c1d1
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-04-12 06:42:06 +0000

    file turns out to be a better name.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@3047 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/scm/SubversionChangeLogSet.java
+++ core/src/main/java/hudson/scm/SubversionChangeLogSet.java
@@ -176,7 +176,7 @@ public final class SubversionChangeLogSet extends ChangeLogSet<LogEntry> {
         /**
          * Path in the repository. Such as <tt>/test/trunk/foo.c</tt>
          */
-        @Exposed(name="path")
+        @Exposed(name="file")
         public String getValue() {
             return value;
         }
