e21588b2bd8b663f3c506d703c29dfdd615758be
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-04-19 14:43:15 +0000

    use RuntimeException so that it can be used elsewhere.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@3178 71c3de6d-444a-0410-be80-ed276b4c234a

--- maven-interceptor/src/main/java/hudson/maven/agent/AbortException.java
+++ maven-interceptor/src/main/java/hudson/maven/agent/AbortException.java
@@ -1,13 +1,11 @@
 package hudson.maven.agent;
 
-import org.apache.maven.plugin.PluginManagerException;
-
 /**
  * Thrown when {@link PluginManagerListener} returned false to orderly
  * abort the execution. The caller shouldn't dump the stack trace for
  * this exception.
  */
-public final class AbortException extends PluginManagerException {
+public final class AbortException extends RuntimeException {
     public AbortException(String message) {
         super(message);
     }
