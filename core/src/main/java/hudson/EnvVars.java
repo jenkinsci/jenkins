027882b8a00d391f7bb870e4bd274717cf126d29
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-04-22 17:06:32 +0000

    with Java5 dependency, there's no need to use Ant for environment variables.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@3199 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/EnvVars.java
+++ core/src/main/java/hudson/EnvVars.java
@@ -1,11 +1,6 @@
 package hudson;
 
-import org.apache.tools.ant.taskdefs.Execute;
-
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -14,17 +9,5 @@ public class EnvVars {
     /**
      * Environmental variables that we've inherited.
      */
-    public static final Map<String,String> masterEnvVars;
-
-    static {
-        @SuppressWarnings("unchecked") // Ant does not use JDK 5
-        Vector<String> envs = Execute.getProcEnvironment();
-        Map<String,String> m = new HashMap<String,String>();
-        for (String e : envs) {
-            int idx = e.indexOf('=');
-            m.put(e.substring(0, idx), e.substring(idx + 1));
-        }
-        masterEnvVars = Collections.unmodifiableMap(m);
-    }
-
+    public static final Map<String,String> masterEnvVars = System.getenv();
 }
