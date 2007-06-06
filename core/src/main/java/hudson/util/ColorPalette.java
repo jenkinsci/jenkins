79a60013a5b2912c88e7c12cbfcbb1d6d9190b9d
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-06-06 15:10:57 +0000

    added color palette for line charts
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@3397 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/ColorPalette.java
+++ core/src/main/java/hudson/util/ColorPalette.java
@@ -1,6 +1,11 @@
 package hudson.util;
 
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
+
 import java.awt.Color;
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
 
 /**
  * Color constants consistent with the Hudson color palette. 
@@ -10,4 +15,23 @@ import java.awt.Color;
 public class ColorPalette {
     public static final Color RED = new Color(0xEF,0x29,0x29);
     public static final Color BLUE = new Color(0x72,0x9F,0xCF);
+
+    /**
+     * Color list usable for generating line charts.
+     */
+    public static List<Color> LINE_GRAPH = Collections.unmodifiableList(Arrays.asList(
+        new Color(0xCC0000),
+        new Color(0x3465a4),
+        new Color(0x73d216),
+        new Color(0xedd400)
+    ));
+
+    /**
+     * Applies {@link #LINE_GRAPH} colors to the given renderer.
+     */
+    public static void apply(LineAndShapeRenderer renderer) {
+        int n=0;
+        for (Color c : LINE_GRAPH)
+            renderer.setSeriesPaint(n++,c);
+    }
 }
