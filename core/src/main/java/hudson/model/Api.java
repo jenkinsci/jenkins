dcf92d7227d2e322ca3aae5497a5e2b9dc8fe68b
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-07-01 23:35:06 +0000

    supported XPath selection in the remote API.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@3601 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/Api.java
+++ core/src/main/java/hudson/model/Api.java
@@ -2,14 +2,22 @@ package hudson.model;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.Flavor;
 import org.kohsuke.stapler.export.SchemaGenerator;
 import org.kohsuke.stapler.export.ModelBuilder;
+import org.kohsuke.stapler.export.Model;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.dom4j.*;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.StringReader;
 
 /**
  * Used to expose remote access API for ".../api/"
@@ -34,8 +42,43 @@ public class Api extends AbstractModelObject {
     /**
      * Exposes the bean as XML.
      */
-    public void doXml(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-        rsp.serveExposedBean(req,bean, Flavor.XML);
+    public void doXml(StaplerRequest req, StaplerResponse rsp, @QueryParameter("xpath") String xpath) throws IOException, ServletException {
+        if(xpath==null) {
+            // serve the whole thing
+            rsp.serveExposedBean(req,bean,Flavor.XML);
+            return;
+        }
+
+        StringWriter sw = new StringWriter();
+
+        // first write to String
+        Model p = MODEL_BUILDER.get(bean.getClass());
+        p.writeTo(bean,Flavor.XML.createDataWriter(bean,sw));
+
+        // apply XPath
+        org.dom4j.Node result;
+        try {
+            Document dom = new SAXReader().read(new StringReader(sw.toString()));
+            result = dom.selectSingleNode(xpath);
+        } catch (DocumentException e) {
+            throw new IOException(e);
+        }
+
+        if(result==null) {
+            // XPath didn't match
+            rsp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            rsp.getWriter().print("XPath "+xpath+" didn't match");
+            return;
+        }
+        if(result instanceof CharacterData) {
+            rsp.setContentType("text/plain");
+            rsp.getWriter().print(result.getText());
+            return;
+        }
+
+        // otherwise XML
+        rsp.setContentType("application/xml;charset=UTF-8");
+        new XMLWriter(rsp.getWriter()).write(result);
     }
 
     /**
@@ -54,4 +97,6 @@ public class Api extends AbstractModelObject {
     public void doJson(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         rsp.serveExposedBean(req,bean, Flavor.JSON);
     }
+
+    private static final ModelBuilder MODEL_BUILDER = new ModelBuilder();
 }
