a0fc86c2d1e3115dc5eb30085bd4f6fb945b3d03
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-08-03 05:04:27 +0000

    added a Digster wrapper to work around DIGESTER-118.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@4003 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ core/src/main/java/hudson/util/Digester2.java
@@ -0,0 +1,37 @@
+package hudson.util;
+
+import org.apache.commons.digester.Digester;
+import org.apache.commons.digester.Rule;
+import org.xml.sax.Attributes;
+
+/**
+ * {@link Digester} wrapper to fix the issue DIGESTER-118.
+ *
+ * @author Kohsuke Kawaguchi
+ * @since 1.125
+ */
+public class Digester2 extends Digester {
+
+    public void addObjectCreate(String pattern, Class clazz) {
+        addRule(pattern,new ObjectCreateRule2(clazz));
+    }
+
+    private static final class ObjectCreateRule2 extends Rule {
+        private final Class clazz;
+        
+        public ObjectCreateRule2(Class clazz) {
+            this.clazz = clazz;
+        }
+
+        @Override
+        public void begin(String namespace, String name, Attributes attributes) throws Exception {
+            Object instance = clazz.newInstance();
+            digester.push(instance);
+        }
+
+        @Override
+        public void end(String namespace, String name) throws Exception {
+            digester.pop();
+        }
+    }
+}
