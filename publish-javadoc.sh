f680d41efe96cf78afeb50abb07362d8bf3c1198
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-08-03 23:29:14 +0000

    updated scripts
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@4027 71c3de6d-444a-0410-be80-ed276b4c234a

--- publish-javadoc.sh
+++ publish-javadoc.sh
@@ -14,7 +14,8 @@ cd ../../../www/javadoc
 # ignore everything under CVS, then
 # ignore all files that are already in CVS, then
 # add the rest of the files
-find . -name CVS -prune -o -exec bash in-cvs.sh {} \; -o \( -print -a -exec cvs add {} \+ \)
+#find . -name CVS -prune -o -exec bash in-cvs.sh {} \; -o \( -print -a -exec cvs add {} \+ \)
+rcvsadd . "commiting javadoc"
 
 # sometimes the first commit fails
-cvs commit -m "commit 1 " || cvs commit -m "commit 2"
+#cvs commit -m "commit 1 " || cvs commit -m "commit 2"
--- update.changelog.rb
+++ update.changelog.rb
@@ -1,12 +1,28 @@
 #!/bin/ruby
 # Updates changelog.html
 #   Usage: update.changelog.rb <nextVer> < changelog.html > output.html
-id=ARGV.shift
+
+# version number manipulation class
+class VersionNumber
+  def initialize(str)
+    @tokens = str.split(/\./)
+  end
+  def inc
+    @tokens[-1] = (@tokens[-1].to_i()+1).to_s()
+  end
+  def to_s
+    @tokens.join(".")
+  end
+end
+
+id=VersionNumber.new(ARGV.shift)
+id.inc()
+
 
 ARGF.each do |line|
   if /=BEGIN=/ =~ line
     puts line
-    puts "<a name=v#{id}><h3>What's new in 1.#{id}</h3></a>"
+    puts "<a name=v#{id}><h3>What's new in #{id}</h3></a>"
     puts "<ul class=image>"
     puts "  <li class=>"
     puts "</ul>"
