8c73c572422d087627fabec059d560afec3db2a4
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-08-04 20:38:23 +0000

    doc improvement.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@4066 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/search/QuickSilver.java
+++ core/src/main/java/hudson/search/QuickSilver.java
@@ -7,6 +7,20 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Target;
 
 /**
+ * Indicates a {@link SearchItem} field/getter.
+ *
+ * <p>
+ * Fields/getters annotated with this annotation must:
+ * <ul>
+ * <li>be on the class that extends from {@link SearchableModelObject}.
+ * <li>have the return type / field type of {@link SearchableModelObject} (or its subtype.)
+ * </ul>
+ *
+ * <p>
+ * Such getter/field indicates an edge in the search graph, and will be added
+ * automatically by {@link SearchIndexBuilder#addAllAnnotations(SearchableModelObject)}
+ * to a search index. 
+ *
  * @author Kohsuke Kawaguchi
  */
 @Retention(RUNTIME)
