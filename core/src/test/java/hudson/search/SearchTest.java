9a154d2ab0e9a6ecf100b055e4367767095b583a
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-08-08 14:45:34 +0000

    expanded test coverage.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@4140 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/test/java/hudson/search/SearchTest.java
+++ core/src/test/java/hudson/search/SearchTest.java
@@ -2,6 +2,8 @@ package hudson.search;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+
 /**
  * @author Kohsuke Kawaguchi
  */
@@ -11,12 +13,17 @@ public class SearchTest extends TestCase {
             .add("abc-def-ghi","abc def ghi")
             .add(SearchItems.create("abc","abc",
                 new SearchIndexBuilder()
-                    .add("def-ghi","def ghi")
+                    .add("def-ghi","def ghixxx")
                     .make()))
             .make();
 
         SuggestedItem x = Search.find(si, "abc def ghi");
         assertNotNull(x);
-        assertEquals(x.getUrl(),"abc-def-ghi");
+        assertEquals(x.getUrl(),"/abc-def-ghi");
+
+        List<SuggestedItem> l = Search.suggest(si, "abc def ghi");
+        assertEquals(2,l.size());
+        assertEquals("/abc-def-ghi",l.get(0).getUrl());
+        assertEquals("/abc/def-ghi",l.get(1).getUrl());
     }
 }
