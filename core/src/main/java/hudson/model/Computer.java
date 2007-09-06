55acc24bacbd8b73ad2002a013929404ff801bd9
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-09-06 04:33:55 +0000

    slave thread dump was not working in JDK5 (#792)
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@4654 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/Computer.java
+++ core/src/main/java/hudson/model/Computer.java
@@ -298,8 +298,19 @@ public abstract class Computer extends AbstractModelObject {
     private static final class GetThreadDump implements Callable<Map<String,String>,RuntimeException> {
         public Map<String,String> call() {
             Map<String,String> r = new LinkedHashMap<String,String>();
-            for (ThreadInfo ti : Functions.getThreadInfos())
-                r.put(ti.getThreadName(),Functions.dumpThreadInfo(ti));
+            try {
+                for (ThreadInfo ti : Functions.getThreadInfos())
+                    r.put(ti.getThreadName(),Functions.dumpThreadInfo(ti));
+            } catch (LinkageError _) {
+                // not in JDK6. fall back to JDK5
+                r.clear();
+                for (Map.Entry<Thread,StackTraceElement[]> t : Thread.getAllStackTraces().entrySet()) {
+                    StringBuffer buf = new StringBuffer();
+                    for (StackTraceElement e : t.getValue())
+                        buf.append(e).append('\n');
+                    r.put(t.getKey().getName(),buf.toString());
+                }
+            }
             return r;
         }
         private static final long serialVersionUID = 1L;
