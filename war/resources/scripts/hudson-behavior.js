649386e58e0b0cf663b1ebede5dcd912331b81fe
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-10-27 05:48:06 +0000

    added support for heterogeneous list support.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@5493 71c3de6d-444a-0410-be80-ed276b4c234a

--- war/resources/scripts/hudson-behavior.js
+++ war/resources/scripts/hudson-behavior.js
@@ -311,6 +311,41 @@ var hudsonRules = {
         e.onmouseout  = function(ev) { return tooltip.onContextMouseOut .call(this,YAHOO.util.Event.getEvent(ev),tooltip); }
         e.title = e.getAttribute("tooltip");
         e = null; // avoid memory leak
+    },
+
+
+    "DIV.hetero-list-container" : function(e) {
+        // components for the add button
+        var menu = document.createElement("SELECT");
+        var btn = findElementsBySelector(e,"INPUT.hetero-list-add")[0];
+        YAHOO.util.Dom.insertAfter(menu,btn);
+
+        var prototypes = e.lastChild;
+        while(!Element.hasClassName(prototypes,"prototypes"))
+            prototypes = prototypes.previousSibling;
+        var insertionPoint = prototypes.previousSibling;    // this is where the new item is inserted.
+
+        // extract templates
+        var templates = []; var i=0;
+        for(var n=prototypes.firstChild;n!=null;n=n.nextSibling,i++) {
+            var name = n.getAttribute("name");
+            menu.options[i] = new Option(n.getAttribute("title"),""+i);
+            templates.push({html:n.innerHTML, name:name});
+        }
+        Element.remove(prototypes);
+
+        var menuButton = new YAHOO.widget.Button(btn, { type: "menu", menu: menu });
+        menuButton.getMenu().clickEvent.subscribe(function(type,args,value) {
+            var t = templates[parseInt(args[1].value)]; // where this args[1] comes is a real mystery
+
+            var nc = document.createElement("div");
+            nc.className = "repeated-chunk";
+            nc.setAttribute("name",t.name);
+            nc.innerHTML = t.html;
+            insertionPoint.parentNode.insertBefore(nc, insertionPoint);
+            
+            Behaviour.applySubtree(nc);
+        });
     }
 };
 
