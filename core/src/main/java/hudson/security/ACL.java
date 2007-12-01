dd646eab1b9100481ae8f86502883bc16ee47425
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-12-01 15:15:34 +0000

    doc improvement.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@6158 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/security/ACL.java
+++ core/src/main/java/hudson/security/ACL.java
@@ -2,18 +2,32 @@ package hudson.security;
 
 import org.acegisecurity.AccessDeniedException;
 import org.acegisecurity.Authentication;
+import org.acegisecurity.context.SecurityContextHolder;
 
 /**
+ * Gate-keeper that controls access to Hudson's model objects.
+ *
  * @author Kohsuke Kawaguchi
  */
 public abstract class ACL {
     /**
      * Checks if the current security principal has this permission.
      *
+     * <p>
+     * This is just a convenience function.
+     *
      * @throws AccessDeniedException
      *      if the user doesn't have the permission.
      */
-    public abstract void checkPermission(Permission p);
+    public final void checkPermission(Permission p) {
+        Authentication a = SecurityContextHolder.getContext().getAuthentication();
 
+        if(!hasPermission(a,p))
+            throw new AccessDeniedException(a.toString()+" is missing "+p.name);
+    }
+
+    /**
+     * Checks if the given principle has the given permission.
+     */
     public abstract boolean hasPermission(Authentication a, Permission permission);
 }
