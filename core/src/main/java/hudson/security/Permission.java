e61301f004a6e83fd0c4c7ddaf0a7e04baac905c
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-12-01 15:51:37 +0000

    defined basic top-level permission constants.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@6162 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/security/Permission.java
+++ core/src/main/java/hudson/security/Permission.java
@@ -16,7 +16,15 @@ import java.util.HashMap;
 public class Permission {
     public final Class owner;
 
+    /**
+     * Human readable name of the permission.
+     *
+     * <p>
+     * This name should allow humans to uniquely identify a permission.
+     * The expected naming convention is something like "Browse Workspace".
+     */
     public final String name;
+
     /**
      * Bundled {@link Permission} that also implies this permission.
      *
@@ -63,10 +71,48 @@ public class Permission {
     /**
      * All the permissions in the system, keyed by their owners.
      */
-    private static final Map<Class, List<Permission>> PERMISSIONS = new HashMap<Class,List<Permission>>();
+    private static final Map<Class,List<Permission>> PERMISSIONS = new HashMap<Class,List<Permission>>();
 
     /**
      * The same as {@link #PERMISSIONS} but in a single list.
      */
     private static final List<Permission> ALL = new ArrayList<Permission>();
+
+//
+//
+// Root Permissions.
+//
+// These permisisons are meant to be used as the 'impliedBy' permission for other more specific permissions.
+// The intention is to allow a simplified AuthorizationStrategy implementation agnostic to
+// specific permissions.
+
+    /**
+     * Root of all permissions
+     */
+    public static final Permission FULL_CONTROL = new Permission(Permission.class,"Full Control");
+
+    /**
+     * Generic read access.
+     */
+    public static final Permission READ = new Permission(Permission.class,"Generic Read",FULL_CONTROL);
+
+    /**
+     * Generic write access.
+     */
+    public static final Permission WRITE = new Permission(Permission.class,"Generic Write",FULL_CONTROL);
+
+    /**
+     * Generic create access.
+     */
+    public static final Permission CREATE = new Permission(Permission.class,"Generic Create",WRITE);
+
+    /**
+     * Generic update access.
+     */
+    public static final Permission UPDATE = new Permission(Permission.class,"Generic Update",WRITE);
+
+    /**
+     * Generic delete access.
+     */
+    public static final Permission DELETE = new Permission(Permission.class,"Generic Delete",WRITE);
 }
