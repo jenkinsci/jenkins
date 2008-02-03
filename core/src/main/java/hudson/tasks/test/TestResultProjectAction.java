53f5f2e16a198bf4b9be68175f6421c7248ea946
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-02-03 06:42:13 +0000

    Even failed builds may still have some tests recorded (#1230)
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@6965 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/tasks/test/TestResultProjectAction.java
+++ core/src/main/java/hudson/tasks/test/TestResultProjectAction.java
@@ -49,11 +49,19 @@ public class TestResultProjectAction implements Action {
     }
 
     protected AbstractTestResultAction getLastTestResultAction() {
-        AbstractBuild<?,?> b = project.getLastSuccessfulBuild();
-        if(b!=null) {
+        final AbstractBuild<?,?> tb = project.getLastSuccessfulBuild();
+
+        AbstractBuild<?,?> b=project.getLastBuild();
+        while(b!=null) {
             AbstractTestResultAction a = b.getTestResultAction();
             if(a!=null) return a;
+            if(b==tb)
+                // if even the last successful build didn't produce the test result,
+                // that means we just don't have any tests configured.
+                return null;
+            b = b.getPreviousBuild();
         }
+
         return null;
     }
 
