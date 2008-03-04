59c9edfa38c908a3ac74012974b890ec165538a3
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-03-04 21:11:56 +0000

    if people manually access the page that expects a form submission, complain gracefully.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@7311 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/StructuredForm.java
+++ core/src/main/java/hudson/StructuredForm.java
@@ -2,6 +2,13 @@ package hudson;
 
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerResponse;
+
+import javax.servlet.http.HttpServletResponse;
+import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import javax.servlet.ServletException;
+import java.io.IOException;
 
 /**
  * Obtains the structured form data from {@link StaplerRequest}.
@@ -10,10 +17,18 @@ import org.kohsuke.stapler.StaplerRequest;
  * @author Kohsuke Kawaguchi
  */
 public class StructuredForm {
-    public static JSONObject get(StaplerRequest req) {
+    public static JSONObject get(StaplerRequest req) throws IOException, ServletException {
         JSONObject o = (JSONObject)req.getAttribute(StructuredForm.class.getName());
         if(o==null) {
-            o = JSONObject.fromObject(req.getParameter("json"));
+            String p = req.getParameter("json");
+            if(p==null) {
+                // no data submitted
+                StaplerResponse rsp = Stapler.getCurrentResponse();
+                rsp.sendError(SC_BAD_REQUEST,"This page expects a form submission");
+                rsp.getWriter().close();
+                throw new ServletException("This page expects a form submission");
+            }
+            o = JSONObject.fromObject(p);
             req.setAttribute(StructuredForm.class.getName(),o);
         }
         return o;
