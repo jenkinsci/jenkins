ca0b72dfb2f15f179d59b1b998a0d84b38c9b9da
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-03-10 05:28:33 +0000

    formatting changes
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@7480 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/matrix/MatrixRun.java
+++ core/src/main/java/hudson/matrix/MatrixRun.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Calendar;
-import java.util.Map.Entry;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.Stapler;
