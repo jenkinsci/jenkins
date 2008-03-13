99befc575ad791d8e5ab9c447d56578cc27c6c96
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-03-13 01:52:25 +0000

    typo
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@7656 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/test/java/hudson/scheduler/CrontabTest.java
+++ core/src/test/java/hudson/scheduler/CrontabTest.java
@@ -18,7 +18,7 @@ public class CrontabTest extends TestCase {
         new CronTab("@yearly");
         new CronTab("@weekly");
         new CronTab("@midnight");
-        new CronTab("@monthyl");
+        new CronTab("@monthly");
         new CronTab("0 0 * 1-10/3 *");
     }
 }
