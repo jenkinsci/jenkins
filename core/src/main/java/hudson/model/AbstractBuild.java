f85cf3e84b2cfeb88739b3a8f9bd87ae51d22c1f
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-04-07 22:14:38 +0000

    Bit of performance optimization.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@8355 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/AbstractBuild.java
+++ core/src/main/java/hudson/model/AbstractBuild.java
@@ -411,12 +411,23 @@ public abstract class AbstractBuild<P extends AbstractProject<P,R>,R extends Abs
     public String getWhyKeepLog() {
         // if any of the downstream project is configured with 'keep dependency component',
         // we need to keep this log
+        OUTER:
         for (AbstractProject<?,?> p : getParent().getDownstreamProjects()) {
             if(!p.isKeepDependencies()) continue;
 
+            AbstractBuild<?, ?> fb = p.getFirstBuild();
+            if(fb==null)        continue; // no active record
+
+            // is there any active build that depends on us?
             for(int i : getDownstreamRelationship(p).listNumbersReverse()) {
+                // TODO: this is essentially a "find intersection between two sparse sequences"
+                // and we should be able to do much better.
+
+                if(i<fb.getNumber())
+                    continue OUTER; // all the other records are younger than the first record, so pointless to search.
+
                 AbstractBuild<?, ?> b = p.getBuildByNumber(i);
-                if(b!=null && b.isKeepLog())
+                if(b!=null)
                     return Messages.AbstractBuild_KeptBecause(b);
             }
         }
