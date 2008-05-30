8a06e6a96260b79a91f522b0a693127c3e3b2b35
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-05-30 17:17:19 +0000

    TAB -> WS
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@9711 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/MyView.java
+++ core/src/main/java/hudson/model/MyView.java
@@ -12,59 +12,59 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 public class MyView extends View {
-	
-	private Hudson owner;
-	private String description;
 
-	public MyView(Hudson owner){
-		this.owner = owner;
-	}
+    private Hudson owner;
+    private String description;
 
-	@Override
-	public boolean contains(TopLevelItem item) {
-		return (item instanceof Job) && ((Job) item).hasPermission(Hudson.ADMINISTER);
-	}
+    public MyView(Hudson owner) {
+        this.owner = owner;
+    }
 
-	@Override
-	public Item doCreateItem(StaplerRequest req, StaplerResponse rsp)
-			throws IOException, ServletException {
-		return owner.doCreateItem(req, rsp);
-	}
+    @Override
+    public boolean contains(TopLevelItem item) {
+        return (item instanceof Job) && ((Job) item).hasPermission(Hudson.ADMINISTER);
+    }
 
-	@Override
-	public String getDescription() {
-		return description;
-	}
+    @Override
+    public Item doCreateItem(StaplerRequest req, StaplerResponse rsp)
+            throws IOException, ServletException {
+        return owner.doCreateItem(req, rsp);
+    }
 
-	@Override
-	public TopLevelItem getItem(String name) {
-		return owner.getItem(name);
-	}
+    @Override
+    public String getDescription() {
+        return description;
+    }
 
-	@Override
-	public Collection<TopLevelItem> getItems() {
-		List<TopLevelItem> items = new ArrayList<TopLevelItem>();
-		for (TopLevelItem item: owner.getItems()) {
-			if ((item instanceof Job) && ((Job) item).hasPermission(Job.CONFIGURE)) {
-				items.add(item);
-		 	}
-		}
-		return Collections.unmodifiableList(items);
-	}
+    @Override
+    public TopLevelItem getItem(String name) {
+        return owner.getItem(name);
+    }
 
-	@Override
-	public String getUrl() {
-		return "view/" + getViewName() + "/";
-	}
+    @Override
+    public Collection<TopLevelItem> getItems() {
+        List<TopLevelItem> items = new ArrayList<TopLevelItem>();
+        for (TopLevelItem item : owner.getItems()) {
+            if ((item instanceof Job) && ((Job) item).hasPermission(Job.CONFIGURE)) {
+                items.add(item);
+            }
+        }
+        return Collections.unmodifiableList(items);
+    }
 
-	@Override
-	public String getViewName() {
-		return "My Projects";
-	}
+    @Override
+    public String getUrl() {
+        return "view/" + getViewName() + "/";
+    }
 
-	public String getDisplayName() {
-		return "My Projects";
-	}
+    @Override
+    public String getViewName() {
+        return "My Projects";
+    }
+
+    public String getDisplayName() {
+        return "My Projects";
+    }
 
     public TopLevelItem getJob(String name) {
         return getItem(name);
@@ -73,7 +73,7 @@ public class MyView extends View {
     /**
      * Accepts the new description.
      */
-    public synchronized void doSubmitDescription( StaplerRequest req, StaplerResponse rsp ) throws IOException, ServletException {
+    public synchronized void doSubmitDescription(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         checkPermission(CONFIGURE);
 
         req.setCharacterEncoding("UTF-8");
