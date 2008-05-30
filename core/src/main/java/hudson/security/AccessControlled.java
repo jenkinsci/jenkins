434c56af6b2ee90276d732b50ea9b8fe469ab4fa
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-05-30 17:14:59 +0000

    documenting this interface.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@9708 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/security/AccessControlled.java
+++ core/src/main/java/hudson/security/AccessControlled.java
@@ -1,8 +1,26 @@
 package hudson.security;
 
+/**
+ * Object that has an {@link ACL}
+ *
+ * @since 1.220
+ */
 public interface AccessControlled {
+    /**
+     * Obtains the ACL associated with this object.
+     *
+     * @return never null.
+     */
+    ACL getACL();
 
-	void checkPermission(Permission permission);
-	boolean hasPermission(Permission permission);
-	
+    /**
+     * Convenient short-cut for {@code getACL().checkPermission(permission)}
+     */
+    void checkPermission(Permission permission);
+
+    /**
+     * Convenient short-cut for {@code getACL().hasPermission(permission)}
+     */
+    boolean hasPermission(Permission permission);
+
 }
