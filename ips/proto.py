408306af0185df215bddcd43698312593665f06d
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-06-21 17:45:00 +0000

    simplified by using the newer version of the glassfish-makepkgs package
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@10290 71c3de6d-444a-0410-be80-ed276b4c234a

--- ips/proto.py
+++ ips/proto.py
@@ -1,41 +1,22 @@
 # definition of the IPS package.
 # see http://wiki.updatecenter.java.net/Wiki.jsp?page=UC20.Docs.Packaging for more about this
 
-pkg = {
-    "name"          : "hudson",
-    "version"       : "1.227,0-0",
+import builder;
+
+pkg = builder.build_pkg(name="hudson", version="1.223,0-0")
+pkg.update({
     "attributes"    : { 
-                      "pkg.summary" : "Hudson", 
-                      "pkg.description" : "Extensible continuous integration system",
-                      },
-    "dirs"          : {},
-    "files"         : {}
-}
+        "pkg.summary" : "Hudson", 
+        "pkg.description" : "Extensible continuous integration system",
+    }
+})
 
 
-# add directories recursively, kind of like "mkdir -p"
-def mkdirs(path,attributes={}):
-    head = ""
-    for i in path.split('/'):
-        if len(i)==0 :
-            continue
-        if len(head) > 0 :
-            head += '/'
-        head += i
-        if head not in pkg["dirs"]:
-            # print 'Adding %s' % head
-            pkg["dirs"][head] = attributes
-
-# add a file, and create parent directories if necessary
-def addfile(fullpath,attributes={}):
-    components = fullpath.rpartition('/')
-    mkdirs(components[0])
-    pkg["files"][fullpath] = attributes;
-
-addfile("/usr/local/bin/hudson.war",{"file":"./hudson.war"})
-addfile("/var/svc/manifest/local/hudson.xml",{"file":"hudson.xml"})
+pkg.addfile("/usr/local/bin/hudson.war",{"file":"./hudson.war"})
+pkg.addfile("/var/svc/manifest/application/hudson.xml",{"file":"hudson.xml"})
 # this is the Hudson home directory
-mkdirs("/var/lib/hudson")
+pkg.mkdirs("/var/lib/hudson")
 
 # TODO: how do I register SMF?
 # see http://www.pauloswald.com/article/29/hudson-solaris-smf-manifest
+# see http://blogs.sun.com/wittyman/entry/postgresql_packages_from_ips_repository
