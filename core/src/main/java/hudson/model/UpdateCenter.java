07f1fbb0fe4670e831032059c42e86554ccfe1bc
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-06-23 01:10:44 +0000

    [FIXED HUDSON-1894] Add hyperlink to error message when showing UnknownHostException
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@10309 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/UpdateCenter.java
+++ core/src/main/java/hudson/model/UpdateCenter.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -366,9 +367,16 @@ public class UpdateCenter implements ModelObject {
                 testConnection(new URL("https://hudson.dev.java.net/?uctest"));
 
                 statuses.add("Success");
+            } catch (UnknownHostException e) {
+                statuses.add("<span class=error>Failed to resolve host name "+e.getMessage()+". Perhaps you need to <a href='../pluginManager/advanced'>configure HTTP proxy?</a></span>");
+                addStatus(e);
             } catch (IOException e) {
                 statuses.add(Functions.printThrowable(e));
             }
+        }
+
+        private void addStatus(UnknownHostException e) {
+            statuses.add("<pre>"+ Functions.xmlEscape(Functions.printThrowable(e))+"</pre>");
         }
 
         public String[] getStatuses() {
