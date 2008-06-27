c2235a43283a4595e5315f9fc06a50616112212b
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-06-27 16:25:13 +0000

    create IPS package during a release
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@10464 71c3de6d-444a-0410-be80-ed276b4c234a

--- ips/proto.py
+++ ips/proto.py
@@ -1,9 +1,14 @@
 # definition of the IPS package.
-# see http://wiki.updatecenter.java.net/Wiki.jsp?page=UC20.Docs.Packaging for more about this
+# see https://updatecenter2.dev.java.net/maven-makepkgs-plugin/ for more about this
 
 import builder;
 
-pkg = builder.build_pkg(name="hudson", version="1.223,0-0")
+# IPS can't do SNAPSHOT
+version = builder.props['version']
+if version.endswith("-SNAPSHOT"):
+    version = version[:-9];
+
+pkg = builder.build_pkg(name="hudson", version=version+",0-0")
 pkg.update({
     "attributes"    : { 
         "pkg.summary" : "Hudson", 
@@ -12,11 +17,11 @@ pkg.update({
 })
 
 
-pkg.addfile("/usr/local/bin/hudson.war",{"file":"./hudson.war"})
-pkg.addfile("/var/svc/manifest/application/hudson.xml",{"file":"hudson.xml"})
+pkg.addfile("/usr/local/bin/hudson.war",{"file":"./target/hudson.war"})
+pkg.addfile("/var/svc/manifest/application/hudson.xml",{"file":"../ips/hudson.xml"})
 # this is the Hudson home directory
 pkg.mkdirs("/var/lib/hudson")
 
-# TODO: how do I register SMF?
+# TODO: register SMF when the feature is available?
 # see http://www.pauloswald.com/article/29/hudson-solaris-smf-manifest
 # see http://blogs.sun.com/wittyman/entry/postgresql_packages_from_ips_repository
