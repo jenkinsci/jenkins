db7f9b50f4ce2a27b03bb3f33d2048855ab8ba47
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-08-08 22:25:46 +0000

    refactored into the base class and added a trivial test.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@11345 71c3de6d-444a-0410-be80-ed276b4c234a

--- test/src/test/java/org/jvnet/hudson/main/AppTest.java
+++ test/src/test/java/org/jvnet/hudson/main/AppTest.java
@@ -5,39 +5,39 @@ import hudson.model.Hudson;
 import hudson.model.FreeStyleBuild;
 import hudson.scm.NullSCM;
 import hudson.FilePath;
+import hudson.tasks.Shell;
 import junit.framework.TestCase;
 import org.kohsuke.stapler.Stapler;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 
+import javax.servlet.ServletContext;
 import java.io.File;
+import java.io.IOException;
+import java.io.FileInputStream;
 import java.util.concurrent.Future;
 
 /**
  * Experimenting with Hudson test suite.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends HudsonTestCase
 {
+    public AppTest(String name) {
+        super(name);
+    }
 
     public void test1() throws Exception {
-        Server server = new Server();
-        Context root = new Context(server, "/", Context.SESSIONS);
-        ServletHolder holder = new ServletHolder(new Stapler());
-        root.addServlet(holder, "/");
-        server.start();
-
-        File home = new File("data");
-        if(home.exists())
-            new FilePath(home).deleteRecursive();
-        home.mkdirs();
-        Hudson h = new Hudson(home,holder.getServletHandler().getServletContext());
-
-        FreeStyleProject project = (FreeStyleProject)h.createProject(FreeStyleProject.DESCRIPTOR, "test" );
+        FreeStyleProject project = (FreeStyleProject)hudson.createProject(FreeStyleProject.DESCRIPTOR, "test" );
         project.setScm(new NullSCM());
+        project.getBuildersList().add(new Shell("echo hello"));
         
         FreeStyleBuild build = project.scheduleBuild2(0).get();
         System.out.println(build.getDisplayName()+" completed");
+
+        String s = FileUtils.readFileToString(build.getLogFile());
+        assertTrue(s.contains("+ echo hello"));
     }
 }
---
+++ test/src/test/java/org/jvnet/hudson/main/HudsonTestCase.java
@@ -0,0 +1,57 @@
+package org.jvnet.hudson.main;
+
+import hudson.FilePath;
+import hudson.model.Hudson;
+import junit.framework.TestCase;
+import org.kohsuke.stapler.Stapler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletContext;
+import java.io.File;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public abstract class HudsonTestCase extends TestCase {
+    protected Hudson hudson;
+
+    protected HudsonTestCase(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        hudson = newHudson();
+    }
+
+    protected void tearDown() throws Exception {
+        new FilePath(hudson.root).deleteRecursive();
+    }
+
+    protected Hudson newHudson() throws Exception {
+        return new Hudson(allocateHome(), createWebServer());
+    }
+
+    protected File allocateHome() throws Exception {
+        File home = new File("data");
+        if(home.exists())
+            new FilePath(home).deleteRecursive();
+        home.mkdirs();
+        return home;
+    }
+
+    /**
+     * Prepares a webapp hosting environment to get {@link ServletContext} implementation
+     * that we need for testing.
+     */
+    protected ServletContext createWebServer() throws Exception {
+        Server server = new Server();
+        Context root = new Context(server, "/", Context.SESSIONS);
+        ServletHolder holder = new ServletHolder(new Stapler());
+        root.addServlet(holder, "/");
+        server.start();
+        return holder.getServletHandler().getServletContext();
+    }
+
+}
