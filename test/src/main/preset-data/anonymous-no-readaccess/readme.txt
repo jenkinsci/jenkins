7f7bb67948b17027787b6ab553c53115a1c87f17
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-08-15 21:21:02 +0000

    adding directories to store preset HUDSON_HOME data sets.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@11503 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ test/src/main/preset-data/anonymous-no-readaccess/readme.txt
@@ -0,0 +1,3 @@
+Hudson set up where the anonymous user do not have any permissions whatsoever (including the read access.)
+
+Any logged in user has full access to the system.
---
+++ test/src/main/preset-data/package.groovy
@@ -0,0 +1,18 @@
+import org.apache.tools.ant.taskdefs.Zip;
+import org.apache.tools.ant.Project;
+
+def baseDir = project.basedir;
+
+File targetDir = new File(baseDir,"target/classes");
+targetDir.mkdirs();
+
+File dataSetRoot = new File(baseDir,"src/main/preset-data");
+dataSetRoot.eachDir { d ->
+    if(!new File(d,"config.xml").exists())  return;
+
+    Zip zip = new Zip();
+    zip.project = new Project();
+    zip.destFile = new File(targetDir,d.name+".zip");
+    zip.basedir = d;
+    zip.execute();
+}
