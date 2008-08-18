f31658f0101ccdda4ca8713319bae8289e750e9e
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-08-18 01:56:46 +0000

    added a test case that involve security.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@11569 71c3de6d-444a-0410-be80-ed276b4c234a

--- test/src/test/java/hudson/model/AbstractProjectTest.java
+++ test/src/test/java/hudson/model/AbstractProjectTest.java
@@ -1,9 +1,12 @@
 package hudson.model;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.tasks.Shell;
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.recipes.PresetData;
+import org.jvnet.hudson.test.recipes.PresetData.DataSet;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -31,5 +34,27 @@ public class AbstractProjectTest extends HudsonTestCase {
 
         assertFalse("Workspace should be gone by now",
                 project.getWorkspace().exists());
+    }
+
+    /**
+     * Makes sure that the workspace deletion is protected.
+     */
+    @PresetData(DataSet.NO_ANONYMOUS_READACCESS)
+    public void testWipeWorkspaceProtected() throws Exception {
+        FreeStyleProject project = createFreeStyleProject();
+        project.getBuildersList().add(new Shell("echo hello"));
+
+        project.scheduleBuild2(0).get();
+
+        assertTrue("Workspace should exist by now",
+                project.getWorkspace().exists());
+
+        // make sure that the action link is protected
+        try {
+            new WebClient().getPage(project,"doWipeOutWorkspace");
+            fail("Should have failed");
+        } catch (FailingHttpStatusCodeException e) {
+            assertEquals(e.getStatusCode(),403);
+        }
     }
 }
