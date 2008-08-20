15c154946555c1d4b0721bfd551bbc50e8f16228
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-08-20 00:58:46 +0000

    Adding a test case to verify the basic configuration round-trip.
    Turns out this excercise became a real deep dive into HtmlUnit.
    
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@11627 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ test/src/test/java/hudson/model/HudsonTest.java
@@ -0,0 +1,62 @@
+package hudson.model;
+
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import hudson.model.Node.Mode;
+import hudson.security.AuthorizationStrategy;
+import hudson.security.SecurityRealm;
+import hudson.tasks.Ant;
+import hudson.tasks.Ant.AntInstallation;
+import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+import java.util.List;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class HudsonTest extends HudsonTestCase {
+    /**
+     * Performs a very basic round-trip of a non-empty system configuration screen.
+     * This makes sure that the structured form submission is working (to some limited extent.)
+     */
+    @LocalData
+    public void testSimpleConfigSubmit() throws Exception {
+        // just load the page and resubmit
+        HtmlPage configPage = new WebClient().goTo("configure");
+        HtmlForm form = configPage.getFormByName("config");
+        form.submit((HtmlButton)last(form.getHtmlElementsByTagName("button")));
+
+        // make sure all the pieces are intact
+        assertEquals(2,hudson.getNumExecutors());
+        assertSame(Mode.NORMAL,hudson.getMode());
+        assertSame(SecurityRealm.NO_AUTHENTICATION,hudson.getSecurityRealm());
+        assertSame(AuthorizationStrategy.UNSECURED,hudson.getAuthorizationStrategy());
+        assertEquals(5,hudson.getQuietPeriod());
+
+        List<JDK> jdks = hudson.getJDKs();
+        assertEquals(2,jdks.size());
+        assertJDK(jdks.get(0),"jdk1","/tmp");
+        assertJDK(jdks.get(1),"jdk2","/tmp");
+
+        AntInstallation[] ants = Ant.DESCRIPTOR.getInstallations();
+        assertEquals(2,ants.length);
+        assertAnt(ants[0],"ant1","/tmp");
+        assertAnt(ants[1],"ant2","/tmp");
+    }
+
+    private <T> T last(List<T> items) {
+        return items.get(items.size()-1);
+    }
+
+    private void assertAnt(AntInstallation ant, String name, String home) {
+        assertEquals(ant.getName(),name);
+        assertEquals(ant.getAntHome(),home);
+    }
+
+    private void assertJDK(JDK jdk, String name, String home) {
+        assertEquals(jdk.getName(),name);
+        assertEquals(jdk.getJavaHome(),home);
+    }
+}
