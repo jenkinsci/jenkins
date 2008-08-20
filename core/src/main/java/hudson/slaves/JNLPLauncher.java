31c9fbf0321e70a4e4be7d62d8157fb13daa8c06
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-08-20 18:08:36 +0000

    adding tunneling option for JNLP slave agents. This came from Ken Hofsass who needed the connection to the server be tunneled for security reasaons.
    jnlp-agent needs to be updated to take this into account.
    
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@11638 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/slaves/JNLPLauncher.java
+++ core/src/main/java/hudson/slaves/JNLPLauncher.java
@@ -2,6 +2,7 @@ package hudson.slaves;
 
 import hudson.model.Descriptor;
 import hudson.util.StreamTaskListener;
+import hudson.Util;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -11,8 +12,26 @@ import org.kohsuke.stapler.DataBoundConstructor;
  * @author Kohsuke Kawaguchi
 */
 public class JNLPLauncher extends ComputerLauncher {
+    /**
+     * If the slave needs to tunnel the connection to the master,
+     * specify the "host:port" here. This can include the special
+     * syntax "host:" and ":port" to indicate the default host/port
+     * shall be used.
+     *
+     * <p>
+     * Null if no tunneling is necessary.
+     *
+     * @since 1.250
+     */
+    public final String tunnel;
+
     @DataBoundConstructor
+    public JNLPLauncher(String tunnel) {
+        this.tunnel = Util.fixEmptyAndTrim(tunnel);
+    }
+
     public JNLPLauncher() {
+        this(null);
     }
 
     @Override
