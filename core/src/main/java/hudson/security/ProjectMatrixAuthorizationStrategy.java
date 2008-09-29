1aedfe9ea0450068dca8fa7722b85a39cec163e9
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-09-29 19:48:15 +0000

    reorganized import statements
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@12417 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/security/ProjectMatrixAuthorizationStrategy.java
+++ core/src/main/java/hudson/security/ProjectMatrixAuthorizationStrategy.java
@@ -1,31 +1,8 @@
 package hudson.security;
 
-import com.thoughtworks.xstream.converters.Converter;
-import com.thoughtworks.xstream.converters.MarshallingContext;
-import com.thoughtworks.xstream.converters.UnmarshallingContext;
-import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-
 import hudson.model.AbstractProject;
 import hudson.model.Descriptor;
-import hudson.model.Job;
-import hudson.model.JobProperty;
 import hudson.model.Jobs;
-import net.sf.json.JSONObject;
-import org.acegisecurity.Authentication;
-import org.acegisecurity.acls.sid.GrantedAuthoritySid;
-import org.acegisecurity.acls.sid.PrincipalSid;
-import org.acegisecurity.acls.sid.Sid;
-import org.kohsuke.stapler.StaplerRequest;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 /**
  * {@link GlobalMatrixAuthorizationStrategy} plus per-project ACL.
