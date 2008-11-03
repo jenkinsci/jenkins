a14c83637c09362adb46aea952099e2069f18ad6
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-11-03 21:48:42 +0000

    early disposal for better GC
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@12952 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/jna/DotNet.java
+++ core/src/main/java/hudson/util/jna/DotNet.java
@@ -17,18 +17,22 @@ public class DotNet {
         // observation in my registry shows that the actual key name can be things like "v2.0 SP1"
         // or "v2.0.50727", so the regexp is written to accomodate this.
         RegistryKey key = RegistryKey.LOCAL_MACHINE.openReadonly("SOFTWARE\\Microsoft\\.NETFramework");
-        for( String keyName : key.getSubKeys() ) {
-            Matcher m = VERSION_PATTERN.matcher(keyName);
-            if(m.matches()) {
-                int mj = Integer.parseInt(m.group(1));
-                if(mj>=major) {
-                    int mn = Integer.parseInt(m.group(2));
-                    if(mn>=minor)
-                        return true;
+        try {
+            for( String keyName : key.getSubKeys() ) {
+                Matcher m = VERSION_PATTERN.matcher(keyName);
+                if(m.matches()) {
+                    int mj = Integer.parseInt(m.group(1));
+                    if(mj>=major) {
+                        int mn = Integer.parseInt(m.group(2));
+                        if(mn>=minor)
+                            return true;
+                    }
                 }
             }
+            return false;
+        } finally {
+            key.dispose();
         }
-        return false;
     }
 
     private static final Pattern VERSION_PATTERN = Pattern.compile("v(\\d+)\\.(\\d+).*");
