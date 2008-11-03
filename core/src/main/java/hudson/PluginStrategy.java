3d3090d1db985085eaa3f0ea942543474b63bb0c
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-11-03 22:40:59 +0000

    doc improvement
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@12958 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/PluginStrategy.java
+++ core/src/main/java/hudson/PluginStrategy.java
@@ -3,7 +3,17 @@ package hudson;
 import java.io.File;
 import java.io.IOException;
 
-public interface PluginStrategy {
+/**
+ * Pluggability point for how to create {@link PluginWrapper}.
+ *
+ * <p>
+ * This extension point was added to allow plugins to be loaded into a different environment
+ * (such as loading it in an existing DI container like Plexus.) A plugin strategy is a singleton
+ * instance, and as such this feature is primarily meant for OEM.
+ *
+ * See {@link PluginManager#createPluginStrategy()} for how this instance is created.
+ */
+public interface PluginStrategy extends ExtensionPoint {
 
 	/**
 	 * Creates a plugin wrapper, which provides a management interface for the plugin
