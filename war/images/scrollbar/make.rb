236011c1669a2a01dc432584eb205a197ddbaafe
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-11-06 18:53:57 +0000

    Switched to ruby and updated to create two images, in red and blue.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@13035 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ war/images/scrollbar/make.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+# width of the band
+$u=20
+$uu=$u*2
+
+def make(step,color)
+  system "convert -size #{$uu}x8 xc:white -fill '#{color}' -stroke '#{color}' -draw \"polyline 0,#{$u-1} #{$u-1},0 #{$uu-1},0 #{$u},#{$u-1}\" -roll +#{step}+0  screen.#{step}.gif" or fail
+end
+
+colors = { "red" => "#cc0000", "blue" => "#3465a4" }
+colors.keys.each do |name|
+  list = []
+  color = colors[name];
+  
+  (0...$uu).each do |i|
+    make(i,color)
+    list << "screen.#{i}.gif"
+  end
+
+  system "convert -delay 10 #{list.join(" ")} -loop 0 progress-unknown-#{name}.gif" or fail
+end
