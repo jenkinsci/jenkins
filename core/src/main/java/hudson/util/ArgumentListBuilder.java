0dd7f1c123bc3ba72e3980ca425159d59ee061bc
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-11-12 19:37:35 +0000

    added a convenience method for adding complex property list
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@13208 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/ArgumentListBuilder.java
+++ core/src/main/java/hudson/util/ArgumentListBuilder.java
@@ -7,8 +7,12 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Map.Entry;
 import java.io.Serializable;
+import java.io.StringReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 /**
  * Used to build up arguments for a process invocation.
@@ -62,14 +66,45 @@ public class ArgumentListBuilder implements Serializable {
     }
 
     /**
-     * Adds key value pairs as "-Dkey=value -Dkey=value"...
+     * Adds key value pairs as "-Dkey=value -Dkey=value ..."
      *
-     * <tt>-D</tt> portion is configurable.
+     * <tt>-D</tt> portion is configurable as the 'prefix' parameter.
      * @since 1.114
      */
     public ArgumentListBuilder addKeyValuePairs(String prefix, Map<String,String> props) {
         for (Entry<String,String> e : props.entrySet())
             add(prefix+e.getKey()+'='+e.getValue());
+        return this;
+    }
+
+    /**
+     * Adds key value pairs as "-Dkey=value -Dkey=value ..." by parsing a given string using {@link Properties}.
+     *
+     * @param prefix
+     *      The '-D' portion of the example.
+     * @param properties
+     *      The persisted form of {@link Properties}. For example, "abc=def\nghi=jkl". Can be null, in which
+     *      case this method becomes no-op.
+     * @param vr
+     *      {@link VariableResolver} to be performed on the values.
+     * @since 1.262
+     */
+    public ArgumentListBuilder addKeyValuePairsFromPropertyString(String prefix, String properties, VariableResolver vr) throws IOException {
+        if(properties==null)    return this;
+
+        Properties p = new Properties();
+        try {
+            p.load(new StringReader(properties));
+        } catch (NoSuchMethodError e) {
+            // load(Reader) method is only available on JDK6.
+            // this fall back version doesn't work correctly with non-ASCII characters,
+            // but there's no other easy ways out it seems.
+            p.load(new ByteArrayInputStream(properties.getBytes()));
+        }
+
+        for (Entry<Object,Object> entry : p.entrySet()) {
+            args.add(prefix + entry.getKey() + "=" + Util.replaceMacro(entry.getValue().toString(),vr));
+        }
         return this;
     }
 
