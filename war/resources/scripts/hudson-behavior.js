4e630858f0a1235cb58e68ac4677fca5a3c23431
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-12-11 00:26:45 +0000

    renamed since usage stats will move to a different code
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@13498 71c3de6d-444a-0410-be80-ed276b4c234a

--- war/resources/scripts/hudson-behavior.js
+++ war/resources/scripts/hudson-behavior.js
@@ -1320,12 +1320,12 @@ var DragDrop = function(id, sGroup, config) {
 
 var updateCenter = {
     postBackURL : null,
-    usageStats: {},
+    info: {},
     completionHandler: null,
 
     checkUpdates : function() {
         var s = document.createElement("script");
-        s.setAttribute("src","https://hudson.dev.java.net/update-center.json?"+Hash.toQueryString(updateCenter.usageStats));
+        s.setAttribute("src","https://hudson.dev.java.net/update-center.json?"+Hash.toQueryString(updateCenter.info));
         document.getElementsByTagName("HEAD")[0].appendChild(s);
     },
 
