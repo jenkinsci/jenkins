bb97d31c69baea60672d556d235873dc31944016
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-12-31 21:09:53 +0000

    added more prominent indentation
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@14057 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/Indenter.java
+++ core/src/main/java/hudson/Indenter.java
@@ -13,4 +13,10 @@ public abstract class Indenter<J extends Job> {
     public final String getCss(J job) {
         return "padding-left: "+getNestLevel(job)*2+"em";
     }
+
+    public final String getRelativeShift(J job) {
+        int i = getNestLevel(job);
+        if(i==0)    return null;
+        return "position:relative; left: "+ i *2+"em";
+    }
 }
