4a646752f3ebe33c979590412f1f4a9e24646b72
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-12-31 19:58:09 +0000

    added animation
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@14054 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/resources/lib/hudson/projectViewNested.js
+++ core/src/main/resources/lib/hudson/projectViewNested.js
@@ -1,28 +1,31 @@
 hudsonRules["IMG.treeview-fold-control"] = function(e) {
-  e.onexpanded = function() {
-    var img = this;
-    var tr = findAncestor(this, "TR");
-    var tail = tr.nextSibling;
+    e.onexpanded = function() {
+        var img = this;
+        var tr = findAncestor(this, "TR");
+        var tail = tr.nextSibling;
 
-    img.oncollapsed = function() {
-      while(tr.nextSibling!=tail)
-        tr.nextSibling.remove();
-    };
+        img.oncollapsed = function() {
+            while (tr.nextSibling != tail)
+                tr.nextSibling.remove();
+        };
 
-    new Ajax.Request(
-            this.getAttribute("url"),
-    {
-      method : 'post',
-      onComplete : function(x) {
-        var cont = document.createElement("div");
-        cont.innerHTML = x.responseText;
-        var rows = $A(cont.firstChild.rows);
-        rows.reverse().each(function(r) {
-          YAHOO.util.Dom.insertAfter(r, tr);
-          Behaviour.applySubtree(r);
+        new Ajax.Request(
+                this.getAttribute("url"),
+        {
+            method : 'post',
+            onComplete : function(x) {
+                var cont = document.createElement("div");
+                cont.innerHTML = x.responseText;
+                var rows = $A(cont.firstChild.rows);
+                var anim = { opacity: { from:0, to:1 } };
+                rows.reverse().each(function(r) {
+                    YAHOO.util.Dom.setStyle(r, 'opacity', 0); // hide
+                    YAHOO.util.Dom.insertAfter(r, tr);
+                    Behaviour.applySubtree(r);
+                    new YAHOO.util.Anim(r, anim, 0.5).animate();
+                });
+            }
         });
-      }
-    });
-  };
-  e = null;
+    };
+    e = null;
 };
