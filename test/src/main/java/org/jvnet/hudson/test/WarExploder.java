c9df23cbae5ff60387fb677b7bde90645fdb6f5c
martinficker <martinficker@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-01-06 09:29:41 +0000

    Attempt to fix Issue-2605
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@14192 71c3de6d-444a-0410-be80-ed276b4c234a

--- test/src/main/java/org/jvnet/hudson/test/WarExploder.java
+++ test/src/main/java/org/jvnet/hudson/test/WarExploder.java
@@ -69,8 +69,8 @@ final class WarExploder {
             System.out.println("Exploding hudson.war at "+war);
             new FilePath(explodeDir).deleteRecursive();
             new FilePath(war).unzip(new FilePath(explodeDir));
-            if(!explodeDir.exists())    // this is supposed to be impossible, but I'm investigating HUDSON-2605
-                throw new IOException("Failed to explode "+war);
+            // Recreating the File Object seems to be necessary to fix HUDSON-2605
+            timestamp = timestamp.getCanonicalFile();
             new FileOutputStream(timestamp).close();
             timestamp.setLastModified(war.lastModified());
         } else {
