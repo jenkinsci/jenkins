9bfced3240112618919d25cc9e2bd7f5a2ca3584
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-01-09 22:29:12 +0000

    Improved the error diagnosis, after seeing http://www.nabble.com/error-installing-hudson-as-a-windows-service-tt21378003.html
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@14320 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/jna/JnaException.java
+++ core/src/main/java/hudson/util/jna/JnaException.java
@@ -8,7 +8,14 @@ import hudson.Util;
  * @author Kohsuke Kawaguchi
  */
 public class JnaException extends RuntimeException {
+    private final int errorCode;
+
     public JnaException(int errorCode) {
         super("Win32 error: "+errorCode+" - "+Util.getWin32ErrorMessage(errorCode));
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
     }
 }
