cbdbbf4619e2b4d44f0ae9d5474d185dd942fa25
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-02-01 19:10:09 +0000

    attached the execution result from a 32bit system
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@14982 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/util/kinfo_proc.c
+++ core/src/main/java/hudson/util/kinfo_proc.c
@@ -1,5 +1,20 @@
 /*
 	Shows the offsets of various fields in kinfo_proc
+
+Execution results from my laptop
+--------------------------------
+% ./a.out 
+kinfo_proc=492
+kp_proc.p_pid=24
+kp_proc.p_comm=163
+kp_eproc.e_ppid=416
+kp_eproc.e_pcred.p_ruid=280
+kp_eproc.e_pcred.p_rgid=288
+kp_eproc.e_ucred.cr_uid=304
+MAXCOMLEN=16
+[~/ws/hudson/main/core/src/main/java/hudson/util@longhorn]
+% uname -a
+Darwin longhorn.local 8.11.1 Darwin Kernel Version 8.11.1: Wed Oct 10 18:23:28 PDT 2007; root:xnu-792.25.20~1/RELEASE_I386 i386 i386
 */
 #include <stdio.h>
 #include <sys/sysctl.h>
