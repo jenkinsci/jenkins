6f0942b3621430759a37a0034f6b0fc90ff572bf
sogabe <sogabe@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-02-10 15:56:51 +0000

    Added license statement. I18N and L10n (Cause).
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@15189 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/Cause.java
+++ core/src/main/java/hudson/model/Cause.java
@@ -1,6 +1,28 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., Michael B. Donohue, Seiji Sogabe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package hudson.model;
 
-import hudson.triggers.Trigger;
 import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
@@ -24,7 +46,7 @@ public abstract class Cause {
 		@Override
 		@Exported
 		public String getShortDescription() {
-			return "Legacy code started this job.  No cause information is available";
+			return Messages.Cause_LegacyCodeCause_ShortDescription();
 		}
 	}
 	
@@ -44,7 +66,7 @@ public abstract class Cause {
 		@Override
 		@Exported
 		public String getShortDescription() {
-			return "Started by upstream project \"" + upstreamProject + "\" build number " + upstreamBuild;
+			return Messages.Cause_UpstreamCause_ShortDescription(upstreamProject, upstreamBuild);
 		}
 	}
 
@@ -58,7 +80,7 @@ public abstract class Cause {
 		@Override
 		@Exported
 		public String getShortDescription() {
-			return "Started by user " + authenticationName;
+			return Messages.Cause_UserCause_ShortDescription(authenticationName);
 		}
 	}
 }
