a9d0558e4e5d6400fd900ea5b55d65513573347f
martinficker <martinficker@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-03-09 19:18:04 +0000

    Add/Updated Copyright Header for commit 16003
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16132 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/views/ListViewColumn.java
+++ core/src/main/java/hudson/views/ListViewColumn.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  * 
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Martin Eigenbrodt
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
