e16b747b66c1827ddb35292f50fd0538ddaec2f9
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-03-23 22:52:08 +0000

    Hudson now monitors the temporary directory to forestall disk out of space problems.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16496 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/node_monitors/TemporarySpaceMonitor.java
+++ core/src/main/java/hudson/node_monitors/TemporarySpaceMonitor.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi
- * 
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,25 +23,27 @@
  */
 package hudson.node_monitors;
 
-import hudson.Extension;
-import hudson.FilePath;
-import hudson.Functions;
+import hudson.node_monitors.DiskSpaceMonitorDescriptor.DiskSpace;
 import hudson.model.Computer;
 import hudson.model.Hudson;
-import hudson.node_monitors.DiskSpaceMonitorDescriptor.DiskSpace;
-import net.sf.json.JSONObject;
+import hudson.FilePath;
+import hudson.Extension;
+import hudson.Functions;
+import hudson.remoting.VirtualChannel;
+import hudson.FilePath.FileCallable;
 import org.kohsuke.stapler.StaplerRequest;
+import org.jvnet.animal_sniffer.IgnoreJRERequirement;
+import net.sf.json.JSONObject;
 
 import java.io.IOException;
+import java.io.File;
 
 /**
- * Checks available disk space of the remote FS root.
- * Requires Mustang.
+ * Monitors the disk space of "/tmp".
  *
  * @author Kohsuke Kawaguchi
- * @since 1.123
  */
-public class DiskSpaceMonitor extends NodeMonitor {
+public class TemporarySpaceMonitor extends NodeMonitor {
     public DiskSpace getFreeSpace(Computer c) {
         return DESCRIPTOR.get(c);
     }
@@ -54,19 +56,19 @@ public class DiskSpaceMonitor extends NodeMonitor {
 
     public static final DiskSpaceMonitorDescriptor DESCRIPTOR = new DiskSpaceMonitorDescriptor() {
         public String getDisplayName() {
-            return Messages.DiskSpaceMonitor_displayName();
+            return "Free Temp Space";
         }
 
         @Override
         public NodeMonitor newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-            return new DiskSpaceMonitor();
+            return new TemporarySpaceMonitor();
         }
 
         protected DiskSpace getFreeSpace(Computer c) throws IOException, InterruptedException {
             FilePath p = c.getNode().getRootPath();
             if(p==null) return null;
 
-            return p.act(new GetUsableSpace());
+            return p.act(new GetTempSpace());
         }
     };
 
@@ -74,5 +76,23 @@ public class DiskSpaceMonitor extends NodeMonitor {
     public static DiskSpaceMonitorDescriptor install() {
         if(Functions.isMustangOrAbove())    return DESCRIPTOR;
         return null;
+    }
+
+    protected static final class GetTempSpace implements FileCallable<DiskSpace> {
+        @IgnoreJRERequirement
+        public DiskSpace invoke(File f, VirtualChannel channel) throws IOException {
+            try {
+                f = File.createTempFile("tmp-space","monitor");
+                long s = f.getUsableSpace();
+                if(s<=0)    return null;
+                return new DiskSpace(s);
+            } catch (LinkageError e) {
+                // pre-mustang
+                return null;
+            } finally {
+                f.delete();
+            }
+        }
+        private static final long serialVersionUID = 1L;
     }
 }
