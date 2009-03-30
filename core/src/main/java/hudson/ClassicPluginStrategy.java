40a050dab6ce20b19d4c3ee3a2c5119ee52c4909
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-03-30 23:02:17 +0000

    this is slated for 1.296 now
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16741 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/ClassicPluginStrategy.java
+++ core/src/main/java/hudson/ClassicPluginStrategy.java
@@ -157,12 +157,12 @@ public class ClassicPluginStrategy implements PluginStrategy {
 			}
 		}
 
-        // native m2 support moved to a plugin starting 1.295, so plugins built before that
+        // native m2 support moved to a plugin starting 1.296, so plugins built before that
         // needs to have an implicit dependency to the maven-plugin, or NoClassDefError will ensue.
         String hudsonVersion = manifest.getMainAttributes().getValue("Hudson-Version");
         String shortName = manifest.getMainAttributes().getValue("Short-Name");
         if (!"maven-plugin".equals(shortName) &&
-                (hudsonVersion == null || hudsonVersion.compareTo("1.295") <= 0)) {
+                (hudsonVersion == null || hudsonVersion.compareTo("1.296") <= 0)) {
             optionalDependencies.add(new PluginWrapper.Dependency("maven-plugin:" + Hudson.VERSION));
         }
 
