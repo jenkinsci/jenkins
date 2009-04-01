bfd251bce64265f38d96fc086e419d795f670bb2
dty <dty@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-04-01 20:42:50 +0000

    Fixed copyright attributions. The copyrights belong to my employer, not me.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16780 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/UpdateCenter.java
+++ core/src/main/java/hudson/model/UpdateCenter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  * 
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Dean Yu, Seiji Sogabe
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Yahoo! Inc., Seiji Sogabe
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
