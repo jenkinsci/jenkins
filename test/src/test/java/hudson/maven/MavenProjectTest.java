c2f46f34383a9971df021c43e40604f500b6bdc1
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-04-05 00:19:03 +0000

    [HUDSON-2154] Attempted to reproduce a problem but it worked.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16856 71c3de6d-444a-0410-be80-ed276b4c234a

--- test/src/test/java/hudson/maven/MavenProjectTest.java
+++ test/src/test/java/hudson/maven/MavenProjectTest.java
@@ -29,6 +29,7 @@ import hudson.tasks.ArtifactArchiver;
 import hudson.tasks.Maven.MavenInstallation;
 import org.jvnet.hudson.test.ExtractResourceSCM;
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.Bug;
 
 /**
  * @author huybrechts
@@ -36,23 +37,24 @@ import org.jvnet.hudson.test.HudsonTestCase;
 public class MavenProjectTest extends HudsonTestCase {
 
     public void testOnMaster() throws Exception {
-        MavenModuleSet project = createMavenProject();
-        MavenInstallation mi = configureDefaultMaven();
-        project.setScm(new ExtractResourceSCM(getClass().getResource(
-                "/simple-projects.zip")));
+        MavenModuleSet project = createSimpleProject();
         project.setGoals("validate");
-        project.setMaven(mi.getName());
 
         assertBuildStatusSuccess(project.scheduleBuild2(0).get());
     }
 
-    public void testOnSlave() throws Exception {
+    private MavenModuleSet createSimpleProject() throws Exception {
         MavenModuleSet project = createMavenProject();
         MavenInstallation mi = configureDefaultMaven();
         project.setScm(new ExtractResourceSCM(getClass().getResource(
                 "/simple-projects.zip")));
-        project.setGoals("validate");
         project.setMaven(mi.getName());
+        return project;
+    }
+
+    public void testOnSlave() throws Exception {
+        MavenModuleSet project = createSimpleProject();
+        project.setGoals("validate");
         project.setAssignedLabel(createSlave().getSelfLabel());
 
         assertBuildStatusSuccess(project.scheduleBuild2(0).get());
@@ -61,7 +63,7 @@ public class MavenProjectTest extends HudsonTestCase {
     /**
      * Makes sure that {@link ArtifactArchiver} doesn't show up in the m2 job type config screen.
      * This is to make sure that the exclusion in {@link MavenModuleSet.DescriptorImpl#isApplicable(Descriptor)}
-     * is working. 
+     * is working.
      */
     public void testExclusion() throws Exception {
         MavenModuleSet p = createMavenProject();
@@ -69,5 +71,16 @@ public class MavenProjectTest extends HudsonTestCase {
         assertFalse(page.getWebResponse().getContentAsString().contains(hudson.getDescriptorByType(ArtifactArchiver.DescriptorImpl.class).getDisplayName()));
         // but this should exist. This verifies that the approach of the test is sane (and for example, to make sure getContentAsString()!="")
         assertTrue(page.getWebResponse().getContentAsString().contains(hudson.getDescriptorByType(RedeployPublisher.DescriptorImpl.class).getDisplayName()));
+    }
+
+    /**
+     * Attempt to reproduce #2154, but this appears to work.
+     */
+    @Bug(2154)
+    public void testSiteBuild() throws Exception {
+        MavenModuleSet project = createSimpleProject();
+        project.setGoals("site");
+
+        assertBuildStatusSuccess(project.scheduleBuild2(0).get());
     }
 }
