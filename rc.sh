ea7f371df88a472c2c4c31be53bde08e02a56b5b
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-04-08 21:04:19 +0000

    wrong directory
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16929 71c3de6d-444a-0410-be80-ed276b4c234a

--- rc.sh
+++ rc.sh
@@ -41,7 +41,7 @@ svn rm -m "deleting the old RC branch" $RC
 svn cp -m "creating a new RC branch" $repo/trunk/hudson/main $RC
 
 # update changelog.html
-WWW=../www
+WWW=../../www
 svn up $WWW/changelog.html
 ruby rc.changelog.rb < $WWW/changelog.html > $WWW/changelog.new
 mv $WWW/changelog.new $WWW/changelog.html
