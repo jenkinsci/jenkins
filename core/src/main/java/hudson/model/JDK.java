5ad3cf61f39f7ac69560200dec29d1420234bdfa
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-04-10 01:05:12 +0000

    this doesn't belong to JDK
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@16992 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/JDK.java
+++ core/src/main/java/hudson/model/JDK.java
@@ -95,10 +95,7 @@ public final class JDK extends ToolInstallation implements NodeSpecific<JDK>, En
     public void buildEnvVars(Map<String,String> env) {
         // see EnvVars javadoc for why this adss PATH.
         env.put("PATH+JDK",getBinDir().getPath());
-
         env.put("JAVA_HOME",getJavaHome());
-        if(!env.containsKey("HUDSON_HOME"))
-            env.put("HUDSON_HOME", Hudson.getInstance().getRootDir().getPath() );
     }
 
     public JDK forNode(Node node) {
