70335eba9802c96b3778322c56649b70b05b3547
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-04-10 06:30:29 +0000

    verifying that the stream is connected
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@17025 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/test/java/hudson/os/SUTester.java
+++ core/src/test/java/hudson/os/SUTester.java
@@ -10,8 +10,9 @@ import java.io.FileOutputStream;
  */
 public class SUTester {
     public static void main(String[] args) throws Throwable {
-        SU.execute(new StreamTaskListener(System.out),null,null,new Callable<Object, Throwable>() {
+        SU.execute(new StreamTaskListener(System.out),"kohsuke","bogus",new Callable<Object, Throwable>() {
             public Object call() throws Throwable {
+                System.out.println("Touching /tmp/x");
                 new FileOutputStream("/tmp/x").close();
                 return null;
             }
