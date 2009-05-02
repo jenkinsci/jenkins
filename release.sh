f3b1920f3039787552540ea4e5b1d01a3ba940df
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-05-02 01:24:24 +0000

    multiple test exuections slow down a release. Let's do it just once during release:perform and be done with it
    git-svn-id: https://hudson.dev.java.net/svn/hudson/branches/rc@17673 71c3de6d-444a-0410-be80-ed276b4c234a

--- release.sh
+++ release.sh
@@ -33,7 +33,7 @@ rm hudson.war || true
 
 tag=hudson-$(show-pom-version pom.xml | sed -e "s/-SNAPSHOT//g" -e "s/\\./_/g")
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
-mvn -B -Dtag=$tag release:prepare || mvn -B -Dtag=$tag install release:prepare || true
+mvn -B -Dtag=$tag -DskipTests release:prepare || mvn -B -Dtag=$tag -DskipTests install release:prepare || true
 #svn up -r head
 #mvn -B -Dtag=$tag -Dresume release:prepare
 mvn release:perform
