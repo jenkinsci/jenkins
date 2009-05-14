501a78fc39b74512aa124e62a640c0a379b209af
jglick <jglick@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-05-14 23:30:25 +0000

    I18N.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@18100 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/tools/InstallSourceProperty.java
+++ core/src/main/java/hudson/tools/InstallSourceProperty.java
@@ -62,7 +62,7 @@ public class InstallSourceProperty extends ToolProperty<ToolInstallation> {
     @Extension
    public static class DescriptorImpl extends ToolPropertyDescriptor {
         public String getDisplayName() {
-            return "Install automatically";
+            return Messages.InstallSourceProperty_DescriptorImpl_displayName();
         }
     }
 }
