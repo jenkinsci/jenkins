daccd83bcad783c96e6aaa97babe65cba51f205b
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-05-18 18:55:37 +0000

    no stack trace with AbortException
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@18188 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/slaves/SlaveComputer.java
+++ core/src/main/java/hudson/slaves/SlaveComputer.java
@@ -34,6 +34,7 @@ import hudson.util.Futures;
 import hudson.FilePath;
 import hudson.lifecycle.WindowsSlaveInstaller;
 import hudson.Util;
+import hudson.AbortException;
 
 import java.io.File;
 import java.io.OutputStream;
@@ -168,6 +169,9 @@ public class SlaveComputer extends Computer {
                 try {
                     launcher.launch(SlaveComputer.this, listener);
                     return null;
+                } catch (AbortException e) {
+                    listener.error(e.getMessage());
+                    throw e;
                 } catch (IOException e) {
                     Util.displayIOException(e,listener);
                     e.printStackTrace(listener.error(Messages.ComputerLauncher_unexpectedError()));
