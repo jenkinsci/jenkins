e950c0e93ce48bd438f766b4b845c8683089a20d
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-07-01 23:25:31 +0000

    added a pointer back to Wiki
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@19351 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/os/windows/ManagedWindowsServiceLauncher.java
+++ core/src/main/java/hudson/os/windows/ManagedWindowsServiceLauncher.java
@@ -182,7 +182,11 @@ public class ManagedWindowsServiceLauncher extends ComputerLauncher {
         } catch (SmbException e) {
             e.printStackTrace(listener.error(e.getMessage()));
         } catch (JIException e) {
-            e.printStackTrace(listener.error(e.getMessage()));
+            if(e.getErrorCode()==5)
+                // access denied error
+                e.printStackTrace(listener.error(Messages.ManagedWindowsServiceLauncher_AccessDenied()));
+            else
+                e.printStackTrace(listener.error(e.getMessage()));
         } catch (DocumentException e) {
             e.printStackTrace(listener.error(e.getMessage()));
         }
