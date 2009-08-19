47bcabbcd0dfb008db3d34aa6f6880679523771a
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-08-19 19:11:20 +0000

    the right place to do tracking initialization is when we create a branch
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@20907 71c3de6d-444a-0410-be80-ed276b4c234a

--- rc-merge.sh
+++ rc-merge.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -ex
 # merge back the RC branch.
-svnmerge init -F https://www.dev.java.net/svn/hudson/branches/rc
-svn commit -F svnmerge-commit-message.txt
 svnmerge merge -S rc .
 svn commit -F svnmerge-commit-message.txt
