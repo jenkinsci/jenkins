a71e8fc6b1bb4c027c03ca4b81758a41236eccd5
mindless <mindless@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-10-13 21:38:00 +0000

    [FIXED HUDSON-4414] attach selector to correct button when nested content also uses f:hetero-list.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@22720 71c3de6d-444a-0410-be80-ed276b4c234a

--- war/resources/scripts/hudson-behavior.js
+++ war/resources/scripts/hudson-behavior.js
@@ -285,7 +285,8 @@ var hudsonRules = {
 
         // components for the add button
         var menu = document.createElement("SELECT");
-        var btn = findElementsBySelector(e,"INPUT.hetero-list-add")[0];
+        var btns = findElementsBySelector(e,"INPUT.hetero-list-add"),
+            btn = btns[btns.length-1]; // In case nested content also uses hetero-list
         YAHOO.util.Dom.insertAfter(menu,btn);
 
         var prototypes = e.lastChild;
