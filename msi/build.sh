522778230542b3310426b8c41f4dc840b40237d6
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-10-28 03:56:27 +0000

    merging both into a single script
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@23268 71c3de6d-444a-0410-be80-ed276b4c234a

--- msi/build.sh
+++ msi/build.sh
@@ -6,3 +6,13 @@ unzip -p tmp/core.jar windows-service/hudson.exe > tmp/hudson.exe
 unzip -p tmp/core.jar windows-service/hudson.xml > tmp/hudson.xm_
 # replace executable name to the bundled JRE
 sed -e 's|executable.*|executable>%BASE%\\jre\\bin\\java</executable>|' < tmp/hudson.xm_ > tmp/hudson.xml
+
+# capture JRE
+JREDIR='c:\Program Files\Java\jre6'
+heat dir "$JREDIR" -o jre.wxs -sfrag -sreg -nologo -srd -gg -cg JreComponents -dr JreDir -var var.JreDir
+
+for v in 1.100 1.101
+do
+  candle -dVERSION=$v -dJreDir="$JREDIR" -nologo -ext WixUIExtension -ext WixUtilExtension hudson.wxs jre.wxs
+  light -o hudson-$v.msi -nologo -ext WixUIExtension -ext WixUtilExtension hudson.wixobj jre.wixobj
+done
