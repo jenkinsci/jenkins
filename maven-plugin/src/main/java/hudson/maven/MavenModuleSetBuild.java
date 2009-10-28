86f6a4f96e5c45592c7e01787b035dc7f6f7da02
abayer <abayer@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-10-28 20:28:32 +0000

    Improving logging for debugging HUDSON-3273
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@23288 71c3de6d-444a-0410-be80-ed276b4c234a

--- maven-plugin/src/main/java/hudson/maven/MavenModuleSetBuild.java
+++ maven-plugin/src/main/java/hudson/maven/MavenModuleSetBuild.java
@@ -503,10 +503,10 @@ public class MavenModuleSetBuild extends AbstractMavenBuild<MavenModuleSet,Maven
                     listener.error(e.getMessage());
                 return Result.FAILURE;
             } catch (InterruptedIOException e) {
-                listener.error("Aborted");
+                e.printStackTrace(listener.error("Aborted Maven execution for InterruptedIOException"));
                 return Result.ABORTED;
             } catch (InterruptedException e) {
-                listener.error("Aborted");
+                e.printStackTrace(listener.error("Aborted Maven execution for InterruptedException"));
                 return Result.ABORTED;
             } catch (IOException e) {
                 e.printStackTrace(listener.error(Messages.MavenModuleSetBuild_FailedToParsePom()));
