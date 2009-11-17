ea2240dfdac379a00e6d6a09229d28ddb76dcd44
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-11-17 21:13:38 +0000

    resurrected the original scripts from http://linux01.gwdg.de/~pbleser/files/rpm/hudson/
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@23787 71c3de6d-444a-0410-be80-ed276b4c234a

--- opensuse/rsync.sh
+++ opensuse/rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 createrepo .
-rsync -avz *.key readme.html hudson.repo RPMS repodata hudson-ci.org:~/www/hudson-ci.org/redhat
+rsync -avz *.key readme.html hudson.repo RPMS repodata hudson-ci.org:~/www/hudson-ci.org/opensuse
