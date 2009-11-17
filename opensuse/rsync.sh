9e67f6d1dbc925a32092e07bab9aa643f4bcb587
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-11-17 22:25:33 +0000

    sign the repo, in the OpenSUSE style
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@23798 71c3de6d-444a-0410-be80-ed276b4c234a

--- opensuse/rsync.sh
+++ opensuse/rsync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -ex
 createrepo .
+GPG_AGENT_INFO= ./repomd-sign $(cat ~/.gpg.passphrase) repodata/repomd.xml
+cp hudson-ci.org.key repodata/repomd.xml.key
 rsync -avz *.key readme.html hudson.repo RPMS repodata hudson-ci.org:~/www/hudson-ci.org/opensuse
