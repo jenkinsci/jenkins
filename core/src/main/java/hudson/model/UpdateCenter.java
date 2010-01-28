eade4cca6565994d55e67889720851c10e9656a7
mindless <mindless@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-01-28 18:09:38 +0000

    [FIXED HUDSON-2313] Show which plugins have already been upgraded in Plugin Manager. Also some css cleanup in this area.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@26639 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/model/UpdateCenter.java
+++ core/src/main/java/hudson/model/UpdateCenter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  * 
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Yahoo! Inc., Seiji Sogabe
+ * Copyright (c) 2004-2010, Sun Microsystems, Inc., Kohsuke Kawaguchi, Yahoo! Inc., Seiji Sogabe
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -59,6 +59,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -150,6 +151,22 @@ public class UpdateCenter extends AbstractModelObject implements Saveable {
         synchronized (jobs) {
             return new ArrayList<UpdateCenterJob>(jobs);
         }
+    }
+
+    /**
+     * Returns latest install/upgrade job for the given plugin.
+     * @return InstallationJob or null if not found
+     */
+    public InstallationJob getJob(Plugin plugin) {
+        List<UpdateCenterJob> jobList = getJobs();
+        Collections.reverse(jobList);
+        for (UpdateCenterJob job : jobList)
+            if (job instanceof InstallationJob) {
+                InstallationJob ij = (InstallationJob)job;
+                if (ij.plugin.name.equals(plugin.name) && ij.plugin.sourceId.equals(plugin.sourceId))
+                    return ij;
+            }
+        return null;
     }
 
     /**
