773d1078716b2f5b7dd013950ce2bdf49dab31fd
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-03-02 19:55:04 +0000

    Added a mechanism to bundle <tt>init.groovy</tt> inside the war for OEM.     (<a href="http://n4.nabble.com/preconfigured-hudson-war-tp1575216p1575216.html">report</a>)
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@28144 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/init/impl/GroovyInitScript.java
+++ core/src/main/java/hudson/init/impl/GroovyInitScript.java
@@ -23,15 +23,19 @@
  */
 package hudson.init.impl;
 
+import groovy.lang.GroovyCodeSource;
 import groovy.lang.GroovyShell;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Logger;
 
+import hudson.FilePath;
 import hudson.model.Hudson;
 import static hudson.init.InitMilestone.JOB_LOADED;
 import hudson.init.Initializer;
+import org.apache.commons.io.FileUtils;
 
 /**
  * Run the initialization script, if it exists.
@@ -41,13 +45,22 @@ import hudson.init.Initializer;
 public class GroovyInitScript {
     @Initializer(after=JOB_LOADED)
     public static void init(Hudson h) throws IOException {
+        URL bundledInitScript = h.servletContext.getResource("/WEB-INF/init.groovy");
+        if (bundledInitScript!=null) {
+            LOGGER.info("Executing bundled init script: "+bundledInitScript);
+            execute(new GroovyCodeSource(bundledInitScript));
+        }
 
         File initScript = new File(h.getRootDir(),"init.groovy");
         if(initScript.exists()) {
             LOGGER.info("Executing "+initScript);
-            GroovyShell shell = new GroovyShell(h.getPluginManager().uberClassLoader);
-            shell.evaluate(initScript);
+            execute(new GroovyCodeSource(initScript));
         }
+    }
+
+    private static void execute(GroovyCodeSource initScript) throws IOException {
+        GroovyShell shell = new GroovyShell(Hudson.getInstance().getPluginManager().uberClassLoader);
+        shell.evaluate(initScript);
     }
 
     private static final Logger LOGGER = Logger.getLogger(GroovyInitScript.class.getName());
