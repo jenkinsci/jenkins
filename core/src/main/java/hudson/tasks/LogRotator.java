e3450e41610a07b70611e026ec68b4f451ac2b8d
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-03-15 17:09:52 +0000

    touch up
    git-svn-id: https://hudson.dev.java.net/svn/hudson/branches/rc@28627 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/tasks/LogRotator.java
+++ core/src/main/java/hudson/tasks/LogRotator.java
@@ -36,6 +36,8 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import static java.util.logging.Level.FINE;
 import static java.util.logging.Level.FINER;
+
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -59,19 +61,23 @@ public class LogRotator implements Describable<LogRotator> {
     private final int numToKeep;
 
     /**
-     * If not -1, artifacts are only kept up to this days.
+     * If not -1 nor null, artifacts are only kept up to this days.
+     * Null handling is necessary to remain data compatible with old versions.
+     * @since 1.350
      */
-    private final String artifactDaysToKeep;
+    private final Integer artifactDaysToKeep;
 
     /**
-     * If not -1, only this number of builds have their artifacts kept.
+     * If not -1 nor null, only this number of builds have their artifacts kept.
+     * Null handling is necessary to remain data compatible with old versions.
+     * @since 1.350
      */
-    private final String artifactNumToKeep;
+    private final Integer artifactNumToKeep;
 
     @DataBoundConstructor
     public LogRotator (String logrotate_days, String logrotate_nums, String logrotate_artifact_days, String logrotate_artifact_nums) {
         this (parse(logrotate_days),parse(logrotate_nums),
-              logrotate_artifact_days,logrotate_artifact_nums);     	
+              parse(logrotate_artifact_days),parse(logrotate_artifact_nums));
     }
 
     public static int parse(String p) {
@@ -84,13 +90,14 @@ public class LogRotator implements Describable<LogRotator> {
     }
 
     /**
-     * @deprecated since 1.350
+     * @deprecated since 1.350.
+     *      Use {@link #LogRotator(int, int, int, int)}
      */
     public LogRotator(int daysToKeep, int numToKeep) {
-        this(daysToKeep, numToKeep, "-1", "-1");
+        this(daysToKeep, numToKeep, -1, -1);
     }
     
-    public LogRotator(int daysToKeep, int numToKeep, String artifactDaysToKeep, String artifactNumToKeep) {
+    public LogRotator(int daysToKeep, int numToKeep, int artifactDaysToKeep, int artifactNumToKeep) {
         this.daysToKeep = daysToKeep;
         this.numToKeep = numToKeep;
         this.artifactDaysToKeep = artifactDaysToKeep;
@@ -106,9 +113,8 @@ public class LogRotator implements Describable<LogRotator> {
         Run lstb = job.getLastStableBuild();
 
         if(numToKeep!=-1) {
-            Run[] builds = job.getBuilds().toArray(new Run[0]);
-            for( int i=numToKeep; i<builds.length; i++ ) {
-                Run r = builds[i];
+            List<? extends Run<?,?>> builds = job.getBuilds();
+            for (Run r : builds.subList(Math.min(builds.size(),numToKeep),builds.size())) {
                 if (r.isKeepLog()) {
                     LOGGER.log(FINER,r.getFullDisplayName()+" is not GC-ed because it's marked as a keeper");
                     continue;
@@ -150,13 +156,9 @@ public class LogRotator implements Describable<LogRotator> {
             }
         }
 
-        int artNumInt = parse(artifactNumToKeep);
-        int artDayInt = parse(artifactDaysToKeep);
-        
-        if(artNumInt!=-1) {
-            Run[] builds = job.getBuilds().toArray(new Run[0]);
-            for( int i=artNumInt; i<builds.length; i++ ) {
-                Run r = builds[i];
+        if(artifactNumToKeep!=null && artifactNumToKeep!=-1) {
+            List<? extends Run<?,?>> builds = job.getBuilds();
+            for (Run r : builds.subList(Math.min(builds.size(),artifactNumToKeep),builds.size())) {
                 if (r.isKeepLog()) {
                     LOGGER.log(FINER,r.getFullDisplayName()+" is not purged of artifacts because it's marked as a keeper");
                     continue;
@@ -173,11 +175,11 @@ public class LogRotator implements Describable<LogRotator> {
             }
         }
 
-        if(artDayInt!=-1) {
+        if(artifactDaysToKeep!=null && artifactDaysToKeep!=-1) {
             Calendar cal = new GregorianCalendar();
-            cal.add(Calendar.DAY_OF_YEAR,-artDayInt);
+            cal.add(Calendar.DAY_OF_YEAR,-artifactDaysToKeep);
             // copy it to the array because we'll be deleting builds as we go.
-            for( Run r : job.getBuilds().toArray(new Run[0]) ) {
+            for( Run r : job.getBuilds() ) {
                 if (r.isKeepLog()) {
                     LOGGER.log(FINER,r.getFullDisplayName()+" is not purged of artifacts because it's marked as a keeper");
                     continue;
@@ -209,30 +211,38 @@ public class LogRotator implements Describable<LogRotator> {
     }
 
     public int getArtifactDaysToKeep() {
-        return parse(artifactDaysToKeep);
+        return unbox(artifactDaysToKeep);
     }
 
     public int getArtifactNumToKeep() {
-        return parse(artifactNumToKeep);
+        return unbox(artifactNumToKeep);
     }
 
     public String getDaysToKeepStr() {
-        if(daysToKeep==-1)  return "";
-        else                return String.valueOf(daysToKeep);
+        return toString(daysToKeep);
     }
 
     public String getNumToKeepStr() {
-        if(numToKeep==-1)   return "";
-        else                return String.valueOf(numToKeep);
+        return toString(numToKeep);
     }
 
     public String getArtifactDaysToKeepStr() {
-        return artifactDaysToKeep;
+        return toString(artifactDaysToKeep);
     }
 
     public String getArtifactNumToKeepStr() {
-        return artifactNumToKeep;
+        return toString(artifactNumToKeep);
     }
+
+    private int unbox(Integer i) {
+        return i==null ? -1: i;
+    }
+
+    private String toString(Integer i) {
+        if (i==null || i==-1)   return "";
+        return String.valueOf(i);
+    }
+
 
     public LRDescriptor getDescriptor() {
         return DESCRIPTOR;
