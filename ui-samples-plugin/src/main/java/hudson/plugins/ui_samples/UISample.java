be1698d9d983208707c8783ee2dbbeaf97a0abf0
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-03-20 19:56:59 +0000

    adding links to source files.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@28991 71c3de6d-444a-0410-be80-ed276b4c234a

--- ui-samples-plugin/src/main/java/hudson/plugins/ui_samples/UISample.java
+++ ui-samples-plugin/src/main/java/hudson/plugins/ui_samples/UISample.java
@@ -5,6 +5,14 @@ import hudson.ExtensionPoint;
 import hudson.model.Action;
 import hudson.model.Describable;
 import hudson.model.Hudson;
+import org.kohsuke.stapler.StaplerResponse;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.commons.io.IOUtils.copy;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -26,6 +34,23 @@ public abstract class UISample implements ExtensionPoint, Action, Describable<UI
     }
 
     /**
+     * Source files associated with this sample.
+     */
+    public List<SourceFile> getSourceFiles() {
+        return Arrays.asList(new SourceFile(getClass().getSimpleName()+".java"));
+    }
+
+    /**
+     * Binds {@link SourceFile}s into URL.
+     */
+    public SourceFile getSourceFile(String name) {
+        for (SourceFile sf : getSourceFiles())
+            if (sf.name.equals(name))
+                return sf;
+        return null;
+    }
+
+    /**
      * Returns a paragraph of natural text that describes this sample.
      * Interpreted as HTML.
      */
@@ -40,6 +65,29 @@ public abstract class UISample implements ExtensionPoint, Action, Describable<UI
      */
     public static ExtensionList<UISample> all() {
         return Hudson.getInstance().getExtensionList(UISample.class);
+    }
+
+    /**
+     * @author Kohsuke Kawaguchi
+     */
+    public class SourceFile {
+        public final String name;
+
+        public SourceFile(String name) {
+            this.name = name;
+        }
+
+        public URL resolve() {
+            return UISample.this.getClass().getResource(name);
+        }
+
+        /**
+         * Serves this source file.
+         */
+        public void doIndex(StaplerResponse rsp) throws IOException {
+            rsp.setContentType("text/plain;charset=UTF-8");
+            copy(resolve().openStream(),rsp.getOutputStream());
+        }
     }
 
 }
