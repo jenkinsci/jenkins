da5faae44c33e91389b508916248dd413a3c1b4d
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-04-19 00:16:41 +0000

    added a regression test for HUDSON-6252
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@30236 71c3de6d-444a-0410-be80-ed276b4c234a

--- test/src/test/java/hudson/console/UrlAnnotatorTest.java
+++ test/src/test/java/hudson/console/UrlAnnotatorTest.java
@@ -2,6 +2,7 @@ package hudson.console;
 
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.Launcher;
+import hudson.MarkupText;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.model.FreeStyleBuild;
@@ -33,5 +34,16 @@ public class UrlAnnotatorTest extends HudsonTestCase {
         assertTrue(rsp, rsp.contains("<a href='http://www.sun.com/'>http://www.sun.com/</a>"));
         assertTrue(rsp, rsp.contains("<a href='http://www.kohsuke.org/'>http://www.kohsuke.org/</a>"));
         assertTrue(rsp, rsp.contains("<a href='http://www.oracle.com/'>http://www.oracle.com/</a>"));
+    }
+
+    /**
+     * Mark up of URL should consider surrounding markers, if any.
+     */
+    public void test2() throws Exception {
+        MarkupText m = new MarkupText("{abc='http://url/',def='ghi'}");
+        new UrlAnnotator().newInstance(null).annotate(null,m);
+        String html = m.toString(false);
+        assertTrue(html.contains("<a href='http://url/'>http://url/</a>"));
+        System.out.println(html);
     }
 }
