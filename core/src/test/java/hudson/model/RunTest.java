f707baf5f8a92953c688241b804abc43e6714583
jglick <jglick@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-10-25 19:56:48 +0000

    Fixing warnings and questionable uses of generics flagged as errors by NetBeans.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@36221 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/test/java/hudson/model/RunTest.java
+++ core/src/test/java/hudson/model/RunTest.java
@@ -32,36 +32,32 @@ import java.util.List;
  * @author Kohsuke Kawaguchi
  */
 public class RunTest extends TestCase {
-    private List<Run<?,?>.Artifact> createArtifactList(String... paths) {
-        Run<?,?> r = new Run(null,new GregorianCalendar()) {
-			public int compareTo(Run arg0) {
-				return 0;
-			}
-        };
-        Run<?,?>.ArtifactList list = r.new ArtifactList();
+    private List<? extends Run<?,?>.Artifact> createArtifactList(String... paths) {
+        Run<FreeStyleProject,FreeStyleBuild> r = new Run<FreeStyleProject,FreeStyleBuild>(null,new GregorianCalendar()) {};
+        Run<FreeStyleProject,FreeStyleBuild>.ArtifactList list = r.new ArtifactList();
         for (String p : paths) {
             list.add(r.new Artifact(p,p,p,"n"+list.size()));  // Assuming all test inputs don't need urlencoding
         }
         list.computeDisplayName();
-        return (List)list;
+        return list;
     }
     
     public void testArtifactListDisambiguation1() {
-        List<Run<?, ?>.Artifact> a = createArtifactList("a/b/c.xml", "d/f/g.xml", "h/i/j.xml");
+        List<? extends Run<?, ?>.Artifact> a = createArtifactList("a/b/c.xml", "d/f/g.xml", "h/i/j.xml");
         assertEquals(a.get(0).getDisplayPath(),"c.xml");
         assertEquals(a.get(1).getDisplayPath(),"g.xml");
         assertEquals(a.get(2).getDisplayPath(),"j.xml");
     }
 
     public void testArtifactListDisambiguation2() {
-        List<Run<?, ?>.Artifact> a = createArtifactList("a/b/c.xml", "d/f/g.xml", "h/i/g.xml");
+        List<? extends Run<?, ?>.Artifact> a = createArtifactList("a/b/c.xml", "d/f/g.xml", "h/i/g.xml");
         assertEquals(a.get(0).getDisplayPath(),"c.xml");
         assertEquals(a.get(1).getDisplayPath(),"f/g.xml");
         assertEquals(a.get(2).getDisplayPath(),"i/g.xml");
     }
 
     public void testArtifactListDisambiguation3() {
-        List<Run<?, ?>.Artifact> a = createArtifactList("a.xml","a/a.xml");
+        List<? extends Run<?, ?>.Artifact> a = createArtifactList("a.xml","a/a.xml");
         assertEquals(a.get(0).getDisplayPath(),"a.xml");
         assertEquals(a.get(1).getDisplayPath(),"a/a.xml");
     }
