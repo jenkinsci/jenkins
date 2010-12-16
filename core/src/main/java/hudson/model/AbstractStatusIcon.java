75d748e6668ac68372ed5bf2c685b9a9430ed912
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-12-15 18:03:29 -0800

    Allowed Item to expose a status icon

--- core/src/main/java/hudson/model/AbstractStatusIcon.java
+++ core/src/main/java/hudson/model/AbstractStatusIcon.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Martin Eigenbrodt
- * 
+ *
+ * Copyright (c) 2010, CloudBees, Inc.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,28 +21,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package hudson.views;
-
-import hudson.Extension;
-import hudson.model.StatusIcon;
-import org.kohsuke.stapler.DataBoundConstructor;
+package hudson.model;
 
 /**
- * Shows the status icon for item. It's colored ball for jobs.
+ * Partial base class for {@link StatusIcon}.
  *
- * @see StatusIcon
+ * @author Kohsuke Kawaguchi
+ * @since 1.390
  */
-public class StatusColumn extends ListViewColumn {
-    @DataBoundConstructor
-    public StatusColumn() {
-    }
-
-    @Extension
-    public static class DescriptorImpl extends ListViewColumnDescriptor {
-        @Override
-        public String getDisplayName() {
-            return Messages.StatusColumn_DisplayName();
-        }
-    }
-
+public abstract class AbstractStatusIcon implements StatusIcon {
 }
--- core/src/main/java/hudson/model/StatusIcon.java
+++ core/src/main/java/hudson/model/StatusIcon.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Martin Eigenbrodt
- * 
+ *
+ * Copyright (c) 2010, CloudBees, Inc.
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,28 +21,38 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package hudson.views;
-
-import hudson.Extension;
-import hudson.model.StatusIcon;
-import org.kohsuke.stapler.DataBoundConstructor;
+package hudson.model;
 
 /**
- * Shows the status icon for item. It's colored ball for jobs.
+ * {@link Item} can return this from the "getIconColor" method so that
+ * its "status icon" can be shown in Hudson UI.
  *
- * @see StatusIcon
+ * <p>
+ * For future compatibility, please extend from {@link AbstractStatusIcon}
+ * instead of implementing this directly, so that we can add methods later.
+ *
+ * <p>
+ * This is a generalization of {@link BallColor}.
+ *
+ * @author Kohsuke Kawaguchi
+ * @since 1.390
  */
-public class StatusColumn extends ListViewColumn {
-    @DataBoundConstructor
-    public StatusColumn() {
-    }
+public interface StatusIcon {
+    /**
+     * Returns the URL to the image.
+     *
+     * @param size
+     *      The size specified. Must support "16x16", "24x24", and "32x32" at least.
+     *      For forward compatibility, if you receive a size that's not supported,
+     *      consider returning your biggest icon (and let the browser rescale.)
+     * @return
+     *      The URL is rendered as is in the img @src attribute, so it must contain
+     *      the context path, etc.
+     */
+    String getImageOf(String size);
 
-    @Extension
-    public static class DescriptorImpl extends ListViewColumnDescriptor {
-        @Override
-        public String getDisplayName() {
-            return Messages.StatusColumn_DisplayName();
-        }
-    }
-
+    /**
+     * Gets the human-readable description used as img/@alt.
+     */
+    String getDescription();
 }
