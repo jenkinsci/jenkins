33ee710df4b2dfbc01b3fb412f4c9cb9bef4e1f2
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-01-13 13:17:49 -0800

    improving error diagnostics

--- core/src/main/java/hudson/model/ViewJob.java
+++ core/src/main/java/hudson/model/ViewJob.java
@@ -158,6 +158,10 @@ public abstract class ViewJob<JobT extends ViewJob<JobT,RunT>, RunT extends Run<
      * Thread that reloads the {@link Run}s.
      */
     private static final class ReloadThread extends Thread {
+        private ReloadThread() {
+            setName("ViewJob reload thread");
+        }
+
         private ViewJob getNext() throws InterruptedException {
             synchronized(reloadQueue) {
                 // reload operations might eat InterruptException,
