d3c05f033884dab39de6fcde954982aa1dfe897d
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-01-30 15:41:32 -0800

    renaming openSUSE packages

--- opensuse/build.sh
+++ opensuse/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 if [ -z "$1" ]; then
-  echo "Usage: build.sh path/to/hudson.war"
+  echo "Usage: build.sh path/to/jenkins.war"
   exit 1
 fi
 
 # figure out the version to package
-cp "$1" $(dirname $0)/SOURCES/hudson.war
+cp "$1" $(dirname $0)/SOURCES/jenkins.war
 pushd $(dirname $0)
-version=$(unzip -p SOURCES/hudson.war META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr - .)
+version=$(unzip -p SOURCES/jenkins.war META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr - .)
 echo Version is $version
 
 # prepare fresh directories
@@ -15,4 +15,4 @@ rm -rf BUILD RPMS SRPMS tmp || true
 mkdir -p BUILD RPMS SRPMS
 
 # real action happens here
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" --define="ver $version" SPECS/hudson.spec
+rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" --define="ver $version" SPECS/jenkins.spec
