6c521259e2b1061c105cb09724e4c29e8bc07ecd
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-02-04 21:14:03 -0800

    [FIXED JENKINS-8660] recording the fix from Jerome Lacoste.

--- core/src/main/java/hudson/widgets/HistoryWidget.java
+++ core/src/main/java/hudson/widgets/HistoryWidget.java
@@ -33,7 +33,6 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
