35023af76c40254826b1a4d6cdcb49a040662645
Paul Weiss <paul@weiss.name>
2011-03-10 13:25:34 -0500

    Fix bug JENKINS-9006: don't fail an email if only one of the recipients is bad.

--- core/src/main/java/hudson/tasks/MailSender.java
+++ core/src/main/java/hudson/tasks/MailSender.java
@@ -412,4 +412,15 @@ public class MailSender {
      * Sometimes the outcome of the previous build affects the e-mail we send, hence this checkpoint.
      */
     private static final CheckPoint CHECKPOINT = new CheckPoint("mail sent");
+    
+    static {
+    	// Fix JENKINS-9006
+    	// When sending to multiple recipients, send to valid recipients even if some are
+    	// invalid, unless we have explicitly said otherwise.
+    	for (String property: Arrays.asList("mail.smtp.sendpartial", "mail.smtps.sendpartial")) {
+    		if (System.getProperty(property) == null) {
+    			System.setProperty(property, "true");
+        	}
+        }
+    }
 }
