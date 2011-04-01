2ae9e56b8d93b508c704a3eea2390e0a0c173377
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-04-01 15:23:59 -0700

    crap, merge error

--- core/src/main/java/hudson/model/Node.java
+++ core/src/main/java/hudson/model/Node.java
@@ -367,7 +367,7 @@ public abstract class Node extends AbstractModelObject implements Reconfigurable
         return getACL().hasPermission(permission);
     }
 
-    public Node reconfigure(final StaplerRequest req, JSONObject form) throws FormException {
+    public Node reconfigure(StaplerRequest req, JSONObject form) throws FormException {
         if (form==null)     return null;
 
         final JSONObject jsonForProperties = form.optJSONObject("nodeProperties");
