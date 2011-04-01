46994700e4d2b3a5ea84607035bb954ed3c4c750
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-04-01 10:37:56 -0700

    adding the credit of Victor Garcia for 4129ac50a39ae2522c20ce73e2b10872a07c04be

--- core/src/main/java/hudson/tasks/junit/SuiteResult.java
+++ core/src/main/java/hudson/tasks/junit/SuiteResult.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Erik Ramfelt, Xavier Le Vourch, Tom Huybrechts, Yahoo!, Inc.
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Erik Ramfelt, Xavier Le Vourch, Tom Huybrechts, Yahoo!, Inc., Victor Garcia
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
