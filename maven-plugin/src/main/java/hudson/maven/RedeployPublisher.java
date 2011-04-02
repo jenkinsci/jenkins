fe9347435fd326cdd3bc2b7482d4b10922f1c160
Martijn Baay <martijn.baay@gmail.com>
2011-04-02 18:24:02 +0200

    Fixed JENKINS-9084: Made sure the correct slave is detected even when it's the Master

--- maven-plugin/src/main/java/hudson/maven/RedeployPublisher.java
+++ maven-plugin/src/main/java/hudson/maven/RedeployPublisher.java
@@ -187,7 +187,7 @@ public class RedeployPublisher extends Recorder {
                 
                 String altSettingsPath = ((MavenModuleSet) project).getAlternateSettings();
                 
-                Node buildNode =  Hudson.getInstance().getNode( build.getBuiltOnStr() );
+                Node buildNode = build.getBuiltOn();
                 
                 if (StringUtils.isBlank( altSettingsPath ) ) {
                     // get userHome from the node where job has been executed
