7ad1c7e249c50db0d61489c3da803ef0432aee59
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-06-07 13:09:22 -0700

    assigning packages to keep IDEs happy and make error diagnostics easier in the stack trace

--- core/src/main/resources/hudson/tasks/Shell/config.groovy
+++ core/src/main/resources/hudson/tasks/Shell/config.groovy
@@ -21,7 +21,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-f=namespace("/lib/form")
+package hudson.tasks.Shell;
+f=namespace(lib.FormTagLib)
 
 f.entry(title:_("Command"),description:_("description",rootURL)) {
     f.textarea(name:"command",value:instance?.command,class:"fixed-width")
