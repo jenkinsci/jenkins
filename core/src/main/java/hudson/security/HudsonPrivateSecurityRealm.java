17a835d505eb415aeca9431db459c527f4c15c83
Jesse Farinacci <jieryn@gmail.com>
2011-06-09 15:43:28 -0400

    fix misleading comment, thank you orrc

--- core/src/main/java/hudson/security/HudsonPrivateSecurityRealm.java
+++ core/src/main/java/hudson/security/HudsonPrivateSecurityRealm.java
@@ -125,9 +125,9 @@ public class HudsonPrivateSecurityRealm extends AbstractPasswordBasedSecurityRea
     }
 
     /**
-     * Checks if captcha is disabled on signup.
+     * Checks if captcha is enabled on user signup.
      *
-     * @return true if captcha is disabled on signup.
+     * @return true if captcha is enabled on signup.
      */
     public boolean isEnableCaptcha() {
         return enableCaptcha;
