af7159c8e796c44c2090137e17c4aa6a3fef5fed
Vojtech Juranek <vjuranek@redhat.com>
2011-07-20 16:29:30 +0200

    JENKINS-10400 Added support for gzip log file also in case of raw text (consoleText link)

--- core/src/main/java/hudson/model/Run.java
+++ core/src/main/java/hudson/model/Run.java
@@ -1675,7 +1675,17 @@ public abstract class Run <JobT extends Job<JobT,RunT>,RunT extends Run<JobT,Run
         rsp.setContentType("text/plain;charset=UTF-8");
         // Prevent jelly from flushing stream so Content-Length header can be added afterwards
         FlushProofOutputStream out = new FlushProofOutputStream(rsp.getCompressedOutputStream(req));
-        getLogText().writeLogTo(0,out);
+        try{
+        	getLogText().writeLogTo(0,out);
+        } catch (IOException e) {
+			// see comment in writeLogTo() method
+			InputStream input = getLogInputStream();
+			try {
+				IOUtils.copy(input, out);
+			} finally {
+				IOUtils.closeQuietly(input);
+			}
+		}
         out.close();
     }
 
