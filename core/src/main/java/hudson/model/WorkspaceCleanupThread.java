3a57b104c9358bfc8f818dab525785ae72eb9e64
Christoph Kutzinski <kutzi@gmx.de>
2011-08-15 23:43:39 +0200

    changelog for JENKINS-8880 and minor follow-up changes

--- core/src/main/java/hudson/model/WorkspaceCleanupThread.java
+++ core/src/main/java/hudson/model/WorkspaceCleanupThread.java
@@ -92,10 +92,10 @@ public class WorkspaceCleanupThread extends AsyncPeriodicWork {
         }
     }
 
-    private boolean shouldBeDeleted(String jobName, FilePath dir, Node n) throws IOException, InterruptedException {
+    private boolean shouldBeDeleted(String workspaceDirectoryName, FilePath dir, Node n) throws IOException, InterruptedException {
         // TODO: the use of remoting is not optimal.
         // One remoting can execute "exists", "lastModified", and "delete" all at once.
-        TopLevelItem item = Jenkins.getInstance().getItem(jobName);
+        TopLevelItem item = Jenkins.getInstance().getItem(workspaceDirectoryName);
 
         if(!dir.exists())
             return false;
@@ -107,15 +107,16 @@ public class WorkspaceCleanupThread extends AsyncPeriodicWork {
             return false;
         }
 
-        // Assuming build name == workspace name breaks custom workspaces. Just skip out if the 30-day window doesn't catch it yet.
-        // TODO: Add a check that covers custom workspaces, if possible.
-        // TODO: If and when we do the above, also add checkbox that lets users configure a workspace to never be auto-cleaned.
+        // Could mean that directory doesn't belong to a job. But can also mean that it's a custom workspace belonging to a job.
+        // So better leave it alone - will still be deleted after 30 days - until we have a proper check for custom workspaces.
+        // TODO: implement proper check for custom workspaces.
+        // TODO: If we do the above, could also be good to add checkbox that lets users configure a workspace to never be auto-cleaned.
         if(item==null) {
             return false;
         }
 
-        if (item instanceof AbstractProject) {
-            AbstractProject p = (AbstractProject) item;
+        if (item instanceof AbstractProject<?,?>) {
+            AbstractProject<?,?> p = (AbstractProject<?,?>) item;
             Node lb = p.getLastBuiltOn();
             LOGGER.finer("Directory "+dir+" is last built on "+lb);
             if(lb!=null && lb.equals(n)) {
@@ -177,5 +178,5 @@ public class WorkspaceCleanupThread extends AsyncPeriodicWork {
     /**
      * Can be used to disable workspace clean up.
      */
-    public static boolean disabled = Boolean.getBoolean(WorkspaceCleanupThread.class.getName()+".disabled");
+    public static final boolean disabled = Boolean.getBoolean(WorkspaceCleanupThread.class.getName()+".disabled");
 }
