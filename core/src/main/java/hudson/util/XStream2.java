46c57afda366cb05eb4717c542dd1a3297c76d78
Richard Mortimer <richm@oldelvet.org.uk>
2011-09-09 17:20:13 +0100

    Serialize ImmutableList in the same manner as ImmutableMap. This is a partial fix for JENKINS-10903. Any new builds will be serialized correctly.
    Based on ImmutableMapConverter.java

--- core/src/main/java/hudson/util/XStream2.java
+++ core/src/main/java/hudson/util/XStream2.java
@@ -23,6 +23,7 @@
  */
 package hudson.util;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.mapper.AnnotationMapper;
@@ -45,6 +46,7 @@ import jenkins.model.Jenkins;
 import hudson.model.Label;
 import hudson.model.Result;
 import hudson.model.Saveable;
+import hudson.util.xstream.ImmutableListConverter;
 import hudson.util.xstream.ImmutableMapConverter;
 import hudson.util.xstream.MapperDelegate;
 
@@ -107,6 +109,7 @@ public class XStream2 extends XStream {
 
         registerConverter(new RobustCollectionConverter(getMapper(),getReflectionProvider()),10);
         registerConverter(new ImmutableMapConverter(getMapper(),getReflectionProvider()),10);
+        registerConverter(new ImmutableListConverter(getMapper(),getReflectionProvider()),10);
         registerConverter(new ConcurrentHashMapConverter(getMapper(),getReflectionProvider()),10);
         registerConverter(new CopyOnWriteMap.Tree.ConverterImpl(getMapper()),10); // needs to override MapConverter
         registerConverter(new DescribableList.ConverterImpl(getMapper()),10); // explicitly added to handle subtypes
@@ -124,6 +127,8 @@ public class XStream2 extends XStream {
             public String serializedClass(Class type) {
                 if (type != null && ImmutableMap.class.isAssignableFrom(type))
                     return super.serializedClass(ImmutableMap.class);
+                else if (type != null && ImmutableList.class.isAssignableFrom(type))
+                    return super.serializedClass(ImmutableList.class);
                 else
                     return super.serializedClass(type);
             }
