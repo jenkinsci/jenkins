6a8d388ac7d8159e591b6a35136a571c9419ed62
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-11-02 12:52:10 -0700

    doc fix and better base class

--- core/src/main/java/hudson/matrix/MatrixConfigurationSorter.java
+++ core/src/main/java/hudson/matrix/MatrixConfigurationSorter.java
@@ -1,9 +1,12 @@
 package hudson.matrix;
 
 import hudson.ExtensionPoint;
+import hudson.model.AbstractDescribableImpl;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
+
+import java.util.Comparator;
 import java.util.List;
 import jenkins.model.Jenkins;
 
@@ -13,7 +16,7 @@ import jenkins.model.Jenkins;
  * @since 1.437
  * @author Lucie Votypkova
  */
-public abstract class MatrixConfigurationSorter implements ExtensionPoint, Describable<MatrixConfigurationSorter> {
+public abstract class MatrixConfigurationSorter extends AbstractDescribableImpl<MatrixConfigurationSorter> implements ExtensionPoint, Comparator<MatrixConfiguration> {
 
     /**
      *
@@ -22,15 +25,15 @@ public abstract class MatrixConfigurationSorter implements ExtensionPoint, Descr
      * @return
      *     int number for their comparing
      */
-   public abstract int compare(MatrixConfiguration configuration1, MatrixConfiguration configuration2);
+    public abstract int compare(MatrixConfiguration configuration1, MatrixConfiguration configuration2);
     
    
     public abstract String getDisplayName();
     
     /**
      *
-     * @param List of chosen axes by user
-     *      
+     * @param axes
+     *      list of chosen axes by user
      * @return
      *     true if the sorting of this axes by this sorter is possible (for example if the list of axes is not empty or contains axis which is needed for sorting.
      *     false if the sorting is impossible
@@ -44,10 +47,6 @@ public abstract class MatrixConfigurationSorter implements ExtensionPoint, Descr
      */
     public abstract String getErrorFormMessage();
 
-    public Descriptor<MatrixConfigurationSorter> getDescriptor() {
-        return Jenkins.getInstance().getDescriptorOrDie(getClass());
-    }
-    
     public static List<MatrixConfigurationSorter> all() {
         return Hudson.getInstance().getExtensionList(MatrixConfigurationSorter.class);
     }
