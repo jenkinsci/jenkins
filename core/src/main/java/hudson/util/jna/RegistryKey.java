1f1f86386146d27284b7c793e58f314726c6f11f
Elbandi <ea333@freemail.hu>
2011-11-17 01:20:09 +0100

    valueExists use the same code as getValue

--- core/src/main/java/hudson/util/jna/RegistryKey.java
+++ core/src/main/java/hudson/util/jna/RegistryKey.java
@@ -156,14 +156,26 @@ public class RegistryKey {
      * Does a specified value exist?
      */
     public boolean valueExists(String name) {
-        int r = Advapi32.INSTANCE.RegQueryValueEx(handle, name, null, new IntByReference(), new byte[1], new IntByReference());
-        switch(r) {
-        case WINERROR.ERROR_FILE_NOT_FOUND:
-            return false;
-        case WINERROR.ERROR_SUCCESS:
-            return true;
-        default:
-            throw new JnaException(r);
+        IntByReference pType, lpcbData;
+        byte[] lpData = new byte[1];
+
+        pType = new IntByReference();
+        lpcbData = new IntByReference();
+
+        OUTER:
+        while(true) {
+            int r = Advapi32.INSTANCE.RegQueryValueEx(handle, name, null, pType, lpData, lpcbData);
+            switch(r) {
+            case WINERROR.ERROR_MORE_DATA:
+                lpData = new byte[lpcbData.getValue()];
+                continue OUTER;
+            case WINERROR.ERROR_FILE_NOT_FOUND:
+                return false;
+            case WINERROR.ERROR_SUCCESS:
+                return true;
+            default:
+                throw new JnaException(r);
+            }
         }
     }
 
