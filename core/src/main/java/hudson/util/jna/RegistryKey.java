a1714c1690a1ddf387d87fee0869024c599a95fe
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-11-30 10:44:43 -0800

    [FIXED JENKINS-11767] missing break in getValues@RegistryKey

--- core/src/main/java/hudson/util/jna/RegistryKey.java
+++ core/src/main/java/hudson/util/jna/RegistryKey.java
@@ -259,6 +259,8 @@ public class RegistryKey {
                 default:
                     break; // not supported yet
                 }
+                break;
+            
             default:
                 check(result);
             }
