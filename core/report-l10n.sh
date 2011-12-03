ad5186442445143f7e4efe99fcffad2eee3e82fa
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-02 22:11:58 -0800

    Ruby version. A lot better.

--- core/report-l10n.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# this script counts available localizations in the core
-(
-    for f in $(find src/main/resources -name '*.properties'); do
-        f=$(basename "$f" | sed -n -e 's/.*\(\(_.._..\.\)\|\(_..\.\)\).*$/\1/p')
-        echo $f
-    done
-) | sort | uniq
