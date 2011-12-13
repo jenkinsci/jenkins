60e42988c4897fd834ae35f06c5ca0ba15a0e35b
Albert So <kingfai@yahoo-inc.com>
2011-12-13 15:46:52 -0800

    [JENKINS-11762] Changes to add a configurable display name to jobs

---
+++ core/src/test/java/hudson/model/AbstractItemTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package hudson.model;
+
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author kingfai
+ *
+ */
+@SuppressWarnings("unchecked")
+public class AbstractItemTest {
+
+    private class StubAbstractItem extends AbstractItem {
+
+        protected StubAbstractItem() {
+            // sending in null as parent as I don't care for my current tests
+            super(null, "StubAbatractItem");
+        }
+
+        @SuppressWarnings("rawtypes")
+        @Override
+        public Collection<? extends Job> getAllJobs() {
+            return null;
+        }
+        
+        /**
+         * Override save so that nothig happens when setDisplayName() is called
+         */
+        @Override
+        public void save() {
+            
+        }
+    }
+    
+    @Test
+    public void testSetDisplayName() throws Exception {
+        final String displayName = "testDisplayName";
+        StubAbstractItem i = new StubAbstractItem();
+        i.setDisplayName(displayName);
+        Assert.assertEquals(displayName, i.getDisplayName());
+    }
+    
+    @Test
+    public void testGetDefaultDisplayName() {
+        final String name = "the item name";
+        StubAbstractItem i = new StubAbstractItem();
+        i.doSetName(name);
+        // assert that if the displayname is not set, the name is actually returned
+        Assert.assertEquals(name,  i.getDisplayName());
+        
+    }
+    
+    @Test
+    public void testSearchNameIsName() throws Exception {
+        final String name = "the item name jlrtlekjtekrjkjr";
+        StubAbstractItem i = new StubAbstractItem();
+        i.doSetName(name);
+        
+        Assert.assertEquals(i.getName(),  i.getSearchName());        
+    }
+}
