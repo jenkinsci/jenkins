ebdba648d944efec3808436b8021072609fa8da5
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-02-21 12:02:11 +0900

    doc improvement

--- core/src/main/java/jenkins/model/ModelObjectWithContextMenu.java
+++ core/src/main/java/jenkins/model/ModelObjectWithContextMenu.java
@@ -1,6 +1,7 @@
 package jenkins.model;
 
 import hudson.Functions;
+import hudson.Util;
 import hudson.model.Action;
 import hudson.model.Actionable;
 import hudson.model.ModelObject;
@@ -28,16 +29,35 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * {@link ModelObject} that has context menu in the breadcrumb
+ * {@link ModelObject} that has context menu in the breadcrumb.
  * 
- * This is just a marker interface. It needs 
+ * <p>
+ * When the user is visiting a particular page, all the ancestor objects that has {@link ModelObject}
+ * appears in the breadcrumbs. Among those which that also implements {@link ModelObjectWithContextMenu}
+ * shows the drop-down menu for providing quicker access to the actions to those objects.
+ *     
  * @author Kohsuke Kawaguchi
  */
 public interface ModelObjectWithContextMenu extends ModelObject {
+    /**
+     * Generates the context menu.
+     * 
+     * The typical implementation is {@code return new ContextMenu().from(this,request,response);},
+     * which implements the default behaviour. See {@link ContextMenu#from(ModelObjectWithContextMenu, StaplerRequest, StaplerResponse)}
+     * for more details of what it does. This should suit most implementations.
+     */
     public ContextMenu doContextMenu(StaplerRequest request, StaplerResponse response) throws Exception;
-    
+
+    /**
+     * Data object that represents the context menu.
+     * 
+     * Via {@link HttpResponse}, this class is capable of converting itself to JSON that &lt;l:breadcrumb/> understands.
+     */
     @ExportedBean
     public class ContextMenu implements HttpResponse {
+        /**
+         * The actual contents of the menu.
+         */
         @Exported(inline=true)
         public final List<MenuItem> items = new ArrayList<MenuItem>();
         
@@ -73,14 +93,29 @@ public interface ModelObjectWithContextMenu extends ModelObject {
                 items.add(new MenuItem(url,icon,text));
             return this;
         }
-        
-        public ContextMenu from(ModelObject self, StaplerRequest request, StaplerResponse response) throws JellyException, IOException {
+
+        /**
+         * Default implementation of the context menu generation.
+         * 
+         * <p>
+         * This method uses {@code sidepanel.groovy} to run the side panel generation, captures
+         * the use of &lt;l:task> tags, and then converts those into {@link MenuItem}s. This is
+         * supposed to make this work with most existing {@link ModelObject}s that follow the standard
+         * convention.
+         * 
+         * <p>
+         * Unconventional {@link ModelObject} implementations that do not use {@code sidepanel.groovy}
+         * can override {@link ModelObjectWithContextMenu#doContextMenu(StaplerRequest, StaplerResponse)}
+         * directly to provide alternative semantics.
+         */
+        public ContextMenu from(ModelObjectWithContextMenu self, StaplerRequest request, StaplerResponse response) throws JellyException, IOException {
             WebApp webApp = WebApp.getCurrent();
             final Script s = webApp.getMetaClass(self).getTearOff(JellyClassTearOff.class).findScript("sidepanel");
             if (s!=null) {
                 JellyFacet facet = webApp.getFacet(JellyFacet.class);
-                request.setAttribute("taskTags",this);
+                request.setAttribute("taskTags",this); // <l:task> will look for this variable and populate us
                 request.setAttribute("mode","side-panel");
+                // run sidepanel but ignore generated HTML
                 facet.scriptInvoker.invokeScript(request,response,new Script() {
                     public Script compile() throws JellyException {
                         return this;
@@ -101,19 +136,36 @@ public interface ModelObjectWithContextMenu extends ModelObject {
         }
     }
 
+    /**
+     * Menu item in {@link ContextMenu}
+     */
     @ExportedBean
     public class MenuItem {
+        /**
+         * Target of the link.
+         *
+         * This can start with '/', but it must not be a relative URL, since
+         * you cannot really tell which page this context menu is used.
+         */
         @Exported
         public String url;
+
+        /**
+         * Human readable caption of the menu item. Do not use HTML.
+         */
         @Exported
         public String displayName;
+
+        /**
+         * Optional URL to the icon image. Rendered as 24x24.
+         */
         @Exported
         public String icon;
 
         public MenuItem(String url, String icon, String displayName) {
             this.url = url;
             this.icon = icon;
-            this.displayName = displayName;
+            this.displayName = Util.escape(displayName);
         }
     }
 }
