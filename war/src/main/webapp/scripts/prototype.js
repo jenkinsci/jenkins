3ebecc3cf79d058af2b451e69c57d4fc851314dd
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-03-01 19:17:03 -0800

    Backporting 918fc6e6cad5ef2a2c5018ecad0b8a791d6bec1f
    made to work with <textarea>s, too.
    
    git-svn-id:
    https://hudson.dev.java.net/svn/hudson/trunk/hudson/main@8172
    71c3de6d-444a-0410-be80-ed276b4c234a

--- war/src/main/webapp/scripts/prototype.js
+++ war/src/main/webapp/scripts/prototype.js
@@ -6408,15 +6408,24 @@ Form.Methods = {
   getInputs: function(form, typeName, name) {
     form = $(form);
     var inputs = form.getElementsByTagName('input');
+    var textareas = form.getElementsByTagName('textarea');
+    // KK patch
+    var selects = form.getElementsByTagName('select');
 
-    if (!typeName && !name) return $A(inputs).map(Element.extend);
+    if (!typeName && !name) return $A(inputs).concat($A(textareas)).concat($A(selects)).map(Element.extend);
 
-    for (var i = 0, matchingInputs = [], length = inputs.length; i < length; i++) {
-      var input = inputs[i];
-      if ((typeName && input.type != typeName) || (name && input.name != name))
-        continue;
-      matchingInputs.push(Element.extend(input));
-    }
+    var matchingInputs = [];
+    var f = function(inputs) {
+      for (var i = 0, length = inputs.length; i < length; i++) {
+        var input = inputs[i];
+        if ((typeName && input.type != typeName) || (name && input.name != name))
+          continue;
+        matchingInputs.push(Element.extend(input));
+      }
+    };
+    f(inputs);
+    f(textareas);
+    f(selects);
 
     return matchingInputs;
   },
