e14eb77cc09ce04f109705063d7d4c2fecf819c3
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-03-03 20:56:45 -0800

    Added OWASP html sanitizer for HTML sanitizing.

--- core/src/main/java/hudson/markup/RawHtmlMarkupFormatter.java
+++ core/src/main/java/hudson/markup/RawHtmlMarkupFormatter.java
@@ -1,7 +1,11 @@
 package hudson.markup;
 
+import com.google.common.base.Throwables;
 import hudson.Extension;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.owasp.html.Handler;
+import org.owasp.html.HtmlSanitizer;
+import org.owasp.html.HtmlStreamRenderer;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -27,7 +31,24 @@ public class RawHtmlMarkupFormatter extends MarkupFormatter {
 
     @Override
     public void translate(String markup, Writer output) throws IOException {
-        output.write(markup);
+        HtmlStreamRenderer renderer = HtmlStreamRenderer.create(
+                output,
+                // Receives notifications on a failure to write to the output.
+                new Handler<IOException>() {
+                    public void handle(IOException ex) {
+                        Throwables.propagate(ex);  // System.out suppresses IOExceptions
+                    }
+                },
+                // Our HTML parser is very lenient, but this receives notifications on
+                // truly bizarre inputs.
+                new Handler<String>() {
+                    public void handle(String x) {
+                        throw new Error(x);
+                    }
+                }
+        );
+        // Use the policy defined above to sanitize the HTML.
+        HtmlSanitizer.sanitize(markup, MyspacePolicy.POLICY_DEFINITION.apply(renderer));
     }
 
     public String getCodeMirrorMode() {
