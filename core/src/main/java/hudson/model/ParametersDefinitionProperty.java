236d4116d4e056a8a7dfc9c508056f8044a7dda9
Reynald Borer <reynald.borer@gmail.com>
2012-04-22 22:13:12 +0200

    [FIXED JENKINS-13546]: return a JSON string when client request it through content-type header
    When a client uses the buildWithParameters API and request a json output, return the job definition as a json string instead of doing a redirect on the job page.

--- core/src/main/java/hudson/model/ParametersDefinitionProperty.java
+++ core/src/main/java/hudson/model/ParametersDefinitionProperty.java
@@ -44,6 +44,7 @@ import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.export.ExportedBean;
 
 import hudson.Extension;
+import org.kohsuke.stapler.export.Flavor;
 
 /**
  * Keeps a list of the parameters defined for a project.
@@ -144,11 +145,16 @@ public class ParametersDefinitionProperty extends JobProperty<AbstractProject<?,
         	}
         }
 
-    	Jenkins.getInstance().getQueue().schedule(
+        Jenkins.getInstance().getQueue().schedule(
                 owner, owner.getDelay(req), new ParametersAction(values), owner.getBuildCause(req));
 
-        // send the user back to the job top page.
-        rsp.sendRedirect(".");
+        if (req.getContentType() != null && req.getContentType().contains("application/json")) {
+            rsp.setContentType("application/json");
+            rsp.serveExposedBean(req, owner, Flavor.JSON);
+        } else {
+            // send the user back to the job top page.
+            rsp.sendRedirect(".");
+        }
     }
 
     /**
