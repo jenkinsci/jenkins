a8ed3eec83e7d0502ac33b1793ef44e9f50474be
Brian Moyles <bmoyles@netflix.com>
2012-05-21 15:59:31 -0700

    synchronizing here can cause deadlock conditions from plugins like the keyboard shortcuts plugin that ask for all views very frequently and do additional locking on the actual view objects

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -1365,7 +1365,7 @@ public class Jenkins extends AbstractCIBase implements ModifiableItemGroup<TopLe
      * Gets the read-only list of all {@link View}s.
      */
     @Exported
-    public synchronized Collection<View> getViews() {
+    public Collection<View> getViews() {
         return viewGroupMixIn.getViews();
     }
 
