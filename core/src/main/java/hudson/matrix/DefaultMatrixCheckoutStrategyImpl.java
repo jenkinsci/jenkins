31167789cd33b62bce727c97834eecabb48300c4
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-05-25 12:26:58 -0700

    Doc improvements

--- core/src/main/java/hudson/matrix/DefaultMatrixCheckoutStrategyImpl.java
+++ core/src/main/java/hudson/matrix/DefaultMatrixCheckoutStrategyImpl.java
@@ -1,43 +1,15 @@
 package hudson.matrix;
 
 import hudson.Extension;
-import hudson.Launcher;
-import hudson.model.AbstractBuild;
-import hudson.model.BuildListener;
-
-import java.io.IOException;
-
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
- * {@link MatrixCheckoutStrategy} implementation that falls back to the default
- * behaviours defined in {@link AbstractBuild.AbstractRunner}, which is the common
- * implementation for not just matrix projects but all sorts of other project types.
+ * Default {@link MatrixCheckoutStrategy} implementation.
  */
 public class DefaultMatrixCheckoutStrategyImpl extends MatrixCheckoutStrategy {
 
     @DataBoundConstructor
     public DefaultMatrixCheckoutStrategyImpl() {}
-
-    @Override
-    public void preCheckout(MatrixRun build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-        build.runner.defaultPreCheckout();
-    }
-
-    @Override
-    public void checkout(MatrixRun build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-        build.runner.defaultCheckout();
-    }
-
-    @Override
-    public void preCheckout(MatrixBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-        build.runner.defaultPreCheckout();
-    }
-
-    @Override
-    public void checkout(MatrixBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-        build.runner.defaultCheckout();
-    }
 
     @Extension
     public static class DescriptorImpl extends MatrixCheckoutStrategyDescriptor {
