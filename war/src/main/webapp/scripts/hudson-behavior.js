3844bf8a350278fd679b3c61694983414605379c
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-07-27 08:25:19 +0900

    refactored

--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -1070,31 +1070,6 @@ var jenkinsRules = {
         updateDropDownList();
     },
 
-    // combobox.jelly
-    "INPUT.combobox2" : function(e) {
-        var items = [];
-
-        var c = new ComboBox(e,function(value) {
-            var candidates = [];
-            for (var i=0; i<items.length; i++) {
-                if (items[i].indexOf(value)==0) {
-                    candidates.push(items[i]);
-                    if (candidates.length>20)   break;
-                }
-            } 
-            return candidates;
-        }, {});
-
-        refillOnChange(e,function(params) {
-            new Ajax.Request(e.getAttribute("fillUrl"),{
-                parameters: params,
-                onSuccess : function(rsp) {
-                    items = eval('('+rsp.responseText+')');
-                }
-            });
-        });
-    },
-
     "A.showDetails" : function(e) {
         e.onclick = function() {
             this.style.display = 'none';
