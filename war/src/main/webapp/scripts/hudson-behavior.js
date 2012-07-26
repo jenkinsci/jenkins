7db5fb34aa4389c93783e824356ad0de84389c04
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-07-27 08:22:36 +0900

    refactored

--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -701,23 +701,6 @@ var jenkinsRules = {
         e = null; // avoid memory leak
     },
 
-    "TEXTAREA.codemirror" : function(e) {
-        var h = e.clientHeight;
-        var config = e.getAttribute("codemirror-config") || "";
-        config = eval('({'+config+'})');
-        var codemirror = CodeMirror.fromTextArea(e,config);
-        e.codemirrorObject = codemirror;
-        if(typeof(codemirror.getScrollerElement) !== "function") {
-            // Maybe older versions of CodeMirror do not provide getScrollerElement method.
-            codemirror.getScrollerElement = function(){
-                return findElementsBySelector(codemirror.getWrapperElement(), ".CodeMirror-scroll")[0];
-            };
-        }
-        var scroller = codemirror.getScrollerElement();
-        scroller.setAttribute("style","border:1px solid black;");
-        scroller.style.height = h+"px";
-    },
-
     // Script Console : settings and shortcut key
     "TEXTAREA.script" : function(e) {
         (function() {
@@ -1128,46 +1111,6 @@ var jenkinsRules = {
 
     ".button-with-dropdown" : function (e) {
         new YAHOO.widget.Button(e, { type: "menu", menu: $(e).next() });
-    },
-
-    "DIV.textarea-preview-container" : function (e) {
-        var previewDiv = findElementsBySelector(e,".textarea-preview")[0];
-        var showPreview = findElementsBySelector(e,".textarea-show-preview")[0];
-        var hidePreview = findElementsBySelector(e,".textarea-hide-preview")[0];
-        $(hidePreview).hide();
-        $(previewDiv).hide();
-
-        showPreview.onclick = function() {
-            // Several TEXTAREAs may exist if CodeMirror is enabled. The first one has reference to the CodeMirror object.
-            var textarea = e.parentNode.getElementsByTagName("TEXTAREA")[0];
-            var text = textarea.codemirrorObject ? textarea.codemirrorObject.getValue() : textarea.value;
-            var render = function(txt) {
-                $(hidePreview).show();
-                $(previewDiv).show();
-                previewDiv.innerHTML = txt;
-                layoutUpdateCallback.call();
-            };
-
-            new Ajax.Request(rootURL + showPreview.getAttribute("previewEndpoint"), {
-                method: "POST",
-                requestHeaders: "Content-Type: application/x-www-form-urlencoded",
-                parameters: {
-                    text: text
-                },
-                onSuccess: function(obj) {
-                    render(obj.responseText)
-                },
-                onFailure: function(obj) {
-                    render(obj.status + " " + obj.statusText + "<HR/>" + obj.responseText)
-                }
-            });
-            return false;
-        }
-
-        hidePreview.onclick = function() {
-            $(hidePreview).hide();
-            $(previewDiv).hide();
-        };
     },
 
     /*
