jenkinsci/jenkins

commit dbb100da58a084fb0a6ce129d3f2b7ad827e87ee

[FIXED JENKINS-14495] Hetero lists not working correctly after adding elements. Unlike [JENKINS-14514] this is a true fix rather than a workaround (now removed), and is more general. cjo9900 discovered that behaviors were being redundantly registered (as of 1.474 the monolithic JS is broken up); this caused some behaviors to be run repeatedly on the same elements, breaking reasonable expectations of some behaviors. The ideal fix would be to change Behavior.register to be idempotent: for example, key it by selector, then maintain a set of distinct behavior functions for each. Unfortunately some adjuncts directly call Behavior.list.unshift, bypassing register(...), which would be tricky to intercept (would need to make a mock of Array). The known one cases are in core, but it is possible plugin adjuncts do this too, in which case it would be incompatible to (say) change the Array<Map<String,Behavior>> to a Map<String,Array<Behavior>>. Instead, permitting redundant registrations as before, and just silently skipping all but the first at runtime when applying behaviors. Beware that since adjuncts are loaded from multiple places, different JS function objects are registered each time, so a naive set of behavior functions does not work; have to identify functions by their toString in order to ensure that each is run only once. (Currently once _per selector_, conceivably >1x per element; could if necessary be refined to make sure a given behavior is only run once on a given element during one call to applySubtree even if the element matches multiple selectors.)

war/src/main/webapp/scripts/behavior.js
@@ -65,12 +65,22 @@ var Behaviour = {
      *      this semantics is preserved.
      */
     applySubtree : function(startNode,includeSelf) {
+        var behaviorsBySelector = {};
         Behaviour.list._each(function(sheet) {
             for (var selector in sheet){
                 function apply(n) {
-                    var list = findElementsBySelector(n,selector,includeSelf);
-                    if (list.length>0)  // just to simplify setting of a breakpoint.
-                        list._each(sheet[selector]);
+                    var behavior = sheet[selector];
+                    var behaviors = behaviorsBySelector[selector];
+                    if (behaviors == null) {
+                        behaviors = [];
+                        behaviorsBySelector[selector] = behaviors;
+                    }
+                    if (behaviors.indexOf(behavior.toString()) == -1) {
+                        behaviors.push(behavior.toString());
+                        var list = findElementsBySelector(n,selector,includeSelf);
+                        if (list.length>0)  // just to simplify setting of a breakpoint.
+                            list._each(sheet[selector]);
+                    }
                 }
 
                 if (startNode instanceof Array) {

core/src/main/resources/lib/form/repeatable/repeatable.js
@@ -18,9 +18,6 @@ var repeatableSupport = {
     // do the initialization
     init : function(container,master,insertionPoint) {
         this.container = $(container);
-        if (this.container.tag != null) {
-            return;
-        }
         this.container.tag = this;
         master = $(master);
         this.blockHTML = master.innerHTML;

commit f39fbdb312c3c87ef77a72446273423d906942ed

[JENKINS-14495] Refined fix - needed to pull uniqueness check out of apply() fn in case we were being passed an Array of nodes. Fixes test failures introduced by  dbb100d, and adds a test for it now that I know it can be done.

war/src/main/webapp/scripts/behavior.js
@@ -68,25 +68,24 @@ var Behaviour = {
         var behaviorsBySelector = {};
         Behaviour.list._each(function(sheet) {
             for (var selector in sheet){
-                function apply(n) {
-                    var behavior = sheet[selector];
-                    var behaviors = behaviorsBySelector[selector];
-                    if (behaviors == null) {
-                        behaviors = [];
-                        behaviorsBySelector[selector] = behaviors;
-                    }
-                    if (behaviors.indexOf(behavior.toString()) == -1) {
-                        behaviors.push(behavior.toString());
+                var behavior = sheet[selector];
+                var behaviors = behaviorsBySelector[selector];
+                if (behaviors == null) {
+                    behaviors = [];
+                    behaviorsBySelector[selector] = behaviors;
+                }
+                if (behaviors.indexOf(behavior.toString()) == -1) {
+                    behaviors.push(behavior.toString());
+                    function apply(n) {
                         var list = findElementsBySelector(n,selector,includeSelf);
                         if (list.length>0)  // just to simplify setting of a breakpoint.
                             list._each(sheet[selector]);
                     }
-                }
-
-                if (startNode instanceof Array) {
-                    startNode._each(apply)
-                } else {
-                    apply(startNode);
+                    if (startNode instanceof Array) {
+                        startNode._each(apply)
+                    } else {
+                        apply(startNode);
+                    }
                 }
             }
         });

test/src/test/java/scripts/BehaviorTest.java
@@ -25,6 +25,7 @@ package scripts;
 
 import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
 
 /**
@@ -52,4 +53,12 @@ public class BehaviorTest extends HudsonTestCase {
     private int asInt(ScriptResult r) {
         return ((Double)r.getJavaScriptResult()).intValue();
     }
+
+    @Bug(14495)
+    public void testDuplicateRegistrations() throws Exception {
+        HtmlPage p = createWebClient().goTo("self/testDuplicateRegistrations");
+        ScriptResult r = p.executeJavaScript("document.getElementsBySelector('DIV.a')[0].innerHTML");
+        assertEquals("initial and appended yet different", r.getJavaScriptResult().toString());
+    }
+
 }

test/src/test/resources/scripts/BehaviorTest/testDuplicateRegistrations.jelly
@@ -1,0 +1,48 @@
+<!--
+The MIT License
+
+Copyright (c) 2012, CloudBees, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-->
+<?jelly escape-by-default='true'?>
+<j:jelly xmlns:j="jelly:core" xmlns:st="jelly:stapler" xmlns:d="jelly:define" xmlns:l="/lib/layout" xmlns:t="/lib/hudson" xmlns:f="/lib/form">
+  <l:layout title="">
+    <l:main-panel>
+      <script>
+        Behaviour.register({
+          ".a" : function (e) {
+            e.innerHTML += ' and appended';
+          }
+        });
+        Behaviour.register({
+          ".a" : function (e) {
+            e.innerHTML += ' and appended';
+          }
+        });
+        Behaviour.register({
+          ".a" : function (e) {
+            e.innerHTML += ' yet different';
+          }
+        });
+      </script>
+      <div class="a">initial</div>
+    </l:main-panel>
+  </l:layout>
+</j:jelly>