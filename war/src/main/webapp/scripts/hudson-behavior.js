951f048bc1223b569a960b6b187aa6ce4711920f
Jesse Glick <jglick@cloudbees.com>
2012-08-22 18:51:32 -0400

    Continuing c9cf3b6, retain jenkinsRule but empty as a placeholder, and register its rules in the new way. Registering them with increasing positions, since there seems to be some implicit order dependency involving TR.rowvg-start.

--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -1163,6 +1163,15 @@ var jenkinsRules = {
 };
 /** @deprecated Use {@link Behaviour.specify} instead. */
 var hudsonRules = jenkinsRules; // legacy name
+(function() {
+    var p = 20;
+    for (var selector in jenkinsRules) {
+        Behaviour.specify(selector, 'hudson-behavior', p++, jenkinsRules[selector]);
+        delete jenkinsRules[selector];
+    }
+})();
+// now empty, but plugins can stuff things in here later:
+Behaviour.register(hudsonRules);
 
 function applyTooltip(e,text) {
         // copied from YAHOO.widget.Tooltip.prototype.configContext to efficiently add a new element
@@ -1214,10 +1223,6 @@ function refillOnChange(e,onChange) {
     }
     h();   // initial fill
 }
-
-Behaviour.register(hudsonRules);
-
-
 
 function xor(a,b) {
     // convert both values to boolean by '!' and then do a!=b
