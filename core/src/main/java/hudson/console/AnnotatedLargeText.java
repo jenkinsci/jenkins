d4657b8f8a5eb1105bf193feda3eaeb55621d0bd
Martin Schroeder <martin.h.schroeder@intel.com>
2012-08-29 15:59:38 +0200

    Added copyright notice, as per company policy.
    Signed-off-by: Martin Schroeder <martin.h.schroeder@intel.com>

--- core/src/main/java/hudson/console/AnnotatedLargeText.java
+++ core/src/main/java/hudson/console/AnnotatedLargeText.java
@@ -3,6 +3,8 @@
  *
  * Copyright (c) 2004-2010, Sun Microsystems, Inc.
  *
+ * Copyright (c) 2012, Martin Schroeder, Intel Mobile Communications GmbH
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
