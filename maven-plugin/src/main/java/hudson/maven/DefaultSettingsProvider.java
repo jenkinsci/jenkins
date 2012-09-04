8d8b3df8ac46045e3cc9c54eb05ecd4d1551defb
Nicolas De Loof <nicolas.deloof@gmail.com>
2012-09-04 09:28:40 +0200

    Default settings provider implementation

---
+++ maven-plugin/src/main/java/hudson/maven/DefaultSettingsProvider.java
@@ -0,0 +1,34 @@
+package hudson.maven;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractBuild;
+import hudson.util.ArgumentListBuilder;
+import hudson.util.FormValidation;
+import org.kohsuke.stapler.AncestorInPath;
+import org.kohsuke.stapler.QueryParameter;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static hudson.Util.fixEmpty;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class DefaultSettingsProvider extends SettingsProvider {
+
+    @Override
+    public void configure(ArgumentListBuilder margs, AbstractBuild project) throws IOException, InterruptedException {
+        return;
+    }
+
+    @Extension(ordinal = 99)
+    public static class DescriptorImpl extends SettingsProviderDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return "Use default maven settings";
+        }
+    }
+}
