1ced53e2d1f398b11a27e8513b4499899824a807
Jesse Glick <jglick@cloudbees.com>
2012-09-07 12:56:16 -0400

    Fixing redundant marking.

--- core/src/test/java/hudson/util/RobustReflectionConverterTest.java
+++ core/src/test/java/hudson/util/RobustReflectionConverterTest.java
@@ -78,6 +78,7 @@ public class RobustReflectionConverterTest extends TestCase {
         Moonwalk s3 = new Moonwalk();
         s3.number = 3;
         s3.boot = new Boot();
+        s3.jacket = new Jacket();
         s3.lover = new Jean();
         Bild b = new Bild();
         b.steppes = new Steppe[] {s1, s2, s3};
@@ -85,8 +86,9 @@ public class RobustReflectionConverterTest extends TestCase {
         p.bildz = new Bild[] {b};
         assertEquals("<Projekt><bildz><Bild><steppes>"
                 + "<Enchufla plugin='p1'><number>1</number><direction>North</direction></Enchufla>"
-                + "<Moonwalk plugin='p2'><number>2</number><boot plugin='p2'/><lover class='Billy' plugin='p3'/></Moonwalk>"
-                + "<Moonwalk plugin='p2'><number>3</number><boot plugin='p2'/><lover class='Jean' plugin='p4'/></Moonwalk>"
+                // note no plugin='p2' on <boot/> since that would be redundant; <jacket/> is quiet even though unowned
+                + "<Moonwalk plugin='p2'><number>2</number><boot/><lover class='Billy' plugin='p3'/></Moonwalk>"
+                + "<Moonwalk plugin='p2'><number>3</number><boot/><jacket/><lover class='Jean' plugin='p4'/></Moonwalk>"
                 + "</steppes></Bild></bildz></Projekt>",
                 xs.toXML(p).replaceAll("\\Q" + RobustReflectionConverterTest.class.getName() + "\\E(_-|[$])", "").replaceAll("\r?\n *", "").replace('"', '\''));
     }
@@ -107,10 +109,12 @@ public class RobustReflectionConverterTest extends TestCase {
     @Owner("p2")
     public static class Moonwalk extends Steppe {
         Boot boot;
+        Jacket jacket;
         Lover lover;
     }
     @Owner("p2")
     public static class Boot {}
+    public static class Jacket {}
     @Owner("p2")
     public static abstract class Lover {}
     @Owner("p3")
