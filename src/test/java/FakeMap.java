b94565cc327a6ea93fda13703b0ed157a585b16b
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-09-15 10:17:35 -0700

    adding a test case

---
+++ src/test/java/FakeMap.java
@@ -0,0 +1,54 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class FakeMap extends Attempt2<Build> {
+    public FakeMap(File dir) {
+        super(dir);
+    }
+
+    @Override
+    protected int getNumberOf(Build build) {
+        return build.n;
+    }
+
+    @Override
+    protected String getIdOf(Build build) {
+        return build.id;
+    }
+
+    @Override
+    protected FilenameFilter createDirectoryFilter() {
+        return new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.startsWith("_");
+            }
+        };
+    }
+
+    @Override
+    protected Build retrieve(File dir) throws IOException {
+        String n = FileUtils.readFileToString(new File(dir, "n")).trim();
+        return new Build(Integer.parseInt(n),dir.getName());
+    }
+}
+
+class Build {
+    final int n;
+    final String id;
+
+    Build(int n, String id) {
+        this.n = n;
+        this.id = id;
+    }
+
+    public void asserts(int n, String id) {
+        assert this.n==n;
+        assert this.id.equals(id);
+    }
+}
---
+++ src/test/java/FakeMapBuilder.java
@@ -0,0 +1,44 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class FakeMapBuilder implements TestRule {
+    private File dir;
+
+    public FakeMapBuilder() {
+    }
+
+    public FakeMapBuilder add(int n, String id) throws IOException {
+        File build = new File(dir, id);
+        build.mkdir();
+        FileUtils.write(new File(build,"n"),Integer.toString(n));
+        return this;
+    }
+
+    public FakeMap make() {
+        return new FakeMap(dir);
+    }
+
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                dir = File.createTempFile("lazyload","test");
+                dir.delete();
+                dir.mkdirs();
+                try {
+                    base.evaluate();
+                } finally {
+                    FileUtils.deleteDirectory(dir);
+                }
+            }
+        };
+    }
+}
