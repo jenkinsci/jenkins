3b4b6475a72272b06640e0483b8b2e9807b08be0
Jesse Glick <jglick@cloudbees.com>
2012-10-16 10:24:04 -0400

    @since, changelog for User.getAuthorities.

--- core/src/main/java/hudson/model/User.java
+++ core/src/main/java/hudson/model/User.java
@@ -630,7 +630,7 @@ public class User extends AbstractModelObject implements AccessControlled, Descr
      * Checks for authorities (groups) associated with this user.
      * If the caller lacks {@link Jenkins#ADMINISTER}, or any problems arise, returns an empty list.
      * {@link SecurityRealm#AUTHENTICATED_AUTHORITY} and the username, if present, are omitted.
-     * @since XXX
+     * @since 1.488
      * @return a possibly empty list
      */
     public @Nonnull List<String> getAuthorities() {
