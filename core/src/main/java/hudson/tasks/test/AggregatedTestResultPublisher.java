8ff2c0e4bf92db23a1d96c28ffb0cfad8e302ad3
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-10-19 11:00:10 +0900

    improved the auto-completion

--- core/src/main/java/hudson/tasks/test/AggregatedTestResultPublisher.java
+++ core/src/main/java/hudson/tasks/test/AggregatedTestResultPublisher.java
@@ -32,6 +32,7 @@ import hudson.Util;
 import static hudson.Util.fixNull;
 import hudson.model.BuildListener;
 import hudson.model.Fingerprint.RangeSet;
+import hudson.model.ItemGroup;
 import jenkins.model.Jenkins;
 import hudson.model.Item;
 import hudson.model.Job;
@@ -351,19 +352,9 @@ public class AggregatedTestResultPublisher extends Recorder {
                 return new AggregatedTestResultPublisher(s.getString("jobs"), req.getParameter("includeFailedBuilds") != null);
         }
 
-        public AutoCompletionCandidates doAutoCompleteJobs(@QueryParameter String value) {
-            AutoCompletionCandidates candidates = new AutoCompletionCandidates();
-            List<Job> jobs = Jenkins.getInstance().getItems(Job.class);
-            for (Job job: jobs) {
-                if (job.getFullName().startsWith(value)) {
-                    if (job.hasPermission(Item.READ)) {
-                        candidates.add(job.getFullName());
-                    }
-                }
-            }
-            return candidates;
+        public AutoCompletionCandidates doAutoCompleteJobs(@QueryParameter String value, @AncestorInPath Item self, @AncestorInPath ItemGroup container) {
+            return AutoCompletionCandidates.ofJobNames(Job.class,value,self,container);
         }
-
     }
 
 }
