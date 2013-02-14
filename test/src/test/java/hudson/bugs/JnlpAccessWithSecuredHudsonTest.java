6270556267ce19e37786d7ca3a95f355a31488b3
Jesse Glick <jglick@cloudbees.com>
2013-02-14 13:59:12 -0500

    [SECURITY-54] Realistic test of connection using -secret as from a service installation.

--- test/src/test/java/hudson/bugs/JnlpAccessWithSecuredHudsonTest.java
+++ test/src/test/java/hudson/bugs/JnlpAccessWithSecuredHudsonTest.java
@@ -29,6 +29,8 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.xml.XmlPage;
 import hudson.model.Node.Mode;
 import hudson.model.Slave;
+import hudson.remoting.Launcher;
+import hudson.remoting.Which;
 import hudson.slaves.JNLPLauncher;
 import hudson.slaves.RetentionStrategy;
 import hudson.slaves.DumbSlave;
@@ -45,6 +47,7 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import org.apache.tools.ant.util.JavaEnvUtils;
 
 /**
  * Makes sure that the jars that web start needs are readable, even when the anonymous user doesn't have any read access. 
@@ -95,6 +98,34 @@ public class JnlpAccessWithSecuredHudsonTest extends HudsonTestCase {
         }
     }
 
-    // XXX try to use -secret
+    @PresetData(DataSet.NO_ANONYMOUS_READACCESS)
+    @SuppressWarnings("SleepWhileInLoop")
+    public void testServiceUsingDirectSecret() throws Exception {
+        Slave slave = createNewJnlpSlave("test");
+        jenkins.setNodes(Collections.singletonList(slave));
+        new WebClient().goTo("computer/test/slave-agent.jnlp?encrypt=true", "application/octet-stream");
+        String secret = slave.getComputer().getJnlpMac();
+        // To watch it fail: secret = secret.replace('1', '2');
+        ProcessBuilder pb = new ProcessBuilder(JavaEnvUtils.getJreExecutable("java"), "-jar", Which.jarFile(Launcher.class).getAbsolutePath(), "-jnlpUrl", getURL() + "computer/test/slave-agent.jnlp", "-secret", secret);
+        try {
+            pb = (ProcessBuilder) ProcessBuilder.class.getMethod("inheritIO").invoke(pb);
+        } catch (NoSuchMethodException x) {
+            // prior to Java 7
+        }
+        System.err.println("Running: " + pb.command());
+        Process p = pb.start();
+        try {
+            for (int i = 0; i < /* one minute */600; i++) {
+                if (slave.getComputer().isOnline()) {
+                    System.err.println("JNLP slave successfully connected");
+                    return;
+                }
+                Thread.sleep(100);
+            }
+            fail("JNLP slave agent failed to connect");
+        } finally {
+            p.destroy();
+        }
+    }
 
 }
