42ee6113b7beff8dccba48593cefe0ad23636051
Jesse Glick <jglick@cloudbees.com>
2013-02-27 16:44:43 -0500

    [JENKINS-15156] Missing call to onCreatedFromScratch caused failures in MatrixProjectTest (when run from Surefire at least).

--- core/src/main/java/hudson/matrix/MatrixProject.java
+++ core/src/main/java/hudson/matrix/MatrixProject.java
@@ -609,6 +609,7 @@ public class MatrixProject extends AbstractProject<MatrixProject,MatrixBuild> im
 	            MatrixConfiguration config = configurations.get(c);
 	            if(config==null) {
 	                config = new MatrixConfiguration(this,c);
+                    config.onCreatedFromScratch();
 	                config.save();
 	                configurations.put(config.getCombination(), config);
 	            }
