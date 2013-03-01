jenkinsci/jenkins

commit 29426012ce377d73686f58f30407eae368a23856

[FIXED JENKINS-16718] check executor has been interrupted

core/src/main/java/hudson/model/Build.java
@@ -195,11 +195,16 @@ public abstract class Build <P extends Project<P,B>,B extends Build<P,B>>
         }
 
         private boolean build(BuildListener listener, Collection<Builder> steps) throws IOException, InterruptedException {
-            for( BuildStep bs : steps )
+            for( BuildStep bs : steps ) {
                 if(!perform(bs,listener)) {
                     LOGGER.fine(MessageFormat.format("{0} : {1} failed", Build.this.toString(), bs));
                     return false;
                 }
+                if (getExecutor().isInterrupted()) {
+                    // someone asked build interruption, let stop the build before trying to run another build step
+                    throw new InterruptedException();
+                }
+            }
             return true;
         }
     }

commit 179f7578bd4b74b0932e5f6f7ed721a0d7a9424b

Fixed Regression: NPE when checking if the executor is interrupted
29426012ce introduced an NPE.
If a project implements Queue.FlyweightTask, build.getExecutor() returns null.
This prevents any flyweight build from finishing successfully.

core/src/main/java/hudson/model/Build.java
@@ -200,7 +200,9 @@ public abstract class Build <P extends Project<P,B>,B extends Build<P,B>>
                     LOGGER.fine(MessageFormat.format("{0} : {1} failed", Build.this.toString(), bs));
                     return false;
                 }
-                if (getExecutor().isInterrupted()) {
+                
+                Executor executor = getExecutor();
+                if (executor != null && executor.isInterrupted()) {
                     // someone asked build interruption, let stop the build before trying to run another build step
                     throw new InterruptedException();
                 }