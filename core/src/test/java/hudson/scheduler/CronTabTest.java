jenkinsci/jenkins

commit 20249e4adc4abaf3c81cd864331508cf68b8777b

[FIXED JENKINS-12357] Crontab#ceil may stuck in an infinite loop or may return an incorrect value if day of week is 7 (=0, Sunday)
To use @Test(timeout), inheritance from TestCase should be removed

core/src/test/java/hudson/scheduler/CronTabTest.java
@@ -29,7 +29,8 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.Test;
 import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.Url;
 
@@ -38,7 +39,7 @@ import static java.util.Calendar.MONDAY;
 /**
  * @author Kohsuke Kawaguchi
  */
-public class CronTabTest extends TestCase {
+public class CronTabTest {
     public void test1() throws ANTLRException {
         new CronTab("@yearly");
         new CronTab("@weekly");
@@ -70,6 +71,16 @@ public class CronTabTest extends TestCase {
         Calendar c = new GregorianCalendar(2010,0,1,15,55);
         // the first such day in 2010 is Aug 1st
         compare(new GregorianCalendar(2010,7,1,0,0),x.ceil(c));
+    }
+
+    @Test(timeout = 1000)
+    @Bug(12357)
+    public void testCeil3_DoW7() throws Exception {
+        // similar to testCeil3, but DoW=7 may stuck in an infinite loop
+        CronTab x = new CronTab("0 0 1 * 7");
+        Calendar c = new GregorianCalendar(2010,0,1,15,55);
+        // the first such day in 2010 is Aug 1st
+        compare(new GregorianCalendar(2010, 7, 1, 0, 0), x.ceil(c));
     }
 
     /**

core/src/main/java/hudson/scheduler/CronTab.java
@@ -89,8 +89,10 @@ public final class CronTab {
         spec = format;
 
         parser.startRule(this);
-        if((dayOfWeek&(1<<7))!=0)
+        if((dayOfWeek&(1<<7))!=0) {
             dayOfWeek |= 1; // copy bit 7 over to bit 0
+            dayOfWeek &= ~(1<<7); // clear bit 7 or CalendarField#ceil will return an invalid value 7
+        }
     }
 
 

core/src/test/java/hudson/scheduler/CronTabDayOfWeekLocaleTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.For;
 import org.jvnet.hudson.test.Url;
 
@@ -273,7 +274,68 @@ public class CronTabDayOfWeekLocaleTest {
         expected.set(2011, 0, 9, 1, 0, 0);
         compare(expected, actual);
     }
-    
+
+    @Test
+    @Bug(12357)
+    public void isSundayAndNextRunIsNextSunday7() throws Exception {
+        final Calendar cal = Calendar.getInstance(locale);
+        cal.set(2011, 0, 16, 1, 0, 0); // Sunday, Jan 16th 2011, 01:00
+        final String cronStr = "0 0 * * 7"; // Sundays(7 not 0) @00:00
+
+        final CronTab cron = new CronTab(cronStr);
+        final Calendar actual = cron.ceil(cal);
+
+        final Calendar expected = Calendar.getInstance();
+        // Expected next: Sunday, Jan 22th 2011, 00:00
+        expected.set(2011, 0, 23, 0, 0, 0);
+        compare(expected, actual);
+    }
+
+    @Test
+    public void isSundayAndPreviousRunIsPreviousSunday7() throws Exception {
+        final Calendar cal = Calendar.getInstance(locale);
+        cal.set(2011, 0, 16, 0, 0, 0); // Sunday, Jan 16th 2011, 00:00
+        final String cronStr = "0 1 * * 7"; // Sundays(7 not 0) @01:00
+
+        final CronTab cron = new CronTab(cronStr);
+        final Calendar actual = cron.floor(cal);
+
+        final Calendar expected = Calendar.getInstance();
+        // Expected next: Sunday, Jan 9th 2011, 01:00
+        expected.set(2011, 0, 9, 1, 0, 0);
+        compare(expected, actual);
+    }
+
+    @Test
+    public void isSaturdayAndNextRunIsSundayAsterisk() throws Exception {
+        final Calendar cal = Calendar.getInstance(locale);
+        cal.set(2011, 0, 15, 1, 0, 0); // Saturday, Jan 15th 2011, 01:00
+        final String cronStr = "0 0 * * *"; // Everyday @00:00
+
+        final CronTab cron = new CronTab(cronStr);
+        final Calendar actual = cron.ceil(cal);
+
+        final Calendar expected = Calendar.getInstance();
+        // Expected next: Sunday, Jan 16th 2011, 00:00
+        expected.set(2011, 0, 16, 0, 0, 0);
+        compare(expected, actual);
+    }
+
+    @Test
+    public void isSundayAndPreviousRunIsSaturdayAsterisk() throws Exception {
+        final Calendar cal = Calendar.getInstance(locale);
+        cal.set(2011, 0, 16, 0, 0, 0); // Sunday, Jan 16th 2011, 00:00
+        final String cronStr = "0 23 * * *"; // Everyday @23:00
+
+        final CronTab cron = new CronTab(cronStr);
+        final Calendar actual = cron.floor(cal);
+
+        final Calendar expected = Calendar.getInstance();
+        // Expected next: Saturday, Jan 15th 2011, 23:00
+        expected.set(2011, 0, 15, 23, 0, 0);
+        compare(expected, actual);
+    }
+
     private void compare(final Calendar expected, final Calendar actual) {
         final DateFormat f = DateFormat.getDateTimeInstance();
         final String msg = "Locale: " + locale + " FirstDayOfWeek: " + actual.getFirstDayOfWeek() + " Expected: "

commit c454eaacf45d37125849192b041431d693386f4b

ohtake’s 20249e4 accidentally suppressed all but one test case.

core/src/test/java/hudson/scheduler/CronTabTest.java
@@ -40,6 +40,8 @@ import static java.util.Calendar.MONDAY;
  * @author Kohsuke Kawaguchi
  */
 public class CronTabTest {
+
+    @Test
     public void test1() throws ANTLRException {
         new CronTab("@yearly");
         new CronTab("@weekly");
@@ -48,6 +50,7 @@ public class CronTabTest {
         new CronTab("0 0 * 1-10/3 *");
     }
 
+    @Test
     public void testCeil1() throws Exception {
         CronTab x = new CronTab("0,30 * * * *");
         Calendar c = new GregorianCalendar(2000,2,1,1,10);
@@ -58,6 +61,7 @@ public class CronTabTest {
         compare(new GregorianCalendar(2000,2,1,2, 0),x.ceil(c));
     }
 
+    @Test
     public void testCeil2() throws Exception {
         // make sure that lower fields are really reset correctly
         CronTab x = new CronTab("15,45 3 * * *");
@@ -65,6 +69,7 @@ public class CronTabTest {
         compare(new GregorianCalendar(2000,2,1,3,15),x.ceil(c));
     }
 
+    @Test
     public void testCeil3() throws Exception {
         // conflict between DoM and DoW. In this we need to find a day that's the first day of a month and Sunday
         CronTab x = new CronTab("0 0 1 * 0");
@@ -87,6 +92,7 @@ public class CronTabTest {
      * Verifies that HUDSON-8656 never crops up again.
      */
     @Url("http://issues.hudson-ci.org/browse/HUDSON-8656")
+    @Test
     public void testCeil4() throws ANTLRException {
         final Calendar cal = Calendar.getInstance(new Locale("de", "de"));
         cal.set(2011, 0, 16, 0, 0, 0); // Sunday, Jan 16th 2011, 00:00
@@ -108,6 +114,7 @@ public class CronTabTest {
      * Verifies that HUDSON-8656 never crops up again.
      */
     @Url("http://issues.hudson-ci.org/browse/HUDSON-8656")
+    @Test
     public void testCeil5() throws ANTLRException {
         final Calendar cal = Calendar.getInstance(new Locale("de", "at"));
         cal.set(2011, 0, 16, 0, 0, 0); // Sunday, Jan 16th 2011, 00:00
@@ -125,6 +132,7 @@ public class CronTabTest {
         assertEquals(expectedDate.get(Calendar.DAY_OF_MONTH), next.get(Calendar.DAY_OF_MONTH)); // FAILS: is Monday, Jan 10th, 23:00
     }
 
+    @Test
     public void testFloor1() throws Exception {
         CronTab x = new CronTab("30 * * * *");
         Calendar c = new GregorianCalendar(2000,2,1,1,40);
@@ -135,6 +143,7 @@ public class CronTabTest {
         compare(new GregorianCalendar(2000,2,1,0,30),x.floor(c));
     }
 
+    @Test
     public void testFloor2() throws Exception {
         // make sure that lower fields are really reset correctly
         CronTab x = new CronTab("15,45 3 * * *");
@@ -142,6 +151,7 @@ public class CronTabTest {
         compare(new GregorianCalendar(2000,2,1,3,45),x.floor(c));
     }
 
+    @Test
     public void testFloor3() throws Exception {
         // conflict between DoM and DoW. In this we need to find a day that's the first day of a month and Sunday in 2010
         CronTab x = new CronTab("0 0 1 * 0");
@@ -151,6 +161,7 @@ public class CronTabTest {
     }
 
     @Bug(8401)
+    @Test
     public void testFloor4() throws Exception {
         // conflict between DoM and DoW. In this we need to find a day that's the first day of a month and Sunday in 2010
         CronTab x = new CronTab("0 0 1 * 0");
@@ -171,6 +182,7 @@ public class CronTabTest {
         assertEquals(a,b);
     }
 
+    @Test
     public void testHash1() throws Exception {
         CronTab x = new CronTab("H H(5-8) * * *",new Hash() {
             public int next(int n) {
@@ -182,6 +194,7 @@ public class CronTabTest {
         assertEquals(x.bits[1],1L<<8);
     }
 
+    @Test
     public void testHash2() throws Exception {
         CronTab x = new CronTab("H H(5-8) * * *",new Hash() {
             public int next(int n) {