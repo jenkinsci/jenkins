6fd541006da466a1731c768b8b6f69fdbdc7c79b
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-16 13:38:54 -0700

    [JENKINS-18352] we want to prefer *.jpi over *.hpi, but the code was other way around
    (cherry picked from commit 5aa4af75c2c4c66541f1fd24ef82c5d46a79940d)

--- core/src/main/java/hudson/init/InitStrategy.java
+++ core/src/main/java/hudson/init/InitStrategy.java
@@ -51,10 +51,10 @@ public class InitStrategy {
         getBundledPluginsFromProperty(r);
 
         // similarly, we prefer *.jpi over *.hpi
-        listPluginFiles(pm, ".hpl", r); // linked plugin. for debugging. (for backward compatibility)
         listPluginFiles(pm, ".jpl", r); // linked plugin. for debugging.
-        listPluginFiles(pm, ".hpi", r); // plugin jar file (for backward compatibility)
+        listPluginFiles(pm, ".hpl", r); // linked plugin. for debugging. (for backward compatibility)
         listPluginFiles(pm, ".jpi", r); // plugin jar file
+        listPluginFiles(pm, ".hpi", r); // plugin jar file (for backward compatibility)
 
         return r;
     }
