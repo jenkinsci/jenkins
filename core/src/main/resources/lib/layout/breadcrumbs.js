0f6b02bd18d3da009805199156b16d23f904e231
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-16 17:10:09 -0700

    Modifying the breadcrumb context menu so that it also requires clicking 'v'.

--- core/src/main/resources/lib/layout/breadcrumbs.js
+++ core/src/main/resources/lib/layout/breadcrumbs.js
@@ -114,7 +114,6 @@ var breadcrumbs = (function() {
             this.style.visibility = "hidden";
         };
         menuSelector.observe("click",function () {
-            this.hide();
             handleHover(this.target);
         });
 
@@ -190,11 +189,11 @@ var breadcrumbs = (function() {
         return false;
     }
 
-    Behaviour.specify("#breadcrumbs LI", 'breadcrumbs', 0, function (e) {
-        // when the mouse hovers over LI, activate the menu
-        if (e.hasClassName("no-context-menu"))  return;
-        e.observe("mouseover", function () { handleHover(e.firstChild) });
-    });
+//    Behaviour.specify("#breadcrumbs LI", 'breadcrumbs', 0, function (e) {
+//        // when the mouse hovers over LI, activate the menu
+//        if (e.hasClassName("no-context-menu"))  return;
+//        e.observe("mouseover", function () { handleHover(e.firstChild) });
+//    });
 
     Behaviour.specify("A.model-link", 'breadcrumbs', 0, function (a) {
         // ditto for model-link, but give it a larger delay to avoid unintended menus to be displayed
