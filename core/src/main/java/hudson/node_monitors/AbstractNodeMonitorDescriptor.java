5c95be34e4607a7c4c5470ffe79942b513188956
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-18 18:39:18 -0700

    don't let a programming error kill this thread

--- core/src/main/java/hudson/node_monitors/AbstractNodeMonitorDescriptor.java
+++ core/src/main/java/hudson/node_monitors/AbstractNodeMonitorDescriptor.java
@@ -215,6 +215,8 @@ public abstract class AbstractNodeMonitorDescriptor<T> extends Descriptor<NodeMo
                         data.put(c,null);
                     else
                         data.put(c,monitor(c));
+                } catch (RuntimeException e) {
+                    LOGGER.log(Level.WARNING, "Failed to monitor "+c.getDisplayName()+" for "+getDisplayName(), e);
                 } catch (IOException e) {
                     LOGGER.log(Level.WARNING, "Failed to monitor "+c.getDisplayName()+" for "+getDisplayName(), e);
                 } catch (InterruptedException e) {
