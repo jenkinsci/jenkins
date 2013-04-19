64da3d5e274eb1ba688439bf00b3e691914fe74d
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-19 13:40:46 -0700

    simplification

--- core/src/main/resources/lib/form/advanced/advanced.js
+++ core/src/main/resources/lib/form/advanced/advanced.js
@@ -1,15 +1,11 @@
 Behaviour.specify("INPUT.advanced-button", 'advanced', 0, function(e) {
         makeButton(e,function(e) {
-            var link = e.target;
-            while(!Element.hasClassName(link,"advancedLink"))
-                link = link.parentNode;
+            var link = $(e.target).up(".advancedLink");
             link.style.display = "none"; // hide the button
 
-            var container = $(link).next().down(); // TABLE -> TBODY
+            var container = link.next().down(); // TABLE -> TBODY
 
-            var tr = link;
-            while (tr.tagName != "TR")
-                tr = tr.parentNode;
+            var tr = link.up("TR");
 
             // move the contents of the advanced portion into the main table
             var nameRef = tr.getAttribute("nameref");
