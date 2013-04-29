6229da1a8700f221192ab24752821a7844425a2f
Patrick McKeown <pmaccamp@yahoo.com>
2013-04-29 01:14:59 -0700

    Adding completed test that verifies threads obtain the correct security context

--- test/src/test/java/jenkins/security/SecurityContextExecutorServiceTest.java
+++ test/src/test/java/jenkins/security/SecurityContextExecutorServiceTest.java
@@ -1,23 +1,41 @@
 package jenkins.security;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
 import org.junit.Rule;
+import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.PresetData;
 
 /**
- * @author Kohsuke Kawaguchi
+ * @author Patrick McKeown
  */
 public class SecurityContextExecutorServiceTest {
+    private int NUM_THREADS = 10;
     @Rule
     public JenkinsRule j = new JenkinsRule();
-    
-    protected void first() throws Exception {
-        j.createDummySecurityRealm();
-        System.out.println(j.jenkins.getSecurityRealm());
-    }
-    
-    public void testSecurity() throws Exception {
 
-      
-    }
+    @Test
+    @PresetData(PresetData.DataSet.NO_ANONYMOUS_READACCESS)
+    public void testExecutorServiceWithSecurity() throws Exception {
+        ScheduledThreadPoolExecutor service = new ScheduledThreadPoolExecutor(
+                NUM_THREADS);
+        SecurityContext initialContext = SecurityContextHolder.getContext();
 
+        Future<Object> result = SecurityContextExecutorService
+                .wrapExecutorWithSecurityContext(service).submit(
+                        new Callable<Object>() {
+                            public Object call() throws Exception {
+                                return SecurityContextHolder.getContext();
+                            }
+                        });
+        SecurityContext threadContext = (SecurityContext) result.get();
+        assertEquals(initialContext, threadContext);
+    }
 }
