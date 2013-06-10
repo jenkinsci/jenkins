0ca88a6ceae605e7d2e126a328a196c046a91a12
Jesse Glick <jglick@cloudbees.com>
2013-06-10 13:33:12 -0400

    [JENKINS-16952] [JENKINS-18274] Slave computers should record all log messages at INFO+, not just those in the hudson.* namespace.

--- core/src/main/java/hudson/slaves/SlaveComputer.java
+++ core/src/main/java/hudson/slaves/SlaveComputer.java
@@ -736,7 +736,7 @@ public class SlaveComputer extends Computer {
             return null;
         }
         private static final long serialVersionUID = 1L;
-        private static final Logger LOGGER = Logger.getLogger("hudson");
+        private static final Logger LOGGER = Logger.getLogger("");
     }
 
     /**
