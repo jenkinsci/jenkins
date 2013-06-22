6badbb3227701f93aeebf208cc47f8b1540617da
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-06-21 17:38:33 -0700

    Added the error handling screen
    This page gets displayed when we throw an unhandled exception.

--- core/src/main/java/hudson/util/PluginServletFilter.java
+++ core/src/main/java/hudson/util/PluginServletFilter.java
@@ -32,6 +32,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Vector;
