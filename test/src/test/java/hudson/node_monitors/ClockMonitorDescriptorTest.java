efb91a838ffbe78d12302672f4dafbe46b72c2b1
Christoph Kutzinski <kutzi@gmx.de>
2013-07-13 13:29:26 +0200

    Use JenkinsRule instead of deprecated HudsonTestCase

--- test/src/test/java/hudson/node_monitors/ClockMonitorDescriptorTest.java
+++ test/src/test/java/hudson/node_monitors/ClockMonitorDescriptorTest.java
@@ -1,22 +1,32 @@
 package hudson.node_monitors;
 
-import org.jvnet.hudson.test.HudsonTestCase;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import hudson.slaves.DumbSlave;
 import hudson.slaves.SlaveComputer;
 import hudson.util.ClockDifference;
 import hudson.util.TimeUnit2;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
 /**
  * @author Richard Mortimer
  */
-public class ClockMonitorDescriptorTest extends HudsonTestCase {
+public class ClockMonitorDescriptorTest {
+    
+    @Rule
+    public JenkinsRule jenkins = new  JenkinsRule();
+    
     /**
      * Makes sure that it returns sensible values.
      */
+    @Test
     public void testClockMonitor() throws Exception {
-        DumbSlave s = createSlave();
+        DumbSlave s = jenkins.createOnlineSlave();
         SlaveComputer c = s.getComputer();
-        c.connect(false).get(); // wait until it's connected
         if(c.isOffline())
             fail("Slave failed to go online: "+c.getLog());
 
