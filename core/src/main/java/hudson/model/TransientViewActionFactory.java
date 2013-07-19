cf81d19aec2bd566dfbdb9d54a748840ccac0c4e
Mark Waite <mwaite@ptc.com>
2013-07-19 11:21:34 -0600

    Terminate javadoc @link tags with curly brace instead of right paren

--- core/src/main/java/hudson/model/TransientViewActionFactory.java
+++ core/src/main/java/hudson/model/TransientViewActionFactory.java
@@ -30,7 +30,7 @@ public abstract class TransientViewActionFactory implements ExtensionPoint {
 	}
 	
     /**
-     * Creates {@link Action)s for a view, using all registered {@link TransientViewActionFactory}s.
+     * Creates {@link Action}s for a view, using all registered {@link TransientViewActionFactory}s.
      */
 	public static List<Action> createAllFor(View v) {
 		List<Action> result = new ArrayList<Action>();
