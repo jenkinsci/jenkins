jenkinsci/jenkins

commit e2134565049fe4352b411bf5283d3a132b3dd365

Completed JSR-250 license information

licenseCompleter.groovy
@@ -31,6 +31,11 @@ complete {
         if (dependency.licenses.isEmpty())
             dependency.licenses=[cddl]
     }
+    
+    // according to JSR-250 1.0-20050927.133100 POM, it came from JAX-WS, which is under CDDL.
+    match("javax.annotation:jsr250-api") {
+        rewriteLicense([], cddl)
+    }
 
     match("antlr:*") {
         rewriteLicense([], license("BSD License","http://www.antlr.org/license.html"))

commit e5a254f5d9b551fb0cfdfe8457d91a9094990225

Hotfix for build broken by e2134565049fe4352b411bf5283d3a132b3dd365.

licenseCompleter.groovy
@@ -32,10 +32,12 @@ complete {
             dependency.licenses=[cddl]
     }
     
+/* TODO
     // according to JSR-250 1.0-20050927.133100 POM, it came from JAX-WS, which is under CDDL.
     match("javax.annotation:jsr250-api") {
         rewriteLicense([], cddl)
     }
+*/
 
     match("antlr:*") {
         rewriteLicense([], license("BSD License","http://www.antlr.org/license.html"))