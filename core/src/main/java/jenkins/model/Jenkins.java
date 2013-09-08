9b058aebc36fdd98d211d7f5bbd18a8661627378
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-09-09 00:57:54 +0100

    killComputer interrupts executors, so no need to do it separately

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -2653,7 +2653,6 @@ public class Jenkins extends AbstractCIBase implements ModifiableTopLevelItemGro
         Set<Future<?>> pending = new HashSet<Future<?>>();
         terminating = true;
         for( Computer c : computers.values() ) {
-            c.interrupt();
             killComputer(c);
             pending.add(c.disconnect(null));
         }
