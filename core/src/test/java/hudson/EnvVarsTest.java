40e55c0dc15bf7ceef50cec074c5d8fb9e97f214
ikedam <devld@ikedam.jp>
2013-10-27 02:12:00 +0900

    [FIX JENKINS-20280] Calculates the order to override variables. This is an improved fix for [JENKINS-19488] and [JENKINS-19926].

--- core/src/test/java/hudson/EnvVarsTest.java
+++ core/src/test/java/hudson/EnvVarsTest.java
@@ -23,9 +23,14 @@
  */
 package hudson;
 
+import hudson.EnvVars.OverrideOrderCalculator;
 import junit.framework.TestCase;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -38,5 +43,108 @@ public class EnvVarsTest extends TestCase {
         EnvVars ev = new EnvVars(Collections.singletonMap("Path","A:B:C"));
         assertTrue(ev.containsKey("PATH"));
         assertEquals("A:B:C",ev.get("PATH"));
+    }
+    
+    public void testOverrideExpandingAll() throws Exception {
+        EnvVars env = new EnvVars();
+        env.put("PATH", "orig");
+        env.put("A", "Value1");
+        
+        EnvVars overrides = new EnvVars();
+        overrides.put("PATH", "append" + Platform.current().pathSeparator + "${PATH}");
+        overrides.put("B", "${A}Value2");
+        overrides.put("C", "${B}${D}");
+        overrides.put("D", "${E}");
+        overrides.put("E", "Value3");
+        overrides.put("PATH+TEST", "another");
+        
+        env.overrideExpandingAll(overrides);
+        
+        assertEquals("Value1Value2Value3", env.get("C"));
+        assertEquals("another" + Platform.current().pathSeparator + "append" + Platform.current().pathSeparator + "orig", env.get("PATH"));
+    }
+    
+    public void testOverrideOrderCalculatorSimple() {
+        EnvVars env = new EnvVars();
+        EnvVars overrides = new EnvVars();
+        overrides.put("A", "NoReference");
+        overrides.put("A+B", "NoReference");
+        overrides.put("B", "Refer1${A}");
+        overrides.put("C", "Refer2${B}");
+        overrides.put("D", "Refer3${B}${Nosuch}");
+        
+        OverrideOrderCalculator calc = new OverrideOrderCalculator(env, overrides);
+        assertEquals(0, calc.getRefereeNum("A"));
+        assertEquals(0, calc.getRefereeNum("A+B"));
+        assertEquals(0, calc.getRefereeNum("Nosuch"));
+        assertEquals(1, calc.getRefereeNum("B"));
+        assertEquals(2, calc.getRefereeNum("C"));
+        assertEquals(3, calc.getRefereeNum("D"));
+        
+        List<Map.Entry<String,String>> order = calc.getOrderedVariables();
+        assertEquals(5, order.size());
+        assertEquals("A", order.get(0).getKey());
+        assertEquals("B", order.get(1).getKey());
+        assertEquals("C", order.get(2).getKey());
+        assertEquals("D", order.get(3).getKey());
+        assertEquals("A+B", order.get(4).getKey());
+    }
+    
+    public void testOverrideOrderCalculatorInOrder() {
+        EnvVars env = new EnvVars();
+        EnvVars overrides = new EnvVars();
+        overrides.put("A", "NoReference");
+        overrides.put("B", "${A}");
+        overrides.put("C", "${B}"); // refers A, B
+        overrides.put("D", "${E}"); // refers A, B, C, E
+        overrides.put("E", "${C}"); // refers A, B, C
+        
+        OverrideOrderCalculator calc = new OverrideOrderCalculator(env, overrides) {
+            @Override
+            protected Iterator<String> rawKeyIterator() {
+                // should process in this order.
+                return Arrays.asList("A", "B", "C", "D", "E", "F").iterator();
+            }
+        };
+        assertEquals(0, calc.getRefereeNum("A"));
+        assertEquals(1, calc.getRefereeNum("B"));
+        assertEquals(2, calc.getRefereeNum("C"));
+        assertEquals(4, calc.getRefereeNum("D"));
+        assertEquals(3, calc.getRefereeNum("E"));
+    }
+    
+    public void testOverrideOrderCalculatorMultiple() {
+        EnvVars env = new EnvVars();
+        EnvVars overrides = new EnvVars();
+        overrides.put("A", "Noreference");
+        overrides.put("B", "${A}");
+        overrides.put("C", "${A}${B}");
+        
+        OverrideOrderCalculator calc = new OverrideOrderCalculator(env, overrides);
+        assertEquals(0, calc.getRefereeNum("A"));
+        assertEquals(1, calc.getRefereeNum("B"));
+        assertEquals(2, calc.getRefereeNum("C"));
+    }
+    
+    public void testOverrideOrderCalculatorSelfReference() {
+        EnvVars env = new EnvVars();
+        EnvVars overrides = new EnvVars();
+        overrides.put("PATH", "some;${PATH}");
+        
+        OverrideOrderCalculator calc = new OverrideOrderCalculator(env, overrides);
+        assertEquals(1, calc.getRefereeNum("PATH"));
+    }
+    
+    public void testOverrideOrderCalculatorCyclic() {
+        EnvVars env = new EnvVars();
+        EnvVars overrides = new EnvVars();
+        overrides.put("A", "${B}");
+        overrides.put("B", "${C}");
+        overrides.put("C", "${A}");
+        
+        OverrideOrderCalculator calc = new OverrideOrderCalculator(env, overrides);
+        assertEquals(3, calc.getRefereeNum("A"));
+        assertEquals(3, calc.getRefereeNum("B"));
+        assertEquals(3, calc.getRefereeNum("C"));
     }
 }
