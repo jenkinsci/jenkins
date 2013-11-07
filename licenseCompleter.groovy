a78bdf094cadbc1d478d76a974463bc8d7a970c2
Jesse Glick <jglick@cloudbees.com>
2013-11-07 11:44:54 -0500

    task-reactor 1.4 avoids an NPE during startup that may be masking some other problem.

--- licenseCompleter.groovy
+++ licenseCompleter.groovy
@@ -68,7 +68,7 @@ complete {
 
     // these are our own modules that have license in the trunk but not in these released versions
     // as we upgrade them, we should just pick up the license info from POM
-    match(["org.jvnet.hudson:task-reactor","org.jvnet.hudson:annotation-indexer","*:jinterop-wmi","*:maven2.1-interceptor","*:lib-jenkins-maven-embedder"]) {
+    match(["*:jinterop-wmi","*:maven2.1-interceptor","*:lib-jenkins-maven-embedder"]) {
         rewriteLicense([],jenkinsLicense)
     }
 
