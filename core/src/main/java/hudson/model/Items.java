e2659353406fbbc025f644a4f9bb374ea83ad6c0
Jesse Glick <jglick@cloudbees.com>
2014-01-08 16:24:05 -0500

    More meaningful parameter/local variable names.

--- core/src/main/java/hudson/model/Items.java
+++ core/src/main/java/hudson/model/Items.java
@@ -216,18 +216,18 @@ public class Items {
     }
 
     // Had difficulty adapting the version in Functions to use no live items, so rewrote it:
-    static String getRelativeNameFrom(String p, String g) {
-        String[] pA = p.isEmpty() ? new String[0] : p.split("/");
-        String[] gA = g.isEmpty() ? new String[0] : g.split("/");
+    static String getRelativeNameFrom(String itemFullName, String groupFullName) {
+        String[] itemFullNameA = itemFullName.isEmpty() ? new String[0] : itemFullName.split("/");
+        String[] groupFullNameA = groupFullName.isEmpty() ? new String[0] : groupFullName.split("/");
         for (int i = 0; ; i++) {
-            if (i == pA.length) {
-                if (i == gA.length) {
-                    // p and g are identical
+            if (i == itemFullNameA.length) {
+                if (i == groupFullNameA.length) {
+                    // itemFullName and groupFullName are identical
                     return ".";
                 } else {
-                    // p is an ancestor of g; insert ../ for rest of g
+                    // itemFullName is an ancestor of groupFullName; insert ../ for rest of groupFullName
                     StringBuilder b = new StringBuilder();
-                    for (int j = 0; j < gA.length - pA.length; j++) {
+                    for (int j = 0; j < groupFullNameA.length - itemFullNameA.length; j++) {
                         if (j > 0) {
                             b.append('/');
                         }
@@ -235,30 +235,30 @@ public class Items {
                     }
                     return b.toString();
                 }
-            } else if (i == gA.length) {
-                // g is an ancestor of p; insert rest of p
+            } else if (i == groupFullNameA.length) {
+                // groupFullName is an ancestor of itemFullName; insert rest of itemFullName
                 StringBuilder b = new StringBuilder();
-                for (int j = i; j < pA.length; j++) {
+                for (int j = i; j < itemFullNameA.length; j++) {
                     if (j > i) {
                         b.append('/');
                     }
-                    b.append(pA[j]);
+                    b.append(itemFullNameA[j]);
                 }
                 return b.toString();
-            } else if (pA[i].equals(gA[i])) {
+            } else if (itemFullNameA[i].equals(groupFullNameA[i])) {
                 // identical up to this point
                 continue;
             } else {
-                // first mismatch; insert ../ for rest of g, then rest of p
+                // first mismatch; insert ../ for rest of groupFullName, then rest of itemFullName
                 StringBuilder b = new StringBuilder();
-                for (int j = i; j < gA.length; j++) {
+                for (int j = i; j < groupFullNameA.length; j++) {
                     if (j > i) {
                         b.append('/');
                     }
                     b.append("..");
                 }
-                for (int j = i; j < pA.length; j++) {
-                    b.append('/').append(pA[j]);
+                for (int j = i; j < itemFullNameA.length; j++) {
+                    b.append('/').append(itemFullNameA[j]);
                 }
                 return b.toString();
             }
