b9426e4f17e551d6d68818b8a0adb88c6bb26a18
Daniel Beck <daniel-beck@github.com>
2014-01-15 00:36:06 +0100

    [FIXED JENKINS-21386] Allow more specific loggers to reduce log level

--- core/src/test/java/hudson/logging/LogRecorderTest.java
+++ core/src/test/java/hudson/logging/LogRecorderTest.java
@@ -42,10 +42,72 @@ public class LogRecorderTest {
         assertTrue(includes("", "hudson.model.Hudson"));
     }
 
-    private static boolean includes(String target, String logger) {
-        LogRecord r = new LogRecord(Level.INFO, "whatever");
+    @Test public void targetMatches() {
+        assertTrue(matches("hudson", "hudson"));
+        assertFalse(matches("hudson", "hudson", Level.FINE));
+        assertNull(matches("hudson", "hudsone"));
+        assertNull(matches("hudson", "hudso"));
+        assertTrue(matches("hudson", "hudson.model.Hudson"));
+        assertFalse(matches("hudson", "hudson.model.Hudson", Level.FINE));
+        assertNull(matches("hudson", "jenkins.model.Jenkins"));
+        assertTrue(matches("", "hudson.model.Hudson"));
+        assertFalse(matches("", "hudson.model.Hudson", Level.FINE));
+    }
+
+    @Test public void testSpecificExclusion() {
+        LogRecorder lr = new LogRecorder("foo");
+
+        LogRecorder.Target targetLevel0 = new LogRecorder.Target("", Level.FINE);
+        LogRecorder.Target targetLevel1 = new LogRecorder.Target("foo", Level.INFO);
+        LogRecorder.Target targetLevel2 = new LogRecorder.Target("foo.bar", Level.SEVERE);
+
+        lr.targets.add(targetLevel1);
+        lr.targets.add(targetLevel2);
+        lr.targets.add(targetLevel0);
+
+        assertEquals(lr.orderedTargets()[0], targetLevel2);
+        assertEquals(lr.orderedTargets()[1], targetLevel1);
+        assertEquals(lr.orderedTargets()[2], targetLevel0);
+
+        LogRecord r1 = createLogRecord("baz", Level.INFO, "visible");
+        LogRecord r2 = createLogRecord("foo", Level.FINE, "hidden");
+        LogRecord r3 = createLogRecord("foo.bar", Level.INFO, "hidden");
+        LogRecord r4 = createLogRecord("foo.bar.baz", Level.INFO, "hidden");
+        LogRecord r5 = createLogRecord("foo.bar.baz", Level.SEVERE, "visible");
+        LogRecord r6 = createLogRecord("foo", Level.INFO, "visible");
+        lr.handler.publish(r1);
+        lr.handler.publish(r2);
+        lr.handler.publish(r3);
+        lr.handler.publish(r4);
+        lr.handler.publish(r5);
+        lr.handler.publish(r6);
+
+        assertTrue(lr.handler.getView().contains(r1));
+        assertFalse(lr.handler.getView().contains(r2));
+        assertFalse(lr.handler.getView().contains(r3));
+        assertFalse(lr.handler.getView().contains(r4));
+        assertTrue(lr.handler.getView().contains(r5));
+        assertTrue(lr.handler.getView().contains(r6));
+    }
+
+    private static LogRecord createLogRecord(String logger, Level level, String message) {
+        LogRecord r = new LogRecord(level, message);
         r.setLoggerName(logger);
+        return r;
+    }
+
+    private static boolean includes(String target, String logger) {
+        LogRecord r = createLogRecord(logger, Level.INFO, "whatever");
         return new LogRecorder.Target(target, Level.INFO).includes(r);
+    }
+
+    private static Boolean matches(String target, String logger) {
+        return matches(target, logger, Level.INFO);
+    }
+
+    private static Boolean matches(String target, String logger, Level loggerLevel) {
+        LogRecord r = createLogRecord(logger, loggerLevel, "whatever");
+        return new LogRecorder.Target(target, Level.INFO).matches(r);
     }
 
 }
