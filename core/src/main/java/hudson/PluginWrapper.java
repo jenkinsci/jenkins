48cb1304c5c4138c97d6817f68c30a50e447ee35
Ulli Hafner <ullrich.hafner@gmail.com>
2014-02-04 15:25:31 +0100

    Removed "Jenkins" prefix from plugin display names.
    Since sorting of plugins in the update manager is based on displayName
    this name should not have a common prefix otherwise it's hard to find
    a given plugin name.

--- core/src/main/java/hudson/PluginWrapper.java
+++ core/src/main/java/hudson/PluginWrapper.java
@@ -45,6 +45,7 @@ import java.util.jar.Manifest;
 import java.util.logging.Logger;
 import static java.util.logging.Level.WARNING;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.LogFactory;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.HttpResponses;
@@ -213,7 +214,7 @@ public class PluginWrapper implements Comparable<PluginWrapper>, ModelObject {
     }
 
     public String getDisplayName() {
-        return getLongName();
+        return StringUtils.removeStart(getLongName(), "Jenkins ");
     }
 
     public Api getApi() {
