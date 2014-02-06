f1ac6f381d5c6046514b0b2827f9bc5b52c2674f
Michael Strasser <michael@michaelstrasser.com>
2014-02-06 21:58:29 +1100

    safe() also replaces '%' with '_'

--- core/src/test/java/hudson/tasks/test/TestObjectTest.java
+++ core/src/test/java/hudson/tasks/test/TestObjectTest.java
@@ -11,7 +11,7 @@ public class TestObjectTest {
 
     @Test
     public void testSafe() {
-        String name = "Foo#approve! is called by approve_on_foo?xyz/\\:";
+        String name = "Foo#approve! is called by approve_on_foo?xyz/\\: 50%";
         String encoded = TestObject.safe(name);
         
         Assert.assertFalse(encoded.contains("#"));
@@ -19,6 +19,7 @@ public class TestObjectTest {
         Assert.assertFalse(encoded.contains("\\"));
         Assert.assertFalse(encoded.contains("/"));
         Assert.assertFalse(encoded.contains(":"));
+        Assert.assertFalse(encoded.contains("%"));
     }
 
     @Test public void uniquifyName() {
