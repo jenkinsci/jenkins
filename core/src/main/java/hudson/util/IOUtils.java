e88de958ca6647b8bd77c3804f603cd1145af736
christ66 <schristou88@gmail.com>
2014-02-24 00:38:46 -0800

    Add alternative field calls for deprecated methods.

--- core/src/main/java/hudson/util/IOUtils.java
+++ core/src/main/java/hudson/util/IOUtils.java
@@ -145,12 +145,37 @@ public class IOUtils {
         }
     }
 
-    @Deprecated public static final char DIR_SEPARATOR_UNIX       = org.apache.commons.io.IOUtils.DIR_SEPARATOR_UNIX;
-    @Deprecated public static final char DIR_SEPARATOR_WINDOWS    = org.apache.commons.io.IOUtils.DIR_SEPARATOR_WINDOWS;
-    @Deprecated public static final char DIR_SEPARATOR            = org.apache.commons.io.IOUtils.DIR_SEPARATOR;
-    @Deprecated public static final String LINE_SEPARATOR_UNIX    = org.apache.commons.io.IOUtils.LINE_SEPARATOR_UNIX;
-    @Deprecated public static final String LINE_SEPARATOR_WINDOWS = org.apache.commons.io.IOUtils.LINE_SEPARATOR_WINDOWS;
-    @Deprecated public static final String LINE_SEPARATOR;
+
+    /**
+     * @deprecated Use instead {@link org.apache.commons.io.IOUtils#DIR_SEPARATOR_UNIX}
+     */
+    public static final char DIR_SEPARATOR_UNIX       = org.apache.commons.io.IOUtils.DIR_SEPARATOR_UNIX;
+
+    /**
+     * @deprecated Use instead {@link org.apache.commons.io.IOUtils#DIR_SEPARATOR_WINDOWS}
+     */
+    public static final char DIR_SEPARATOR_WINDOWS    = org.apache.commons.io.IOUtils.DIR_SEPARATOR_WINDOWS;
+
+    /**
+     * @deprecated Use instead {@link org.apache.commons.io.IOUtils#DIR_SEPARATOR}
+     */
+    public static final char DIR_SEPARATOR            = org.apache.commons.io.IOUtils.DIR_SEPARATOR;
+
+    /**
+     * @deprecated Use instead {@link org.apache.commons.io.IOUtils#LINE_SEPARATOR_UNIX}
+     */
+    public static final String LINE_SEPARATOR_UNIX    = org.apache.commons.io.IOUtils.LINE_SEPARATOR_UNIX;
+
+    /**
+     * @deprecated Use instead {@link org.apache.commons.io.IOUtils#LINE_SEPARATOR_WINDOWS}
+     */
+    public static final String LINE_SEPARATOR_WINDOWS = org.apache.commons.io.IOUtils.LINE_SEPARATOR_WINDOWS;
+
+    /**
+     * @deprecated Use instead {@link org.apache.commons.io.IOUtils#LINE_SEPARATOR}
+     */
+    public static final String LINE_SEPARATOR;
+
     static {
         // avoid security issues
         StringWriter buf = new StringWriter(4);
