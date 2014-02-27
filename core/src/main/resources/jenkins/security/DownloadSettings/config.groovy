7d6e02e8d965ad66c03d4023d95febe10be61dbd
Jesse Glick <jglick@cloudbees.com>
2014-02-27 17:10:01 -0500

    [JENKINS-19081] @kohsuke prefers that the option to disable signature checks not even be presented unless -Dhudson.model.DownloadService.noSignatureCheck is used. (cherry picked from commit 54c85007f355dadfbf8b0fc5924ded55f1614280)

--- core/src/main/resources/jenkins/security/DownloadSettings/config.groovy
+++ core/src/main/resources/jenkins/security/DownloadSettings/config.groovy
@@ -6,7 +6,9 @@ f.section(title: _("Download Preferences")) {
     f.entry(title: _("Use Browser"), field: "useBrowser") {
         f.checkbox()
     }
-    f.entry(title: _("Check Signatures"), field: "checkSignature") {
-        f.checkbox()
+    if (!instance.checkSignature || !hudson.model.DownloadService.signatureCheck) { // do not display this option by default
+        f.entry(title: _("Check Signatures"), field: "checkSignature") {
+            f.checkbox()
+        }
     }
 }
