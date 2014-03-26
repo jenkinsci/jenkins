jenkinsci/jenkins

commit 8e6a3f7f03c8cdd93f1fb352088bac9cd2676c49

Converted the build queue and executors into Widgets.

core/src/main/resources/jenkins/widgets/BuildQueueWidget/index.groovy
@@ -1,0 +1,7 @@
+package jenkins.widgets.BuildQueueWidget;
+
+def t = namespace(lib.JenkinsTagLib.class)
+
+text(request.ancestors.last().object)
+
+t.queue(items:view.approximateQueueItemsQuickly)

core/src/main/resources/jenkins/widgets/ExecutorsWidget/index.groovy
@@ -1,0 +1,5 @@
+package jenkins.widgets.ExecutorsWidget
+
+def t = namespace(lib.JenkinsTagLib.class)
+
+t.executors(computers:view.computers)

core/src/main/java/hudson/widgets/Widget.java
@@ -24,6 +24,7 @@
 package hudson.widgets;
 
 import hudson.ExtensionPoint;
+import hudson.model.View;
 
 /**
  * Box to be rendered in the side panel.
@@ -32,6 +33,8 @@ import hudson.ExtensionPoint;
  * <ul>
  * <li><b>index.jelly</b> should display the widget. It should have:
  *   &lt;l:pane width="2" title="..."> ...body... &lt;/l:pane> structure.
+ *   In this view, "it" points to the {@link Widget} and "view" points to {@link View}
+ *   that's rendering the widget.
  *
  * @author Kohsuke Kawaguchi
  * @since 1.146

core/src/main/java/jenkins/widgets/BuildQueueWidget.java
@@ -1,0 +1,18 @@
+package jenkins.widgets;
+
+import hudson.Extension;
+import hudson.widgets.Widget;
+import jenkins.model.Jenkins;
+
+/**
+ * Show the default build queue.
+ *
+ * A plugin may remove this from {@link Jenkins#getWidgets()} and swap in their own.
+ *
+ * @author Kohsuke Kawaguchi
+ * @sine 1.514
+ */
+@Extension(ordinal=200) // historically this was the top most widget
+public class BuildQueueWidget extends Widget {
+
+}

core/src/main/java/jenkins/widgets/ExecutorsWidget.java
@@ -1,0 +1,17 @@
+package jenkins.widgets;
+
+import hudson.Extension;
+import hudson.widgets.Widget;
+import jenkins.model.Jenkins;
+
+/**
+ * The default executors widget.
+ *
+ * A plugin may remove this from {@link Jenkins#getWidgets()} and swap in their own.
+ *
+ * @author Kohsuke Kawaguchi
+ * @since 1.514
+ */
+@Extension(ordinal=100) // historically this was above normal widgets and below BuildQueueWidget
+public class ExecutorsWidget extends Widget {
+}

core/src/main/resources/hudson/model/View/sidepanel.jelly
@@ -70,9 +70,8 @@ THE SOFTWARE.
       <st:include page="tasks-bottom.jelly" it="${it.owner}" optional="true" />
       <t:actions />
     </l:tasks>
-    <t:queue items="${it.approximateQueueItemsQuickly}" />
-    <t:executors computers="${it.computers}" />
     <j:forEach var="w" items="${it.widgets}">
+      <j:set var="view" value="${it}" /><!-- expose the view that's rendering this sidepanel to the widget -->
       <st:include it="${w}" page="index.jelly" />
     </j:forEach>
   </l:side-panel>

core/src/main/resources/jenkins/model/Jenkins/sidepanel.jelly
@@ -22,5 +22,5 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 -->
 
-<?jelly escape-by-default='true'?>
+Li<?jelly escape-by-default='true'?>
 <st:include it="${it.primaryView}" page="sidepanel.jelly" xmlns:st="jelly:stapler" />

commit 20ea8d1092f5642cc889d80f7c2f7d3334e3739e

[FIXED JENKINS-20500] Pass ${it} as an alias for ${view} to t:queue and t:executors, correcting regression introduced in 8e6a3f7.

core/src/main/resources/jenkins/widgets/BuildQueueWidget/index.groovy
@@ -2,4 +2,4 @@ package jenkins.widgets.BuildQueueWidget;
 
 def t = namespace(lib.JenkinsTagLib.class)
 
-t.queue(items:view.approximateQueueItemsQuickly)
+t.queue(items:view.approximateQueueItemsQuickly, it:view)

core/src/main/resources/jenkins/widgets/ExecutorsWidget/index.groovy
@@ -2,4 +2,4 @@ package jenkins.widgets.ExecutorsWidget
 
 def t = namespace(lib.JenkinsTagLib.class)
 
-t.executors(computers:view.computers)
+t.executors(computers:view.computers, it:view)