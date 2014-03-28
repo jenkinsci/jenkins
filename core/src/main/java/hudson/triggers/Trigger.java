ce1df7d3ce732e12e675461debac8a5b2be30dee
Jesse Glick <jglick@cloudbees.com>
2014-03-28 11:34:10 -0400

    Improved logging.

--- core/src/main/java/hudson/triggers/Trigger.java
+++ core/src/main/java/hudson/triggers/Trigger.java
@@ -200,7 +200,7 @@ public abstract class Trigger<J extends Item> implements Describable<Trigger<?>>
 
         public void doRun() {
             while(new Date().getTime()-cal.getTimeInMillis()>1000) {
-                LOGGER.fine("cron checking "+cal.getTime().toLocaleString());
+                LOGGER.log(Level.FINE, "cron checking {0}", cal.getTime());
 
                 try {
                     checkTriggers(cal);
@@ -250,17 +250,19 @@ public abstract class Trigger<J extends Item> implements Describable<Trigger<?>>
         for (ParameterizedJobMixIn.ParameterizedJob p : inst.getAllItems(ParameterizedJobMixIn.ParameterizedJob.class)) {
             for (Trigger t : p.getTriggers().values()) {
                 if (! (t instanceof SCMTrigger && scmd.synchronousPolling)) {
-                    LOGGER.fine("cron checking "+p.getName());
+                    LOGGER.log(Level.FINE, "cron checking {0} with spec ‘{1}’", new Object[] {p, t.spec.trim()});
 
                     if (t.tabs.check(cal)) {
-                        LOGGER.config("cron triggered "+p.getName());
+                        LOGGER.log(Level.CONFIG, "cron triggered {0}", p);
                         try {
                             t.run();
                         } catch (Throwable e) {
                             // t.run() is a plugin, and some of them throw RuntimeException and other things.
                             // don't let that cancel the polling activity. report and move on.
-                            LOGGER.log(Level.WARNING, t.getClass().getName()+".run() failed for "+p.getName(),e);
+                            LOGGER.log(Level.WARNING, t.getClass().getName() + ".run() failed for " + p, e);
                         }
+                    } else {
+                        LOGGER.log(Level.FINER, "did not trigger {0}", p);
                     }
                 }
             }
