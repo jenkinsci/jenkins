c1f234f9ad2ffdb989e713e6e1bad0c6f733875f
Olivier Lamy <olamy@apache.org>
2014-04-04 09:12:15 +1100

    add logging if file does not exist

--- core/src/main/java/hudson/model/ItemGroupMixIn.java
+++ core/src/main/java/hudson/model/ItemGroupMixIn.java
@@ -24,6 +24,7 @@
 package hudson.model;
 
 import hudson.Util;
+import hudson.XmlFile;
 import hudson.model.listeners.ItemListener;
 import hudson.remoting.Callable;
 import hudson.security.AccessControlled;
@@ -102,8 +103,11 @@ public abstract class ItemGroupMixIn {
                 // Try to retain the identity of an existing child object if we can.
                 V item = (V) parent.getItem(subdir.getName());
                 if (item == null) {
-                    if (Items.getConfigFile( subdir ).exists()) {
+                    XmlFile xmlFile = Items.getConfigFile( subdir );
+                    if (xmlFile.exists()) {
                         item = (V) Items.load( parent, subdir );
+                    }else{
+                        Logger.getLogger( ItemGroupMixIn.class.getName() ).log( Level.WARNING, "could not find file " + xmlFile.getFile());
                     }
                 } else {
                     item.onLoad(parent, subdir.getName());
