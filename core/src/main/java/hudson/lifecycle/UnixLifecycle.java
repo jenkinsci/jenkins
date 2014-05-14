jenkinsci/jenkins

commit 547d4ed8d35de6be4b2788e4816019bb8cc94902

Fix jenkins restart on Linux after a JRE system update
Daemon.getCurrentExecutable() use /proc/PID/exe to retrieve the used JRE. This link is broken when the target changes (ie after a JRE system update) because the kernel appends ' (deleted)' to the symlink. This patch uses the first argument of the command line (in /proc/PID/cmdline) instead.

core/src/main/java/hudson/lifecycle/UnixLifecycle.java
@@ -79,7 +79,7 @@ public class UnixLifecycle extends Lifecycle {
         }
 
         // exec to self
-        String exe = Daemon.getCurrentExecutable();
+        String exe = args.get(0);
         LIBC.execv(exe, new StringArray(args.toArray(new String[args.size()])));
         throw new IOException("Failed to exec '"+exe+"' "+LIBC.strerror(Native.getLastError()));
     }

commit 2b4da167cd0ceb7f5ebd6b5eaa846fc5d8f08a24

[FIX JENKINS-22818] fix jenkins restart on Linux after plugin update if the java executable running Jenkins is started without its full path
The patch uses 'execvp' instead of 'execv' to lookup the java executable filename in the PATH if the specified filename does not contain a slash (/) character.
Regression introduced in 547d4ed8d35de6be4b2788e4816019bb8cc94902

core/src/main/java/hudson/lifecycle/UnixLifecycle.java
@@ -80,7 +80,7 @@ public class UnixLifecycle extends Lifecycle {
 
         // exec to self
         String exe = args.get(0);
-        LIBC.execv(exe, new StringArray(args.toArray(new String[args.size()])));
+        LIBC.execvp(exe, new StringArray(args.toArray(new String[args.size()])));
         throw new IOException("Failed to exec '"+exe+"' "+LIBC.strerror(Native.getLastError()));
     }
 

core/src/main/java/hudson/util/jna/GNUCLibrary.java
@@ -55,7 +55,8 @@ public interface GNUCLibrary extends Library {
     int chdir(String dir);
     int getdtablesize();
 
-    int execv(String file, StringArray args);
+    int execv(String path, StringArray args);
+    int execvp(String file, StringArray args);
     int setenv(String name, String value,int replace);
     int unsetenv(String name);
     void perror(String msg);