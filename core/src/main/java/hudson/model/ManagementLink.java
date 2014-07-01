92e45e0af19804040c0130b06ebf8401144a72f5
Bruno Kühnen Meneguello <bruno@meneguello.com>
2014-07-01 18:55:58 -0300

    Convert the management link to POST and allow other management links to require POST

--- core/src/main/java/hudson/model/ManagementLink.java
+++ core/src/main/java/hudson/model/ManagementLink.java
@@ -112,4 +112,14 @@ public abstract class ManagementLink implements ExtensionPoint, Action {
     public Permission getRequiredPermission() {
         return null;
     }
+
+    /**
+     * Define if the rendered link will use the default GET method or POST.
+     * @return true if POST must be used
+     * @see RequirePOST
+     * @since 1.574
+     */
+    public boolean getRequiresPOST() {
+        return false;
+    }
 }
