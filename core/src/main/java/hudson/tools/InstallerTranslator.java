17d90931655e6c67651ec371344552d7c23bdcda
Jesse Glick <jglick@cloudbees.com>
2014-07-15 13:36:10 -0400

    [FIXED JENKINS-17667] Fixed race condition when running tool installers on many slaves at once. Correcting change made in #1176, which introduced an NPE, to restore original logic merely wrapped in a synchronized block. Reproduced NPE in new functional test (original bug probably very hard to reproduce).

--- core/src/main/java/hudson/tools/InstallerTranslator.java
+++ core/src/main/java/hudson/tools/InstallerTranslator.java
@@ -53,19 +53,17 @@ public class InstallerTranslator extends ToolLocationTranslator {
 
         for (ToolInstaller installer : isp.installers) {
             if (installer.appliesTo(node)) {
-            	Map<ToolInstallation, Semaphore> mutexByTool;
-            	Semaphore semaphore;
-            	
-                synchronized(mutexByNode) {
-                    mutexByTool = mutexByNode.get(node);
+                Semaphore semaphore;
+                synchronized (mutexByNode) {
+                    Map<ToolInstallation, Semaphore> mutexByTool = mutexByNode.get(node);
                     if (mutexByTool == null) {
                         mutexByNode.put(node, mutexByTool = new WeakHashMap<ToolInstallation, Semaphore>());
+                    }
+                    semaphore = mutexByTool.get(tool);
+                    if (semaphore == null) {
                         mutexByTool.put(tool, semaphore = new Semaphore(1));
-                    } else {
-                    	semaphore = mutexByTool.get(tool);
                     }
                 }
-
                 semaphore.acquire();
                 try {
                     return installer.performInstallation(tool, node, log).getRemote();
