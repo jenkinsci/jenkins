7cbfc2486e43ddbf18f41828553e6d25812c89ca
Oliver Gondža <ogondza@redhat.com>
2014-07-28 11:04:34 +0200

    Getting back to existing CLIMethod command as I failed to register CLIMethod from jenkins-test-harness

--- test/src/test/java/hudson/cli/CLIRegistererTest.java
+++ test/src/test/java/hudson/cli/CLIRegistererTest.java
@@ -2,17 +2,16 @@ package hudson.cli;
 
 import static hudson.cli.CLICommandInvoker.Matcher.failedWith;
 import static hudson.cli.CLICommandInvoker.Matcher.succeededSilently;
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import jenkins.model.Jenkins;
+import hudson.cli.CLICommandInvoker;
 import hudson.cli.CLICommandInvoker.Result;
-import hudson.cli.declarative.CLIMethod;
-import hudson.model.User;
 
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.kohsuke.args4j.Option;
 
 public class CLIRegistererTest {
 
@@ -23,27 +22,15 @@ public class CLIRegistererTest {
     public void testAuthWithSecurityRealmCLIAuthenticator() {
         j.getInstance().setSecurityRealm(j.createDummySecurityRealm());
 
-        CLICommandInvoker command = new CLICommandInvoker(j, "command-for-test");
+        CLICommandInvoker command = new CLICommandInvoker(j, "quiet-down");
 
-        Result invocation = command.invokeWithArgs("--username", "foo", "--password", "foo", "--expected", "foo");
-        assertThat(invocation, succeededSilently());
-
-        invocation = command.invokeWithArgs("--expected", "anonymous");
-        assertThat(invocation, failedWith(1));
-        assertThat(invocation.stderr(), containsString("Not authenticated"));
-
-        invocation = command.invokeWithArgs("--username", "foo", "--password", "invalid", "--expected", "anonymous");
+        Result invocation = command.invokeWithArgs("--username", "foo", "--password", "invalid");
         assertThat(invocation, failedWith(1));
         assertThat(invocation.stderr(), containsString("BadCredentialsException: foo"));
-    }
+        Assert.assertFalse("Unauthorized command was executed", Jenkins.getInstance().isQuietingDown());
 
-    @CLIMethod(name = "command-for-test")
-    public static int commandForTest(@Option(name = "--expected") String expected) {
-        final User user = User.current();
-        if (user == null) throw new RuntimeException("Not authenticated");
-
-        Assert.assertEquals(expected, user.getId());
-
-        return 0;
+        invocation = command.invokeWithArgs("--username", "foo", "--password", "foo");
+        assertThat(invocation, succeededSilently());
+        Assert.assertTrue("Authorized command was not executed", Jenkins.getInstance().isQuietingDown());
     }
 }
