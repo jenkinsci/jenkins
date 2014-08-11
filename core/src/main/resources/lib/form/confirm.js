jenkinsci/jenkins

commit a6b7f4b0763af6d6e3ba306a1a14c6245e609d8e

if applied, the data is saved so flag should be reset.
Technically we should doing so only after the server reported success, but unfortunately there's no good hook for that.

core/src/main/resources/lib/form/confirm.js
@@ -1,5 +1,5 @@
 (function() {
-    var errorMessage = "Leave without saving?";
+    var errorMessage = "You have modified configuration";
     var needToConfirm = false;
 
     function confirm() {
@@ -19,11 +19,10 @@
       var name;
       for ( var i = 0; i < buttons.length; i++) {
         name = buttons[i].parentNode.parentNode.getAttribute('name');
-        if (name == "Submit") {
+        if (name == "Submit" || name == "Apply") {
           $(buttons[i]).on('click', function() {
             needToConfirm = false;
           });
-        } else if (name == "Apply") {
         } else {
           $(buttons[i]).on('click', confirm);
         }
@@ -52,6 +51,6 @@
     }
 
     window.onbeforeunload = confirmExit;
-    $(window).on('load', initConfirm);
+    Event.on(window,'load', initConfirm);
 
 })();

commit b24741bc751726aaac43bf92628cd22196a8ed74

[FIXED JENKINS-17825] Configuration page crashes in IE from regression in a6b7f4b.

core/src/main/resources/lib/form/confirm.js
@@ -82,6 +82,6 @@
     }
 
     window.onbeforeunload = confirmExit;
-    Event.on(window,'load', initConfirm);
+    $(window).on('load', initConfirm);
 
 })();