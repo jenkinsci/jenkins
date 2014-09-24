91c516200f8d7df05c6f6c25b9504fb843a81a02
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-09-23 19:03:05 -0700

    Reimplemented the checker as RoleChecker

--- core/src/main/java/jenkins/security/CallableDirectionChecker.java
+++ core/src/main/java/jenkins/security/CallableDirectionChecker.java
@@ -2,35 +2,59 @@ package jenkins.security;
 
 import hudson.Extension;
 import hudson.remoting.Callable;
-import hudson.remoting.CallableDecorator;
 import hudson.remoting.ChannelBuilder;
 import hudson.slaves.ComputerListener;
 import hudson.slaves.SlaveComputer;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.jenkinsci.remoting.Role;
+import org.jenkinsci.remoting.RoleChecker;
+import org.jenkinsci.remoting.RoleSensitive;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.DoNotUse;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 
+import javax.annotation.Nonnull;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Rejects non-{@link SlaveToMaster} callables.
+ * Inspects {@link Callable}s that run on the master.
  *
  * @author Kohsuke Kawaguchi
  * @since 1.THU
  */
 @Restricted(NoExternalUse.class) // used implicitly via listener
-public class CallableDirectionChecker extends CallableDecorator {
+public class CallableDirectionChecker extends RoleChecker {
 
     private static final String BYPASS_PROP = CallableDirectionChecker.class.getName()+".allow";
+
+    /**
+     * Switch to disable all the defense mechanism completely.
+     *
+     * This is an escape hatch in case the fix breaks something critical, to allow the user
+     * to keep operation.
+     */
     public static boolean BYPASS = Boolean.getBoolean(BYPASS_PROP);
-    private static final PrintWriter BYPASS_LOG;
+
+    /**
+     * Whitelist of {@link RoleSensitive} class names (mostly {@link Callable} classes) that are allowed
+     * to pass through.
+     */
+    public static ConcurrentMap<String,Void> BYPASS_CALLABLES = new ConcurrentHashMap<String,Void>();
+
+    /**
+     * If non-null, this object will receive the names of the classes that are dubious.
+     * Useful in conjunction with {@link #BYPASS}
+     */
+    public static PrintWriter BYPASS_LOG;
     static {
-        String log = System.getProperty("jenkins.security.CallableDirectionChecker.log");
+        String log = System.getProperty(CallableDirectionChecker.class.getName()+".log");
         if (log == null) {
             BYPASS_LOG = null;
         } else {
@@ -45,39 +69,44 @@ public class CallableDirectionChecker extends CallableDecorator {
     private CallableDirectionChecker() {}
 
     @Override
-    public <V, T extends Throwable> Callable<V, T> userRequest(Callable<V, T> op, Callable<V, T> stem) {
-        Class<?> c = op.getClass();
-        String name = c.getName();
-
-        if (name.startsWith("hudson.remoting")) { // TODO probably insecure
-            LOGGER.log(Level.FINE, "Sending {0} to master is allowed since it is in remoting", name);
-            return stem;    // lower level services provided by remoting, such IOSyncer, RPCRequest, Ping, etc. that we allow
+    public void check(RoleSensitive subject, @Nonnull Collection<Role> roles) throws SecurityException {
+        if (Roles.FOR_MASTER.containsAll(roles)) {
+            LOGGER.log(Level.FINE, "Executing {0} is allowed since it is targeted for the master role");
+            return;    // known to be safe
         }
 
-        if (c.isAnnotationPresent(SlaveToMaster.class)) {
-            LOGGER.log(Level.FINE, "Sending {0} is allowed since it is marked @SlaveToMaster", name);
-            return stem;    // known to be safe
+        final String name = subject.getClass().getName();
+
+        if (isWhitelisted(name)) {
+            // this subject is dubious, but we are letting it through as per whitelisting
+            LOGGER.log(Level.FINE, "Explicitly allowing {0} to be sent from slave to master", name);
+
+            // record those that we are letting through
+            if (BYPASS_LOG != null)
+                BYPASS_LOG.println(name);
+
+            return;
         }
 
-        if (BYPASS_LOG != null) {
-            BYPASS_LOG.println(name);
-            return stem;
+        throw new SecurityException("Sending " + name + " from slave to master is prohibited");
+    }
+
+    /**
+     * Is this subject class name whitelisted?
+     */
+    private boolean isWhitelisted(String name) {
+        if (BYPASS) // everything is whitelisted
+            return true;
+
+        if (BYPASS_CALLABLES.containsKey(name))
+            return true;
+
+        if (Boolean.getBoolean(BYPASS_PROP+"."+name)) {
+            BYPASS_CALLABLES.put(name,null);
+            return true;
         }
 
-        if (c.isAnnotationPresent(MasterToSlave.class)) {
-            throw new SecurityException("Sending " + name + " from slave to master is prohibited");
-        } else {
-            // No annotation provided, so we do not know whether it is safe or not.
-            if (BYPASS) {
-                LOGGER.log(Level.FINE, "Allowing {0} to be sent from slave to master", name);
-                return stem;
-            } else if (Boolean.getBoolean(BYPASS_PROP + "." + name)) {
-                LOGGER.log(Level.FINE, "Explicitly allowing {0} to be sent from slave to master", name);
-                return stem;
-            } else {
-                throw new SecurityException("Sending from slave to master is prohibited unless you run with: -D" + BYPASS_PROP + "." + name);
-            }
-        }
+        return false;
     }
 
     /**
@@ -88,7 +117,7 @@ public class CallableDirectionChecker extends CallableDecorator {
     public static class ComputerListenerImpl extends ComputerListener {
         @Override
         public void onChannelBuilding(ChannelBuilder builder, SlaveComputer sc) {
-            builder.with(new CallableDirectionChecker());
+            builder.withRoleChecker(new CallableDirectionChecker());
         }
     }
 
