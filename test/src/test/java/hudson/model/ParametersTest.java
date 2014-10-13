30715dfd46d8b9987635c6a39cdd21c55d0bcc87
Oliver Gondža <ogondza@gmail.com>
2014-10-13 22:02:57 +0200

    [JENKINS-11543] Unit test
    (cherry picked from commit 75f5d2180f7340ce2efd4f41c8670afb049eb530)

--- test/src/test/java/hudson/model/ParametersTest.java
+++ test/src/test/java/hudson/model/ParametersTest.java
@@ -1,13 +1,20 @@
 package hudson.model;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.CaptureEnvironmentBuilder;
+
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
 import com.gargoylesoftware.htmlunit.html.HtmlOption;
+
 import java.util.Set;
 
 /**
@@ -156,5 +163,24 @@ public class ParametersTest extends HudsonTestCase {
         assertFalse(sensitiveVars.contains("string"));
         assertTrue(sensitiveVars.contains("password"));
         assertFalse(sensitiveVars.contains("string2"));
+    }
+
+    @Bug(11543)
+    public void testUnicodeParametersArePresetedCorrectly() throws Exception {
+        final FreeStyleProject p = createFreeStyleProject();
+        ParametersDefinitionProperty pdb = new ParametersDefinitionProperty(
+                new StringParameterDefinition("sname:a¶‱ﻷ", "svalue:a¶‱ﻷ", "sdesc:a¶‱ﻷ"),
+                new FileParameterDefinition("fname:a¶‱ﻷ", "fdesc:a¶‱ﻷ")
+        );
+        p.addProperty(pdb);
+
+        WebClient wc = createWebClient();
+        wc.setThrowExceptionOnFailingStatusCode(false); // Ignore 405
+        HtmlPage page = wc.getPage(p, "build");
+
+        // java.lang.IllegalArgumentException: No such parameter definition: <gibberish>.
+        wc.setThrowExceptionOnFailingStatusCode(true);
+        final HtmlForm form = page.getFormByName("parameters");
+        form.submit(form.getButtonByCaption("Build"));
     }
 }
