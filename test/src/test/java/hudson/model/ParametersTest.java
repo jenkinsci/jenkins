190f0b6faf8aa49a60f1d2177a0b51d7a5170c42
Daniel Beck <daniel-beck@github.com>
2014-10-14 22:19:07 +0200

    [FIXED JENKINS-3539] Prevent empty file creation if left empty

--- test/src/test/java/hudson/model/ParametersTest.java
+++ test/src/test/java/hudson/model/ParametersTest.java
@@ -8,6 +8,8 @@ import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
 import com.gargoylesoftware.htmlunit.html.HtmlOption;
+import org.jvnet.hudson.test.Issue;
+
 import java.util.Set;
 
 /**
@@ -157,4 +159,25 @@ public class ParametersTest extends HudsonTestCase {
         assertTrue(sensitiveVars.contains("password"));
         assertFalse(sensitiveVars.contains("string2"));
     }
+
+    @Issue("JENKINS-3539")
+    public void testFileParameterNotSet() throws Exception {
+        FreeStyleProject project = createFreeStyleProject();
+        ParametersDefinitionProperty pdp = new ParametersDefinitionProperty(
+                new FileParameterDefinition("filename", "description"));
+        project.addProperty(pdp);
+
+        WebClient wc = new WebClient();
+        wc.setThrowExceptionOnFailingStatusCode(false);
+        HtmlPage page = wc.goTo("/job/" + project.getName() + "/build?delay=0sec");
+        HtmlForm form = page.getFormByName("parameters");
+
+        submit(form);
+        Queue.Item q = jenkins.getQueue().getItem(project);
+        if (q != null) q.getFuture().get();
+        else Thread.sleep(1000);
+
+        assertFalse("file must not exist", project.getSomeWorkspace().child("filename").exists());
+    }
+
 }
