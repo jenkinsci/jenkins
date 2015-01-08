ee309ef92096610259b0ec01e82940ba022bed1a
christ66 <schristou88@gmail.com>
2015-01-08 11:01:12 -0800

    [JENKINS-22811] Fix log message to report more verbose message.
    (cherry picked from commit da1c74f619feea134aa887b1e5b1774307ea0e47)

--- core/src/main/java/hudson/model/AbstractProject.java
+++ core/src/main/java/hudson/model/AbstractProject.java
@@ -755,7 +755,7 @@ public abstract class AbstractProject<P extends AbstractProject<P,R>,R extends A
             try {
                 ta.addAll(Util.fixNull(tpaf.createFor(this))); // be defensive against null
             } catch (Exception e) {
-                LOGGER.log(Level.SEVERE, "Error loading transient project action factory.");
+                LOGGER.log(Level.SEVERE, "Could not load actions from " + tpaf + " for " + this, e);
             }
         }
         return ta;
