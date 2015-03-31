2020bbd5137d5970add3e7ac2cebb5cc585e60fe
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-03-31 14:53:51 +0100

    Name should never be null, but just in case

--- core/src/main/java/jenkins/model/Nodes.java
+++ core/src/main/java/jenkins/model/Nodes.java
@@ -213,7 +213,7 @@ public class Nodes implements Saveable {
      */
     @CheckForNull
     public Node getNode(String name) {
-        return nodes.get(name);
+        return name == null ? null : nodes.get(name);
     }
 
     /**
