f5585971ce6daa1f737b3f85c7001c94b513cce9
Oliver Gondža <ogondza@gmail.com>
2015-04-03 14:13:54 +0200

    Refactor WorkspaceCleanupThreadTest

--- test/src/test/java/hudson/model/WorkspaceCleanupThreadTest.java
+++ test/src/test/java/hudson/model/WorkspaceCleanupThreadTest.java
@@ -28,14 +28,17 @@ import hudson.FilePath;
 import hudson.remoting.VirtualChannel;
 import hudson.slaves.DumbSlave;
 import hudson.util.StreamTaskListener;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import jenkins.MasterToSlaveFileCallable;
 import static org.junit.Assert.*;
+
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Rule;
@@ -58,49 +61,38 @@ public class WorkspaceCleanupThreadTest {
         handler.setLevel(Level.ALL);
         logger.addHandler(handler);
     }
-    
+
     @Test public void cleanUpSlaves() throws Exception {
-        DumbSlave s1 = r.createOnlineSlave();
         FreeStyleProject p = r.createFreeStyleProject();
-        p.setAssignedNode(s1);
-        FreeStyleBuild b1 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
-        assertEquals(s1, b1.getBuiltOn());
-        FilePath ws1 = b1.getWorkspace();
-        assertNotNull(ws1);
-        ws1.act(new Detouch());
-        DumbSlave s2 = r.createOnlineSlave();
-        p.setAssignedNode(s2);
-        FreeStyleBuild b2 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
-        assertEquals(s2, b2.getBuiltOn());
-        FilePath ws2 = b2.getWorkspace();
-        assertNotNull(ws2);
-        ws2.act(new Detouch());
+
+        FilePath ws1 = createOldWorkspaceOn(r.createOnlineSlave(), p);
+
+        FilePath ws2 = createOldWorkspaceOn(r.createOnlineSlave(), p);
+
         p.setAssignedNode(r.jenkins);
         FreeStyleBuild b3 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertEquals(r.jenkins, b3.getBuiltOn());
         assertEquals(r.jenkins, p.getLastBuiltOn());
-        new WorkspaceCleanupThread().execute(StreamTaskListener.fromStdout());
+
+        performCleanup();
+
         assertFalse(ws1.exists());
         assertFalse(ws2.exists());
+        assertTrue(b3.getWorkspace().exists());
     }
 
     @Issue("JENKINS-21023")
     @Test public void modernMasterWorkspaceLocation() throws Exception {
         FreeStyleProject p = r.createFreeStyleProject();
-        FreeStyleBuild b1 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
-        assertEquals(r.jenkins, b1.getBuiltOn());
-        FilePath ws1 = b1.getWorkspace();
-        assertNotNull(ws1);
-        ws1.act(new Detouch());
+
+        FilePath ws1 = createOldWorkspaceOn(r.jenkins, p);
+
         DumbSlave s = r.createOnlineSlave();
-        p.setAssignedNode(s);
-        FreeStyleBuild b2 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
-        assertEquals(s, b2.getBuiltOn());
-        FilePath ws2 = b2.getWorkspace();
-        assertNotNull(ws2);
-        ws2.act(new Detouch());
+        FilePath ws2 = createOldWorkspaceOn(s, p);
         assertEquals(s, p.getLastBuiltOn());
-        new WorkspaceCleanupThread().execute(StreamTaskListener.fromStdout());
+
+        performCleanup();
+
         assertFalse(ws1.exists());
         assertTrue(ws2.exists());
     }
@@ -109,40 +101,39 @@ public class WorkspaceCleanupThreadTest {
     @Test public void jobInFolder() throws Exception {
         MockFolder d = r.createFolder("d");
         FreeStyleProject p1 = d.createProject(FreeStyleProject.class, "p");
-        FreeStyleBuild b1 = r.assertBuildStatusSuccess(p1.scheduleBuild2(0));
-        assertEquals(r.jenkins, b1.getBuiltOn());
-        FilePath ws1 = b1.getWorkspace();
-        assertNotNull(ws1);
-        ws1.act(new Detouch());
+        FilePath ws1 = createOldWorkspaceOn(r.jenkins, p1);
+
         DumbSlave s1 = r.createOnlineSlave();
-        p1.setAssignedNode(s1);
-        FreeStyleBuild b2 = r.assertBuildStatusSuccess(p1.scheduleBuild2(0));
-        assertEquals(s1, b2.getBuiltOn());
-        FilePath ws2 = b2.getWorkspace();
-        assertNotNull(ws2);
-        ws2.act(new Detouch());
+        FilePath ws2 = createOldWorkspaceOn(s1, p1);
         DumbSlave s2 = r.createOnlineSlave();
-        p1.setAssignedNode(s2);
-        FreeStyleBuild b3 = r.assertBuildStatusSuccess(p1.scheduleBuild2(0));
-        assertEquals(s2, b3.getBuiltOn());
-        FilePath ws3 = b3.getWorkspace();
-        assertNotNull(ws3);
-        ws3.act(new Detouch());
+        FilePath ws3 = createOldWorkspaceOn(s2, p1);
         assertEquals(s2, p1.getLastBuiltOn());
+
         FreeStyleProject p2 = d.createProject(FreeStyleProject.class, "p2");
-        p2.setAssignedNode(s1);
-        FreeStyleBuild b4 = r.assertBuildStatusSuccess(p2.scheduleBuild2(0));
-        assertEquals(s1, b4.getBuiltOn());
-        FilePath ws4 = b4.getWorkspace();
-        assertNotNull(ws4);
-        ws4.act(new Detouch());
+        FilePath ws4 = createOldWorkspaceOn(s1, p2);
         assertEquals(s1, p2.getLastBuiltOn());
         ws2.getParent().act(new Detouch()); // ${s1.rootPath}/workspace/d/
-        new WorkspaceCleanupThread().execute(StreamTaskListener.fromStdout());
+
+        performCleanup();
+
         assertFalse(ws1.exists());
         assertFalse(ws2.exists());
         assertTrue(ws3.exists());
         assertTrue(ws4.exists());
+    }
+
+    private FilePath createOldWorkspaceOn(Node slave, FreeStyleProject p) throws Exception {
+        p.setAssignedNode(slave);
+        FreeStyleBuild b1 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
+        assertEquals(slave, b1.getBuiltOn());
+        FilePath ws = b1.getWorkspace();
+        assertNotNull(ws);
+        ws.act(new Detouch());
+        return ws;
+    }
+
+    private void performCleanup() throws InterruptedException, IOException {
+        new WorkspaceCleanupThread().execute(StreamTaskListener.fromStdout());
     }
 
     private static final class Detouch extends MasterToSlaveFileCallable<Void> {
@@ -151,5 +142,4 @@ public class WorkspaceCleanupThreadTest {
             return null;
         }
     }
-
 }
