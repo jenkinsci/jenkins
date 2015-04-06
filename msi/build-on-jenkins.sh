50dcc54bbf05273895116a130de3d5533364ff2a
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-05 19:45:50 -0700

    Packaging scripts have moved to jenkinsci/packaging repo

--- msi/build-on-jenkins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-if [ "$2" == "" ]; then
-  echo "Usage: build-on-jenkins path/to/war path/to/output/msi"
-  exit 1
-fi
-tar cvzf bundle.tgz FindJava.java build.sh jenkins.wxs
-v=$(unzip -p "$1" META-INF/MANIFEST.MF | grep Implementation-Version | cut -d ' ' -f2 | tr -d '\r')
-java -jar jenkins-cli.jar dist-fork -z bundle.tgz -f jenkins.war="$1" -l windows -F "jenkins-$v.msi=$2" bash -ex build.sh jenkins.war
-
