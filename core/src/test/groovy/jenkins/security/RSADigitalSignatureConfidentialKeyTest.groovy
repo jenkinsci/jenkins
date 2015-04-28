aa50f169e4bdd101890f1a178092b8561b71b2f8
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-28 08:37:52 -0700

    Test for RSADigitalSignatureConfidentialKey

---
+++ core/src/test/groovy/jenkins/security/RSADigitalSignatureConfidentialKeyTest.groovy
@@ -0,0 +1,54 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jenkins.security
+
+import hudson.remoting.Base64
+import org.junit.Rule
+import org.junit.Test
+
+import java.security.Signature
+
+/**
+ *
+ *
+ * @author Kohsuke Kawaguchi
+ */
+class RSADigitalSignatureConfidentialKeyTest {
+    @Rule
+    public ConfidentialStoreRule store = new ConfidentialStoreRule()
+
+    def key = new RSADigitalSignatureConfidentialKey("test");
+
+    @Test
+    void dsigSignAndVerify() {
+        def msg = key.sign("Hello world");
+        println msg;
+
+        def sig = Signature.getInstance("SHA256withRSA");
+        sig.initVerify(key.publicKey);
+        sig.update(msg.getBytes("UTF-8"));
+
+        assert sig.verify(Base64.decode(msg))
+    }
+}
