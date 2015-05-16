f68a9128795c457d8d80bba506c9a3b6b1cb57f3
Oleg Nenashev <o.v.nenashev@gmail.com>
2015-05-16 15:12:22 +0300

    Noting #1649

--- core/src/main/java/jenkins/slaves/DefaultJnlpSlaveReceiver.java
+++ core/src/main/java/jenkins/slaves/DefaultJnlpSlaveReceiver.java
@@ -20,7 +20,8 @@ import java.util.logging.Logger;
  * Match the name against the slave name and route the incoming JNLP agent as {@link Slave}.
  *
  * @author Kohsuke Kawaguchi
- * @since 1.561
+ * @since 1.561  
+ * @since 1.614 handle() returns true on handshake error as it required in JnlpAgentReceiver.
  */
 @Extension
 public class DefaultJnlpSlaveReceiver extends JnlpAgentReceiver {
