2680db64394af89a9df62700e1a04341a82b1444
tfennelly <tom.fennelly@gmail.com>
2015-07-20 16:14:06 +0100

    Create jenkins.js bundle and load it via layout.jelly

--- war/gulpfile.js
+++ war/gulpfile.js
@@ -3,10 +3,17 @@ var builder = require('jenkins-js-builder');
 //
 // Use the predefined tasks from jenkins-js-builder.
 //
-builder.defineTasks(['test']);
+builder.defineTasks(['test', 'bundle']);
 
 //
 // Need to override the default src locations.
 //
 builder.src('./src/main/js');
 builder.tests('./src/test/js');
+
+//
+// Create the jenkins.js JavaScript bundle.
+//
+builder.bundle('src/main/js/jenkins.js')
+    .withExternalModuleMapping('jquery-detached-2.1.4', 'jquery-detached:jquery2')
+    .inDir('src/main/webapp/bundles');
