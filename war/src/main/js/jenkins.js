580d68a650b92cad2b7fdd2e9161a49a68ad0201
tfennelly <tom.fennelly@gmail.com>
2015-07-22 14:49:31 +0100

    Commented out bootstrap and moment examples to reduce noise

--- war/src/main/js/jenkins.js
+++ war/src/main/js/jenkins.js
@@ -15,14 +15,13 @@ $(document).ready(function() {
         
 });
 
-
-// Example of using bootstrap.
-// NB: Don't use it here though, Gus!!!
-var bootstrap = require('bootstrap-detached-3.3');
-var $bs = bootstrap.getBootstrap();
-console.log('Bootstrap version: ' + $bs.fn.tooltip.Constructor.VERSION);
-
-// Example of using moment.
-// NB: Don't use it here though, Gus!!!
-var moment = require('moment');
-console.log('Today is: ' + moment().format("MMM Do YY"));
+//// Example of using bootstrap.
+//// NB: Don't use it here though, Gus!!!
+//var bootstrap = require('bootstrap-detached-3.3');
+//var $bs = bootstrap.getBootstrap();
+//console.log('Bootstrap version: ' + $bs.fn.tooltip.Constructor.VERSION);
+//
+//// Example of using moment.
+//// NB: Don't use it here though, Gus!!!
+//var moment = require('moment');
+//console.log('Today is: ' + moment().format("MMM Do YY"));
