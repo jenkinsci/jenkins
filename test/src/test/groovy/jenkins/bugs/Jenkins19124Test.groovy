34f2756011e6975c804a1e7adc94076f02b1e878
tfennelly <tom.fennelly@gmail.com>
2015-08-23 14:34:43 +0100

    Jenkins19124Test fixes

--- test/src/test/groovy/jenkins/bugs/Jenkins19124Test.groovy
+++ test/src/test/groovy/jenkins/bugs/Jenkins19124Test.groovy
@@ -1,5 +1,6 @@
 package jenkins.bugs
 
+import com.gargoylesoftware.htmlunit.WebClientUtil
 import hudson.Launcher
 import hudson.model.AbstractBuild
 import hudson.model.AbstractProject
@@ -40,10 +41,12 @@ class Jenkins19124Test {
         def wc = j.createWebClient();
         def c = wc.getPage(p, "configure");
         c.getElementByName("_.alpha").valueAttribute = "hello";
+        WebClientUtil.waitForJSExec(wc);
         assert d.alpha=="hello";
         assert d.bravo=="2";
 
         c.getElementByName("_.bravo").setSelectedAttribute("1",true);
+        WebClientUtil.waitForJSExec(wc);
         assert d.alpha=="hello";
         assert d.bravo=="1";
     }
