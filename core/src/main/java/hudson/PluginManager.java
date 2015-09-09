25d946368183d3b86fbb2d5c3b161c92c35cb4f4
tfennelly <tom.fennelly@gmail.com>
2015-09-09 17:40:05 +0100

    Extract non web install function in PluginManager (from doInstall)

--- core/src/main/java/hudson/PluginManager.java
+++ core/src/main/java/hudson/PluginManager.java
@@ -79,6 +79,7 @@ import org.kohsuke.stapler.export.ExportedBean;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.xml.parsers.ParserConfigurationException;
@@ -98,6 +99,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
@@ -778,11 +780,31 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
      * Performs the installation of the plugins.
      */
     public void doInstall(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
-        boolean dynamicLoad = req.getParameter("dynamicLoad")!=null;
+        Set<String> plugins = new LinkedHashSet<>();
 
         Enumeration<String> en = req.getParameterNames();
         while (en.hasMoreElements()) {
             String n =  en.nextElement();
+            if(n.startsWith("plugin.")) {
+                plugins.add(n);
+            }
+        }
+
+        boolean dynamicLoad = req.getParameter("dynamicLoad")!=null;
+        install(plugins, dynamicLoad);
+
+        rsp.sendRedirect("../updateCenter/");
+    }
+
+    /**
+     * Performs the installation of the plugins.
+     * @param plugins The list of plugins to install.
+     * @param dynamicLoad If true, the plugin will be dynamically loaded into this Jenkins. If false, 
+     *                    the plugin will only take effect after the reboot.
+     *                    See {@link UpdateCenter#isRestartRequiredForCompletion()}
+     */
+    public void install(@Nonnull Set<String> plugins, boolean dynamicLoad) {
+        for (String n : plugins) {
             if(n.startsWith("plugin.")) {
                 n = n.substring(7);
                 // JENKINS-22080 plugin names can contain '.' as could (according to rumour) update sites
@@ -814,9 +836,7 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
                 p.deploy(dynamicLoad);
             }
         }
-        rsp.sendRedirect("../updateCenter/");
-    }
-
+    }    
 
     /**
      * Bare-minimum configuration mechanism to change the update center.
