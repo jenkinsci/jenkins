c5a494ca0fb48a107adf564e895f6bff1047ddfb
tfennelly <tom.fennelly@gmail.com>
2015-09-24 12:20:11 +0100

    gulpfile cleanups

--- war/gulpfile.js
+++ war/gulpfile.js
@@ -4,27 +4,7 @@
 var builder = require('jenkins-js-builder');
 
 //
-// Use the predefined tasks from jenkins-js-builder.
-// See https://github.com/tfennelly/jenkins-js-builder#predefined-gulp-tasks
-//
-builder.defineTasks(['test', 'bundle', 'rebundle']);
-
-var gulp = require('gulp');
-gulp.task('default', ['bundle']);
-gulp.task('watch:less', function() {
-	gulp.watch('src/main/less/**/*.less', ['bundle'])
-});
-gulp.task('watch', ['watch:less', 'rebundle']);
-
-//
-// Sources are not in the default locations. Following a more maven-like pattern here.
-// See https://github.com/tfennelly/jenkins-js-builder#setting-src-and-test-spec-paths
-//
-builder.src('src/main/js');
-builder.tests('src/test/js');
-
-//
-// Bundle the modules.
+// Bundle the Install Wizard.
 // See https://github.com/tfennelly/jenkins-js-builder#bundling
 //
 builder.bundle('src/main/js/pluginSetupWizard.js')
