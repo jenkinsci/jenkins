6ea4534001a684fc1e1821fde020eb35e8e4f16d
Oleg Nenashev <o.v.nenashev@gmail.com>
2015-09-28 23:04:42 +0300

    Update the unit test

--- test/src/test/groovy/hudson/cli/CLIActionTest.groovy
+++ test/src/test/groovy/hudson/cli/CLIActionTest.groovy
@@ -39,9 +39,15 @@ class CLIActionTest {
 
     @Test
     @PresetData(DataSet.NO_ANONYMOUS_READACCESS)
-    public void serveCliActionToAnonymousUser() throws Exception {
+    public void serveCliActionToAnonymousUserWithoutPermissions() throws Exception {
         def wc = j.createWebClient();
         // The behavior changed due to SECURITY-192. index page is no longer accessible to anonymous
-        wc.assertFails(new URL(j.URL,"cli").toString(), HttpURLConnection.HTTP_FORBIDDEN);
+        wc.assertFails("cli", HttpURLConnection.HTTP_FORBIDDEN);
+    }
+    
+    @Test
+    public void serveCliActionToAnonymousUser() throws Exception {
+        def wc = j.createWebClient();
+        wc.goTo("cli");
     }
 }
