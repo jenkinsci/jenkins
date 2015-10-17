725b6afeac4ac5b8821c095f3b093cd0daa3a526
Oleg Nenashev <o.v.nenashev@gmail.com>
2015-10-17 20:53:22 +0300

    [FIXED JENKINS-30730] Prevent ClassCastException in Abstractbuild::reportError() if the build step is not Publisher.
    This pull request integrates https://github.com/jenkinsci/jenkins/pulls/1851, which is a reworked version of https://github.com/jenkinsci/jenkins/pull/1846 from @fbelzunc (no original codelines left)

--- core/src/main/java/hudson/model/AbstractBuild.java
+++ core/src/main/java/hudson/model/AbstractBuild.java
@@ -742,13 +742,19 @@ public abstract class AbstractBuild<P extends AbstractProject<P,R>,R extends Abs
         }
 
         private void reportError(BuildStep bs, Throwable e, BuildListener listener, boolean phase) {
-            final String publisher = ((Publisher) bs).getDescriptor().getDisplayName();
+            final String buildStep;
+
+            if (bs instanceof Describable) {
+                buildStep = ((Describable) bs).getDescriptor().getDisplayName();
+            } else {
+                buildStep = bs.getClass().getName();
+            }
 
             if (e instanceof AbortException) {
-                LOGGER.log(Level.FINE, "{0} : {1} failed", new Object[] {AbstractBuild.this, publisher});
-                listener.error("Publisher '" + publisher + "' failed: " + e.getMessage());
+                LOGGER.log(Level.FINE, "{0} : {1} failed", new Object[] {AbstractBuild.this, buildStep});
+                listener.error("Step ‘" + buildStep + "’ failed: " + e.getMessage());
             } else {
-                String msg = "Publisher '" + publisher + "' aborted due to exception: ";
+                String msg = "Step ‘" + buildStep + "’ aborted due to exception: ";
                 e.printStackTrace(listener.error(msg));
                 LOGGER.log(WARNING, msg, e);
             }
