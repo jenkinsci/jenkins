3909f5acc4f43d73bdd6749d9762f3c338486b1f
Vincent Latombe <vincent@latombe.net>
2015-11-04 10:56:32 +0100

    Fix a NPE while trying to compare two data bound beans with null Iterable field.

--- test/src/main/java/org/jvnet/hudson/test/JenkinsRule.java
+++ test/src/main/java/org/jvnet/hudson/test/JenkinsRule.java
@@ -1453,7 +1453,7 @@ public class JenkinsRule implements TestRule, MethodRule, RootAction {
             Object lv = ReflectionUtils.getPublicProperty(lhs, names[i]);
             Object rv = ReflectionUtils.getPublicProperty(rhs, names[i]);
 
-            if (Iterable.class.isAssignableFrom(types[i])) {
+            if (lv != null && rv != null && Iterable.class.isAssignableFrom(types[i])) {
                 Iterable lcol = (Iterable) lv;
                 Iterable rcol = (Iterable) rv;
                 Iterator ltr,rtr;
