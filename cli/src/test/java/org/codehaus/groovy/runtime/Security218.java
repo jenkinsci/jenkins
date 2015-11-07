12a970dffacc7c5d980a4c31b7fbb8c5990af535
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-11-07 11:10:09 -0800

    A modest test case for the change

---
+++ cli/src/test/java/org/codehaus/groovy/runtime/Security218.java
@@ -0,0 +1,11 @@
+package org.codehaus.groovy.runtime;
+
+import java.io.Serializable;
+
+/**
+ * Test payload.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class Security218 implements Serializable {
+}
