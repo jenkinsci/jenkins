e8f2002c466c49eff4a7be0cc4633580515cd7a5
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-11-17 10:39:02 +0000

    [FIXED JENKINS-31591] Correct MasterKillSwitchConfiguration visibility
    - The MasterKillSwitchConfiguration was not available when you have clouds but no fixed slaves.
    - The MasterKillSwitchConfiguration was not available when it was enabled but for a non-relevant configuration.

--- core/src/main/java/jenkins/security/s2m/MasterKillSwitchConfiguration.java
+++ core/src/main/java/jenkins/security/s2m/MasterKillSwitchConfiguration.java
@@ -1,13 +1,12 @@
 package jenkins.security.s2m;
 
 import hudson.Extension;
+import javax.inject.Inject;
 import jenkins.model.GlobalConfiguration;
 import jenkins.model.GlobalConfigurationCategory;
 import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
-
-import javax.inject.Inject;
 
 /**
  * Exposes {@link AdminWhitelistRule#masterKillSwitch} to the admin.
@@ -47,8 +46,13 @@ public class MasterKillSwitchConfiguration extends GlobalConfiguration {
      * Unless this option is relevant, we don't let users choose this.
      */
     public boolean isRelevant() {
-        return jenkins.getComputers().length>1  // if there's no slave, there's no point
-            && jenkins.isUseSecurity()          // if security is off, likewise this is pointless
+        if (rule.getMasterKillSwitch()) {
+            return true; // always relevant if it is enabled.
+        }
+        return jenkins.isUseSecurity()            // if security is off, there's no point
+            && (jenkins.getComputers().length>1   // if there's no slave,
+                || !jenkins.clouds.isEmpty()      // and no clouds, likewise this is pointless
+            )
         ;
     }
 }
