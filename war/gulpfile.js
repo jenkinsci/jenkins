ac5a4a4d7219640f873ed06e5175a7c12afc4a3c
tfennelly <tom.fennelly@gmail.com>
2015-11-24 10:10:45 +0000

    Load Jenkins JS Modules from 'core-assets'

--- war/gulpfile.js
+++ war/gulpfile.js
@@ -8,8 +8,8 @@ var builder = require('jenkins-js-builder');
 // See https://github.com/tfennelly/jenkins-js-builder#bundling
 //
 builder.bundle('src/main/js/pluginSetupWizard.js')
-    .withExternalModuleMapping('jquery-detached', 'jquery-detached:jquery2')
-    .withExternalModuleMapping('bootstrap', 'bootstrap:bootstrap3')
-    .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
+    .withExternalModuleMapping('jquery-detached', 'core-assets/jquery-detached:jquery2')
+    .withExternalModuleMapping('bootstrap', 'core-assets/bootstrap:bootstrap3')
+    .withExternalModuleMapping('handlebars', 'core-assets/handlebars:handlebars3')
     .less('src/main/less/pluginSetupWizard.less')
     .inDir('src/main/webapp/jsbundles');
