5b685a8e970689b952bfd997feae222ad5c0ce0a
Surya Gaddpati <surya.gaddipati@gmail.com>
2015-11-30 14:17:33 -0600

    Add documentation for TopLevelItemLoader Extension point

--- core/src/main/java/jenkins/model/TopLevelItemLoader.java
+++ core/src/main/java/jenkins/model/TopLevelItemLoader.java
@@ -6,6 +6,10 @@ import hudson.model.TopLevelItem;
 import java.io.IOException;
 import java.util.Collection;
 
+/**
+ * Extension point to contribute @TopLevelItem's to Jenkins.
+ * @see DiskItemLoader
+ */
 
 public abstract class TopLevelItemLoader implements ExtensionPoint {
      public abstract Collection<TopLevelItem> load(Jenkins jenkins) throws IOException;
