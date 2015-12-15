0925b78ffcf79d609712fab828f82b4306238ec8
tfennelly <tom.fennelly@gmail.com>
2015-12-15 08:24:28 +0000

    Merge asset manager stuff

--- war/gulpfile.js
+++ war/gulpfile.js
@@ -5,11 +5,11 @@ var builder = require('jenkins-js-builder');
 
 //
 // Bundle the Install Wizard.
-// See https://github.com/tfennelly/jenkins-js-builder#bundling
+// See https://github.com/jenkinsci/js-builder#bundling
 //
 builder.bundle('src/main/js/pluginSetupWizard.js')
     .withExternalModuleMapping('jquery-detached', 'core-assets/jquery-detached:jquery2')
-    .withExternalModuleMapping('bootstrap', 'core-assets/bootstrap:bootstrap3')
+    .withExternalModuleMapping('bootstrap', 'core-assets/bootstrap:bootstrap3', {addDefaultCSS: true})
     .withExternalModuleMapping('handlebars', 'core-assets/handlebars:handlebars3')
     .less('src/main/less/pluginSetupWizard.less')
     .inDir('src/main/webapp/jsbundles');
