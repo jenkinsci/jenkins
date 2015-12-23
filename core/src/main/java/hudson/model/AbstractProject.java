c22169b75214fbc4db2bd46735188c4791aea548
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-12-23 14:27:25 +0100

    [JENKINS-32183] TODO stmt and Long class instead long primitive as suggested in the PR review

--- core/src/main/java/hudson/model/AbstractProject.java
+++ core/src/main/java/hudson/model/AbstractProject.java
@@ -212,8 +212,9 @@ public abstract class AbstractProject<P extends AbstractProject<P,R>,R extends A
 
     /**
      * When the job is disabled.
+     * TODO: @since
      */
-    protected long disabledSince;
+    protected Long disabledSince = null;
 
     /**
      * True to keep builds of this project in queue when downstream projects are
@@ -696,7 +697,7 @@ public abstract class AbstractProject<P extends AbstractProject<P,R>,R extends A
     /**
      * When the Job is disabled.
      *
-     * @since 1.640
+     * TODO: @since
      */
     @Exported
     public @Nonnull Calendar getDisabledSince() {
