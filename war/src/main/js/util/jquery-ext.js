465edd83a959dd49b11f2b6166e5470e8c003665
tfennelly <tom.fennelly@gmail.com>
2016-01-21 15:19:26 +0000

    Make jquery-ext.js more unit test friendly

--- war/src/main/js/util/jquery-ext.js
+++ war/src/main/js/util/jquery-ext.js
@@ -3,23 +3,38 @@
  */
 
 var jQD = require('jquery-detached');
-
-// We are going to be adding "stuff" to jQuery. We create a totally new jQuery instance
-// because we do NOT want to run the risk of polluting the shared instance.
-var $ext = jQD.newJQuery();
-
-/**
- * A pseudo selector that performs a case insensitive text contains search i.e. the same
- * as the standard ':contains' selector, but case insensitive.
- */
-$ext.expr[":"].containsci = $ext.expr.createPseudo(function(text) {
-    return function(element) {
-        var elementText = $ext(element).text();
-        var result = (elementText.toUpperCase().indexOf(text.toUpperCase()) !== -1);
-        return result;
-    };
-});
+var $ext;
 
 exports.getJQuery = function() {
+    if (!$ext) {
+        initJQueryExt();
+    }
     return $ext;
 };
+
+/*
+ * Clear the $ext instance if hte window changes. Primarily for unit testing.
+ */
+var windowHandle = require('window-handle');
+windowHandle.getWindow(function() {
+    $ext = undefined;
+});
+
+function initJQueryExt() {
+// We are going to be adding "stuff" to jQuery. We create a totally new jQuery instance
+// because we do NOT want to run the risk of polluting the shared instance.
+    $ext = jQD.newJQuery();
+
+    /**
+     * A pseudo selector that performs a case insensitive text contains search i.e. the same
+     * as the standard ':contains' selector, but case insensitive.
+     */
+    $ext.expr[":"].containsci = $ext.expr.createPseudo(function (text) {
+        return function (element) {
+            var elementText = $ext(element).text();
+            var result = (elementText.toUpperCase().indexOf(text.toUpperCase()) !== -1);
+            return result;
+        };
+    });
+}
+initJQueryExt();
