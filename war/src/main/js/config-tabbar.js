daa52d7f2b877e636e189658fa1616866a2842b6
gusreiber <greiber@cloudbees.com>
2016-03-07 17:49:33 -0800

    PR-2046:removing junk comment

--- war/src/main/js/config-tabbar.js
+++ war/src/main/js/config-tabbar.js
@@ -78,7 +78,6 @@ function addFinderToggle(configTableMetadata) {
     var findToggle = $('<div class="find-toggle" title="Find"></div>');
     var finderShowPreferenceKey = 'config:showfinder';
     
-    //$('.tabBar', configTableMetadata.configWidgets).append(findToggle);
     findToggle.click(function() {
         var findContainer = $('.find-container', configTableMetadata.configWidgets);
         if (findContainer.hasClass('visible')) {
