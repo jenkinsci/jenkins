3c10f1f7b905feb5b1f7c4b3a1d98048defda3a3
James Nord <jnord+github@cloudbees.com>
2016-03-08 15:44:50 +0000

    [FIXED JENKINS-33395] install an uncaught exception handler

--- core/src/main/java/hudson/init/impl/InstallUncaughtExceptionHandler.java
+++ core/src/main/java/hudson/init/impl/InstallUncaughtExceptionHandler.java
@@ -11,6 +11,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.kohsuke.stapler.Stapler;
 
 /**
@@ -37,5 +40,22 @@ public class InstallUncaughtExceptionHandler {
                 }
             }
         });
+        Thread.setDefaultUncaughtExceptionHandler(new DefaultUncaughtExceptionHandler());
+    }
+
+    /** An UncaughtExceptionHandler that just logs the exception */
+    private static class DefaultUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+        private static final Logger LOGGER = Logger.getLogger(InstallUncaughtExceptionHandler.class.getName());
+
+        @Override
+        public void uncaughtException(Thread t, Throwable ex) {
+            // if this was an OutOfMemoryError then all bets about logging are off - but in the absence of anything else...
+            LOGGER.log(Level.SEVERE,
+                       "A thread (" + t.getName() + '/' + t.getId()
+                                     + ") died unexepectedly due to an uncaught exception, this may leave your Jenkins in a bad way and is usually indicitive of a bug in the code.",
+                       ex);
+        }
+
     }
 }
