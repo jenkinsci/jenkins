4e79342885d45f4cbd818d75dd5a5092a08c1986
tfennelly <tom.fennelly@gmail.com>
2016-03-27 10:37:20 +0100

    Small refactoring of TabBarOverflow
    Extracting some functions and cleaning up tab activation.

--- war/src/main/js/widgets/tabs/TabBarOverflow.js
+++ war/src/main/js/widgets/tabs/TabBarOverflow.js
@@ -90,6 +90,27 @@ TabBarOverflow.prototype.isOverflown = function() {
     return (this.tabBar.width() > this.tabBarFrame.width());
 };
 
+TabBarOverflow.prototype.tabIndex = function(tab) {
+    for (var i = 0; i < this.tabs.length; i++) {
+        if (this.tabs[i] === tab) {
+            return i;
+        }
+    }
+    throw 'Unknown tab.';
+};
+
+TabBarOverflow.prototype.hideAllTabs = function() {
+    var $ = jQD.getJQuery();
+    for (var i = 0; i < this.tabs.length; i++) {
+        $(this.tabs[i]).removeClass('taboverflow-tab-visible');
+    }
+};
+
+TabBarOverflow.prototype.visibleTabCount = function() {
+    var $ = jQD.getJQuery();
+    return $('.taboverflow-tab-visible', this.tabBar).size();
+};
+
 TabBarOverflow.prototype.onTabActivate = function(activatedTab, refresh) {
     // We need to check if this table is visible to the user and if not,
     // we need to make it visible.
@@ -101,53 +122,41 @@ TabBarOverflow.prototype.onTabActivate = function(activatedTab, refresh) {
         }
     }
 
-    var $ = jQD.getJQuery();
-
     // Ok, the tab that's just been activated is not visible to the user. So,
     // we make it (and local sibling tabs) visible by hiding other tabs before it.
     // We start by hiding all tabs. Then, we start showing tabs (starting from
     // the supplied tab). We stop showing tabs once we show a tab that overflows the tabBar.
 
+    var $ = jQD.getJQuery();
+
     // Hide them all..
-    for (var i1 = 0; i1 < this.tabs.length; i1++) {
-        $(this.tabs[i1]).removeClass('taboverflow-tab-visible');
-    }
+    this.hideAllTabs();
 
     // Start showing tabs, starting from the tab that was just activated...
-    var addTabs = false;
-    var addedCount = 0;
-    var activeTabIdx = 0;
-    for (var i2 = 0; i2 < this.tabs.length; i2++) {
-        var tabForward = this.tabs[i2];
-        if (tabForward === activatedTab) {
-            addTabs = true;
-            activeTabIdx = i2;
-        }
-        if (addTabs) {
-            $(tabForward).addClass('taboverflow-tab-visible');
-            if (this.isOverflown()) {
-                $(tabForward).removeClass('taboverflow-tab-visible');
-                break;
-            }
-            addedCount++;
+    var activeTabIdx = this.tabIndex(activatedTab);
+    for (var i1 = activeTabIdx; i1 < this.tabs.length; i1++) {
+        var tabForward = this.tabs[i1];
+        $(tabForward).addClass('taboverflow-tab-visible');
+        if (this.isOverflown()) {
+            $(tabForward).removeClass('taboverflow-tab-visible');
+            break;
         }
     }
 
     if (!this.isOverflown() && activeTabIdx > 0) {
         // There's room for more tabs. If there are tabs before the "active"
         // tab, then lets show some of those too.
-        for (var i3 = activeTabIdx - 1; i3 >= 0 ; i3--) {
-            var tabBack = this.tabs[i3];
+        for (var i2 = activeTabIdx - 1; i2 >= 0 ; i2--) {
+            var tabBack = this.tabs[i2];
             $(tabBack).addClass('taboverflow-tab-visible');
             if (this.isOverflown()) {
                 $(tabBack).removeClass('taboverflow-tab-visible');
                 break;
             }
-            addedCount++;
         }
     }
 
-    if (addedCount < this.tabs.length) {
+    if (this.visibleTabCount() < this.tabs.length) {
         this.taboverflowBtn.show();
     } else {
         this.taboverflowBtn.hide();
