04a34a353d0c64c401a73facce16df086e091747
kzantow <kzantow@gmail.com>
2016-04-04 11:50:13 -0400

    JENKINS-33805 - rename install/upgrade files to be more clear & consistent

--- core/src/main/java/jenkins/install/UpgradeWizard.java
+++ core/src/main/java/jenkins/install/UpgradeWizard.java
@@ -47,7 +47,7 @@ public class UpgradeWizard extends PageDecorator {
      * This file records the vesrion number that the installation has upgraded to.
      */
     /*package*/ File getStateFile() {
-        return new File(Jenkins.getInstance().getRootDir(),"upgraded");
+        return new File(Jenkins.getInstance().getRootDir(),"last_upgrade_version");
     }
 
     public UpgradeWizard() throws IOException {
