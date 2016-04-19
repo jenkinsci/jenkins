82b42359797e42fa4b2428d5e390e7ce91b74998
Jesse Glick <jglick@cloudbees.com>
2016-04-19 17:44:31 -0400

    [SECURITY-266] Conceal ciphertext from the web UI for a user who has no Item.CONFIGURE permission.

--- test/src/test/java/lib/form/PasswordTest.java
+++ test/src/test/java/lib/form/PasswordTest.java
@@ -28,8 +28,20 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import hudson.Extension;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
+import hudson.model.FreeStyleProject;
+import hudson.model.Item;
+import hudson.model.JobProperty;
+import hudson.model.JobPropertyDescriptor;
+import hudson.security.GlobalMatrixAuthorizationStrategy;
 import hudson.util.Secret;
+import jenkins.model.Jenkins;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.TestExtension;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -55,4 +67,51 @@ public class PasswordTest extends HudsonTestCase implements Describable<Password
             return null;
         }
     }
+
+    @Issue("SECURITY-266")
+    public void testExposedCiphertext() throws Exception {
+        boolean saveEnabled = Item.EXTENDED_READ.getEnabled();
+        try {
+            jenkins.setSecurityRealm(createDummySecurityRealm());
+            // TODO 1.645+ use MockAuthorizationStrategy
+            GlobalMatrixAuthorizationStrategy pmas = new GlobalMatrixAuthorizationStrategy();
+            pmas.add(Jenkins.ADMINISTER, "admin");
+            pmas.add(Jenkins.READ, "dev");
+            pmas.add(Item.READ, "dev");
+            Item.EXTENDED_READ.setEnabled(true);
+            pmas.add(Item.EXTENDED_READ, "dev");
+            jenkins.setAuthorizationStrategy(pmas);
+            Secret secret = Secret.fromString("s3cr3t");
+            FreeStyleProject p = createFreeStyleProject("p");
+            p.addProperty(new VulnerableProperty(secret));
+            WebClient wc = createWebClient();
+            wc.login("admin");
+            HtmlPage configure = wc.getPage(p, "configure");
+            assertThat(configure.getWebResponse().getContentAsString(), containsString(secret.getEncryptedValue()));
+            submit(configure.getFormByName("config"));
+            VulnerableProperty vp = p.getProperty(VulnerableProperty.class);
+            assertNotNull(vp);
+            assertEquals(secret, vp.secret);
+            wc.login("dev");
+            configure = wc.getPage(p, "configure");
+            assertThat(configure.getWebResponse().getContentAsString(), not(containsString(secret.getEncryptedValue())));
+        } finally {
+            Item.EXTENDED_READ.setEnabled(saveEnabled);
+        }
+    }
+    public static class VulnerableProperty extends JobProperty<FreeStyleProject> {
+        public final Secret secret;
+        @DataBoundConstructor
+        public VulnerableProperty(Secret secret) {
+            this.secret = secret;
+        }
+        @TestExtension("testExposedCiphertext")
+        public static class DescriptorImpl extends JobPropertyDescriptor {
+            @Override // TODO delete in 1.635+
+            public String getDisplayName() {
+                return "VulnerableProperty";
+            }
+        }
+    }
+
 }
