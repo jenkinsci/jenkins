bbb38ffade7f39f3d10a0f827b3c617f54f79cdf
Félix Belzunce Arcos <fbelzunc@gmail.com>
2016-05-11 16:33:28 +0200

    [JENKINS-25164] Change LEAF_JOB_NAME for JOB_BASE_NAME

--- core/src/main/java/hudson/model/Job.java
+++ core/src/main/java/hudson/model/Job.java
@@ -369,7 +369,7 @@ public abstract class Job<JobT extends Job<JobT, RunT>, RunT extends Run<JobT, R
         env.put("JENKINS_SERVER_COOKIE",SERVER_COOKIE.get());
         env.put("HUDSON_SERVER_COOKIE",SERVER_COOKIE.get()); // Legacy compatibility
         env.put("JOB_NAME",getFullName());
-        env.put("LEAF_JOB_NAME", getName());
+        env.put("JOB_BASE_NAME", getName());
         return env;
     }
 
--- core/src/main/resources/jenkins/model/CoreEnvironmentContributor/buildEnv.groovy
+++ core/src/main/resources/jenkins/model/CoreEnvironmentContributor/buildEnv.groovy
@@ -3,7 +3,7 @@ package jenkins.model.CoreEnvironmentContributor;
 def l = namespace(lib.JenkinsTagLib)
 
 // also advertises those contributed by Run.getCharacteristicEnvVars()
-["BUILD_NUMBER","BUILD_ID","BUILD_DISPLAY_NAME","JOB_NAME", "LEAF_JOB_NAME","BUILD_TAG","EXECUTOR_NUMBER","NODE_NAME","NODE_LABELS","WORKSPACE","JENKINS_HOME","JENKINS_URL","BUILD_URL","JOB_URL"].each { name ->
+["BUILD_NUMBER","BUILD_ID","BUILD_DISPLAY_NAME","JOB_NAME", "JOB_BASE_NAME","BUILD_TAG","EXECUTOR_NUMBER","NODE_NAME","NODE_LABELS","WORKSPACE","JENKINS_HOME","JENKINS_URL","BUILD_URL","JOB_URL"].each { name ->
     l.buildEnvVar(name:name) {
         raw(_("${name}.blurb"))
     }
