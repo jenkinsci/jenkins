f83b71eb0f4b5445788ac08a9dc1e3b56f822800
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-05-12 17:41:29 +0200

    [JENKINS-34745] - Fix typo in the validator

--- core/src/main/java/jenkins/util/JSONSignatureValidator.java
+++ core/src/main/java/jenkins/util/JSONSignatureValidator.java
@@ -83,7 +83,7 @@ public class JSONSignatureValidator {
             // this is for computing a signature
             Signature sig = Signature.getInstance("SHA1withRSA");
             if (certs.isEmpty()) {
-                return FormValidation.error("No certificate found in " + name + ". Canot verify the signature");
+                return FormValidation.error("No certificate found in " + name + ". Cannot verify the signature");
             } else {    
                 sig.initVerify(certs.get(0));
             }
