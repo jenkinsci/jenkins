fae393fb3572d82d06dfb3a735802cd4b34e7b1f
kzantow <kzantow@gmail.com>
2016-05-13 14:50:58 -0400

    [JENKINS-34676] Update to new way of doing things with js-builder (+2 squashed commits) Squashed commits: [6d2965d] Update for @jenkins-cd org npm modules [0582df0] Fix some test failures due to environmental differences

--- war/gulpfile.js
+++ war/gulpfile.js
@@ -20,7 +20,7 @@ builder.bundle('src/main/js/page-init.js')
 //
 builder.bundle('src/main/js/pluginSetupWizard.js')
     .withExternalModuleMapping('jquery-detached', 'core-assets/jquery-detached:jquery2')
-    .withExternalModuleMapping('bootstrap', 'core-assets/bootstrap:bootstrap3', {addDefaultCSS: true})
+    .withExternalModuleMapping('bootstrap-detached', 'core-assets/bootstrap:bootstrap3', {addDefaultCSS: true})
     .withExternalModuleMapping('handlebars', 'core-assets/handlebars:handlebars3')
     .less('src/main/less/pluginSetupWizard.less')
     .inDir('src/main/webapp/jsbundles');
