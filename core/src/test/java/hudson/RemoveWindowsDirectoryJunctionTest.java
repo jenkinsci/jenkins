96956343e19e0425e54f4daa32b23649e538e2a6
Darin Broady <dbroady@lexmark.com>
2016-05-27 08:29:36 -0400

    Remove debugging output, per Baptiste's request

--- core/src/test/java/hudson/RemoveWindowsDirectoryJunctionTest.java
+++ core/src/test/java/hudson/RemoveWindowsDirectoryJunctionTest.java
@@ -29,14 +29,7 @@ public class RemoveWindowsDirectoryJunctionTest {
         File f1 = new File(subdir1, "testfile1.txt");
         assertTrue("Unable to create temporary file in notJunction directory", f1.createNewFile());
         File j1 = makeJunction(tmp.getRoot(), subdir1);
-        System.out.println("Before Util.deleteRecursive(j1)");
-        print(tmp.getRoot());
-        print(subdir1);
-        print(j1);
         Util.deleteRecursive(j1);
-        System.out.println("After Util.deleteRecursive(j1)");
-        print(subdir1);
-        print(tmp.getRoot());
         assertTrue("Contents of Windows Junction should not be removed", f1.exists());
     }
 
@@ -48,13 +41,5 @@ public class RemoveWindowsDirectoryJunctionTest {
        Process p = pb.start();
        assertTrue("Running mklink failed (cmd=" + cmd + ")", p.waitFor() == 0);
        return junc;
-    }
-    
-    private void print(File d) {
-        System.out.println("  '" + d.getPath() + "'");
-        String[] c = d.list();
-        for (String s : c) {
-            System.out.println("    '" + s + "'");
-        }
     }
 }
