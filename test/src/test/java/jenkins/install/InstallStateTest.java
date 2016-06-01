01d77d1825f6cdc512d4e81a4e2d81de25cd44ce
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-06-01 16:20:57 +0200

    [JENKINS-35206] - Fix the license header in thre test file

--- test/src/test/java/jenkins/install/InstallStateTest.java
+++ test/src/test/java/jenkins/install/InstallStateTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2016 Oleg Nenashev.
+ * Copyright (c) 2016 CloudBees, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
