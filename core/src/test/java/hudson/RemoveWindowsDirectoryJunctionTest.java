b36fbf2affa36ce769a0c248f703e320e87402e9
Darin Broady <dbroady@lexmark.com>
2016-06-02 11:53:56 -0400

    Fix compilation errors by importing functions

--- core/src/test/java/hudson/RemoveWindowsDirectoryJunctionTest.java
+++ core/src/test/java/hudson/RemoveWindowsDirectoryJunctionTest.java
@@ -3,6 +3,8 @@
  */
 package hudson;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
