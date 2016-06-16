8b58c0c3327e0f5dcb52ecd8a9a6fa62b70c3a2b
Nicolas De Loof <nicolas.deloof@gmail.com>
2016-06-16 13:25:21 +0200

    javadoc

--- core/src/main/java/hudson/model/WorkspaceListener.java
+++ core/src/main/java/hudson/model/WorkspaceListener.java
@@ -13,6 +13,7 @@ public abstract class WorkspaceListener implements ExtensionPoint {
     /**
      * Called after a workspace is deleted successfully.
      * @param project
+     *      The ${@link AbstractProject} this workspace was created for
      * @deprecated as of TODO_BEFORE_MERGE Use {@link #afterDelete(Job)}.
      */
     @Deprecated public void afterDelete(AbstractProject project) {
@@ -25,6 +26,8 @@ public abstract class WorkspaceListener implements ExtensionPoint {
     /**
      * Called after a workspace is deleted successfully.
      * @param job
+     *      The ${@link Job} this workspace was created for
+     * @since TODO_BEFORE_MERGE
      */
     public void afterDelete(Job job) {
         // this implementation is backward compatibility thunk in case subtypes only override the
@@ -39,8 +42,11 @@ public abstract class WorkspaceListener implements ExtensionPoint {
     /**
      * Called before a build uses a workspace. IE, before any SCM checkout.
      * @param b
+     *      The ${@link AbstractBuild} to use this workspace
      * @param workspace
+     *      The workspace being acquired by r
      * @param listener
+     *      Run's build listener
      * @deprecated as of TODO_BEFORE_MERGE Use {@link #beforeUse(Run, FilePath, BuildListener)}.
      */
     @Deprecated public void beforeUse(AbstractBuild b, FilePath workspace, BuildListener listener) {
@@ -53,8 +59,12 @@ public abstract class WorkspaceListener implements ExtensionPoint {
     /**
      * Called before a build uses a workspace. IE, before any SCM checkout.
      * @param r
+     *      The ${@link Run} to use this workspace
      * @param workspace
+     *      The workspace being acquired by r
      * @param listener
+     *      Run's build listener
+     * @since TODO_BEFORE_MERGE
      */
     public void beforeUse(Run r, FilePath workspace, BuildListener listener) {
         // this implementation is backward compatibility thunk in case subtypes only override the
