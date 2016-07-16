6b89ce4a3367b917130848fbbf4f397d27db062c
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-07-16 03:00:21 +0300

    FindBugs: Suppress MS_SHOULD_BE_REFACTORED_TO_BE_FINAL in JnlpSlaveAgentProtocol3#Enabled

--- core/src/main/java/jenkins/slaves/JnlpSlaveAgentProtocol3.java
+++ core/src/main/java/jenkins/slaves/JnlpSlaveAgentProtocol3.java
@@ -1,5 +1,6 @@
 package jenkins.slaves;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.AbortException;
 import hudson.Extension;
 import hudson.Util;
@@ -124,6 +125,8 @@ public class JnlpSlaveAgentProtocol3 extends AgentProtocol {
      * an escape hatch for those who hit any issues and those who are trying this out.
      */
     @Restricted(NoExternalUse.class)
+    @SuppressFBWarnings(value = "MS_SHOULD_BE_REFACTORED_TO_BE_FINAL",
+            justification = "Part of the administrative API for System Groovy scripts.")
     public static boolean ENABLED;
 
     static {
