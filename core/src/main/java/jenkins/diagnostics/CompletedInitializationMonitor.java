a61d21128178080c397be6eddbf8b238306ddbde
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-09-01 00:18:03 +0200

    [JENKINS-37874] - Add Administrative monitor for the COMPLETED state

---
+++ core/src/main/java/jenkins/diagnostics/CompletedInitializationMonitor.java
@@ -0,0 +1,50 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jenkins.diagnostics;
+
+import hudson.Extension;
+import hudson.init.InitMilestone;
+import hudson.model.AdministrativeMonitor;
+import jenkins.model.Jenkins;
+import org.jenkinsci.Symbol;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
+/**
+ * Performs monitoring of {@link Jenkins} {@link InitMilestone} status.
+ *
+ * @author Oleg Nenashev
+ * @since TODO
+ */
+@Restricted(NoExternalUse.class)
+@Extension @Symbol("completedInitialization")
+public class CompletedInitializationMonitor extends AdministrativeMonitor {
+    @Override
+    public boolean isActivated() {
+        final Jenkins instance = Jenkins.getInstance();
+        // Safe to check in such way, because monitors are being checked in UI only.
+        // So Jenkins class construction and initialization must be always finished by the call of this extension.
+        return instance.getInitLevel() != InitMilestone.COMPLETED;
+    }
+}
