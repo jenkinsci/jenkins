45e4636042249cee8a0a073f55f45c754adf147b
Laurențiu Păncescu <lpancescu@gmail.com>
2016-09-06 00:39:36 +0200

    [JENKINS-10912] Use setInterval in refreshPart
    The current implementation of refreshPart creates a new closure every 5
    seconds, which, in combination with XMLHttpRequest objects, results in a
    significant memory leak in all major browsers. By using
    window.setInterval to schedule periodic refreshes, only one closure per
    id is created. Please see issue #10912 in the Jenkins tracker for
    further details.

--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -1486,6 +1486,7 @@ function expandTextArea(button,id) {
 // refresh a part of the HTML specified by the given ID,
 // by using the contents fetched from the given URL.
 function refreshPart(id,url) {
+    var intervalID = null;
     var f = function() {
         if(isPageVisible()) {
             new Ajax.Request(url, {
@@ -1506,21 +1507,18 @@ function refreshPart(id,url) {
                     Behaviour.applySubtree(node);
                     layoutUpdateCallback.call();
 
-                    if(isRunAsTest) return;
-                    refreshPart(id,url);
+                    if (isRunAsTest && intervalID) {
+                        window.clearInterval(intervalID);
+                        intervalID = null;
+                    }
                 }
-            });    
-        } else {
-            // Reschedule
-            if(isRunAsTest) return;
-            refreshPart(id,url);
+            });
         }
-        
     };
     // if run as test, just do it once and do it now to make sure it's working,
     // but don't repeat.
     if(isRunAsTest) f();
-    else    window.setTimeout(f, 5000);
+    else intervalID = window.setInterval(f, 5000);
 }
 
 
