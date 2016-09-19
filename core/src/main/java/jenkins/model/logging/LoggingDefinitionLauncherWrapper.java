5c287b3f2c6564ee83db722ec24d94a1a67c99c0
Xing Yan <xyan@xyan0.mtv.corp.google.com>
2016-09-19 13:47:53 -0700

    Hide launcher complexity from plugin.

--- core/src/main/java/jenkins/model/logging/LoggingDefinitionLauncherWrapper.java
+++ core/src/main/java/jenkins/model/logging/LoggingDefinitionLauncherWrapper.java
@@ -23,10 +23,160 @@
  */
 package jenkins.model.logging;
 
+import hudson.Launcher;
+import hudson.Launcher.RemoteProcess;
+import hudson.Proc;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.remoting.Channel;
+import hudson.remoting.RemoteInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import jenkins.model.Jenkins;
+import jenkins.security.MasterToSlaveCallable;
+import org.apache.commons.io.input.NullInputStream;
+
 /**
  *
  * @author Oleg Nenashev
  */
 public class LoggingDefinitionLauncherWrapper {
-    
+
+    /**
+     * Default local launcher, doesn't do anything.
+     */
+    public static class DefaultLocalLauncher extends Launcher.DecoratedLauncher {
+        public DefaultLocalLauncher(Launcher inner) {
+            super(inner);
+        }
+    }
+
+    /**
+     * Default remote launcher which redirects all the output and error to the stream {@link LoggingMethod} provides.
+     */
+    public static class DefaultRemoteLauncher extends Launcher.DecoratedLauncher {
+        private static final NullInputStream NULL_INPUT_STREAM = new NullInputStream(0);
+        private final Run run;
+        private final LoggingMethod loggingMethod;
+
+        public DefaultRemoteLauncher(Launcher inner, Run run, LoggingMethod loggingMethod) {
+            super(inner);
+            this.run = run;
+            this.loggingMethod = loggingMethod;
+        }
+
+        @Override
+        public Proc launch(Launcher.ProcStarter ps) throws IOException {
+            final LoggingMethod.OutputStreamWrapper streamOut = loggingMethod.provideOutStream(run);
+            final LoggingMethod.OutputStreamWrapper streamErr = loggingMethod.provideErrStream(run);
+
+            // RemoteLogstashReporterStream(new CloseProofOutputStream(ps.stdout()
+            final OutputStream out = ps.stdout() == null ? null : (streamOut == null ? ps.stdout() : streamOut);
+            final OutputStream err = ps.stdout() == null ? null : (streamErr == null ? ps.stdout() : streamErr);
+            final InputStream in = (ps.stdin() == null || ps.stdin() == NULL_INPUT_STREAM) ? null : new RemoteInputStream(ps.stdin(), false);
+            final String workDir = ps.pwd() == null ? null : ps.pwd().getRemote();
+
+            // TODO: we do not reverse streams => the parameters
+            try {
+                final RemoteLaunchCallable callable = new RemoteLaunchCallable(
+                    ps.cmds(), ps.masks(), ps.envs(), in,
+                    false, out, false, err,
+                    false, ps.quiet(), workDir, listener);
+
+                return new Launcher.RemoteLauncher.ProcImpl(getChannel().call(callable));
+            } catch (InterruptedException e) {
+                throw (IOException) new InterruptedIOException().initCause(e);
+            }
+        }
+
+        private static class RemoteLaunchCallable extends
+            MasterToSlaveCallable<RemoteProcess, IOException> {
+
+            private final List<String> cmd;
+            private final boolean[] masks;
+            private final String[] env;
+            private final InputStream in;
+            private final OutputStream out;
+            private final OutputStream err;
+            private final String workDir;
+            private final TaskListener listener;
+            private final boolean reverseStdin, reverseStdout, reverseStderr;
+            private final boolean quiet;
+
+            RemoteLaunchCallable(List<String> cmd, boolean[] masks, String[] env, InputStream in, boolean reverseStdin, OutputStream out, boolean reverseStdout, OutputStream err, boolean reverseStderr, boolean quiet, String workDir, TaskListener listener) {
+                this.cmd = new ArrayList<String>(cmd);
+                this.masks = masks;
+                this.env = env;
+                this.in = in;
+                this.out = out;
+                this.err = err;
+                this.workDir = workDir;
+                this.listener = listener;
+                this.reverseStdin = reverseStdin;
+                this.reverseStdout = reverseStdout;
+                this.reverseStderr = reverseStderr;
+                this.quiet = quiet;
+            }
+
+            public Launcher.RemoteProcess call() throws IOException {
+                Launcher.ProcStarter ps = new Launcher.LocalLauncher(listener).launch();
+                ps.cmds(cmd).masks(masks).envs(env).stdin(in).stdout(out).stderr(err).quiet(quiet);
+                if (workDir != null) {
+                    ps.pwd(workDir);
+                }
+                if (reverseStdin) {
+                    ps.writeStdin();
+                }
+                if (reverseStdout) {
+                    ps.readStdout();
+                }
+                if (reverseStderr) {
+                    ps.readStderr();
+                }
+
+                final Proc p = ps.start();
+
+                return Channel.current().export(Launcher.RemoteProcess.class, new Launcher.RemoteProcess() {
+                    public int join() throws InterruptedException, IOException {
+                        try {
+                            return p.join();
+                        } finally {
+                            // make sure I/O is delivered to the remote before we return
+                            try {
+                                Channel.current().syncIO();
+                            } catch (Throwable _) {
+                                // this includes a failure to sync, slave.jar too old, etc
+                            }
+                        }
+                    }
+
+                    public void kill() throws IOException, InterruptedException {
+                        p.kill();
+                    }
+
+                    public boolean isAlive() throws IOException, InterruptedException {
+                        return p.isAlive();
+                    }
+
+                    public Launcher.IOTriplet getIOtriplet() {
+                        Launcher.IOTriplet r = new Launcher.IOTriplet();
+                    /* TODO: we do not need reverse?
+                    if (reverseStdout)  r.stdout = new RemoteInputStream(p.getStdout());
+                    if (reverseStderr)  r.stderr = new RemoteInputStream(p.getStderr());
+                    if (reverseStdin)   r.stdin  = new RemoteOutputStream(p.getStdin());
+                     */
+                        return r;
+
+                    }
+                });
+            }
+
+            private static final long serialVersionUID = 1L;
+        }
+    }
 }
--- core/src/main/java/jenkins/model/logging/LoggingMethod.java
+++ core/src/main/java/jenkins/model/logging/LoggingMethod.java
@@ -5,9 +5,13 @@ import hudson.console.ConsoleLogFilter;
 import hudson.model.AbstractProject;
 import hudson.model.Node;
 import hudson.model.Run;
+import java.io.OutputStream;
 import java.io.Serializable;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import jenkins.model.Jenkins;
+import jenkins.model.logging.LoggingDefinitionLauncherWrapper.DefaultLocalLauncher;
+import jenkins.model.logging.LoggingDefinitionLauncherWrapper.DefaultRemoteLauncher;
 
 /**
  * Defines logging method for Jenkins runs.
@@ -16,32 +20,52 @@ import javax.annotation.Nonnull;
  * @since TODO
  */
 public abstract class LoggingMethod implements Serializable {
-  
-  /**
-   * Decorates logging on the Jenkins master side.
-   * These filters can be also used for log redirection and multi-reporting.
-   * @param build Build to be decorated
-   * @return Log filter on the master. {@code null} if no custom implementation
-   */
-  @CheckForNull
-  public abstract ConsoleLogFilter createLoggerDecorator(Run<?,?> build);
-  
-  /**
-   * Decorates external process launcher running on a node.
-   * @param original Original launcher
-   * @param run Run, for which the decoration should be performed
-   * @param node Target node. May be {@code master} as well
-   * @return Decorated launcher or {@code original} launcher
-   */
-  @Nonnull
-  public abstract Launcher decorateLauncher(@Nonnull Launcher original, 
-          @Nonnull Run<?,?> run, @Nonnull Node node); 
-  
-  /** 
-   * Fallback Logging methods for jobs, which do not define the implementation. 
-   */
-  public static final LoggingMethod NOOP = new NoopLoggingMethod();
-  private static class NoopLoggingMethod extends LoggingMethod {
+
+    /**
+     * Decorates logging on the Jenkins master side.
+     * These filters can be also used for log redirection and multi-reporting.
+     * @param build Build to be decorated
+     * @return Log filter on the master. {@code null} if no custom implementation
+     */
+    @CheckForNull
+    public abstract ConsoleLogFilter createLoggerDecorator(Run<?,?> build);
+
+    /**
+     * Decorates external process launcher running on a node.
+     * @param original Original launcher
+     * @param run Run, for which the decoration should be performed
+     * @param node Target node. May be {@code master} as well
+     * @return Decorated launcher or {@code original} launcher
+     */
+    @Nonnull
+    public Launcher decorateLauncher(@Nonnull Launcher original,
+        @Nonnull Run<?,?> run, @Nonnull Node node) {
+        if (node instanceof Jenkins) {
+            return new DefaultLocalLauncher(original);
+        } else {
+            return new DefaultRemoteLauncher(original, run, this);
+        }
+    }
+
+    /**
+     * Provides the output stream for given run.
+     * @param run Run
+     * @return
+     */
+    public abstract OutputStreamWrapper provideOutStream(Run run);
+
+    /**
+     * Provides the error stream for given run.
+     * @param run Run
+     * @return
+     */
+    public abstract OutputStreamWrapper provideErrStream(Run run);
+
+    /**
+     * Fallback Logging methods for jobs, which do not define the implementation.
+     */
+    public static final LoggingMethod NOOP = new NoopLoggingMethod();
+    private static class NoopLoggingMethod extends LoggingMethod {
         @Override
         public Launcher decorateLauncher(Launcher l, Run run, Node node) {
             return l;
@@ -51,12 +75,26 @@ public abstract class LoggingMethod implements Serializable {
         public ConsoleLogFilter createLoggerDecorator(Run<?, ?> build) {
             return null;
         }
-  } 
-  
-  /**
-   * Default logging method for {@link AbstractProject} classes
-   */
-  public static class DefaultAbstractBuildLoggingMethod extends NoopLoggingMethod {
-      // Default impl
-  } 
+
+        @Override
+        public OutputStreamWrapper provideOutStream(Run run) {
+            return null;
+        }
+
+        @Override
+        public OutputStreamWrapper provideErrStream(Run run) {
+            return null;
+        }
+    }
+
+    /**
+     * Default logging method for {@link AbstractProject} classes
+     */
+    public static class DefaultAbstractBuildLoggingMethod extends NoopLoggingMethod {
+        // Default impl
+    }
+
+    public static abstract class OutputStreamWrapper extends OutputStream implements Serializable {
+        public abstract Object readResolve();
+    }
 }
