2a5fcda8368c67a5363d0602cdcfcbd442d7611a
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-11-11 10:21:45 +0100

    [JENKINS-38721] - Fix typo

--- core/src/main/resources/jenkins/model/CauseOfInterruption/UserInterruption/summary.groovy
+++ core/src/main/resources/jenkins/model/CauseOfInterruption/UserInterruption/summary.groovy
@@ -5,5 +5,5 @@ def user = my.userOrNull
 if (user != null) {
   raw(_("blurb", user.fullName, rootURL+'/'+user.url))
 } else {
-  raw(_("userNotFound", user.userId))
+  raw(_("userNotFound", my.userId))
 }
