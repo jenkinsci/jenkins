3ba3a3220470107fd2dc0bb648bff6388783f5ab
Sam Van Oort <samvanoort@gmail.com>
2016-11-11 18:06:43 -0500

    Add entries for the new ysoserial tests and license headers

--- test/src/test/java/jenkins/security/Security218CliTest.java
+++ test/src/test/java/jenkins/security/Security218CliTest.java
@@ -45,7 +45,14 @@ public class Security218CliTest {
 
     @Rule
     public JenkinsRule r = new JenkinsRule();
-    
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeCommonsBeanutils1() throws Exception {
+        probe(Payload.CommonsBeanutils1, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
     @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
     @Test
     @Issue("SECURITY-218")
@@ -61,12 +68,75 @@ public class Security218CliTest {
         // in newer commons-collections version => remoting implementation should filter this class anyway
         probe(Payload.CommonsCollections2, PayloadCaller.EXIT_CODE_REJECTED);
     }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeCommonsCollections3() throws Exception {
+        probe(Payload.CommonsCollections3, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeCommonsCollections4() throws Exception {
+        probe(Payload.CommonsCollections4, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeCommonsCollections5() throws Exception {
+        probe(Payload.CommonsCollections5, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeCommonsCollections6() throws Exception {
+        probe(Payload.CommonsCollections6, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeFileUpload1() throws Exception {
+        probe(Payload.FileUpload1, PayloadCaller.EXIT_CODE_REJECTED);
+    }
     
     @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
     @Test
     @Issue("SECURITY-218")
     public void probeGroovy1() throws Exception {
         probe(Payload.Groovy1, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeJdk7u21() throws Exception {
+        probe(Payload.Jdk7u21, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeJRMPClient() throws Exception {
+        probe(Payload.JRMPClient, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeJRMPListener() throws Exception {
+        probe(Payload.JRMPListener, PayloadCaller.EXIT_CODE_REJECTED);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeJSON1() throws Exception {
+        probe(Payload.JSON1, PayloadCaller.EXIT_CODE_REJECTED);
     }
     
     //TODO: Fix the conversion layer (not urgent)
@@ -77,6 +147,13 @@ public class Security218CliTest {
     @Issue("SECURITY-218")
     public void probeSpring1() throws Exception {
         probe(Payload.Spring1, -1);
+    }
+
+    @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
+    @Test
+    @Issue("SECURITY-317")
+    public void probeSpring2() throws Exception {
+        probe(Payload.Spring2, -1);
     }
     
     private void probe(Payload payload, int expectedResultCode) throws Exception {
