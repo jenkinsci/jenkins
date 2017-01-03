e0036d4e843dda11edfb9947abae943bb89ea3a5
Benoît Marcelin <benoit.marcelin@stormshield.eu>
2017-01-03 13:39:32 +0100

    Add missing backtick in log message, reword the keepUndefinedParameters log

--- core/src/main/java/hudson/model/ParametersAction.java
+++ core/src/main/java/hudson/model/ParametersAction.java
@@ -329,7 +329,7 @@ public class ParametersAction implements RunAction2, Iterable<ParameterValue>, Q
             } else if ("false".equalsIgnoreCase(shouldKeepFlag)) {
                 LOGGER.log(Level.WARNING, "Skipped parameter `{0}` as it is undefined on `{1}`. Set `-D{2}=true` to allow "
                         + "undefined parameters to be injected as environment variables or `-D{3}=[comma-separated list]` to whitelist specific parameter names, "
-                        + "even though it represents a security breach or `-D{2}=false to explicitly forbid it without warning.",
+                        + "even though it represents a security breach or `-D{2}=false` to no longer show this message.",
                         new Object [] { v.getName(), run.getParent().getFullName(), KEEP_UNDEFINED_PARAMETERS_SYSTEM_PROPERTY_NAME, SAFE_PARAMETERS_SYSTEM_PROPERTY_NAME });
             }
         }
