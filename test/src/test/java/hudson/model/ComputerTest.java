3cd946cbef82c6da5ccccf3890d0ae4e091c4265
Jesse Glick <jglick@cloudbees.com>
2017-01-12 16:34:49 -0500

    Merge pull request #80 from jenkinsci-cert/SECURITY-362
    [SECURITY-362] Do not persist User in OfflineCause.UserCause

--- test/src/test/java/hudson/model/ComputerTest.java
+++ test/src/test/java/hudson/model/ComputerTest.java
@@ -23,16 +23,22 @@
  */
 package hudson.model;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
+
 
 import java.io.File;
 
+import com.gargoylesoftware.htmlunit.xml.XmlPage;
+import hudson.slaves.OfflineCause;
 import jenkins.model.Jenkins;
 import hudson.slaves.DumbSlave;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
 
 public class ComputerTest {
 
@@ -51,5 +57,28 @@ public class ComputerTest {
         assertFalse("Slave log directory should be deleted", logFile.getParentFile().exists());
 
         assertTrue("Slave log should be kept", keep.toComputer().getLogFile().exists());
+    }
+
+    @Test
+    public void doNotShowUserDetailsInOfflineCause() throws Exception {
+        DumbSlave slave = j.createOnlineSlave();
+        final Computer computer = slave.toComputer();
+        computer.setTemporarilyOffline(true, new OfflineCause.UserCause(User.get("username"), "msg"));
+        verifyOfflineCause(computer);
+    }
+
+    @Test @LocalData
+    public void removeUserDetailsFromOfflineCause() throws Exception {
+        Computer computer = j.jenkins.getComputer("deserialized");
+        verifyOfflineCause(computer);
+    }
+
+    private void verifyOfflineCause(Computer computer) throws Exception {
+        XmlPage page = j.createWebClient().goToXml("computer/" + computer.getName() + "/config.xml");
+        String content = page.getWebResponse().getContentAsString("UTF-8");
+        assertThat(content, containsString("temporaryOfflineCause"));
+        assertThat(content, containsString("<userId>username</userId>"));
+        assertThat(content, not(containsString("ApiTokenProperty")));
+        assertThat(content, not(containsString("apiToken")));
     }
 }
