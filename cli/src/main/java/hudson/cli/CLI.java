7d2d5f936de8e9f75881865ef6f86cbe5790dd6a
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2017-01-12 10:25:19 +0100

    Use the logger to output the error message, not System.out

--- cli/src/main/java/hudson/cli/CLI.java
+++ cli/src/main/java/hudson/cli/CLI.java
@@ -191,7 +191,7 @@ public class CLI implements AutoCloseable {
             String head = new BufferedReader(new StringReader(rsp.toString("ISO-8859-1"))).readLine();
             if (!(head.startsWith("HTTP/1.0 200 ") || head.startsWith("HTTP/1.1 200 "))) {
                 s.close();
-                System.out.println("Failed to tunnel the CLI port through the HTTP proxy. Falling back to HTTP.");
+                LOGGER.log(Level.SEVERE, "Failed to tunnel the CLI port through the HTTP proxy. Falling back to HTTP.");
                 throw new IOException("Failed to establish a connection through HTTP proxy: " + rsp);
             }
 
