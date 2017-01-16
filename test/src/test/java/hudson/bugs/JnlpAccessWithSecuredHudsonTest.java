dd3dc92df2dc8dd30a50d71b138b5a36aae5ea8e
Kohsuke Kawaguchi <kk@kohsuke.org>
2017-01-15 20:30:05 -0800

    Fixed a regression in 9fbcb99ded55c7cb9ca5a1c5349fe888bbbcdfe3
    The test was not updated to reflect the URL change. This is blocking a
    release.

--- test/src/test/java/hudson/bugs/JnlpAccessWithSecuredHudsonTest.java
+++ test/src/test/java/hudson/bugs/JnlpAccessWithSecuredHudsonTest.java
@@ -124,7 +124,7 @@ public class JnlpAccessWithSecuredHudsonTest extends HudsonTestCase {
                         fail("SECURITY-206: " + channel.call(new Attack(f.getAbsolutePath())));
                     } catch (SecurityException x) {
                         System.out.println("expected: " + x);
-                        assertTrue(x.getMessage().contains("http://jenkins-ci.org/security-144"));
+                        assertTrue(x.getMessage().contains("https://jenkins.io/redirect/security-144"));
                     }
                     return;
                 }
