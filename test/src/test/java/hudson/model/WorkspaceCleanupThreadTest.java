5db3efd1b54fe08b37ae25fcd2ce069aa04b736c
Josh Soref <jsoref@users.noreply.github.com>
2017-02-07 04:14:17 +0000

    spelling: hours

--- test/src/test/java/hudson/model/WorkspaceCleanupThreadTest.java
+++ test/src/test/java/hudson/model/WorkspaceCleanupThreadTest.java
@@ -149,7 +149,7 @@ public class WorkspaceCleanupThreadTest {
         assertFalse(ws.exists());
     }
 
-    @Test @WithoutJenkins public void reocurencePeriodIsInhours() {
+    @Test @WithoutJenkins public void reocurencePeriodIsInHours() {
         assertEquals(
                 WorkspaceCleanupThread.recurrencePeriodHours * 60 * 60 * 1000 ,
                 new WorkspaceCleanupThread().getRecurrencePeriod()
