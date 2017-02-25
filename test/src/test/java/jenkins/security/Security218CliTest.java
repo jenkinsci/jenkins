46d3f2e1d0bee7098e630d9c6913fe25bb2b3753
PJ Fanning <pjfanning@users.noreply.github.com>
2017-02-25 18:36:01 +0000

    [JENKINS-31598] upgrade commons-collections due to CVE against v3.2.1 (#2761)
    * [JENKINS-31598] upgrade commons-collections due to CVE against v3.2.1
    
    * Fix broken tests

--- test/src/test/java/jenkins/security/Security218CliTest.java
+++ test/src/test/java/jenkins/security/Security218CliTest.java
@@ -57,7 +57,7 @@ public class Security218CliTest {
     @Test
     @Issue("SECURITY-218")
     public void probeCommonsCollections1() throws Exception {
-        probe(Payload.CommonsCollections1, PayloadCaller.EXIT_CODE_REJECTED);
+        probe(Payload.CommonsCollections1, 1);
     }
     
     @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
@@ -73,7 +73,7 @@ public class Security218CliTest {
     @Test
     @Issue("SECURITY-317")
     public void probeCommonsCollections3() throws Exception {
-        probe(Payload.CommonsCollections3, PayloadCaller.EXIT_CODE_REJECTED);
+        probe(Payload.CommonsCollections3, 1);
     }
 
     @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
@@ -87,14 +87,14 @@ public class Security218CliTest {
     @Test
     @Issue("SECURITY-317")
     public void probeCommonsCollections5() throws Exception {
-        probe(Payload.CommonsCollections5, PayloadCaller.EXIT_CODE_REJECTED);
+        probe(Payload.CommonsCollections5, 1);
     }
 
     @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
     @Test
     @Issue("SECURITY-317")
     public void probeCommonsCollections6() throws Exception {
-        probe(Payload.CommonsCollections6, PayloadCaller.EXIT_CODE_REJECTED);
+        probe(Payload.CommonsCollections6, 1);
     }
 
     @PresetData(PresetData.DataSet.ANONYMOUS_READONLY)
