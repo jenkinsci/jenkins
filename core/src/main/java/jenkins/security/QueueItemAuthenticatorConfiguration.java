8199ec5ae4efbb3dbf0f57c6256c7efcfda445f8
Jesse Glick <jglick@cloudbees.com>
2017-05-19 02:17:17 -0400

    Queue.Item.authenticate honors QueueItemAuthenticatorProvider; Tasks.getAuthenticationOf should as well (#2880)
    * Queue.Item.authenticate honors QueueItemAuthenticatorProvider; Tasks.getAuthenticationOf should as well.
    
    * @oleg-nenashev suggested adding some Javadoc to QueueItemAuthenticatorConfiguration.getAuthenticators.

--- core/src/main/java/jenkins/security/QueueItemAuthenticatorConfiguration.java
+++ core/src/main/java/jenkins/security/QueueItemAuthenticatorConfiguration.java
@@ -1,6 +1,7 @@
 package jenkins.security;
 
 import hudson.Extension;
+import hudson.model.queue.Tasks;
 import hudson.util.DescribableList;
 import jenkins.model.GlobalConfiguration;
 import jenkins.model.GlobalConfigurationCategory;
@@ -38,6 +39,14 @@ public class QueueItemAuthenticatorConfiguration extends GlobalConfiguration {
         return GlobalConfigurationCategory.get(GlobalConfigurationCategory.Security.class);
     }
 
+    /**
+     * Provides all user-configured authenticators.
+     * Note that if you are looking to determine all <em>effective</em> authenticators,
+     * including any potentially supplied by plugins rather than user configuration,
+     * you should rather call {@link QueueItemAuthenticatorProvider#authenticators};
+     * or if you are looking for the authentication of an actual project, build, etc., use
+     * {@link hudson.model.Queue.Item#authenticate} or {@link Tasks#getAuthenticationOf}.
+     */
     public DescribableList<QueueItemAuthenticator, QueueItemAuthenticatorDescriptor> getAuthenticators() {
         return authenticators;
     }
