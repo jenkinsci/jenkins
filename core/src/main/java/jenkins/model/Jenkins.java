ce07ccbcd74cb9954c54137af8268e00629f7363
Andres Rodriguez <andresrc@gmail.com>
2017-06-01 12:14:04 +0200

    [JENKINS-44608] Don't check for monitor activation if it is disabled

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -288,6 +288,7 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static hudson.Util.*;
 import static hudson.init.InitMilestone.*;
@@ -2170,6 +2171,13 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
             if(m.id.equals(id))
                 return m;
         return null;
+    }
+
+    /**
+     * Returns the enabled and activated administrative monitors.
+     */
+    public List<AdministrativeMonitor> getActiveAdministrativeMonitors() {
+        return administrativeMonitors.stream().filter(m -> m.isEnabled() && m.isActivated()).collect(Collectors.toList());
     }
 
     public NodeDescriptor getDescriptor() {
