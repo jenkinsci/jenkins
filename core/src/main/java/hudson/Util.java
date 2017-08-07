16d10394525245eb1638974a310f1aaf52cba2ee
rsandell <rsandell@cloudbees.com>
2017-08-07 15:19:38 +0200

    [JENKINS-44657] isDescendant doesn't throw IOException any more

--- core/src/main/java/hudson/Util.java
+++ core/src/main/java/hudson/Util.java
@@ -572,10 +572,9 @@ public class Util {
      * @param forParent the parent the child should be a descendant of
      * @param potentialChild the path to check
      * @return true if so
-     * @throws IOException potentially from {@link Files#isSameFile(Path, Path)}
      * @since FIXME
      */
-    public static boolean isDescendant(File forParent, File potentialChild) throws IOException {
+    public static boolean isDescendant(File forParent, File potentialChild) {
         Path child = potentialChild.getAbsoluteFile().toPath().normalize();
         Path parent = forParent.getAbsoluteFile().toPath().normalize();
         return child.startsWith(parent);
