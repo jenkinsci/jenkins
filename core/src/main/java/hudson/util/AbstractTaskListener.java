9ed6b01317424f3f441022b3a2f23fbbc5ae1543
Jesse Glick <jglick@cloudbees.com>
2017-10-31 19:06:32 -0400

    Implement default methods in TaskListener and BuildListener.

--- core/src/main/java/hudson/util/AbstractTaskListener.java
+++ core/src/main/java/hudson/util/AbstractTaskListener.java
@@ -1,17 +1,11 @@
 package hudson.util;
 
-import hudson.console.HyperlinkNote;
 import hudson.model.TaskListener;
 
-import java.io.IOException;
 
 /**
- * Partial default implementation of {@link TaskListener}
- * @author Kohsuke Kawaguchi
+ * @deprecated implement {@link TaskListener} directly
  */
+@Deprecated
 public abstract class AbstractTaskListener implements TaskListener {
-    public void hyperlink(String url, String text) throws IOException {
-        annotate(new HyperlinkNote(url,text.length()));
-        getLogger().print(text);
-    }
 }
--- core/src/main/java/hudson/util/LogTaskListener.java
+++ core/src/main/java/hudson/util/LogTaskListener.java
@@ -27,11 +27,10 @@ package hudson.util;
 import hudson.console.ConsoleNote;
 import hudson.model.TaskListener;
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -39,38 +38,27 @@ import java.util.logging.Logger;
 /**
  * {@link TaskListener} which sends messages to a {@link Logger}.
  */
-public class LogTaskListener extends AbstractTaskListener implements Serializable {
-    
+public class LogTaskListener implements TaskListener, Closeable {
+
+    // would be simpler to delegate to the LogOutputStream but this would incompatibly change the serial form
     private final TaskListener delegate;
 
     public LogTaskListener(Logger logger, Level level) {
         delegate = new StreamTaskListener(new LogOutputStream(logger, level, new Throwable().getStackTrace()[1]));
     }
 
+    @Override
     public PrintStream getLogger() {
         return delegate.getLogger();
     }
 
-    public PrintWriter error(String msg) {
-        return delegate.error(msg);
-    }
-
-    public PrintWriter error(String format, Object... args) {
-        return delegate.error(format, args);
-    }
-
-    public PrintWriter fatalError(String msg) {
-        return delegate.fatalError(msg);
-    }
-
-    public PrintWriter fatalError(String format, Object... args) {
-        return delegate.fatalError(format, args);
-    }
-
+    @Override
+    @SuppressWarnings("rawtypes")
     public void annotate(ConsoleNote ann) {
         // no annotation support
     }
 
+    @Override
     public void close() {
         delegate.getLogger().close();
     }
@@ -82,12 +70,13 @@ public class LogTaskListener extends AbstractTaskListener implements Serializabl
         private final StackTraceElement caller;
         private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        public LogOutputStream(Logger logger, Level level, StackTraceElement caller) {
+        LogOutputStream(Logger logger, Level level, StackTraceElement caller) {
             this.logger = logger;
             this.level = level;
             this.caller = caller;
         }
 
+        @Override
         public void write(int b) throws IOException {
             if (b == '\r' || b == '\n') {
                 flush();
