0e7106bd2ababba338ce2b0d726ed4dffa4388b4
Wadeck Follonier <wadeck.follonier@gmail.com>
2017-11-03 16:00:28 +0100

    - remove whitespace

--- test/src/test/java/hudson/security/SecurityRealmTest.java
+++ test/src/test/java/hudson/security/SecurityRealmTest.java
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package hudson.security;
 
 import com.gargoylesoftware.htmlunit.WebResponse;
