13c8d3b32702f218d49bfe188caa93e7cdb4c3c2
Wadeck Follonier <wadeck.follonier@gmail.com>
2017-11-03 17:00:22 +0100

    - second edit...

--- test/src/test/java/hudson/security/SecurityRealmTest.java
+++ test/src/test/java/hudson/security/SecurityRealmTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2017, CloudBees, Inc. and others
+ * Copyright (c) 2017, CloudBees, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
