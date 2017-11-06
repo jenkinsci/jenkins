d5705eb8889a9b76912a941c4dd6dd8d6f1d104c
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2017-11-06 10:59:32 +0100

    [JENKINS-47819] Redirection with protocol change

--- core/src/main/java/hudson/tools/ZipExtractionInstaller.java
+++ core/src/main/java/hudson/tools/ZipExtractionInstaller.java
@@ -100,7 +100,17 @@ public class ZipExtractionInstaller extends ToolInstaller {
                 URLConnection conn = ProxyConfiguration.open(new URL(value));
                 conn.connect();
                 if (conn instanceof HttpURLConnection) {
-                    if (((HttpURLConnection) conn).getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    // JENKINS-47819
+                    // A redirection from http to https (or vise versa) returns a 302 response status. Force redirection
+                    int responseCode = ((HttpURLConnection)conn).getResponseCode();
+                    if(HttpURLConnection.HTTP_MOVED_PERM == responseCode || HttpURLConnection.HTTP_MOVED_TEMP == responseCode || HttpURLConnection.HTTP_SEE_OTHER == responseCode) {
+                        // In case of redirection, we have to connect to the new URL
+                        String redirection = ((HttpURLConnection) conn).getHeaderField("Location");
+                        conn = ProxyConfiguration.open(new URL(redirection));
+                        responseCode = ((HttpURLConnection) conn).getResponseCode();
+                    }
+                    // JENKINS-47819
+                    if (responseCode != HttpURLConnection.HTTP_OK) {
                         return FormValidation.error(Messages.ZipExtractionInstaller_bad_connection());
                     }
                 }
