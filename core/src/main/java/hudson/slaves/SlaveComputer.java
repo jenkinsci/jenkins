1b9c3002cd3f7acc8286ac5aae35e3d76f23689c
surenpi <zxjlwt@126.com>
2017-12-26 15:57:26 +0800

    export slave remove fs for json api

--- core/src/main/java/hudson/slaves/SlaveComputer.java
+++ core/src/main/java/hudson/slaves/SlaveComputer.java
@@ -66,6 +66,7 @@ import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 import org.kohsuke.stapler.WebMethod;
+import org.kohsuke.stapler.export.Exported;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.annotation.CheckForNull;
@@ -469,6 +470,7 @@ public class SlaveComputer extends Computer {
      * @since 1.606
      */
     @CheckForNull
+    @Exported
     public String getAbsoluteRemoteFs() {
         return channel == null ? null : absoluteRemoteFs;
     }
