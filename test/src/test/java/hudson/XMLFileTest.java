b9c2148d6e53ef4c7f0f01fab591b0333b4c69f1
mike cirioli <mcirioli@cloudbees.com>
2018-01-12 08:55:34 -0500

    [JENKINS-48463] Added a unit test that validates jenkins xml v1.0 configs are silently migrated to xml v1.1 as they are persisted

--- test/src/test/java/hudson/XMLFileTest.java
+++ test/src/test/java/hudson/XMLFileTest.java
@@ -1,5 +1,8 @@
 package hudson;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -19,5 +22,24 @@ public class XMLFileTest {
 
         assertThat(j.jenkins.getLabelString(),is("LESS_TERMCAP_mb=\u001B[01;31m"));
 
+    }
+
+    /**
+     *
+     * This test validates that xml v1.0 configs silently get migrated to xml v1.1 when they are persisted
+     *
+     */
+    @Test
+    @LocalData
+    public void silentlyMigrateConfigsTest() throws Exception {
+        j.jenkins.save();
+        // verify that we did indeed load our test config.xml
+        assertThat(j.jenkins.getLabelString(), is("LESS_TERMCAP_mb=\u001B[01;31m"));
+        //verify that the persisted top level config.xml is v1.1
+        File configFile = new File(j.jenkins.getRootPath().getRemote() + File.separator + "config.xml");
+        assertThat(configFile.exists(), is(true));
+        BufferedReader config = new BufferedReader(new FileReader(configFile));
+        assertThat(config.readLine(), is("<?xml version='1.1' encoding='UTF-8'?>"));
+        config.close();
     }
 }
