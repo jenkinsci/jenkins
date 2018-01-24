aa26f4b986525e002cde65fb14ab74d49818f766
Daniel Beck <daniel-beck@users.noreply.github.com>
2018-01-24 11:31:09 +0100

    [JENKINS-25910] Sort Jenkins master at beginning

--- core/src/main/java/hudson/model/Label.java
+++ core/src/main/java/hudson/model/Label.java
@@ -200,7 +200,7 @@ public abstract class Label extends Actionable implements Comparable<Label>, Mod
     private static class NodeSorter implements Comparator<Node> {
         @Override
         public int compare(Node o1, Node o2) {
-            return o1.getNodeName().compareTo(o2.getNodeName());
+            return o1 == Jenkins.get() ? -1 : (o2 == Jenkins.get() ? 1 : o1.getNodeName().compareTo(o2.getNodeName()));
         }
     }
 
