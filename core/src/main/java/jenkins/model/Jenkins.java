fa9326c6527ed2967531e5ad2af77cb9da6dbbb4
Baptiste Mathus <batmat@batmat.net>
2018-03-22 16:35:44 +0100

    workspaceDir & buildDir not modifiable anymore from the UI!

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -3659,9 +3659,6 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
 
             JSONObject json = req.getSubmittedForm();
 
-            workspaceDir = json.getString("rawWorkspaceDir");
-            buildsDir = json.getString("rawBuildsDir");
-
             systemMessage = Util.nullify(req.getParameter("system_message"));
 
             boolean result = true;
