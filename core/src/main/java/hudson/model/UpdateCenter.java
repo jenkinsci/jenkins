2d21aeb14b9698f4e3a5debc7a85263768616c6f
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-06-18 16:38:39 +0200

    [JENKINS-52010] - Switch Java 10 support build to the experimental update center by default

--- core/src/main/java/hudson/model/UpdateCenter.java
+++ core/src/main/java/hudson/model/UpdateCenter.java
@@ -148,7 +148,7 @@ import org.kohsuke.stapler.interceptor.RequirePOST;
 @ExportedBean
 public class UpdateCenter extends AbstractModelObject implements Saveable, OnMaster {
 
-    private static final String UPDATE_CENTER_URL = SystemProperties.getString(UpdateCenter.class.getName()+".updateCenterUrl","https://updates.jenkins.io/");
+    private static final String UPDATE_CENTER_URL = SystemProperties.getString(UpdateCenter.class.getName()+".updateCenterUrl","https://updates.jenkins.io/experimental/");
 
     /**
      * Read timeout when downloading plugins, defaults to 1 minute
