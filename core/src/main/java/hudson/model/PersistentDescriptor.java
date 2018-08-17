c728546c05daac63fdba0aec8bad2d588dd490c1
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-08-17 19:14:53 +0200

    typo
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- core/src/main/java/hudson/model/PersistentDescriptor.java
+++ core/src/main/java/hudson/model/PersistentDescriptor.java
@@ -5,7 +5,7 @@ import javax.annotation.PostConstruct;
 /**
  * Marker interface for Descriptors which use xml persistent data, and as such need to load from disk when instantiated.
  * <p>
- * {@link Descriptor#load()} method is annotated as {@link PostConstruct} so it get autmatically invoked after
+ * {@link Descriptor#load()} method is annotated as {@link PostConstruct} so it get automatically invoked after
  * constructor and field injection.
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
