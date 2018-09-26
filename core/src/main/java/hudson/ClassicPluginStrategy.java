297e4e3d85fa53496209f2acecf16d7a67616668
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-09-27 00:31:15 +0200

    [JENKINS-53793] - Suppress DMI_COLLECTION_OF_URLS in ClassicPluginStrategy
    It starts failing FindBugs runs against Java 11.
    The implementation is not efficient, but it should not be a serious issue in this case

--- core/src/main/java/hudson/ClassicPluginStrategy.java
+++ core/src/main/java/hudson/ClassicPluginStrategy.java
@@ -31,6 +31,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jenkins.util.AntWithFindResourceClassLoader;
 import jenkins.util.SystemProperties;
 import com.google.common.collect.Lists;
@@ -798,6 +799,8 @@ public class ClassicPluginStrategy implements PluginStrategy {
         }
 
         @Override
+        @SuppressFBWarnings(value = "DMI_COLLECTION_OF_URLS",
+                            justification = "Should not produce network overheads since the URL is local. JENKINS-53793 is a follow-up")
         protected Enumeration<URL> findResources(String name) throws IOException {
             HashSet<URL> result = new HashSet<URL>();
 
