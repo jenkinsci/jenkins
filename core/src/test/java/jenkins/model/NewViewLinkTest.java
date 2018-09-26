d714a622eafebe983618df0facf3745eab549230
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-09-27 01:13:32 +0200

    [JENKINS-53693] - Ignore java.xml components in core tests.
    It is require to make the tests passing on Java 11, but apparently it also speeds up tests on Java 8.

--- core/src/test/java/jenkins/model/NewViewLinkTest.java
+++ core/src/test/java/jenkins/model/NewViewLinkTest.java
@@ -15,11 +15,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({NewViewLink.class, Jenkins.class})
+@PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*"})
 public class NewViewLinkTest {
 	
     @Mock
