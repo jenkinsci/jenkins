da8aaa71fa43993f7860f2c8b54c8aa2bf627e3b
Kohsuke Kawaguchi <kk@kohsuke.org>
2018-09-28 10:16:26 -0700

    All Describables should get implicit DataModel

--- core/src/main/java/jenkins/data/DefaultDataModelFactoryImpl.java
+++ core/src/main/java/jenkins/data/DefaultDataModelFactoryImpl.java
@@ -2,6 +2,9 @@ package jenkins.data;
 
 import hudson.Extension;
 import hudson.ExtensionList;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
+import jenkins.model.Jenkins;
 import org.jvnet.tiger_types.Types;
 
 import java.lang.reflect.Type;
@@ -18,10 +21,20 @@ public class DefaultDataModelFactoryImpl implements DataModelFactory {
     @Override
     public DataModel find(Type type) {
         Class<Object> c = Types.erasure(type);
+
+        // if a custom data model is defined for the given type, go for it
         for (DataModel dm : dataModels) {
             if (dm.getType().equals(c))
                 return dm;
         }
+
+        // Describables have databinding annotatoins, so this should work
+        // TODO: see the notes about how the type should declare its intent to participate in Data API
+        // we don't know if Describable or not is the right thing to test
+        if (Describable.class.isAssignableFrom(c)) {
+            return DataModel.byReflection(c);
+        }
+
         return null;
     }
 }
