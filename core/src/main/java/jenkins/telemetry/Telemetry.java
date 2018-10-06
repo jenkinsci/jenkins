c7e731d92356c78b503ac9b60aa3f8d642894abd
Daniel Beck <daniel-beck@users.noreply.github.com>
2018-10-06 07:08:42 +0200

    Minor telemetry improvements
    (cherry picked from commit aeaa4d68603c3b60a4e53fbaa86d65ecd7e3a218)

--- core/src/main/java/jenkins/telemetry/Telemetry.java
+++ core/src/main/java/jenkins/telemetry/Telemetry.java
@@ -54,7 +54,12 @@ import java.util.logging.Logger;
 /**
  * Extension point for collecting JEP-214 telemetry.
  *
- * @see <a href="https://github.com/jenkinsci/jep/tree/master/jep/214">JEP-214</a>
+ * Implementations should provide a <code>description.jelly</code> file with additional details about their purpose and
+ * behavior which will be included in <code>help-usageStatisticsCollected.jelly</code> for {@link UsageStatistics}.
+ *
+ * @see <a href="https://jenkins.io/jep/214">JEP-214</a>
+ *
+ * @since 2.143
  */
 @Restricted(NoExternalUse.class)
 public abstract class Telemetry implements ExtensionPoint {
@@ -67,7 +72,7 @@ public abstract class Telemetry implements ExtensionPoint {
     private static final Logger LOGGER = Logger.getLogger(Telemetry.class.getName());
 
     /**
-     * ID of this collector, typically a basic alphanumeric string (and _- characters).
+     * ID of this collector, typically an alphanumeric string (and punctuation).
      *
      * Good IDs are globally unique and human readable (i.e. no UUIDs).
      *
@@ -76,7 +81,9 @@ public abstract class Telemetry implements ExtensionPoint {
      * @return ID of the collector, never null or empty
      */
     @Nonnull
-    public abstract String getId();
+    public String getId() {
+        return getClass().getName();
+    }
 
     /**
      * User friendly display name for this telemetry collector, ideally localized.
@@ -111,7 +118,7 @@ public abstract class Telemetry implements ExtensionPoint {
      *
      * This method is called periodically, once per content submission.
      *
-     * @return
+     * @return The JSON payload
      */
     @Nonnull
     public abstract JSONObject createContent();
