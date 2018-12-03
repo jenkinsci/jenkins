578b6226118296dfdfba3c8e71faf0cc0f2bfab0
Wadeck Follonier <Wadeck@users.noreply.github.com>
2018-12-03 09:47:27 +0100

    Second ignore for flaky test

--- core/src/test/java/hudson/UtilTest.java
+++ core/src/test/java/hudson/UtilTest.java
@@ -409,6 +409,7 @@ public class UtilTest {
     }
 
     @Test
+    @Ignore("Frequently flaky test")
     public void testDeleteRecursive_onWindows() throws Exception {
         Assume.assumeTrue(Functions.isWindows());
         final File dir = tmp.newFolder();
