a78312ea0f4ba28f8bc81220c6d7c0dfa72a67df
Baptiste Mathus <bmathus@gmail.com>
2019-02-03 10:56:26 +0100

    [JENKINS-55681] - Make JAXB a detached plugin only on Java 11 (#3865)
    * Move detached plugins logic into a dedicated class
    
    ClassicPluginStrategy is already long enough, so trying to make things a
    bit more focused before adding Java 11 logic. (and even if not, the change
    still splits a class that is almost 1k lines).
    
    * Install jaxb as detached only on Java 11+
    
    * Remove duplicate @Restricted
    
    Given the enclosing class already has it.
    
    * Use 1.5 release
    
    * Add more test coverage
    
    * getMinJavaVersion => getMinimumJavaVersion
    
    * Rename DetachedPluginsManager as DetachedPluginsUtil
    
    as requested by Oleg and Jesse. Mainly because of the misleading
    relationship (there's no inheritance) with the existing `PluginManager`
    class.
    
    * :facepalm: adjust to 1.5 release method renaming
    
    * Clarify minimum Java version column
    
    Co-Authored-By: batmat <bmathus@gmail.com>
    
    * Update to 2.3.0 version of the jaxb plugin
    
    * Bump minimum core version to next expected release version
    
    * Add preventive cycle breaks for jaxb while waiting for JENKINS-28942
    
    As requested by Oleg.
    
    * Add test to fail fast on wrong format for split-plugins.txt

---
+++ core/src/test/java/jenkins/plugins/DetachedPluginsUtilTest.java
@@ -0,0 +1,70 @@
+package jenkins.plugins;
+
+import hudson.util.VersionNumber;
+import jenkins.util.java.JavaUtils;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DetachedPluginsUtilTest {
+    @Test
+    public void checkJaxb() {
+        final List<DetachedPluginsUtil.DetachedPlugin> plugins =
+                DetachedPluginsUtil.DETACHED_LIST.stream()
+                        .filter(plugin -> plugin.getShortName().equals("jaxb"))
+                        .collect(Collectors.toList());
+        assertEquals(1, plugins.size());
+
+        DetachedPluginsUtil.DetachedPlugin jaxb = plugins.get(0);
+
+        assertEquals(new VersionNumber("11"), jaxb.getMinimumJavaVersion());
+
+        final List<DetachedPluginsUtil.DetachedPlugin> detachedPlugins = DetachedPluginsUtil.getDetachedPlugins();
+        if (JavaUtils.isRunningWithJava8OrBelow()) {
+            assertEquals(0, detachedPlugins.stream()
+                    .filter(plugin -> plugin.getShortName().equals("jaxb"))
+                    .collect(Collectors.toList()).size());
+        } else if (JavaUtils.getCurrentJavaRuntimeVersionNumber().isNewerThanOrEqualTo(new VersionNumber("11.0.2"))) {
+            assertEquals(1, detachedPlugins.stream()
+                    .filter(plugin -> plugin.getShortName().equals("jaxb"))
+                    .collect(Collectors.toList()).size());
+
+            final List<DetachedPluginsUtil.DetachedPlugin> detachedPluginsSince2_161 =
+                    DetachedPluginsUtil.getDetachedPlugins(new VersionNumber("2.161"));
+
+            assertEquals(1, detachedPluginsSince2_161.size());
+            assertEquals("jaxb", detachedPluginsSince2_161.get(0).getShortName());
+        }
+    }
+
+    /**
+     * Checks the format of the <code>/jenkins/split-plugins.txt</code> file has maximum 4 columns.
+     */
+    @Test
+    public void checkSplitPluginsFileFormat() throws IOException {
+        final List<String> splitPluginsLines = IOUtils.readLines(getClass().getResourceAsStream("/jenkins/split-plugins.txt"));
+        assertTrue(!splitPluginsLines.isEmpty());
+
+        // File is not only comments
+        final List<String> linesWithoutComments = splitPluginsLines.stream()
+                .filter(line -> !line.startsWith("#")).collect(Collectors.toList());
+        assertFalse( "weird, split-plugins.txt only has comments?", linesWithoutComments.isEmpty());
+
+        //
+        assertFalse("no whitespaces only lines allowed" ,linesWithoutComments.stream()
+                            .filter(line -> line.trim().isEmpty())
+                            .anyMatch(line -> !line.isEmpty()));
+
+
+        assertTrue( "max 4 columns is supported", linesWithoutComments.stream()
+                           .map(line -> line.split(" "))
+                           .noneMatch(line -> line.length > 4));
+    }
+}
