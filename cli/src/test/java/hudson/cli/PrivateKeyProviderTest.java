06ab879463319efae5a0553ba9bcc9eaa7e2309f
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-02-19 11:20:33 +0100

    Update PrivateKeyProviderTest.java

--- cli/src/test/java/hudson/cli/PrivateKeyProviderTest.java
+++ cli/src/test/java/hudson/cli/PrivateKeyProviderTest.java
@@ -11,8 +11,14 @@ import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.assertNotNull;
 
+/**
+keys were generated with ssh-keygen from OpenSSH_7.9p1, LibreSSL 2.7.3
+*/
 public class PrivateKeyProviderTest {
 
+    /**
+    key command: ssh-keygen -f dsa -t dsa -b 1024 -m PEM
+    */
     @Test
     public void loadKeyDSA() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("dsa").getFile());
@@ -23,6 +29,9 @@ public class PrivateKeyProviderTest {
         assertNotNull(keyPair.getPublic());
     }
 
+    /**
+    key command: ssh-keygen -f dsa-password -t dsa -b 1024 -m PEM -p password
+    */
     @Test
     public void loadKeyDSAPassword() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("dsa-password").getFile());
@@ -33,6 +42,9 @@ public class PrivateKeyProviderTest {
         assertNotNull(keyPair.getPublic());
     }
     
+    /**
+    key command: ssh-keygen -f rsa -t rsa -b 1024 -m PEM
+    */
     @Test
     public void loadKeyRSA() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("rsa").getFile());
@@ -43,6 +55,9 @@ public class PrivateKeyProviderTest {
         assertNotNull(keyPair.getPublic());
     }
 
+    /**
+    key command: ssh-keygen -f rsa-password -t rsa -b 1024 -m PEM -p password
+    */
     @Test
     public void loadKeyRSAPassword() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("rsa-password").getFile());
@@ -52,7 +67,10 @@ public class PrivateKeyProviderTest {
         assertNotNull(keyPair.getPrivate());
         assertNotNull(keyPair.getPublic());
     }
-
+    
+    /**
+    key command: ssh-keygen -f openssh -t rsa -b 1024
+    */
     @Test
     public void loadKeyOpenSSH() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("openssh").getFile());
@@ -62,7 +80,10 @@ public class PrivateKeyProviderTest {
         assertNotNull(keyPair.getPrivate());
         assertNotNull(keyPair.getPublic());
     }
-
+    
+    /**
+    key command: ssh-keygen -f openssh-unsupported -t rsa -b 1024 -p password
+    */
     @Test(expected = NoSuchAlgorithmException.class)
     public void loadKeyUnsupportedCipher() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("openssh-unsuported").getFile());
@@ -70,6 +91,10 @@ public class PrivateKeyProviderTest {
         PrivateKeyProvider.loadKey(file, password);
     }
 
+    /**
+    key command: ssh-keygen -f openssh -t rsa -b 1024
+    in this key we remove some lines to break the key.
+    */    
     @Test(expected = IllegalArgumentException.class)
     public void loadKeyBroken() throws IOException, GeneralSecurityException {
         File file = new File(this.getClass().getResource("openssh-broken").getFile());
