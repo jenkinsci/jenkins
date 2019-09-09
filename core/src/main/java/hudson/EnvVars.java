05598c6bc3816a13654d9529f89e5cd7a34734d1
Karthick Prabu Ponnusamy <Karthick.Ponnusamy@viacomcontractor.com>
2019-09-09 11:05:48 +0530

    Updated EnvVars to add a method that extends putAll functionality

--- core/src/main/java/hudson/EnvVars.java
+++ core/src/main/java/hudson/EnvVars.java
@@ -388,6 +388,14 @@ public class EnvVars extends TreeMap<String,String> {
         if (value!=null)
             put(key,value);
     }
+
+    /**
+     * Add entire map but filter null values out.
+     */
+    public void putAllExceptNull(Map<String, String> map) {
+        map.forEach(this::putIfNotNull);
+    }
+
     
     /**
      * Takes a string that looks like "a=b" and adds that to this map.
