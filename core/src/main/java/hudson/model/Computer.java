66c17a08e07a4f36aca88f4a117fbfdbe1b27a5f
Oliver Gondža <ogondza@gmail.com>
2019-09-23 11:42:58 +0200

    [JENKINS-59283] Refine the icons semantics

--- core/src/main/java/hudson/model/Computer.java
+++ core/src/main/java/hudson/model/Computer.java
@@ -731,12 +731,11 @@ public /*transient*/ abstract class Computer extends Actionable implements Acces
      */
     @Exported
     public String getIcon() {
-        if(getChannel() == null)
-            return "computer-x.png";
-        else if (isTemporarilyOffline())
-            return "computer-u.png";
-        else
-            return "computer.png";
+        // The machine was taken offline by someone
+        if (isTemporarilyOffline() && getOfflineCause() instanceof OfflineCause.UserCause) return "computer-user-offline.png";
+        // There is a "technical" reason the computer will not accept new builds
+        if (isOffline() || !isAcceptingTasks()) return "computer-x.png";
+        return "computer.png";
     }
 
     /**
@@ -752,21 +751,19 @@ public /*transient*/ abstract class Computer extends Actionable implements Acces
      */
     @Exported
     public String getIconClassName() {
-        if(getChannel() == null)
-            return "icon-computer-x";
-        else if (isTemporarilyOffline())
-            return "icon-computer-u";
-        else
-            return "icon-computer";
+        // The machine was taken offline by someone
+        if (isTemporarilyOffline() && getOfflineCause() instanceof OfflineCause.UserCause) return "icon-computer-user-offline";
+        // There is a "technical" reason the computer will not accept new builds
+        if (isOffline() || !isAcceptingTasks()) return "icon-computer-x";
+        return "icon-computer";
     }
 
     public String getIconAltText() {
-        if(getChannel() == null)
-            return "[offline]";
-        else if (isTemporarilyOffline())
-            return "[temporarily offline]";
-        else
-            return "[online]";
+        // The machine was taken offline by someone
+        if (isTemporarilyOffline() && getOfflineCause() instanceof OfflineCause.UserCause) return "[temporarily offline by user]";
+        // There is a "technical" reason the computer will not accept new builds
+        if (isOffline() || !isAcceptingTasks()) return "[offline]";
+        return "[online]";
     }
 
     @Exported
@@ -1783,9 +1780,9 @@ public /*transient*/ abstract class Computer extends Actionable implements Acces
     private static final Logger LOGGER = Logger.getLogger(Computer.class.getName());
 
     static {
-        IconSet.icons.addIcon(new Icon("icon-computer-u icon-sm", "16x16/computer-u.png", Icon.ICON_SMALL_STYLE));
-        IconSet.icons.addIcon(new Icon("icon-computer-u icon-md", "24x24/computer-u.png", Icon.ICON_MEDIUM_STYLE));
-        IconSet.icons.addIcon(new Icon("icon-computer-u icon-lg", "32x32/computer-u.png", Icon.ICON_LARGE_STYLE));
-        IconSet.icons.addIcon(new Icon("icon-computer-u icon-xlg", "48x48/computer-u.png", Icon.ICON_XLARGE_STYLE));
+        IconSet.icons.addIcon(new Icon("icon-computer-user-offline icon-sm", "16x16/computer-user-offline.png", Icon.ICON_SMALL_STYLE));
+        IconSet.icons.addIcon(new Icon("icon-computer-user-offline icon-md", "24x24/computer-user-offline.png", Icon.ICON_MEDIUM_STYLE));
+        IconSet.icons.addIcon(new Icon("icon-computer-user-offline icon-lg", "32x32/computer-user-offline.png", Icon.ICON_LARGE_STYLE));
+        IconSet.icons.addIcon(new Icon("icon-computer-user-offline icon-xlg", "48x48/computer-user-offline.png", Icon.ICON_XLARGE_STYLE));
     }
 }
