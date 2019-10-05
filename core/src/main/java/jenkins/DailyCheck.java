2bbc99c6a395c102d4387d8ea16c74ad7b0faa4a
Daniel Beck <daniel-beck@users.noreply.github.com>
2019-10-05 16:34:43 +0200

    License header and note of origin for DailyCheck

--- core/src/main/java/jenkins/DailyCheck.java
+++ core/src/main/java/jenkins/DailyCheck.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Jesse Glick.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package jenkins;
 
 import hudson.Extension;
@@ -20,6 +43,7 @@ import java.util.logging.Logger;
 @Extension
 @Restricted(NoExternalUse.class)
 @Symbol("updateCenterCheck")
+// Moved from DownloadSettings.DailyCheck when the option to download in the browser was removed
 public final class DailyCheck extends AsyncPeriodicWork {
     private static final Logger LOGGER = Logger.getLogger(DailyCheck.class.getName());
 
