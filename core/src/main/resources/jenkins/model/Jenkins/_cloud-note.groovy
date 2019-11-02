3e0890dcd7897fa13a9ad9fd7f26fd6857892026
Daniel Beck <daniel-beck@users.noreply.github.com>
2019-11-02 12:54:01 +0100

    Move cloud configuration out of Configure System

---
+++ core/src/main/resources/jenkins/model/Jenkins/_cloud-note.groovy
@@ -0,0 +1,17 @@
+package jenkins.model.Jenkins
+
+import hudson.slaves.Cloud
+
+// TODO remove this once it's been long enough that users got used to this.
+
+def f = namespace(lib.FormTagLib)
+
+def clouds = Cloud.all()
+
+if (!clouds.isEmpty()) {
+    f.block {
+        div(class: 'alert alert-info') {
+            raw(_("note", rootURL))
+        }
+    }
+}
