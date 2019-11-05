e09abecb9a942c2ee539a7179bdb1d13f046a3c3
Jesse Glick <jglick@cloudbees.com>
2019-11-04 19:56:05 -0500

    Pluggable handlers.

--- core/src/main/java/jenkins/WebSocketEndpoint.java
+++ core/src/main/java/jenkins/WebSocketEndpoint.java
@@ -25,9 +25,13 @@
 package jenkins;
 
 import hudson.Extension;
+import hudson.ExtensionList;
+import hudson.ExtensionPoint;
 import hudson.model.InvisibleAction;
 import hudson.model.UnprotectedRootAction;
 import java.lang.reflect.Proxy;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Future;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -56,15 +60,37 @@ public final class WebSocketEndpoint extends InvisibleAction implements Unprotec
             webSocketServletFactoryClass.getMethod("start").invoke(factory);
             Class<?> webSocketCreatorClass = cl.loadClass("org.eclipse.jetty.websocket.servlet.WebSocketCreator");
             webSocketServletFactoryClass.getMethod("setCreator", webSocketCreatorClass).invoke(factory, Proxy.newProxyInstance(cl, new Class<?>[] {webSocketCreatorClass}, (proxy1, method1, args1) -> {
+                Object servletUpgradeRequest = args1[0];
+                String requestPath = (String) servletUpgradeRequest.getClass().getMethod("getRequestPath").invoke(servletUpgradeRequest);
+                assert requestPath.startsWith("/ws/");
+                Session session = ExtensionList.lookup(Service.class).stream().filter(s -> requestPath.substring(4).equals(s.name())).findFirst().get().start();
                 return Proxy.newProxyInstance(cl, new Class<?>[] {cl.loadClass("org.eclipse.jetty.websocket.api.WebSocketListener")}, (proxy2, method2, args2) -> {
-                    System.err.println("TODO running " + method2.getName());
-                    return null;
+                    switch (method2.getName()) {
+                    case "onWebSocketConnect":
+                        session.remoteEndpoint = args2[0].getClass().getMethod("getRemote").invoke(args2[0]);
+                        return null;
+                    case "onWebSocketClose":
+                        session.closed((Integer) args2[0], (String) args2[1]);
+                        return null;
+                    case "onWebSocketError":
+                        session.error((Throwable) args2[0]);
+                        return null;
+                    case "onWebSocketBinary":
+                        session.binary((byte[]) args2[0], (Integer) args2[1], (Integer) args2[2]);
+                        return null;
+                    case "onWebSocketText":
+                        session.text((String) args2[0]);
+                        return null;
+                    default:
+                        throw new AssertionError();
+                    }
                 });
             }));
         }
     }
 
     public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws Exception {
+        // TODO check rest of path against registered Service.name
         init();
         if (!((Boolean) webSocketServletFactoryClass.getMethod("isUpgradeRequest", HttpServletRequest.class, HttpServletResponse.class).invoke(factory, req, rsp))) {
             throw HttpResponses.error(HttpServletResponse.SC_BAD_REQUEST, "only WS connections accepted here");
@@ -73,6 +99,53 @@ public final class WebSocketEndpoint extends InvisibleAction implements Unprotec
             throw HttpResponses.error(HttpServletResponse.SC_BAD_REQUEST, "did not manage to upgrade");
         }
         // OK!
+    }
+
+    public interface Service extends ExtensionPoint {
+        String name();
+        Session start();
+    }
+
+    public static abstract class Session {
+        Object remoteEndpoint;
+        protected void closed(int statusCode, String reason) {}
+        protected void error(Throwable cause) {}
+        protected void binary(byte[] payload, int offset, int len) {}
+        protected void text(String message) {}
+        @SuppressWarnings("unchecked")
+        protected final Future<Void> sendBinary(ByteBuffer data) {
+            try {
+                return (Future<Void>) remoteEndpoint.getClass().getMethod("sendBytesByFuture", ByteBuffer.class).invoke(remoteEndpoint, data);
+            } catch (Exception x) {
+                throw new RuntimeException(x);
+            }
+        }
+        @SuppressWarnings("unchecked")
+        protected final Future<Void> sendText(String text) {
+            try {
+                return (Future<Void>) remoteEndpoint.getClass().getMethod("sendStringByFuture", String.class).invoke(remoteEndpoint, text);
+            } catch (Exception x) {
+                throw new RuntimeException(x);
+            }
+        }
+        // TODO ping/pong
+    }
+
+    @Extension
+    public static final class Example implements Service {
+        @Override
+        public String name() {
+            return "example";
+        }
+        @Override
+        public Session start() {
+            return new Session() {
+                @Override
+                protected void text(String message) {
+                    sendText("hello " + message);
+                }
+            };
+        }
     }
 
 }
