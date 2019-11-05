e6f5ce0224ed52f35cc0a86d44a9848fdbee280e
Esther Alvarez Feijoo <ealvarez@cloudbees.com>
2019-11-05 14:29:10 +0100

    Don't allow CONFIGURE_JENKINS to install tools from zip in nodes

--- core/src/main/java/hudson/tools/ZipExtractionInstaller.java
+++ core/src/main/java/hudson/tools/ZipExtractionInstaller.java
@@ -99,7 +99,7 @@ public class ZipExtractionInstaller extends ToolInstaller {
 
         @RequirePOST
         public FormValidation doCheckUrl(@QueryParameter String value) {
-            Jenkins.get().checkPermission(Jenkins.CONFIGURE_JENKINS);
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
             
             try {
                 URLConnection conn = ProxyConfiguration.open(new URL(value));
