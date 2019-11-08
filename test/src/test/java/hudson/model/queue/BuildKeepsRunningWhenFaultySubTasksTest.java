5b4799ae1e0edef2a3f660905c5ac8240022eea1
M Ramon Leon <manuelramonleonjimenez@gmail.com>
2019-11-08 13:42:26 +0100

    [FNDJEN-59793] Change test name and avoid extra assertions

--- test/src/test/java/hudson/model/queue/BuildKeepsRunningWhenFaultySubTasksTest.java
+++ test/src/test/java/hudson/model/queue/BuildKeepsRunningWhenFaultySubTasksTest.java
@@ -1,7 +1,6 @@
 package hudson.model.queue;
 
 import hudson.model.AbstractProject;
-import hudson.model.Executor;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Label;
@@ -12,39 +11,32 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.LoggerRule;
 import org.jvnet.hudson.test.TestExtension;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class BuildKeepsRunningWhenFaultySubTasksTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
-    
-    @Rule
-    public LoggerRule logs = new LoggerRule().record(Executor.class.getName(), Level.SEVERE).capture(100); //just in case future versions add more logs
 
     public static final String ERROR_MESSAGE = "My unexpected exception";
     
     // When using SubTaskContributor (FailingSubTaskContributor) the build never ends
     @Test
     @Issue("JENKINS-59793")
-    public void buildDoesntFinishWhenSubTaskFails() throws Exception {
+    public void buildFinishesWhenSubTaskFails() throws Exception {
         FreeStyleProject p = j.createProject(FreeStyleProject.class);
         QueueTaskFuture<FreeStyleBuild> future = p.scheduleBuild2(0);
         assertThat("Build should be actually scheduled by Jenkins", future, notNullValue());
 
-        // We get stalled waiting the finalization of the job
-        FreeStyleBuild build = future.get(5, TimeUnit.SECONDS);
-        assertTrue("The message is printed in the log", logs.getRecords().stream().anyMatch(r -> r.getThrown().getMessage().equals(ERROR_MESSAGE)));
+        // We don't get stalled waiting the finalization of the job
+        future.get(5, TimeUnit.SECONDS);
     }
     
     // A SubTask failing with an exception
