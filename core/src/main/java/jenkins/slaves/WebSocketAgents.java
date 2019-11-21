61a8f11dac6a6334886095537880387a05e1d70e
Jesse Glick <jglick@cloudbees.com>
2019-11-20 19:11:55 -0500

    Reworked protocol to negotiate remote capabilities.

--- core/src/main/java/jenkins/slaves/WebSocketAgents.java
+++ core/src/main/java/jenkins/slaves/WebSocketAgents.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package jenkins.slaves;
 
 import hudson.Extension;
@@ -33,18 +34,18 @@ import hudson.remoting.Channel;
 import hudson.remoting.ChannelBuilder;
 import hudson.slaves.JNLPLauncher;
 import hudson.slaves.SlaveComputer;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import jenkins.WebSockets;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.HttpResponse;
-import org.kohsuke.stapler.HttpResponses;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
 
 @Extension
 public final class WebSocketAgents extends InvisibleAction implements UnprotectedRootAction {
@@ -56,59 +57,71 @@ public final class WebSocketAgents extends InvisibleAction implements Unprotecte
         return "wsagents";
     }
 
-    public HttpResponse doDynamic(StaplerRequest req, StaplerResponse rsp) {
-        String agent = req.getRestOfPath().substring(1);
-        Computer c = Jenkins.get().getComputer(agent);
-        if (!(c instanceof SlaveComputer)) {
-            throw HttpResponses.notFound();
-        }
-        SlaveComputer sc = (SlaveComputer) c;
-        if (!(sc.getLauncher() instanceof JNLPLauncher)) {
-            throw HttpResponses.errorWithoutStack(400, "not an inbound agent");
-        }
-        String secret = req.getParameter("secret");
-        if (!MessageDigest.isEqual(secret.getBytes(StandardCharsets.US_ASCII), sc.getJnlpMac().getBytes(StandardCharsets.US_ASCII))) {
-            throw HttpResponses.forbidden(); // TODO unless anonymous has CONNECT?
-        }
-        LOGGER.fine(() -> "connecting " + agent);
+    public HttpResponse doIndex() {
+        LOGGER.fine("connecting");
         return WebSockets.upgrade(new WebSockets.Session() {
+            String agent;
+            String secret;
+            Capability remoteCapability;
             AbstractByteArrayCommandTransport.ByteArrayReceiver receiver;
+            // Expect to receive agent, then secret, then remoteCapability; then will send a capability; then channel is started.
             @Override
-            protected void opened() {
-                Computer.threadPoolForRemoting.submit(() -> {
-                    sc.setChannel(new ChannelBuilder(agent, Computer.threadPoolForRemoting).withHeaderStream(sc.openLogFile()), new AbstractByteArrayCommandTransport() {
-                        @Override
-                        public void setup(AbstractByteArrayCommandTransport.ByteArrayReceiver bar) {
-                            receiver = bar;
+            protected void text(String message) {
+                if (agent == null) {
+                    agent = message;
+                } else if (secret == null) {
+                    secret = message;
+                } else {
+                    LOGGER.warning("unexpected text frame");
+                }
+            }
+            @Override
+            protected void binary(byte[] payload, int offset, int len) {
+                if (remoteCapability == null) {
+                    if (agent == null || secret == null) {
+                        LOGGER.warning("unexpected binary frame");
+                        return; // TODO close connection
+                    }
+                    try (ByteArrayInputStream bais = new ByteArrayInputStream(payload, offset, len)) {
+                        remoteCapability = Capability.read(bais);
+                        LOGGER.fine(() -> "received " + remoteCapability);
+                    } catch (IOException x) {
+                        LOGGER.log(Level.WARNING, "could not read remote capability", x);
+                        return; // TODO close connection
+                    }
+                    Computer c = Jenkins.get().getComputer(agent);
+                    if (!(c instanceof SlaveComputer)) {
+                        LOGGER.warning("no such agent " + agent);
+                        return; // TODO close connection
+                    }
+                    SlaveComputer sc = (SlaveComputer) c;
+                    if (!(sc.getLauncher() instanceof JNLPLauncher)) {
+                        LOGGER.warning(agent + " is not inbound");
+                        return; // TODO close connection
+                    }
+                    if (!MessageDigest.isEqual(secret.getBytes(StandardCharsets.US_ASCII), sc.getJnlpMac().getBytes(StandardCharsets.US_ASCII))) { // TODO unless anonymous has CONNECT?
+                        LOGGER.warning("incorrect secret");
+                        return; // TODO close connection
+                    }
+                    Computer.threadPoolForRemoting.submit(() -> {
+                        LOGGER.fine(() -> "sending capabilities for " + agent);
+                        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                            new Capability().write(baos);
+                            sendBinary(ByteBuffer.wrap(baos.toByteArray()));
                         }
-                        @Override
-                        public void writeBlock(Channel chnl, byte[] bytes) throws IOException {
-                            LOGGER.finest(() -> "writing block of length " + bytes.length + " to " + agent);
-                            try {
-                                sendBinary(ByteBuffer.wrap(bytes)).get();
-                            } catch (Exception x) {
-                                x.printStackTrace();
-                                throw new IOException(x);
-                            }
-                        }
-                        @Override
-                        public Capability getRemoteCapability() throws IOException {
-                            return new Capability(); // TODO figure out how to negotiate
-                        }
-                        @Override
-                        public void closeWrite() throws IOException {
-                            LOGGER.finest(() -> "closeWrite");
-                            // TODO
-                        }
-                        @Override
-                        public void closeRead() throws IOException {
-                            LOGGER.finest(() -> "closeRead");
-                            // TODO
-                        }
-                    }, null);
-                    LOGGER.fine(() -> "set up channel for " + agent);
-                    return null;
-                });
+                        LOGGER.fine(() -> "setting up channel for " + agent);
+                        sc.setChannel(new ChannelBuilder(agent, Computer.threadPoolForRemoting).withHeaderStream(sc.openLogFile()), new Transport(), null);
+                        LOGGER.fine(() -> "set up channel for " + agent);
+                        return null;
+                    });
+                } else {
+                    LOGGER.finest(() -> "reading block of length " + len + " from " + agent);
+                    if (offset == 0 && len == payload.length) {
+                        receiver.handle(payload);
+                    } else {
+                        receiver.handle(Arrays.copyOfRange(payload, offset, offset + len));
+                    }
+                }
             }
             @Override
             protected void closed(int statusCode, String reason) {
@@ -116,17 +129,42 @@ public final class WebSocketAgents extends InvisibleAction implements Unprotecte
                 // TODO
             }
             @Override
-            protected void binary(byte[] payload, int offset, int len) {
-                LOGGER.finest(() -> "reading block of length " + len + " from " + agent);
-                if (offset == 0 && len == payload.length) {
-                    receiver.handle(payload);
-                } else {
-                    receiver.handle(Arrays.copyOfRange(payload, offset, offset + len));
-                }
+            protected void error(Throwable cause) {
+                LOGGER.log(Level.WARNING, null, cause);
             }
             @Override
             protected boolean keepAlive() {
                 return true; // Remoting ping thread may be too slow
+            }
+            class Transport extends AbstractByteArrayCommandTransport {
+                @Override
+                public void setup(AbstractByteArrayCommandTransport.ByteArrayReceiver bar) {
+                    receiver = bar;
+                }
+                @Override
+                public void writeBlock(Channel chnl, byte[] bytes) throws IOException {
+                    LOGGER.finest(() -> "writing block of length " + bytes.length + " to " + agent);
+                    try {
+                        sendBinary(ByteBuffer.wrap(bytes)).get();
+                    } catch (Exception x) {
+                        x.printStackTrace();
+                        throw new IOException(x);
+                    }
+                }
+                @Override
+                public Capability getRemoteCapability() throws IOException {
+                    return remoteCapability;
+                }
+                @Override
+                public void closeWrite() throws IOException {
+                    LOGGER.finest(() -> "closeWrite");
+                    // TODO
+                }
+                @Override
+                public void closeRead() throws IOException {
+                    LOGGER.finest(() -> "closeRead");
+                    // TODO
+                }
             }
         });
     }
