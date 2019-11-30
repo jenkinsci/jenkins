30f9a0f23735a14f4a5ab9b3d54f0e84454c8564
Stefan Spieker <S.Spieker@gmx.net>
2019-11-30 14:39:37 +0100

    added missing import

--- core/src/main/java/hudson/Functions.java
+++ core/src/main/java/hudson/Functions.java
@@ -85,6 +85,7 @@ import hudson.views.ViewsTabBar;
 import hudson.widgets.RenderOnDemandClosure;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
