3204382ce1d06044ac332b5c9e12dae801f2f8c2
aHenryJard <ajard@cloudbees.com>
2019-12-05 15:27:06 +0100

    Some changes regarding feedback on CONFIGURE.

--- core/src/main/java/hudson/logging/LogRecorderManager.java
+++ core/src/main/java/hudson/logging/LogRecorderManager.java
@@ -206,7 +206,7 @@ public class LogRecorderManager extends AbstractModelObject implements ModelObje
     @Restricted(NoExternalUse.class)
     public Object getTarget() {
         if (!SKIP_PERMISSION_CHECK) {
-            Jenkins.get().checkPermission(Jenkins.CONFIGURE);
+            Jenkins.get().checkPermission(Jenkins.ADMINISTER);
         }
         return this;
     }
