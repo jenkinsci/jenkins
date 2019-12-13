e8e8bd4db8cc0c7c246e1a6f0fe41ef96fee4437
mike cirioli <mcirioli@cloudbees.com>
2019-12-13 09:30:48 -0500

    [JENKINS-60266] revert back to require Jenkins.ADMINISTER when viewing logs

--- core/src/main/java/jenkins/management/SystemLogLink.java
+++ core/src/main/java/jenkins/management/SystemLogLink.java
@@ -59,6 +59,6 @@ public class SystemLogLink extends ManagementLink {
 
     @Override
     public Permission getRequiredPermission() {
-        return Jenkins.CONFIGURE;
+        return Jenkins.ADMINISTER;
     }
 }
