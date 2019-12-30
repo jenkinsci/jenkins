27292910d218d648c8e13bd01ef3ea677899bbb9
Josh Soref <jsoref@users.noreply.github.com>
2019-12-30 16:12:23 -0500

    Deprecation: write()

--- core/src/test/java/hudson/UtilTest.java
+++ core/src/test/java/hudson/UtilTest.java
@@ -40,6 +40,7 @@ import org.jvnet.hudson.test.Issue;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermissions;
@@ -559,7 +560,7 @@ public class UtilTest {
         File aa = new File(a, "aa");
         aa.mkdirs();
         File aaTxt = new File(aa, "aa.txt");
-        FileUtils.write(aaTxt, "aa");
+        FileUtils.write(aaTxt, "aa", StandardCharsets.US_ASCII, false);
 
         File b = new File(root, "b");
         b.mkdir();
