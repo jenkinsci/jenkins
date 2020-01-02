d7caf24a2e177414598c4286eca81092cb55fd74
Raihaan Shouhell <raihaan.shouhell@autodesk.com>
2020-01-02 11:04:48 +0800

    Fix import

--- test/src/test/java/hudson/tasks/FingerprinterTest.java
+++ test/src/test/java/hudson/tasks/FingerprinterTest.java
@@ -62,6 +62,7 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
