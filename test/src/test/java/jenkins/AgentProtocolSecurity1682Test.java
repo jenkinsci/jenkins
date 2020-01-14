5054bc6e12e1022993d719f66e289ab1d22ae854
Jeff Thompson <jeffret.g@gmail.com>
2020-01-14 12:16:59 +0100

    [SECURITY-1682]

--- test/src/test/java/jenkins/AgentProtocolSecurity1682Test.java
+++ test/src/test/java/jenkins/AgentProtocolSecurity1682Test.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2017 CloudBees, Inc.
+ * Copyright (c) 2020 CloudBees, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,71 +23,54 @@
  */
 package jenkins;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import javax.annotation.CheckForNull;
-import jenkins.install.SetupWizardTest;
 import jenkins.model.Jenkins;
 import jenkins.slaves.DeprecatedAgentProtocolMonitor;
-import org.apache.commons.collections.ListUtils;
+import jenkins.slaves.JnlpSlaveAgentProtocol3;
 import org.apache.commons.lang.StringUtils;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-import org.hamcrest.core.StringContains;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
+import javax.annotation.CheckForNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 /**
  * Tests for {@link AgentProtocol}.
  * 
  * @author Oleg Nenashev
  */
-public class AgentProtocolTest {
+public class AgentProtocolSecurity1682Test {
     
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
-    //TODO: Test is unstable on CI due to the race condition, needs to be reworked
-    /**
-     * Checks that Jenkins does not disable agent protocols by default after the upgrade.
-     * 
-     * @throws Exception Test failure
-     * @see SetupWizardTest#shouldDisableUnencryptedProtocolsByDefault() 
-     */
-    @Test
-    @Ignore
-    @LocalData
-    @Issue("JENKINS-45841")
-    public void testShouldNotDisableProtocolsForMigratedInstances() throws Exception {
-        assertProtocols(true, "Legacy Non-encrypted JNLP/CLI protocols should be enabled", 
-                "JNLP-connect", "JNLP2-connect", "JNLP4-connect", "CLI-connect");
-        assertProtocols(true, "Default encrypted protocols should be enabled", "JNLP4-connect", "CLI2-connect");
-        assertProtocols(true, "Protocol should be enabled due to CLI settings", "CLI2-connect");
-        assertProtocols(false, "JNLP3-connect protocol should be disabled by default", "JNLP3-connect");
-        assertMonitorTriggered("JNLP-connect", "JNLP2-connect", "CLI-connect");
+    @BeforeClass
+    public static void setupClass() {
+        System.setProperty(JnlpSlaveAgentProtocol3.ALLOW_UNSAFE, "true");
     }
 
     @Test
     @LocalData
-    @Issue("JENKINS-45841")
-    public void testShouldNotOverrideUserConfiguration() throws Exception {
-        assertEnabled("JNLP-connect");
+    @Issue("SECURITY-1682")
+    public void testShouldReallyEnable3() {
+        assertEnabled("JNLP-connect", "JNLP3-connect");
         assertDisabled("JNLP2-connect", "JNLP4-connect");
         assertProtocols(true, "System protocols should be always enabled", "Ping");
-        assertMonitorTriggered("JNLP-connect");
+        assertMonitorTriggered("JNLP-connect", "JNLP3-connect");
     }
-    
+
     private void assertEnabled(String ... protocolNames) throws AssertionError {
         assertProtocols(true, null, protocolNames);    
     }
