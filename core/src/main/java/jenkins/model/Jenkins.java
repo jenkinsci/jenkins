354ee66c8257cae8b6363ef8dc80f12a8d1595c5
Jeff Thompson <jeffret.g@gmail.com>
2020-01-23 15:56:26 -0700

    Don't use public static boolean property.

--- core/src/main/java/jenkins/model/Jenkins.java
+++ core/src/main/java/jenkins/model/Jenkins.java
@@ -5174,10 +5174,10 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
         }
     }
 
-    @Restricted(Beta.class)
+    @Restricted(NoExternalUse.class)
     public boolean shouldShowStackTrace() {
         // Used by oops.jelly
-        return SHOW_STACK_TRACE;
+        return Boolean.getBoolean(Jenkins.class.getName() + ".SHOW_STACK_TRACE");
     }
 
     /**
@@ -5266,9 +5266,6 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
      */
     static final String WORKSPACES_DIR_PROP = Jenkins.class.getName() + ".workspacesDir";
 
-
-    @Restricted(Beta.class)
-    public static boolean SHOW_STACK_TRACE = Boolean.getBoolean(Jenkins.class.getName() + ".SHOW_STACK_TRACE");
 
     /**
      * Automatically try to launch an agent when Jenkins is initialized or a new agent computer is created.
