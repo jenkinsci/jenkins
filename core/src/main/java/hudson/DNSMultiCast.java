746d004f66202f1623df1e667a40092fa824d090
Jeff Thompson <jeffret.g@gmail.com>
2020-01-29 15:12:41 -0700

    Refine clean up with DNSMultiCast.
    Doesn't need to be referenced in the other pom any more.
    We need to keep the class and the property around for a while.
    Jenkins Test Harness sets the property. Until we can get all
    versions aligned so it isn't needed, it's best to keep the property
    doing nothing.

---
+++ core/src/main/java/hudson/DNSMultiCast.java
@@ -0,0 +1,20 @@
+package hudson;
+
+import jenkins.util.SystemProperties;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * Registers a DNS multi-cast service-discovery support.
+ *
+ * @author Kohsuke Kawaguchi
+ *
+ */
+public class DNSMultiCast implements Closeable {
+    public static boolean disabled = SystemProperties.getBoolean(DNSMultiCast.class.getName()+".disabled", true);
+
+    @Override
+    public void close() throws IOException {
+    }
+}
