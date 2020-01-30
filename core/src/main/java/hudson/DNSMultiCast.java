c7afc8a982668fa39866ddb4fcefb841cbd3455d
Jeff Thompson <37345299+jeffret-b@users.noreply.github.com>
2020-01-30 15:33:56 -0700

    Improve code comments for external usages.
    Co-Authored-By: Jesse Glick <jglick@cloudbees.com>

--- core/src/main/java/hudson/DNSMultiCast.java
+++ core/src/main/java/hudson/DNSMultiCast.java
@@ -9,10 +9,11 @@ import java.io.IOException;
  * Registers a DNS multi-cast service-discovery support.
  *
  * @author Kohsuke Kawaguchi
- * @deprecated
+ * @deprecated No longer does anything.
  */
 @Deprecated
 public class DNSMultiCast implements Closeable {
+    /** retained for compatibility with {@code jenkins-test-harness} and {@code jenkinsfile-runner} */
     public static boolean disabled = SystemProperties.getBoolean(DNSMultiCast.class.getName()+".disabled", true);
 
     @Override
