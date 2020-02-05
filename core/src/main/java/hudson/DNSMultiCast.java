8fec924c913644eccfdf85765771f1f82fbfa751
Jeff Thompson <jeffret.g@gmail.com>
2020-02-05 11:13:49 -0700

    Add log warning messages.

--- core/src/main/java/hudson/DNSMultiCast.java
+++ core/src/main/java/hudson/DNSMultiCast.java
@@ -1,9 +1,15 @@
 package hudson;
 
+import hudson.init.Initializer;
 import jenkins.util.SystemProperties;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.logging.Logger;
+
+import static hudson.init.InitMilestone.COMPLETED;
 
 /**
  * Registers a DNS multi-cast service-discovery support.
@@ -12,8 +18,18 @@ import java.io.IOException;
  * @deprecated
  */
 @Deprecated
+@Restricted(NoExternalUse.class)
 public class DNSMultiCast implements Closeable {
+    private static final Logger logger = Logger.getLogger(DNSMultiCast.class.getName());
+
     public static boolean disabled = SystemProperties.getBoolean(DNSMultiCast.class.getName()+".disabled", true);
+
+    @Initializer(before=COMPLETED)
+    public void warn() {
+        if (!disabled) {
+            logger.warning("DNS multicast capability has been removed from Jenkins.");
+        }
+    }
 
     @Override
     public void close() throws IOException {
