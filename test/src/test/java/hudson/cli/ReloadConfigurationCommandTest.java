78561c11542e692c71442264b2f94ea7e04a0263
Esther Alvarez Feijoo <ealvarez@cloudbees.com>
2020-02-07 17:00:25 +0100

    Fix tests affected by MANAGE permission

--- test/src/test/java/hudson/cli/ReloadConfigurationCommandTest.java
+++ test/src/test/java/hudson/cli/ReloadConfigurationCommandTest.java
@@ -76,7 +76,7 @@ public class ReloadConfigurationCommandTest {
 
         assertThat(result, failedWith(6));
         assertThat(result, hasNoStandardOutput());
-        assertThat(result.stderr(), containsString("user is missing the Overall/Administer permission"));
+        assertThat(result.stderr(), containsString("user is missing the Overall/Manage permission"));
     }
 
     @Test
@@ -86,7 +86,7 @@ public class ReloadConfigurationCommandTest {
 
         assertThat(result, failedWith(6));
         assertThat(result, hasNoStandardOutput());
-        assertThat(result.stderr(), containsString("user is missing the Overall/Administer permission"));
+        assertThat(result.stderr(), containsString("user is missing the Overall/Manage permission"));
     }
 
     @Test
