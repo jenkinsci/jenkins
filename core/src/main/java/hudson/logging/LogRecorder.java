32a72e1ee72b37b599239ef32766ca59691867c6
Tim Jacomb <timjacomb1+github@gmail.com>
2020-03-03 12:48:08 +0000

    Update LogRecorder.java

--- core/src/main/java/hudson/logging/LogRecorder.java
+++ core/src/main/java/hudson/logging/LogRecorder.java
@@ -407,6 +407,8 @@ public class LogRecorder extends AbstractModelObject implements Saveable {
      * RSS feed for log entries.
      */
     public void doRss( StaplerRequest req, StaplerResponse rsp ) throws IOException, ServletException {
+        Jenkins.get().checkPermission(Jenkins.ADMINISTER);
+        
         LogRecorderManager.doRss(req,rsp,getLogRecords());
     }
 
