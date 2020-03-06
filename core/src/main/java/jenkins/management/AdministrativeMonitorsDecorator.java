fcede3f4ee037b6bf3b4449be4cb5e6fc4fc5168
Tim Jacomb <timjacomb1+github@gmail.com>
2020-03-06 09:00:04 +0000

    Show admin monitor alert when SYSTEM_READ

--- core/src/main/java/jenkins/management/AdministrativeMonitorsDecorator.java
+++ core/src/main/java/jenkins/management/AdministrativeMonitorsDecorator.java
@@ -95,7 +95,7 @@ public class AdministrativeMonitorsDecorator extends PageDecorator {
      * @throws ServletException
      */
     public boolean shouldDisplay() throws IOException, ServletException {
-        if (!Functions.hasPermission(Jenkins.ADMINISTER)) {
+        if (!Functions.hasPermission(Jenkins.SYSTEM_READ)) {
             return false;
         }
 
