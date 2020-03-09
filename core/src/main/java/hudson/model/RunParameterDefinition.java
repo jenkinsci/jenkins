9eed9da2664aab9a0ff8da40749d78299e8fcebc
andrei <neaguandrei47@gmail.com>
2020-03-09 19:57:24 +0100

    no star imports

--- core/src/main/java/hudson/model/RunParameterDefinition.java
+++ core/src/main/java/hudson/model/RunParameterDefinition.java
@@ -28,11 +28,15 @@ import jenkins.model.Jenkins;
 import net.sf.json.JSONObject;
 
 import org.jenkinsci.Symbol;
-import org.kohsuke.stapler.*;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.export.Exported;
 import hudson.Extension;
 import hudson.util.EnumConverter;
 import hudson.util.RunList;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.QueryParameter;
 
 public class RunParameterDefinition extends SimpleParameterDefinition {
 
