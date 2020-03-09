be11a021b56b959b970c445647cd004d17d206c3
Andrei Neagu <neaguandrei47@gmail.com>
2020-03-09 15:58:45 +0100

    Test try 1

--- test/src/test/java/hudson/model/AutoCompletionCandidatesTest.java
+++ test/src/test/java/hudson/model/AutoCompletionCandidatesTest.java
@@ -60,6 +60,39 @@ public class AutoCompletionCandidatesTest {
         assertContains(c, "../bar", "../foo");
     }
 
+    @Test
+    public void completion2() throws Exception {
+        FreeStyleProject foo = j.createFreeStyleProject("foo");
+        MatrixProject bar = j.jenkins.createProject(MatrixProject.class, "bar");
+        bar.setAxes(new AxisList(new TextAxis("x","1","2","3")));
+        MatrixConfiguration x3 = bar.getItem("x=3");
+
+        AutoCompletionCandidates c;
+
+        c = AutoCompletionCandidates.ofJobNames("", j.jenkins);
+        assertContains(c, "foo", "bar");
+
+        c = AutoCompletionCandidates.ofJobNames("ba", j.jenkins);
+        assertContains(c, "bar");
+
+        c = AutoCompletionCandidates.ofJobNames("bar/", j.jenkins);
+        assertContains(c, "bar/x=1", "bar/x=2", "bar/x=3");
+
+        c = AutoCompletionCandidates.ofJobNames("", x3.getParent());
+        assertContains(c, "x=1", "x=2", "x=3");
+
+        c = AutoCompletionCandidates.ofJobNames("/", x3.getParent());
+        assertContains(c, "/foo", "/bar");
+
+        c = AutoCompletionCandidates.ofJobNames("/bar/", x3.getParent());
+        assertContains(c, "/bar/x=1", "/bar/x=2", "/bar/x=3");
+
+        // relative path
+        c = AutoCompletionCandidates.ofJobNames("../", x3.getParent());
+        assertContains(c, "../bar", "../foo");
+    }
+
+
     private void assertContains(AutoCompletionCandidates c, String... values) {
         assertEquals(new TreeSet<String>(Arrays.asList(values)), new TreeSet<String>(c.getValues()));
     }
