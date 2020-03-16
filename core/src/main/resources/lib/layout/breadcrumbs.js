6f3c326ed6fac5888bf603744840991427bc5b3f
Daniel Beck <daniel-beck@users.noreply.github.com>
2020-03-16 13:27:08 +0100

    Add section titles to Manage Jenkins context menu

--- core/src/main/resources/lib/layout/breadcrumbs.js
+++ core/src/main/resources/lib/layout/breadcrumbs.js
@@ -203,7 +203,11 @@ var breadcrumbs = (function() {
                 onComplete:function (x) {
                     var a = x.responseText.evalJSON().items;
                     function fillMenuItem(e) {
-                        e.text = makeMenuHtml(e.icon, e.displayName);
+                        if (e.header) {
+                            e.text = makeMenuHtml(e.icon, "<span class='header'>" + e.displayName + "</span>");
+                        } else {
+                            e.text = makeMenuHtml(e.icon, e.displayName);
+                        }
                         if (e.subMenu!=null)
                             e.subMenu = {id:"submenu"+(iota++), itemdata:e.subMenu.items.each(fillMenuItem)};
                         if (e.requiresConfirmation) {
