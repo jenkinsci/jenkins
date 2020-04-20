a54823769a8ee966099f586d3ef419075d235d77
Daniel Beck <daniel-beck@users.noreply.github.com>
2020-04-20 19:42:18 +0200

    Fix Available tab sort order and support popularity outside 0..1 range

--- core/src/main/java/hudson/PluginManager.java
+++ core/src/main/java/hudson/PluginManager.java
@@ -136,6 +136,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ServiceLoader;
 import java.util.Set;
@@ -2240,7 +2241,7 @@ public abstract class PluginManager extends AbstractModelObject implements OnMas
 
     @Restricted(DoNotUse.class)
     public String unscientific(double d) {
-        return String.format("%.4f", d);
+        return String.format(Locale.US, "%15.4f", d);
     }
 
     @Override
