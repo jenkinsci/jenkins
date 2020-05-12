9fe3ea1704ef72a05bab9824a4deacf3eeeae944
Basil Crow <me@basilcrow.com>
2020-05-12 09:11:58 -0700

    Fix merge conflict

--- test/src/test/java/lib/form/PasswordTest.java
+++ test/src/test/java/lib/form/PasswordTest.java
@@ -59,8 +59,10 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Rule;
