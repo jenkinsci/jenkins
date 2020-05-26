0e36d012d865fe2171adaa9e6cfb8fed5e5a9963
Aytunc BEKEN <aytunc@authenteq.com>
2020-05-26 16:03:23 +0200

    JENKINS-62419 is fixed

--- core/src/main/java/hudson/Functions.java
+++ core/src/main/java/hudson/Functions.java
@@ -2224,4 +2224,14 @@ public class Functions {
             return true;
         }
     }
+
+    /**
+     * Get Locale Display Language depending on the Request's Locale
+     * @param request @{@link HttpServletRequest}
+     * @return @{@link String} Locale Display Language
+     */
+    public static String getLocale(HttpServletRequest request){
+        return request.getLocale().getDisplayLanguage(request.getLocale());
+    }
+
 }
