6e7e212252235b83e49a63a08a61247eee3a24f0
Aytunc BEKEN <aytunc@authenteq.com>
2020-05-26 16:29:06 +0200

    JENKINS-62419 Merge from master

--- core/src/main/java/hudson/Functions.java
+++ core/src/main/java/hudson/Functions.java
@@ -2301,4 +2301,14 @@ public class Functions {
             return true;
         }
     }
+
+    /**
+     * Get Locale Display Language depending on the Request's Locale
+     * @param request @{@link HttpServletRequest}
+     * @return @{@link String} Locale Display Language
+     */
+    public static String getLocale(HttpServletRequest request){
+        return request.getLocale().getDisplayLanguage(request.getLocale());
+    }
+
 }
