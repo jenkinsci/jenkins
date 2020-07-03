e31fdc3483e51b8f8a952d668ca66e747e28bef1
Singh, Abhinav Kumar <abhinavkumar.singh@kronos.com>
2020-07-03 09:10:39 +0530

    escape special character | adding test case

--- test/src/test/java/hudson/console/HyperlinkNoteTest.java
+++ test/src/test/java/hudson/console/HyperlinkNoteTest.java
@@ -90,6 +90,7 @@ public class HyperlinkNoteTest {
         r.waitUntilNoActivity();
         HtmlPage rsp = r.createWebClient().goTo(b.getUrl()+"console");
         assertThat(rsp.querySelector(".console-output").asText(), containsString("Triggering a new build of"));
+		assertThat(String.valueOf(rsp.getAnchorByText("d0wnstr3'am").click().getWebResponse().getStatusCode()), containsString("200"));
     }
 
     
