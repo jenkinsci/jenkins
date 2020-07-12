f017c04d2e45cb8fe2d534f12d6980e1455bdd62
Stefan Spieker <S.Spieker@gmx.net>
2020-07-12 16:34:04 +0200

    minor refactorings either for performance or readability

--- test/src/test/java/jenkins/security/apitoken/ApiTokenStatsTest.java
+++ test/src/test/java/jenkins/security/apitoken/ApiTokenStatsTest.java
@@ -40,7 +40,7 @@ import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.JenkinsRule.WebClient;
 
 import java.net.URL;
-import java.util.Arrays;
+import java.util.Collections;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
@@ -74,7 +74,7 @@ public class ApiTokenStatsTest {
         final String TOKEN_NAME = "New Token Name";
         
         WebRequest request = new WebRequest(new URL(j.getURL() + "user/" + u.getId() + "/descriptorByName/" + ApiTokenProperty.class.getName() + "/generateNewToken"), HttpMethod.POST);
-        request.setRequestParameters(Arrays.asList(new NameValuePair("newTokenName", TOKEN_NAME)));
+        request.setRequestParameters(Collections.singletonList(new NameValuePair("newTokenName", TOKEN_NAME)));
         
         Page page = wc.getPage(request);
         assertEquals(200, page.getWebResponse().getStatusCode());
