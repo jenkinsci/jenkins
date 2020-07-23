aa73f2014ed4ce06bb8472c80a19fffebc1df812
Jesse Glick <jglick@cloudbees.com>
2020-07-23 09:52:16 -0400

    @Wadeck helped me with TokenBasedRememberMeServices2

--- core/src/main/java/hudson/security/TokenBasedRememberMeServices2.java
+++ core/src/main/java/hudson/security/TokenBasedRememberMeServices2.java
@@ -25,7 +25,6 @@ package hudson.security;
 
 import com.google.common.annotations.VisibleForTesting;
 import hudson.model.User;
-import java.util.Base64;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -139,6 +138,8 @@ public class TokenBasedRememberMeServices2 extends TokenBasedRememberMeServices 
 			return;
 		}
 
+        // TODO is it really still necessary to reimplement all of the below, or could we simply override rememberMeRequested?
+
 		Assert.notNull(successfulAuthentication.getPrincipal());
 		Assert.isInstanceOf(UserDetails.class, successfulAuthentication.getPrincipal());
 
@@ -146,168 +147,15 @@ public class TokenBasedRememberMeServices2 extends TokenBasedRememberMeServices 
 		String username = ((UserDetails) successfulAuthentication.getPrincipal()).getUsername();
 
 		String signatureValue = makeTokenSignature(expiryTime, username, ((UserDetails) successfulAuthentication.getPrincipal()).getPassword());
-		String tokenValue = username + ":" + expiryTime + ":" + signatureValue;
-		String tokenValueBase64 = Base64.getEncoder().encodeToString(tokenValue.getBytes());
 		int tokenLifetime = calculateLoginLifetime(request, successfulAuthentication);
-        /* TODO unclear what the Spring Security equivalent to this is:
-		response.addCookie(makeValidCookie(tokenValueBase64, request, getTokenValiditySeconds()));
-        // something like this, but where is the token signature?
 		setCookie(new String[] { username, Long.toString(expiryTime), signatureValue },
 				tokenLifetime, request, response);
-        */
 
 		if (logger.isDebugEnabled()) {
 			logger.debug("Added remember-me cookie for user '" + username + "', expiry: '" + new Date(expiryTime)
 							+ "'");
 		}
 	}
-
-    /* TODO now final
-    @Override
-    public Authentication autoLogin(HttpServletRequest request, HttpServletResponse response) {
-        Jenkins j = Jenkins.getInstanceOrNull();
-        if (j == null) {
-            // as this filter could be called during restart, this corrects at least the symptoms
-            return null;
-        }
-        if (j.isDisableRememberMe()) {
-            cancelCookie(request, response, null);
-            return null;
-        } else {
-            try {
-                // we use a patched version of the super.autoLogin
-                String rememberMeValue = findRememberMeCookieValue(request, response);
-                if (rememberMeValue == null) {
-                    return null;
-                }
-                return retrieveAuthFromCookie(request, response, rememberMeValue);
-            } catch (Exception e) {
-                cancelCookie(request, response, "Failed to handle remember-me cookie: " + Functions.printThrowable(e));
-                return null;
-            }
-        }
-    }
-
-    /**
-     * Patched version of the super.autoLogin with a time-independent equality check for the token validation
-     * /
-    private String findRememberMeCookieValue(HttpServletRequest request, HttpServletResponse response) {
-        Cookie[] cookies = request.getCookies();
-
-        if ((cookies == null) || (cookies.length == 0)) {
-            return null;
-        }
-
-        for (Cookie cookie : cookies) {
-            if (TokenBasedRememberMeServices.SPRING_SECURITY_REMEMBER_ME_COOKIE_KEY.equals(cookie.getName())) {
-                return cookie.getValue();
-            }
-        }
-
-        return null;
-    }
-
-    // Taken from TokenBasedRememberMeService with slight modification
-    // around the token equality check to avoid timing attack
-    // and reducing drastically the information provided in the log to avoid potential disclosure
-    private @CheckForNull Authentication retrieveAuthFromCookie(HttpServletRequest request, HttpServletResponse response, String cookieValueBase64){
-        String cookieValue = decodeCookieBase64(cookieValueBase64);
-        if (cookieValue == null) {
-            String reason = "Cookie token was not Base64 encoded; value was '" + cookieValueBase64 + "'";
-            cancelCookie(request, response, reason);
-            return null;
-        }
-        if (logger.isDebugEnabled()) {
-            logger.debug("Remember-me cookie detected");
-        }
-
-        String[] cookieTokens = StringUtils.delimitedListToStringArray(cookieValue, ":");
-
-        if (cookieTokens.length != 3) {
-            cancelCookie(request, response, "Cookie token did not contain 3 tokens separated by [:]");
-            return null;
-        }
-
-        long tokenExpiryTime;
-
-        try {
-            tokenExpiryTime = Long.parseLong(cookieTokens[1]);
-        }
-        catch (NumberFormatException nfe) {
-            cancelCookie(request, response, "Cookie token[1] did not contain a valid number");
-            return null;
-        }
-
-        if (isTokenExpired(tokenExpiryTime)) {
-            cancelCookie(request, response, "Cookie token[1] has expired");
-            return null;
-        }
-
-        // Check the user exists
-        // Defer lookup until after expiry time checked, to
-        // possibly avoid expensive lookup
-        UserDetails userDetails = loadUserDetails(request, response, cookieTokens);
-
-        if (userDetails == null) {
-            cancelCookie(request, response, "Cookie token[0] contained a username without user associated");
-            return null;
-        }
-
-        if (!isValidUserDetails(request, response, userDetails, cookieTokens)) {
-            return null;
-        }
-
-        String receivedTokenSignature = cookieTokens[2];
-        String expectedTokenSignature = makeTokenSignature(tokenExpiryTime, userDetails);
-
-        boolean tokenValid = MessageDigest.isEqual(
-                expectedTokenSignature.getBytes(StandardCharsets.US_ASCII),
-                receivedTokenSignature.getBytes(StandardCharsets.US_ASCII)
-        );
-        if (!tokenValid) {
-            cancelCookie(request, response, "Cookie token[2] contained invalid signature");
-            return null;
-        }
-
-        // By this stage we have a valid token
-        if (logger.isDebugEnabled()) {
-            logger.debug("Remember-me cookie accepted");
-        }
-
-        RememberMeAuthenticationToken auth = new RememberMeAuthenticationToken(this.getKey(), userDetails,
-                userDetails.getAuthorities());
-        auth.setDetails(authenticationDetailsSource.buildDetails(request));
-
-        // Ensure this session is linked to the user's seed
-        if (!UserSeedProperty.DISABLE_USER_SEED) {
-            User user = User.get(auth);
-            UserSeedProperty userSeed = user.getProperty(UserSeedProperty.class);
-            String sessionSeed = userSeed.getSeed();
-            request.getSession().setAttribute(UserSeedProperty.USER_SESSION_SEED, sessionSeed);
-        }
-
-        return auth;
-    }
-
-    /**
-     * @return the decoded base64 of the cookie or {@code null} if the value was not correctly encoded
-     * /
-    private @CheckForNull String decodeCookieBase64(@NonNull String base64EncodedValue){
-        StringBuilder base64EncodedValueBuilder = new StringBuilder(base64EncodedValue);
-        for (int j = 0; j < base64EncodedValueBuilder.length() % 4; j++) {
-            base64EncodedValueBuilder.append("=");
-        }
-        base64EncodedValue = base64EncodedValueBuilder.toString();
-
-        try {
-            // any charset should be fine but better safe than sorry
-            byte[] decodedPlainValue = Base64.getDecoder().decode(base64EncodedValue.getBytes(StandardCharsets.UTF_8));
-            return new String(decodedPlainValue, StandardCharsets.UTF_8);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
-    */
 
     /**
      * In addition to the expiration requested by the super class, we also check the expiration is not too far in the future.
