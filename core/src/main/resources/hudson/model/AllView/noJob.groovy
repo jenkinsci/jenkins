df5af8d28d5fa52eef4c9b1265abe4730da35a76
Félix Queiruga <fqueiruga@cloudbees.com>
2020-07-30 12:14:56 +0200

    wip - extract conditional vars

--- core/src/main/resources/hudson/model/AllView/noJob.groovy
+++ core/src/main/resources/hudson/model/AllView/noJob.groovy
@@ -4,35 +4,42 @@ import hudson.model.Computer
 import hudson.model.Item
 import jenkins.model.Jenkins
 
+def isTopLevelAllView = my.owner == Jenkins.get();
+def canSetUpDistributedBuilds = Jenkins.get().hasPermission(Computer.CREATE) &&
+        Jenkins.get().clouds.isEmpty() &&
+        Jenkins.get().getNodes().isEmpty();
+def hasAdministerJenkinsPermission = Jenkins.get().hasPermission(Jenkins.ADMINISTER);
+def hasItemCreatePermission = my.owner.hasPermission(Item.CREATE);
+
 div {
     h1(_("Welcome to Jenkins!"))
 
-    if (my.owner == Jenkins.get()) {
+    if (isTopLevelAllView) {
         // we're a top-level 'All' view
-        if (Jenkins.get().hasPermission(Computer.CREATE) && Jenkins.get().clouds.isEmpty() && Jenkins.get().getNodes().isEmpty()) {
+        if (canSetUpDistributedBuilds) {
             div(class: 'call-to-action') {
-                if (Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+                if (hasAdministerJenkinsPermission) {
                     raw(_("distributedBuildsWithCloud"))
                 } else {
                     raw(_("distributedBuilds"))
                 }
             }
         }
-        if (my.owner.hasPermission(Item.CREATE)) {
+        if (hasItemCreatePermission) {
             div(class: 'call-to-action') {
                 raw(_("newJob"))
             }
         }
     } else {
         // we're in a folder
-        if (my.owner.hasPermission(Item.CREATE)) {
+        if (hasItemCreatePermission) {
             div(class: 'call-to-action') {
                 raw(_("newJob"))
             }
         }
     }
 
-    if (h.isAnonymous() && !my.owner.hasPermission(Item.CREATE)) {
+    if (h.isAnonymous() && !hasItemCreatePermission) {
         div(class:'call-to-action') {
             raw(_("login", rootURL, app.securityRealm.loginUrl, request.requestURI))
             if (app.securityRealm.allowsSignup()) {
