f8ce37f4dfea14a174f336e256797959a761e99d
Vincent Latombe <vincent@latombe.net>
2020-10-01 16:38:12 +0200

    [JENKINS-63828] Fix expected signature for CloudProvisioningListener#canProvision

---
+++ core/src/test/java/hudson/slaves/CloudProvisioningListenerTest.java
@@ -0,0 +1,34 @@
+package hudson.slaves;
+
+import hudson.model.queue.CauseOfBlockage;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class CloudProvisioningListenerTest {
+    public static class CloudProvisioningListenerFailing extends CloudProvisioningListener {}
+
+    public static class CloudProvisioningListenerSuccess extends CloudProvisioningListener {
+        @Override
+        public CauseOfBlockage canProvision(Cloud cloud, Cloud.CloudState state, int numExecutors) {
+            return null;
+        }
+    }
+
+    @Test
+    public void failing(){
+        Cloud.CloudState state = new Cloud.CloudState(null, 0);
+        AbstractMethodError error = Assert.assertThrows(AbstractMethodError.class, () -> {
+            new CloudProvisioningListenerFailing().canProvision(null, state, 0);
+        });
+        assertEquals("You must override at least one of the CloudProvisioningListener.canProvision methods", error.getMessage());
+    }
+
+    @Test
+    public void success() {
+        Cloud.CloudState state = new Cloud.CloudState(null, 0);
+        assertNull(new CloudProvisioningListenerSuccess().canProvision(null, state, 0));
+    }
+}
