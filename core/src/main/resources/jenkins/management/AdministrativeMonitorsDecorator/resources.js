1e8a4e914ec5a7caf91ab4bca22cf5603e54a923
Félix Queiruga <fqueiruga@cloudbees.com>
2020-11-19 16:20:18 +0100

    Fixes form submission inside the popup
    - Wraps it in an l:ajax template so that proper variables are set
    - Applies the csrf crumbs to the forms after the popup is constructed

--- core/src/main/resources/jenkins/management/AdministrativeMonitorsDecorator/resources.js
+++ core/src/main/resources/jenkins/management/AdministrativeMonitorsDecorator/resources.js
@@ -1,82 +1,90 @@
-function initializeAmMonitor(amMonitorRoot, options) {
-    var button = amMonitorRoot.querySelector('.am-button');
-    var url = button.getAttribute('data-href');
-    var amList = amMonitorRoot.querySelector('.am-list');
+(function () {
+    function initializeAmMonitor(amMonitorRoot, options) {
+        var button = amMonitorRoot.querySelector('.am-button');
+        var url = button.getAttribute('data-href');
+        var amList = amMonitorRoot.querySelector('.am-list');
 
-    function onClose(e) {
-        var list = amList;
-        var el = e.target;
-        while (el) {
-            if (el === list) {
-                return; // clicked in the list
+        function onClose(e) {
+            var list = amList;
+            var el = e.target;
+            while (el) {
+                if (el === list) {
+                    return; // clicked in the list
+                }
+                el = el.parentElement;
             }
-            el = el.parentElement;
-        }
-        close();
-    }
-    function onEscClose(e) {
-        if (e.keyCode === 27) {
             close();
         }
-    }
-
-    function show() {
-        if (options.closeAll) options.closeAll();
-
-        new Ajax.Request(url, {
-            method: "GET",
-            onSuccess: function(rsp) {
-                var popupContent = rsp.responseText;
-                amList.innerHTML = popupContent;
-                amMonitorRoot.classList.add('visible');
-                document.addEventListener('click', onClose);
-                document.addEventListener('keydown', onEscClose);
+        function onEscClose(e) {
+            if (e.keyCode === 27) {
+                close();
             }
-        })
-    }
-
-    function close() {
-        amMonitorRoot.classList.remove('visible');
-        amList.innerHTML = '';
-        document.removeEventListener('click', onClose);
-        document.removeEventListener('keydown', onEscClose);
-    }
-
-    function toggle(e) {
-        if (amMonitorRoot.classList.contains('visible')) {
-            close();
-        } else {
-            show();
         }
-        e.preventDefault();
+
+        function show() {
+            if (options.closeAll) options.closeAll();
+
+            new Ajax.Request(url, {
+                method: "GET",
+                onSuccess: function(rsp) {
+                    var popupContent = rsp.responseText;
+                    amList.innerHTML = popupContent;
+                    amMonitorRoot.classList.add('visible');
+                    document.addEventListener('click', onClose);
+                    document.addEventListener('keydown', onEscClose);
+
+                    // Applies all initialization code to the elements within the popup
+                    // Among other things, this sets the CSRF crumb to the forms within
+                    Behaviour.applySubtree(amList);
+                }
+            });
+        }
+
+        function close() {
+            amMonitorRoot.classList.remove('visible');
+            amList.innerHTML = '';
+            document.removeEventListener('click', onClose);
+            document.removeEventListener('keydown', onEscClose);
+        }
+
+        function toggle(e) {
+            if (amMonitorRoot.classList.contains('visible')) {
+                close();
+            } else {
+                show();
+            }
+            e.preventDefault();
+        }
+
+        function startListeners() {
+            button.addEventListener('click', toggle);
+        }
+
+        return {
+            close: close,
+            startListeners: startListeners,
+        }
     }
 
+    document.addEventListener('DOMContentLoaded', function () {
+        var monitorWidgets;
 
-    return {
-        button: button,
-        toggle: toggle,
-        close: close,
-    }
-}
+        function closeAll() {
+            monitorWidgets.forEach(function (widget) {
+                widget.close();
+            })
+        }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var monitorWidgets;
+        var normalMonitors = initializeAmMonitor(document.getElementById('visible-am-container'), {
+            closeAll: closeAll,
+        });
+        var securityMonitors = initializeAmMonitor(document.getElementById('visible-sec-am-container'), {
+            closeAll: closeAll,
+        });
+        monitorWidgets = [normalMonitors, securityMonitors];
 
-    function closeAll() {
         monitorWidgets.forEach(function (widget) {
-            widget.close();
-        })
-    }
-
-    var normalMonitors = initializeAmMonitor(document.getElementById('visible-am-container'), {
-        closeAll: closeAll,
+            widget.startListeners();
+        });
     });
-    var securityMonitors = initializeAmMonitor(document.getElementById('visible-sec-am-container'), {
-        closeAll: closeAll,
-    });
-    monitorWidgets = [normalMonitors, securityMonitors];
-
-    monitorWidgets.forEach(function (widget) {
-        widget.button.addEventListener('click', widget.toggle);
-    });
-});
+})();
