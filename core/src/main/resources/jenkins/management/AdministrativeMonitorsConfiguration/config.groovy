8e0aa48c13560d8a62520bcae50ae1b8947d37da
Daniel Beck <daniel-beck@users.noreply.github.com>
2020-11-24 09:50:45 +0100

    Indicate in configuration which admin monitors are security-related

--- core/src/main/resources/jenkins/management/AdministrativeMonitorsConfiguration/config.groovy
+++ core/src/main/resources/jenkins/management/AdministrativeMonitorsConfiguration/config.groovy
@@ -41,6 +41,9 @@ f.section(title: _("Administrative monitors configuration")) {
                                 title: am.displayName,
                                 checked: am.enabled,
                                 json: am.id)
+                        if (am.isSecurity()) {
+                            span(style: 'border: 1px solid grey; color: grey; border-radius: 4px; padding: 3px; margin-left: 5px;', _("Security"))
+                        }
                     }
                     div(class: "tr") {
                         div(class: "setting-description") {
