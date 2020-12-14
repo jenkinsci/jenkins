7c4ce906c92e1f571ce4312461ab957b0f72170b
Tim Jacomb <timjacomb1+github@gmail.com>
2020-12-14 05:29:14 +0000

    JENKINS-64426 Don't tell user's to signup if they can't

--- core/src/main/resources/hudson/model/AllView/noJob.groovy
+++ core/src/main/resources/hudson/model/AllView/noJob.groovy
@@ -111,7 +111,11 @@ div {
 
             h1(_("Welcome to Jenkins!"))
 
-            p(_("anonymousDescription"))
+            if (canSignUp) {
+                p(_("anonymousDescriptionSignUpEnabled"))
+            } else {
+                p(_("anonymousDescription"))
+            }
 
             section(class: "empty-state-section") {
                 ul(class: "empty-state-section-list") {
