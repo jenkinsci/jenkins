e253e81903665b3fc4110c18123469cde7f8cd7b
Casey Duquette <casey.duquette@snapchat.com>
2021-01-06 13:32:31 -0800

    Update license?

--- test/src/test/java/hudson/slaves/AgentInboundUrlTest.java
+++ test/src/test/java/hudson/slaves/AgentInboundUrlTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2021, CloudBees, Inc.
+ * Copyright (c) 2021, Snap, Inc., Casey Duquette
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
