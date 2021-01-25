15549a67207140c0bda43bff16d9be14a92d59fd
res0nance <res0nance@users.noreply.github.com>
2021-01-25 17:21:26 +0800

    Minor cleanups to Fingerprint writing codepaths

--- core/src/main/java/hudson/model/Fingerprint.java
+++ core/src/main/java/hudson/model/Fingerprint.java
@@ -1015,7 +1015,7 @@ public class Fingerprint implements ModelObject, Saveable {
      * @deprecated Use {@link #addFor(hudson.model.Run)}
      */
     @Deprecated
-    public synchronized void add(@NonNull AbstractBuild b) throws IOException {
+    public void add(@NonNull AbstractBuild b) throws IOException {
         addFor(b);
     }
 
@@ -1024,7 +1024,7 @@ public class Fingerprint implements ModelObject, Saveable {
      * @param b {@link Run} to be referenced in {@link #usages}
      * @since 1.577
      */
-    public synchronized void addFor(@NonNull Run b) throws IOException {
+    public void addFor(@NonNull Run b) throws IOException {
         add(b.getParent().getFullName(), b.getNumber());
     }
 
