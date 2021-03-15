a244267356e7de3d3e2c3b23ce67392b6972df30
res0nance <res0nance@users.noreply.github.com>
2021-03-16 00:29:42 +0800

    Remove unused import

--- core/src/main/java/hudson/model/UpdateSite.java
+++ core/src/main/java/hudson/model/UpdateSite.java
@@ -47,7 +47,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
