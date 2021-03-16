195ecafefc9dd760b83af017130020c8cadb2056
benebsiny <stu995106@hotmail.com.tw>
2021-03-16 19:25:49 +0800

    Test for min / max support of <f:input>
    In hudson-behaviour.js:
    1. Convert the min and max to integer when they are comparing.
    2. Since the HtmlUnit can't recognize the string literals, I changed it to string concat.

--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -628,26 +628,35 @@ function registerMinMaxValidator(e) {
         const min = this.getAttribute('min');
         const max = this.getAttribute('max');
 
-        if (this.value.match(/^\d*$/)) {  // Ensure the value is an integer
-            if (min !== null && max !== null) {  // Both min and max attributes are exist
+        function isInteger(str) {
+            return str.match(/^-?\d*$/) !== null;
+        }
+
+        if (isInteger(this.value)) {  // Ensure the value is an integer
+            if ((min !== null && isInteger(min)) && (max !== null && isInteger(max))) {  // Both min and max attributes are available
+
                 if (min <= max) {  // Add the validator if min <= max
-                    if (min > parseInt(this.value) || parseInt(this.value) > max) {  // The value is out of range
-                        this.targetElement.innerHTML = `<div class=error>This value should be between ${min} and ${max}</div>`;
+                    if (parseInt(min) > parseInt(this.value) || parseInt(this.value) > parseInt(max)) {  // The value is out of range
+                        this.targetElement.innerHTML = "<div class=error>This value should be between " + min + " and " + max + "</div>";
                         set = true;
                     } else {
                         if (!set) this.targetElement.innerHTML = "<div/>";  // The value is valid
                     }
                 }
-            } else if (min !== null && max === null) {  // There is only a min attribute
-                if (min > parseInt(this.value)) {
-                    this.targetElement.innerHTML = `<div class=error>This value should be larger than ${min}</div>`;
+
+            } else if ((min !== null && isInteger(min)) && (max === null || !isInteger(max))) {  // There is only 'min' available
+
+                if (parseInt(min) > parseInt(this.value)) {
+                    this.targetElement.innerHTML = "<div class=error>This value should be larger than " + min + "</div>";
                     set = true;
                 } else {
                     if (!set) this.targetElement.innerHTML = "<div/>";
                 }
-            } else if (min === null && max !== null) {  // There is only a max attribute
-                if (max < parseInt(this.value)) {
-                    this.targetElement.innerHTML = `<div class=error>This value should be less than ${max}</div>`;
+
+            } else if ((min === null || !isInteger(min)) && (max !== null && isInteger(max))) {  // There is only 'max' available
+
+                if (parseInt(max) < parseInt(this.value)) {
+                    this.targetElement.innerHTML = "<div class=error>This value should be less than " + max + "</div>";
                     set = true;
                 } else {
                     if (!set) this.targetElement.innerHTML = "<div/>";
