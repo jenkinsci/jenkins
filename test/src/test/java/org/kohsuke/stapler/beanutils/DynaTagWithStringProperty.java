6d9b1fed238bc872c3fdcbf569bb332b9edc3fbc
Daniel Beck <daniel-beck@users.noreply.github.com>
2021-03-19 00:52:11 +0100

    More tests

---
+++ test/src/test/java/org/kohsuke/stapler/beanutils/DynaTagWithStringProperty.java
@@ -0,0 +1,42 @@
+package org.kohsuke.stapler.beanutils;
+
+import org.apache.commons.jelly.DynaTag;
+import org.apache.commons.jelly.JellyTagException;
+import org.apache.commons.jelly.MissingAttributeException;
+import org.apache.commons.jelly.TagSupport;
+import org.apache.commons.jelly.XMLOutput;
+import org.xml.sax.SAXException;
+
+public class DynaTagWithStringProperty extends TagSupport implements DynaTag {
+    private String clazz;
+
+    public void setClass(String clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public void doTag(XMLOutput output) throws MissingAttributeException, JellyTagException {
+        try {
+            output.writeComment("Tag with string property\n");
+            output.writeCDATA(getClass().getName() + ":" + clazz);
+        } catch (SAXException e) {
+            // ignore
+        }
+        invokeBody(output);
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) throws JellyTagException {
+        if ("class".equals(name)) {
+            this.setClass((String) value);
+        }
+    }
+
+    @Override
+    public Class getAttributeType(String name) throws JellyTagException {
+        if ("class".equals(name)) {
+            return String.class;
+        }
+        return null;
+    }
+}
--- test/src/test/java/org/kohsuke/stapler/beanutils/TagTest.java
+++ test/src/test/java/org/kohsuke/stapler/beanutils/TagTest.java
@@ -14,6 +14,7 @@ import org.kohsuke.stapler.jelly.StaplerTagLibrary;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
 
 public class TagTest {
     public static final String ROOT_ACTION_URL = "tagtest";
@@ -21,7 +22,36 @@ public class TagTest {
     public JenkinsRule j = new JenkinsRule();
 
     @Test
-    public void testTagLib() throws Exception {
+    public void testWeird() throws Exception {
+        final JenkinsRule.WebClient wc = j.createWebClient().withThrowExceptionOnFailingStatusCode(false);
+        {
+            // This Jelly page, standalone, does cannot resolve the 'my' tag library
+            final HtmlPage page = wc.goTo(ROOT_ACTION_URL + "/jellyWithMyTagLibClassName");
+            final String content = page.getWebResponse().getContentAsString();
+            assertThat(content, not(containsString(":thisIsJellyInclude")));
+            assertThat(content, containsString("xmlns:my="));
+        }
+        {
+            // With a Groovy wrapper defining the tag library so it can be resolved by class name, it works
+            final HtmlPage page = wc.goTo(ROOT_ACTION_URL + "/groovyCallingJellyBuilderJellyThenInclude");
+            assertThat(page.getWebResponse().getContentAsString(), containsString(":thisIsJellyInclude"));
+            // We're called from StaticTagLibrary, and assumed to be DynaTag
+        }
+        {
+            // Groovy with DynaTag works
+            final HtmlPage page = wc.goTo(ROOT_ACTION_URL + "/groovyCallingJellyBuilderJellyThenTagWithStringProperty");
+            assertThat(page.getWebResponse().getContentAsString(), containsString(":stringParam"));
+        }
+        {
+            // Groovy without DynaTag fails -- this seems to be mostly the st.include case that would fail without workaround
+            final HtmlPage page = wc.goTo(ROOT_ACTION_URL + "/groovyCallingJellyBuilderJellyThenTagWithObjectProperty");
+            assertThat(page.getWebResponse().getContentAsString(), containsString("This tag does not understand"));
+        }
+    }
+
+
+    @Test
+    public void testIncludeTag() throws Exception {
         final JenkinsRule.WebClient wc = j.createWebClient().withThrowExceptionOnFailingStatusCode(false);
         { // Jelly views with basic include variants
             {
