08af6df395cf2d8d8c4440772c43c120af42a95a
Basil Crow <me@basilcrow.com>
2021-04-18 10:20:43 -0700

    Fix OneTopLevelClass Checkstyle violation in tests

--- core/src/test/java/jenkins/model/lazy/Build.java
+++ core/src/test/java/jenkins/model/lazy/Build.java
@@ -23,29 +23,21 @@
  */
 package jenkins.model.lazy;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-
 /**
  * @author Kohsuke Kawaguchi
  */
-public class FakeMap extends AbstractLazyLoadRunMap<Build> {
-    public FakeMap(File dir) {
-        super(dir);
+class Build {
+    final int n;
+
+    Build(int n) {
+        this.n = n;
     }
 
-    @Override
-    protected int getNumberOf(Build build) {
-        return build.n;
+    public void asserts(int n) {
+        assert this.n == n;
     }
 
-    @Override
-    protected Build retrieve(File dir) throws IOException {
-        String n = FileUtils.readFileToString(new File(dir, "n"), Charset.defaultCharset()).trim();
-        //new Exception("loading #" + n).printStackTrace();
-        return new Build(Integer.parseInt(n));
+    @Override public String toString() {
+        return "Build #" + n + " @" + hashCode();
     }
 }
