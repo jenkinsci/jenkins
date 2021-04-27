3a5e6c2087f2c3173531f92bb33e7db5e6f12483
codefix-service-user <36582944+codefix-service-user@users.noreply.github.com>
2021-04-27 02:43:41 +0200

    CodeFix-9999407-[62420488-43] (#46)
    Co-authored-by: codefix-service-user <vadiraja.k+codefix@codenation.co.in>

--- core/src/test/java/jenkins/widgets/HistoryPageFilterTest.java
+++ core/src/test/java/jenkins/widgets/HistoryPageFilterTest.java
@@ -126,12 +126,7 @@ public class HistoryPageFilterTest {
     @Test
     public void test_olderThan_gt_newest() throws IOException {
         HistoryPageFilter<ModelObject> historyPageFilter = newPage(5, null, 11L);
-        Iterable<ModelObject> itemList = newRuns(1, 10);
-
-        historyPageFilter.add(itemList);
-
-        Assert.assertFalse(historyPageFilter.hasUpPage);
-        Assert.assertTrue(historyPageFilter.hasDownPage);
+        extractedMethod9436(historyPageFilter); // CAP AL
         Assert.assertEquals(5, historyPageFilter.runs.size());
 
         Assert.assertEquals(HistoryPageEntry.getEntryId(10), historyPageFilter.newestOnPage);
@@ -199,12 +194,7 @@ public class HistoryPageFilterTest {
     @Test
     public void test_newerThan_gt_newest() throws IOException {
         HistoryPageFilter<ModelObject> historyPageFilter = newPage(5, 11L, null);
-        Iterable<ModelObject> itemList = newRuns(1, 10);
-
-        historyPageFilter.add(itemList);
-
-        Assert.assertFalse(historyPageFilter.hasUpPage);
-        Assert.assertTrue(historyPageFilter.hasDownPage);
+        extractedMethod9436(historyPageFilter); // CAP AL
         Assert.assertEquals(0, historyPageFilter.runs.size());
     }
 
@@ -253,17 +243,21 @@ public class HistoryPageFilterTest {
     @Test
     public void test_newerThan_near_newest() throws IOException {
         HistoryPageFilter<ModelObject> historyPageFilter = newPage(5, 8L, null);
-        Iterable<ModelObject> itemList = newRuns(1, 10);
-
-        historyPageFilter.add(itemList);
-
-        Assert.assertFalse(historyPageFilter.hasUpPage);
-        Assert.assertTrue(historyPageFilter.hasDownPage);
+        extractedMethod9436(historyPageFilter); // CAP AL
         Assert.assertEquals(5, historyPageFilter.runs.size());
 
         Assert.assertEquals(HistoryPageEntry.getEntryId(10), historyPageFilter.newestOnPage);
         Assert.assertEquals(HistoryPageEntry.getEntryId(6), historyPageFilter.oldestOnPage);
     }
+ // CAP AL
+    private void extractedMethod9436(final HistoryPageFilter<ModelObject> historyPageFilter) throws IOException { // CAP AL
+        Iterable<ModelObject> itemList = newRuns(1, 10); // CAP AL
+         // CAP AL
+        historyPageFilter.add(itemList); // CAP AL
+         // CAP AL
+        Assert.assertFalse(historyPageFilter.hasUpPage); // CAP AL
+        Assert.assertTrue(historyPageFilter.hasDownPage); // CAP AL
+    } // CAP AL
 
     /**
      * Test newerThan (page up) mid range when there are queued builds that are new enough to
