8b68857ccbe4b66c6600a80047d68d0badd8b153
Daniel Beck <daniel-beck@users.noreply.github.com>
2021-05-03 09:16:52 +0200

    ...

--- core/src/main/java/jenkins/util/SystemProperties.java
+++ core/src/main/java/jenkins/util/SystemProperties.java
@@ -37,13 +37,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
