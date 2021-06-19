36528c1a7cfeea4aea494aaf7dc68820f5d02c19
Basil Crow <me@basilcrow.com>
2021-06-18 21:02:39 -0700

    Remove `jna-posix` (#5560)

--- core/src/main/java/hudson/os/PosixException.java
+++ core/src/main/java/hudson/os/PosixException.java
@@ -1,28 +1,17 @@
 package hudson.os;
 
-import org.jruby.ext.posix.POSIX.ERRORS;
-
 /**
  * Indicates an error during POSIX API call.
  * @see PosixAPI
  * @author Kohsuke Kawaguchi
  */
 public class PosixException extends RuntimeException {
-    private final ERRORS errors;
 
-    public PosixException(String message, ERRORS errors) {
+    public PosixException(String message) {
         super(message);
-        this.errors = errors;
     }
 
-    /** @deprecated Leaks reference to deprecated jna-posix API. */
-    @Deprecated
-    public ERRORS getErrorCode() {
-        return errors;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString()+" "+errors;
+    public PosixException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
