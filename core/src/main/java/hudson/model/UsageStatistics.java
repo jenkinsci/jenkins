38ee66ca9468b0720dffa14769f1667b03e092b2
Ramon Leon <manuelramonleonjimenez@gmail.com>
2021-07-19 18:59:50 +0200

    Update core/src/main/java/hudson/model/UsageStatistics.java
    Co-authored-by: James Nord <jtnord@users.noreply.github.com>

--- core/src/main/java/hudson/model/UsageStatistics.java
+++ core/src/main/java/hudson/model/UsageStatistics.java
@@ -196,7 +196,7 @@ public class UsageStatistics extends PageDecorator implements PersistentDescript
             }
 
             return new String(Base64.getEncoder().encode(baos.toByteArray()));
-        } catch (Exception e) { // the exception could be GeneralSecurityException, InvalidParameterException or any other depending on the security provider you have installed
+        } catch (Throwable e) { // the exception could be GeneralSecurityException, InvalidParameterException or any other depending on the security provider you have installed
             LOG.log(Level.INFO, "Usage statistics could not be sent ({0})", e.getMessage());
             LOG.log(Level.FINE, "Error sending usage statistics", e);
             return null;
