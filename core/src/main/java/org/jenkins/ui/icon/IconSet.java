db1e7a3b0907d3df8be367fc9a07eb4e38e8a33c
Jan Faracik <j.faracik@kainos.com>
2021-08-15 18:56:50 +0100

    Move to SVGs

--- core/src/main/java/org/jenkins/ui/icon/IconSet.java
+++ core/src/main/java/org/jenkins/ui/icon/IconSet.java
@@ -469,15 +469,15 @@ public class IconSet {
         icons.addIcon(new Icon("icon-up icon-md", "24x24/up.png", Icon.ICON_MEDIUM_STYLE));
         icons.addIcon(new Icon("icon-user icon-md", "24x24/user.png", Icon.ICON_MEDIUM_STYLE));
 
-        icons.addIcon(new Icon("icon-up icon-md", "scalable/up.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-search icon-md", "scalable/search.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-clock icon-md", "scalable/clock.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-edit-delete icon-md", "scalable/edit-delete.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-gear2 icon-md", "scalable/gear.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-new-package icon-md", "scalable/new-package.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-notepad icon-md", "scalable/notepad.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-user icon-md", "scalable/user.svg", Icon.ICON_MEDIUM_STYLE));
-        icons.addIcon(new Icon("icon-folder icon-md", "scalable/folder.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-up icon-md", "svgs/up.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-search icon-md", "svgs/search.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-clock icon-md", "svgs/clock.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-edit-delete icon-md", "svgs/edit-delete.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-gear2 icon-md", "svgs/gear.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-new-package icon-md", "svgs/new-package.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-notepad icon-md", "svgs/notepad.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-user icon-md", "svgs/user.svg", Icon.ICON_MEDIUM_STYLE));
+        icons.addIcon(new Icon("icon-folder icon-md", "svgs/folder.svg", Icon.ICON_MEDIUM_STYLE));
 
         // Large icons.
         // .png versions will override .gif versions => only time a gif should be returned by
