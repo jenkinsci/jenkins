364799cd7b23d0ad6e7b8f84cf26b501d9921510
James Nord <jtnord@users.noreply.github.com>
2021-09-06 18:04:36 +0100

    Update core/src/test/java/jenkins/Junit4TestsRanTest.java
    Co-authored-by: Mark Waite <mark.earl.waite@gmail.com>

--- core/src/test/java/jenkins/Junit4TestsRanTest.java
+++ core/src/test/java/jenkins/Junit4TestsRanTest.java
@@ -7,7 +7,7 @@ class Junit4TestsRanTest {
     @Test
     void anything() {
         // intentionally blank.
-        // we just want a test that runs with junit 4 so that if tests get skipped due to the introduction of ju[iter engine we are allerted
+        // we just want a test that runs with junit 4 so that if tests get skipped due to the introduction of jupiter engine we are alerted
     }
 
 }
