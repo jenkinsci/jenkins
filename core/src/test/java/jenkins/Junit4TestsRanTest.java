67a0c4b6c830e732a4756d82304436ab456bced4
James Nord <jtnord@users.noreply.github.com>
2021-09-07 14:33:10 +0100

    test case needs to be public!

--- core/src/test/java/jenkins/Junit4TestsRanTest.java
+++ core/src/test/java/jenkins/Junit4TestsRanTest.java
@@ -2,10 +2,10 @@ package jenkins;
 
 import org.junit.Test;
 
-class Junit4TestsRanTest {
+public class Junit4TestsRanTest {
 
     @Test
-    void anything() {
+    public void anything() {
         // intentionally blank.
         // we just want a test that runs with junit 4 so that if tests get skipped due to the introduction of jupiter engine we are alerted
     }
