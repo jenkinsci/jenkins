6682cf354d49531a4e000d2950056fe2ea0d5687
Mark Waite <mark.earl.waite@gmail.com>
2021-09-08 13:47:49 -0600

    Satisfy checkstyle...

--- test/src/test/java/hudson/slaves/SlaveComputerTest.java
+++ test/src/test/java/hudson/slaves/SlaveComputerTest.java
@@ -156,7 +156,7 @@ public class SlaveComputerTest {
     @TestExtension(value = "startupShouldFailOnErrorOnlineListener")
     public static final class ErrorOnOnlineListener extends ComputerListener {
 
-        volatile static int onOnlineCount = 0;
+        static volatile int onOnlineCount = 0;
 
         @Override
         public void onOnline(Computer c, TaskListener listener) throws IOException, InterruptedException {
