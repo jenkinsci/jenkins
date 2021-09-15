c9fd3bf3aed78e733d2c1a92fccf1c7c6c5315d9
Jesse Glick <jglick@cloudbees.com>
2021-09-15 14:39:08 -0400

    Adding `@deprecated` messages for `Fingerprinter.recordBuildArtifacts`

--- core/src/main/java/hudson/tasks/Fingerprinter.java
+++ core/src/main/java/hudson/tasks/Fingerprinter.java
@@ -124,6 +124,9 @@ public class Fingerprinter extends Recorder implements Serializable, DependencyD
         this.caseSensitive = caseSensitive;
     }
 
+    /**
+     * @deprecated use {@link #Fingerprinter(String)} and {@link ArtifactArchiver#setFingerprint}
+     */
     @Deprecated
     public Fingerprinter(String targets, boolean recordBuildArtifacts) {
         this(targets);
@@ -160,6 +163,9 @@ public class Fingerprinter extends Recorder implements Serializable, DependencyD
         return this;
     }
 
+    /**
+     * @deprecated use {@link ArtifactArchiver#isFingerprint}
+     */
     @Deprecated
     public boolean getRecordBuildArtifacts() {
         return recordBuildArtifacts != null && recordBuildArtifacts;
