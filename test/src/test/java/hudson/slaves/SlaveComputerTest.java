3c0f717d16ed5ce6e5a3799c47d424d12c5fd915
James Nord <jtnord@users.noreply.github.com>
2021-09-21 13:15:49 +0100

    Attempt to fix SlaveComputerTest.startupShouldFailOnErrorOnlineListener (#5715)
    Co-authored-by: Mark Waite <mark.earl.waite@gmail.com>

--- test/src/test/java/hudson/slaves/SlaveComputerTest.java
+++ test/src/test/java/hudson/slaves/SlaveComputerTest.java
@@ -155,7 +155,7 @@ public class SlaveComputerTest {
     @TestExtension(value = "startupShouldFailOnErrorOnlineListener")
     public static final class ErrorOnOnlineListener extends ComputerListener {
 
-        static int onOnlineCount = 0;
+        static volatile int onOnlineCount = 0;
 
         @Override
         public void onOnline(Computer c, TaskListener listener) throws IOException, InterruptedException {
