29732c65ba64eeb4c4209182055ee71a5f695814
Pauline Iogna <pauline.iogna@lunatech.fr>
2021-10-30 21:35:44 +0200

    fix variables

--- core/src/main/resources/hudson/model/View/AsynchPeople/people-resources.js
+++ core/src/main/resources/hudson/model/View/AsynchPeople/people-resources.js
@@ -1,5 +1,7 @@
 function display(data) {
     var p = document.getElementById('people');
+    var rootURL = document.head.getAttribute('data-rooturl');
+    var iconSize = document.getElementById("asynch-people-description").getAttribute("data-iconsize");
     for (var x = 0; data.length > x; x++) {
         var e = data[x];
         var id = 'person-' + e.id;
@@ -16,25 +18,25 @@ function display(data) {
 
         var d = document.createElement('td');
         var a = document.createElement('a');
-        a.href = '${rootURL}/' + e.url;
+        a.href = rootURL + "/" + e.url;
         a.className = 'model-link inside';
         var i = document.createElement('img');
         i.src = e.avatar;
-        i.className = 'icon${iconSize}';
+        i.className = 'icon' + iconSize;
         a.appendChild(i);
         d.appendChild(a);
         r.appendChild(d);
 
         d = document.createElement('td');
         var a = document.createElement('a');
-        a.href = '${rootURL}/' + e.url;
+        a.href = rootURL + "/" + e.url;
         a.appendChild(document.createTextNode(e.id));
         d.appendChild(a);
         r.appendChild(d);
 
         d = document.createElement('td');
         var a = document.createElement('a');
-        a.href = '${rootURL}/' + e.url;
+        a.href = rootURL + "/" + e.url;
         a.appendChild(document.createTextNode(e.fullName));
         d.appendChild(a);
         r.appendChild(d);
@@ -47,7 +49,7 @@ function display(data) {
         d = document.createElement('td');
         if (e.projectUrl != null) {
             a = document.createElement('a');
-            a.href = '${rootURL}/' + e.projectUrl;
+            a.href = rootURL + "/" + e.projectUrl;
             a.className = 'model-link inside';
             a.appendChild(document.createTextNode(e.projectFullDisplayName));
             d.appendChild(a);
