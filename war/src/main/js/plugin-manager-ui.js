143ece3d986fa91a786ba9bc5be18759a9d6dc0f
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2021-11-12 20:03:24 +0000

    Update plugin-manager-ui.js

--- war/src/main/js/plugin-manager-ui.js
+++ war/src/main/js/plugin-manager-ui.js
@@ -7,65 +7,65 @@ import pluginManager from './api/pluginManager';
 var filterInput = document.getElementById('filter-box');
 
 function applyFilter(searchQuery) {
-  // debounce reduces number of server side calls while typing
-  pluginManager.availablePluginsSearch(searchQuery.toLowerCase().trim(), 50, function (plugins) {
-    var pluginsTable = document.getElementById('plugins');
-    var tbody = pluginsTable.querySelector('tbody');
-    var admin = pluginsTable.dataset.hasadmin === 'true';
-    var selectedPlugins = [];
+    // debounce reduces number of server side calls while typing
+    pluginManager.availablePluginsSearch(searchQuery.toLowerCase().trim(), 50, function (plugins) {
+        var pluginsTable = document.getElementById('plugins');
+        var tbody = pluginsTable.querySelector('tbody');
+        var admin = pluginsTable.dataset.hasadmin === 'true';
+        var selectedPlugins = [];
 
-    filterInput.parentElement.classList.remove("jenkins-search--loading");
+        filterInput.parentElement.classList.remove("jenkins-search--loading");
 
-    function clearOldResults() {
-      if (!admin) {
-        tbody.innerHTML = '';
-      } else {
-        var rows = tbody.querySelectorAll('tr');
-        if (rows) {
-          selectedPlugins = []
-          rows.forEach(function (row) {
-            var input = row.querySelector('input');
-            if (input.checked === true) {
-              var pluginName = input.name.split('.')[1];
-              selectedPlugins.push(pluginName)
+        function clearOldResults() {
+            if (!admin) {
+                tbody.innerHTML = '';
             } else {
-              row.remove();
+                var rows = tbody.querySelectorAll('tr');
+                if (rows) {
+                    selectedPlugins = []
+                    rows.forEach(function (row) {
+                        var input = row.querySelector('input');
+                        if (input.checked === true) {
+                            var pluginName = input.name.split('.')[1];
+                            selectedPlugins.push(pluginName)
+                        } else {
+                            row.remove();
+                        }
+                    })
+                }
             }
-          })
         }
-      }
-    }
 
-    clearOldResults()
-    var rows = pluginManagerAvailable({
-      plugins: plugins.filter(plugin => selectedPlugins.indexOf(plugin.name) === -1),
-      admin
-    });
+        clearOldResults()
+        var rows = pluginManagerAvailable({
+            plugins: plugins.filter(plugin => selectedPlugins.indexOf(plugin.name) === -1),
+            admin
+        });
 
-    tbody.insertAdjacentHTML('beforeend', rows);
+        tbody.insertAdjacentHTML('beforeend', rows);
 
-    // @see JENKINS-64504 - Update the sticky buttons position after each search.
-    requestAnimationFrame(() => {
-      layoutUpdateCallback.call()
+        // @see JENKINS-64504 - Update the sticky buttons position after each search.
+        requestAnimationFrame(() => {
+            layoutUpdateCallback.call()
+        })
     })
-  })
 }
 
 var handleFilter = function (e) {
-  applyFilter(e.target.value)
+    applyFilter(e.target.value)
 };
 
 var debouncedFilter = debounce(handleFilter, 150);
 
 document.addEventListener("DOMContentLoaded", function () {
-  filterInput.addEventListener('input', function (e) {
-    debouncedFilter(e);
-    filterInput.parentElement.classList.add("jenkins-search--loading");
-  });
+    filterInput.addEventListener('input', function (e) {
+        debouncedFilter(e);
+        filterInput.parentElement.classList.add("jenkins-search--loading");
+    });
 
-  applyFilter(filterInput.value);
+    applyFilter(filterInput.value);
 
-  setTimeout(function () {
-    layoutUpdateCallback.call();
-  }, 350)
+    setTimeout(function () {
+        layoutUpdateCallback.call();
+    }, 350)
 });
