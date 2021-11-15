6fe15520b3b7fa327c0c7ba89cf3f67da5f46ce9
imonteroperez <imontero@cloudbees.com>
2021-11-15 13:17:44 +0100

    Do not force to use ExtensionFilter

--- core/src/main/java/jenkins/views/Header.java
+++ core/src/main/java/jenkins/views/Header.java
@@ -1,7 +1,6 @@
 package jenkins.views;
 
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.kohsuke.accmod.Restricted;
@@ -19,8 +18,6 @@ import hudson.ExtensionPoint;
  */
 public abstract class Header implements ExtensionPoint {
     
-    private static Logger LOGGER = Logger.getLogger(JenkinsHeader.class.getName());
-    
     /**
      * Checks if header is enabled.
      * @return if header is enabled
@@ -31,13 +28,9 @@ public abstract class Header implements ExtensionPoint {
     @CheckForNull
     public static Header get() {
         List<Header> headers = ExtensionList.lookup(Header.class).stream().filter(header -> header.isHeaderEnabled()).collect(Collectors.toList());
-        if (!headers.isEmpty()) {
-            if (headers.size() > 1) {
-                LOGGER.warning("More than one configured header. This should not happen. Serving the Jenkins default header and please review");
-            } else {
-                return headers.get(0);
-            }
+        if (headers.isEmpty()) {
+            return new JenkinsHeader();
         }
-        return new JenkinsHeader();
+        return headers.get(0);
     }
 }
