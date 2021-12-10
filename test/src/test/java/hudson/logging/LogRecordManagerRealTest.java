345d4c60924341b6ae4c4a8978e7ac443fe61b1d
Jesse Glick <jglick@cloudbees.com>
2021-12-10 08:55:12 -0500

    Fixing test style

--- test/src/test/java/hudson/logging/LogRecordManagerRealTest.java
+++ test/src/test/java/hudson/logging/LogRecordManagerRealTest.java
@@ -1,5 +1,10 @@
 package hudson.logging;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
 import java.util.List;
 import java.util.logging.LogRecord;
 import jenkins.model.Jenkins;
@@ -8,11 +13,6 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.RealJenkinsRule;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-
 public class LogRecordManagerRealTest {
 
     @Rule
@@ -20,15 +20,11 @@ public class LogRecordManagerRealTest {
 
     @Test
     public void logRecordsArePresentOnController() throws Throwable {
-        rr.then(new LogRecordsArePresent());
+        rr.then(LogRecordManagerRealTest::_logRecordsArePresentOnController);
+    }
+    private static void _logRecordsArePresentOnController(JenkinsRule r) throws Throwable {
+        List<LogRecord> logRecords = Jenkins.logRecords;
+        assertThat(logRecords, is(not(empty())));
     }
 
-    private static class LogRecordsArePresent implements RealJenkinsRule.Step {
-
-        @Override
-        public void run(JenkinsRule r) throws Throwable {
-            List<LogRecord> logRecords = Jenkins.logRecords;
-            assertThat(logRecords, is(not(empty())));
-        }
-    }
 }
