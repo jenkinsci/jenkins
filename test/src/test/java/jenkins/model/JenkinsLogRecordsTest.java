9191a17776465f5b2be358a1917a4ceb27930d5e
Jesse Glick <jglick@cloudbees.com>
2021-12-10 09:21:25 -0500

    Verify reverse order of `RingBufferLogHandler.getView`

--- test/src/test/java/jenkins/model/JenkinsLogRecordsTest.java
+++ test/src/test/java/jenkins/model/JenkinsLogRecordsTest.java
@@ -1,12 +1,13 @@
 package jenkins.model;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInRelativeOrder;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 import java.util.List;
 import java.util.logging.LogRecord;
+import java.util.stream.Collectors;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -23,7 +24,10 @@ public class JenkinsLogRecordsTest {
     }
     private static void _logRecordsArePresentOnController(JenkinsRule r) throws Throwable {
         List<LogRecord> logRecords = Jenkins.logRecords;
-        assertThat(logRecords, is(not(empty())));
+        assertThat(logRecords, not(empty()));
+        assertThat("Records are displayed in reverse order",
+            logRecords.stream().map(LogRecord::getMessage).collect(Collectors.toList()),
+            containsInRelativeOrder("Completed initialization", "Started initialization"));
     }
 
 }
