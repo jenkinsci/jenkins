5b552023eaa655561131b20d604043db7d44169d
Basil Crow <me@basilcrow.com>
2021-12-27 13:17:04 -0800

    Fix `EQ_GETCLASS_AND_CLASS_CONSTANT` SpotBugs violations

--- core/src/main/java/hudson/model/StringParameterDefinition.java
+++ core/src/main/java/hudson/model/StringParameterDefinition.java
@@ -25,6 +25,7 @@ package hudson.model;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Extension;
 import hudson.Util;
 import java.util.Objects;
@@ -172,6 +173,7 @@ public class StringParameterDefinition extends SimpleParameterDefinition {
     }
 
     @Override
+    @SuppressFBWarnings(value = "EQ_GETCLASS_AND_CLASS_CONSTANT", justification = "ParameterDefinitionTest tests that subclasses are not equal to their parent classes, so the behavior appears to be intentional")
     public boolean equals(Object obj) {
         if (StringParameterDefinition.class != getClass())
             return super.equals(obj);
