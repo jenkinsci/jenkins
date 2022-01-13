6b5e3d11700c086d9b3b6e14132240691a6bebbd
Zbynek Konecny <zbynek1729@gmail.com>
2022-01-13 08:46:54 +0100

    Unify labels in plugin manager (#6151)
    * Map outdated labels to canonical ones
    
    * Fix checkstyle issue (missing space)

---
+++ core/src/test/java/jenkins/util/PluginLabelUtilTest.java
@@ -0,0 +1,19 @@
+package jenkins.util;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import net.sf.json.JSONArray;
+import org.junit.Test;
+
+public class PluginLabelUtilTest {
+
+    @Test
+    public void testCanonicalLabels() {
+        JSONArray labels = new JSONArray();
+        labels.add("slaves");
+        labels.add("api-plugin");
+        labels.add("library");
+        assertArrayEquals(new String[]{"agent", "api-plugin"},
+                PluginLabelUtil.canonicalLabels(labels));
+    }
+}
