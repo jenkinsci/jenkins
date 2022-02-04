525c4387196a359d29ef066886abf0df554250ac
Simon Sohrt <sohrt@his.de>
2022-02-04 14:59:22 +0100

    [JENKINS-67237] Attempt to fix a broken test

--- test/src/test/java/hudson/model/QueueTest.java
+++ test/src/test/java/hudson/model/QueueTest.java
@@ -288,6 +288,8 @@ public class QueueTest {
         assertTrue("Got "+items[0], items[0] instanceof BlockedItem);
 
         q.save();
+        //For testing purposes we join with the dependency graph thread
+        r.jenkins.getFutureDependencyGraph().get();
     }
 
     public static final class FileItemPersistenceTestServlet extends HttpServlet {
