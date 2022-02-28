b937f77335773ee4229447963ea9cc20036a56a4
Zbynek Konecny <zbynek1729@gmail.com>
2022-02-28 01:47:35 +0100

    Remove unused import

--- core/src/main/java/hudson/Functions.java
+++ core/src/main/java/hudson/Functions.java
@@ -130,7 +130,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TimeZone;
