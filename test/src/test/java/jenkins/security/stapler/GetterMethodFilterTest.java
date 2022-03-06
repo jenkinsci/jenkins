8c4844c1db89d5e420571800c5c7c7a25ee66cb8
Stefan Spieker <S.Spieker@gmx.net>
2022-03-06 09:00:27 +0100

    fixed imports

--- test/src/test/java/jenkins/security/stapler/GetterMethodFilterTest.java
+++ test/src/test/java/jenkins/security/stapler/GetterMethodFilterTest.java
@@ -36,7 +36,6 @@ import java.util.List;
 import java.util.Map;
 import jenkins.model.Jenkins;
 import org.apache.commons.codec.Encoder;
-import org.apache.commons.codec.EncoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 import org.jvnet.hudson.test.For;
--- test/src/test/java/jenkins/security/stapler/StaticRoutingDecisionProvider2Test.java
+++ test/src/test/java/jenkins/security/stapler/StaticRoutingDecisionProvider2Test.java
@@ -34,7 +34,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-
 import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
