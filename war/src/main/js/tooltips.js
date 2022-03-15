940d51af70026e1f12751d8baea7aacb1adf0f0a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-03-15 00:47:18 +0000

    WB

--- war/src/main/js/tooltips.js
+++ war/src/main/js/tooltips.js
@@ -4,7 +4,8 @@ tippy("[tooltip]", {
   content: element => element.getAttribute("tooltip"),
   arrow: false,
   theme: "tooltip",
-  animation: "tooltip"
+  animation: "tooltip",
+  appendTo: document.body,
 })
 
 tippy("[html-tooltip]", {
@@ -12,19 +13,25 @@ tippy("[html-tooltip]", {
   allowHTML: true,
   arrow: false,
   theme: "tooltip",
-  animation: "tooltip"
+  animation: "tooltip",
+  appendTo: document.body,
 })
 
+/**
+ * Displays a tooltip for three seconds on the provided element after interaction
+ * @param {string} text - The tooltip text
+ * @param {HTMLElement} element - The element to show the tooltip
+ */
 function hoverNotification(text, element) {
   let tooltip = tippy(element, {
     interactive: true,
     trigger: "hover",
-    followCursor: "initial",
     arrow: false,
     theme: "tooltip",
     offset: [0, 0],
     animation: "tooltip",
     content: text,
+    appendTo: document.body,
     onShow(instance) {
       setTimeout(() => {
         instance.hide()
