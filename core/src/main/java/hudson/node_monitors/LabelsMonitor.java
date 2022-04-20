52deec362bd996c62c030e658f115d634518ac2a
Markus Winter <m.winter@sap.com>
2022-04-20 14:44:23 +0200

    [JENKINS-43727] show assigned labels of agents
    If enabled this monitor will show the labels of agents on the /computer
    overview page.
    The maximum number of labels to show is configurable. When more labels
    are defined, those will be shown when hovering over the list.
    
    This is no using model-link as this doesn't work together with the
    hovering.

---
+++ core/src/main/java/hudson/node_monitors/LabelsMonitor.java
@@ -0,0 +1,80 @@
+package hudson.node_monitors;
+
+import hudson.Extension;
+import hudson.model.Computer;
+import hudson.model.Label;
+import hudson.model.Node;
+import hudson.model.labels.LabelAtom;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class LabelsMonitor extends NodeMonitor
+{
+
+  private boolean includeDynamic = false;
+  private int maxLabelCount = 5;
+
+  public LabelsMonitor() {}
+
+  @DataBoundConstructor
+  public LabelsMonitor(boolean includeDynamic, int maxLabelCount)
+  {
+    this.includeDynamic = includeDynamic;
+    if (maxLabelCount == 0) {
+      maxLabelCount = 1;
+    }
+    this.maxLabelCount = maxLabelCount;
+  }
+
+  @Override
+  public String getColumnCaption()
+  {
+    if (!isIgnored())
+      return Messages.LabelsMonitor_DisplayName();
+    return null;
+  }
+
+  public boolean getIncludeDynamic()
+  {
+    return includeDynamic;
+  }
+
+  public int getMaxLabelCount()
+  {
+    return maxLabelCount;
+  }
+
+  @Override
+  public Object data(Computer c) {
+    Node n = c.getNode();
+    Set<LabelAtom> data = Collections.emptySet();
+    if (n != null)
+    {
+      data = Label.parse(n.getLabelString());
+      if (includeDynamic)
+      {
+        data.addAll(n.getDynamicLabels());
+      }
+    }
+    return data;
+  }
+
+  @Extension
+  @Symbol("labels")
+  public static class DescriptorImpl extends AbstractNodeMonitorDescriptor<Set<LabelAtom>> {
+
+    @Override
+    public String getDisplayName() {
+        return Messages.LabelsMonitor_DisplayName();
+    }
+
+    @Override
+    protected Set<LabelAtom> monitor(Computer c) throws IOException, InterruptedException
+    {
+      return null;
+    }
+  }
+}
