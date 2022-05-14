752dc644b2a913d50890b9d5028a53510d795ab7
Basil Crow <me@basilcrow.com>
2022-05-14 11:15:50 -0700

    Be more flexible

--- core/src/main/java/jenkins/util/SetContextClassLoader.java
+++ core/src/main/java/jenkins/util/SetContextClassLoader.java
@@ -5,13 +5,13 @@ import hudson.remoting.ObjectInputStreamEx;
 import java.io.ObjectInputStream;
 
 /**
- * Java defines a {@link Thread#getContextClassLoader()}. Jenkins does not use this much; it will
+ * Java defines a {@link Thread#contextClassLoader}. Jenkins does not use this much; it will
  * normally be set by the servlet container to the Jenkins core class loader.
  *
  * <p>Some Java libraries have a fundamental design flaw, originating in premodular systems with a
- * "flat classpath", whereby they expect {@link Thread#getContextClassLoader()} to have access to
- * the same classes as the class loader of the calling class. This fails in Jenkins, because {@link
- * Thread#getContextClassLoader()} can only see Jenkins core, not plugins.
+ * "flat classpath", whereby they expect {@link Thread#contextClassLoader} to have access to the
+ * same classes as the class loader of the calling class. This fails in Jenkins, because {@link
+ * Thread#contextClassLoader} can only see Jenkins core, not plugins.
  *
  * <p>It is a design flaw in the library if it fails to allow clients to directly specify a {@link
  * ClassLoader} to use for lookups (or preregister {@link Class} instances for particular names).
@@ -34,12 +34,14 @@ import java.io.ObjectInputStream;
  * }
  * </pre>
  *
- * <p>When called from a plugin, the {@code clazz} argument should typically be the calling class,
- * whose class loader has access to all the plugin's direct and transitive dependencies. When the
- * particular class loader needed is unclear, {@link PluginManager.UberClassLoader} can be used as a
- * fallback, though this is not as safe since lookups could be ambiguous in case two unrelated
- * plugins both bundled the same library. In functional tests, {@code RealJenkinsRule.Endpoint} can
- * be used to reference a class loader that has access to the plugins defined in the test scenario.
+ * <p>When called from a plugin, {@link #SetContextClassLoader(Class)} should typically be used, its
+ * argument being the calling class, whose class loader has access to all the plugin's direct and
+ * transitive dependencies. When the particular class loader needed is unclear, {@link
+ * #SetContextClassLoader(ClassLoader)} can be used as a fallback with {@link
+ * PluginManager.UberClassLoader} as the argument, though this is not as safe since lookups could be
+ * ambiguous in case two unrelated plugins both bundled the same library. In functional tests,
+ * {@code RealJenkinsRule.Endpoint} can be used to reference a class loader that has access to the
+ * plugins defined in the test scenario.
  *
  * <p>See <a
  * href="https://www.jenkins.io/doc/developer/plugin-development/dependencies-and-class-loading/#context-class-loaders">the
@@ -56,6 +58,12 @@ public final class SetContextClassLoader implements AutoCloseable {
         t = Thread.currentThread();
         orig = t.getContextClassLoader();
         t.setContextClassLoader(clazz.getClassLoader());
+    }
+
+    public SetContextClassLoader(ClassLoader cl) {
+        t = Thread.currentThread();
+        orig = t.getContextClassLoader();
+        t.setContextClassLoader(cl);
     }
 
     @Override
