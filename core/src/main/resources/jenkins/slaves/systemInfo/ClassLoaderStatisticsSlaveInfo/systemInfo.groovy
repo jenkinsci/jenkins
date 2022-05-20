8363a65dac573814cc5bd031b09bf8f09aee92d0
Mark Waite <mark.earl.waite@gmail.com>
2022-05-20 08:07:48 -0600

    Use thead to match existing layout
    Thanks to @NotMyFault for the pointers to improve the layout and
    simplify the code

--- core/src/main/resources/jenkins/slaves/systemInfo/ClassLoaderStatisticsSlaveInfo/systemInfo.groovy
+++ core/src/main/resources/jenkins/slaves/systemInfo/ClassLoaderStatisticsSlaveInfo/systemInfo.groovy
@@ -1,20 +1,21 @@
 import hudson.slaves.SlaveComputer
 
 def fmt = new java.text.DecimalFormat("0.0")
-def right = 'text-align: right'
 if (my instanceof SlaveComputer) {
     SlaveComputer c = my
 
     table(class: 'jenkins-table') {
-        tr {
-            th               { text(_('Loading Type')) }
-            th(style: right) { text(_('Time (s)')) }
-            th(style: right) { text(_('Count')) }
+        thead {
+            tr {
+                th { text(_('Loading Type')) }
+                th { text(_('Time (s)')) }
+                th { text(_('Count')) }
+            }
         }
         tr {
             td _('Classes')
-            td(style: right) {text(fmt.format(c.classLoadingTime / 1000000000))}
-            td(style: right) {
+            td {text(fmt.format(c.classLoadingTime / 1000000000))}
+            td {
                 text(c.classLoadingCount)
                 def classLoadingPrefetchCacheCount = c.classLoadingPrefetchCacheCount
                 if (classLoadingPrefetchCacheCount != -1) {
@@ -26,8 +27,8 @@ if (my instanceof SlaveComputer) {
         }
         tr {
             td _('Resources')
-            td(style: right) {text(fmt.format(c.resourceLoadingTime / 1000000000))}
-            td(style: right) {text(c.resourceLoadingCount)}
+            td {text(fmt.format(c.resourceLoadingTime / 1000000000))}
+            td {text(c.resourceLoadingCount)}
         }
     }
 }
