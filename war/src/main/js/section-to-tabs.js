67abb94b78f82454396919c2ed958b83089a911c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-06-09 12:26:27 +0100

    Init

--- war/src/main/js/section-to-tabs.js
+++ war/src/main/js/section-to-tabs.js
@@ -7,11 +7,17 @@ tabPanes.forEach((tabPane) => {
   tabPane.style.display = "none"
 })
 
-// Show the first
+// Show the first tab pane
 tabPanes[0].style.display = "block"
 
 const tabBar = document.createElement("div")
-tabBar.className = "tabBar"
+tabBar.className = "jenkins-tabs jenkins-tabs--js"
+
+const activeTabBackdrop = document.createElement("div")
+activeTabBackdrop.className = "jenkins-tabs__tab-backdrop"
+tabBar.append(activeTabBackdrop)
+
+content.insertBefore(tabBar, tabPanes[0])
 
 // Add tabs for each tab pane
 tabPanes.forEach((tabPane, index) => {
@@ -19,30 +25,29 @@ tabPanes.forEach((tabPane, index) => {
   tabPaneTitle.style.display = "none"
 
   const tab = document.createElement("div")
-  tab.className = "tab"
+  tab.className = "jenkins-tabs__tab"
+  tab.innerText = tabPaneTitle.textContent
 
-  if (index === 0) {
-    tab.classList.add("active")
-  }
+  tabBar.append(tab)
 
   tab.addEventListener("click", function () {
-    document.querySelectorAll(".tab").forEach((tab) => {
-      tab.classList.remove("active")
+    document.querySelectorAll(".jenkins-tabs__tab").forEach((tab) => {
+      tab.classList.remove("jenkins-tabs__tab--active")
     })
-    tab.classList.add("active")
+    tab.classList.add("jenkins-tabs__tab--active")
 
     tabPanes.forEach((tabPane) => {
       tabPane.style.display = "none"
     })
     tabPanes[index].style.display = "block"
+
+    const leftOffset = tab.getBoundingClientRect().left - tabBar.getBoundingClientRect().left
+    activeTabBackdrop.style.left = leftOffset + "px"
+    activeTabBackdrop.style.width = tab.getBoundingClientRect().width + "px"
   })
 
-  const tabLink = document.createElement("a")
-  tabLink.innerText = tabPaneTitle.textContent
-
-  tab.append(tabLink)
-
-  tabBar.append(tab)
+  // Select the first tab
+  if (index === 0) {
+    tab.click()
+  }
 })
-
-content.insertBefore(tabBar, content.children[0])
