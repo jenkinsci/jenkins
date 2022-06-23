9228c0e5baef679039396f51579d205ec6b7dfc1
Kevin-CB <kguerroudj@cloudbees.com>
2022-06-23 08:29:07 +0200

    IconSet doesn't reset attributes with ampersand

---
+++ core/src/test/java/org/jenkins/ui/icon/IconSetWithAmpersandTest.java
@@ -0,0 +1,22 @@
+package org.jenkins.ui.icon;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+
+import org.junit.jupiter.api.Test;
+
+public class IconSetWithAmpersandTest {
+
+    /**
+     * Culprit: https://github.com/Kevin-CB/jenkins/blob/49c4cda2d41719d66a4eb4d6f9c31ba8298f2fbf/core/src/main/java/org/jenkins/ui/icon/IconSet.java#L94
+     * If the tooltip contains an ampersand symbol (&), it won't be removed.
+     */
+    @Test
+    void getSymbol_notSettingTooltipDoesntAddTooltipAttribute_evenWithAmpersand() {
+        String symbolWithTooltip = IconSet.getSymbol("download", "Title", "With&Ampersand", "class1 class2", "", "id");
+        String symbolWithoutTooltip = IconSet.getSymbol("download", "Title", "", "class1 class2", "", "id");
+
+        assertThat(symbolWithoutTooltip, not(containsString("tooltip")));
+    }
+}
