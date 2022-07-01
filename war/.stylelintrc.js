674e24752333604f838afc3bee8a37fbb86abe30
Alexander Brandes <mc.cache@web.de>
2022-07-01 23:31:15 +0200

    style: Put line length in place

--- war/.stylelintrc.js
+++ war/.stylelintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   rules: {
     indentation: null,
     "selector-list-comma-newline-after": "never-multi-line",
-    "selector-list-comma-space-after": "always"
+    "selector-list-comma-space-after": "always",
+    "max-line-length": 150
   },
   // Keeps the default level to warn to avoid breaking the current
   // CI build environment
