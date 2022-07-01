a499d885928b6253d805d5cff1fb41537aa05813
Alexander Brandes <mc.cache@web.de>
2022-07-01 21:44:41 +0200

    style: Put a couple of stylelint rules in place

--- war/.stylelintrc.js
+++ war/.stylelintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    indentation: null
+    indentation: null,
+    "selector-list-comma-newline-after": "never-multi-line",
+    "selector-list-comma-space-after": "always"
   },
   // Keeps the default level to warn to avoid breaking the current
   // CI build environment
