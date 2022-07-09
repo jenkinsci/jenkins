17787412fef37308750c912ad38c93319e8b3841
Basil Crow <me@basilcrow.com>
2022-07-09 11:34:30 -0700

    Move to primary test class

--- core/src/test/java/org/jenkins/ui/icon/IconSetTest.java
+++ core/src/test/java/org/jenkins/ui/icon/IconSetTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.not;
 
 import java.util.Map;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class IconSetTest {
@@ -65,5 +66,18 @@ public class IconSetTest {
         String symbol = IconSet.getSymbol("download", "Title", "", "class1 class2", "", "id");
 
         assertThat(symbol, not(containsString("tooltip")));
+    }
+
+    /**
+     * Culprit: https://github.com/jenkinsci/jenkins/blob/ab0bb8495819bd807a9211ac0df3f08e420226f1/core/src/main/java/org/jenkins/ui/icon/IconSet.java#L97=
+     * If the tooltip contains an ampersand symbol (&amp;), it won't be removed.
+     */
+    @Disabled("TODO see JENKINS-68805")
+    @Test
+    void getSymbol_notSettingTooltipDoesntAddTooltipAttribute_evenWithAmpersand() {
+        String symbolWithTooltip = IconSet.getSymbol("download", "Title", "With&Ampersand", "class1 class2", "", "id");
+        String symbolWithoutTooltip = IconSet.getSymbol("download", "Title", "", "class1 class2", "", "id");
+
+        assertThat(symbolWithoutTooltip, not(containsString("tooltip")));
     }
 }
