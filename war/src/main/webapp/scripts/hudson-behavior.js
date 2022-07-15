2000e610aa48e4851ccd7be289ca3cc8146e98ea
Daniel Beck <daniel-beck@users.noreply.github.com>
2022-07-15 21:15:57 +0200

    [JENKINS-60866] Extract inline JS for stop button

--- war/src/main/webapp/scripts/hudson-behavior.js
+++ war/src/main/webapp/scripts/hudson-behavior.js
@@ -1503,6 +1503,21 @@ function rowvgStartEachRow(recursive,f) {
         spanTag.innerHTML = labelText
         label.appendChild(spanTag)
     });
+
+    // stop button JS cannot be done as adjunct, as it can be inside an Ajax response
+    Behaviour.specify('.stop-button-link', 'stop-button-link', 0, function(link) {
+        let question = link.getAttribute('data-confirm');
+        let url = link.getAttribute('href');
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            if (question !== null) {
+                if (!confirm(question)) {
+                    return;
+                }
+            }
+            new Ajax.Request(url);
+        });
+    });
 })();
 
 var hudsonRules = {}; // legacy name
