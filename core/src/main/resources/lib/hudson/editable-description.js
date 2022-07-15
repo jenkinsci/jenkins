94d5752aef054d4ba501ff356976445ab2af2fb9
Daniel Beck <daniel-beck@users.noreply.github.com>
2022-07-15 17:22:16 +0200

    Add inline editing of node description

---
+++ core/src/main/resources/lib/hudson/editable-description.js
@@ -0,0 +1,15 @@
+(function(){
+  document.addEventListener("DOMContentLoaded", function() {
+    let descriptionLink = document.querySelector('#description-link');
+    descriptionLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      let url = descriptionLink.getAttribute('data-url');
+      let description = descriptionLink.getAttribute('data-description');
+      if (url == null && description == null) {
+        return replaceDescription();
+      } else {
+        return replaceDescription(description, url);
+      }
+    });
+  });
+})();
