bc489d69ea8476852e1858cd3459f65f61448d00
Tim Jacomb <timjacomb1@gmail.com>
2022-08-25 22:31:13 +0100

    Fix all lint errors

--- core/src/main/resources/lib/hudson/projectViewNested.js
+++ core/src/main/resources/lib/hudson/projectViewNested.js
@@ -21,34 +21,39 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-Behaviour.specify("IMG.treeview-fold-control", 'projectViewNested', 0, function(e) {
-    e.onexpanded = function() {
-        var img = this;
-        var tr = findAncestor(img, "TR");
-        var tail = tr.nextSibling;
+Behaviour.specify(
+  "IMG.treeview-fold-control",
+  "projectViewNested",
+  0,
+  function (e) {
+    e.onexpanded = function () {
+      var img = this;
+      var tr = findAncestor(img, "TR");
+      var tail = tr.nextSibling;
 
-        img.oncollapsed = function() {
-            while (tr.nextSibling != tail) {
-              tr.nextSibling.remove();
-            }
-        };
+      img.oncollapsed = function () {
+        while (tr.nextSibling != tail) {
+          tr.nextSibling.remove();
+        }
+      };
 
-        // fetch the nested view and load it when it's ready
-        new Ajax.Request(img.getAttribute("url"), {
-            method : 'post',
-            onComplete : function(x) {
-                var cont = document.createElement("div");
-                cont.innerHTML = x.responseText;
-                var rows = $A(cont.firstElementChild.rows);
-                var anim = { opacity: { from:0, to:1 } };
-                rows.reverse().each(function(r) {
-                    YAHOO.util.Dom.setStyle(r, 'opacity', 0); // hide
-                    YAHOO.util.Dom.insertAfter(r, tr);
-                    Behaviour.applySubtree(r);
-                    new YAHOO.util.Anim(r, anim, 0.3).animate();
-                });
-            }
-        });
+      // fetch the nested view and load it when it's ready
+      new Ajax.Request(img.getAttribute("url"), {
+        method: "post",
+        onComplete: function (x) {
+          var cont = document.createElement("div");
+          cont.innerHTML = x.responseText;
+          var rows = $A(cont.firstElementChild.rows);
+          var anim = { opacity: { from: 0, to: 1 } };
+          rows.reverse().each(function (r) {
+            YAHOO.util.Dom.setStyle(r, "opacity", 0); // hide
+            YAHOO.util.Dom.insertAfter(r, tr);
+            Behaviour.applySubtree(r);
+            new YAHOO.util.Anim(r, anim, 0.3).animate();
+          });
+        },
+      });
     };
     e = null;
-});
+  }
+);
