51d279279f6355f691242c0968c42a018685544e
Tim Jacomb <timjacomb1@gmail.com>
2022-08-25 22:11:48 +0100

    Fix all lint errors

--- war/.eslintrc.js
+++ war/.eslintrc.js
@@ -1,36 +1,35 @@
 /* eslint-env node */
 module.exports = {
-    env: {
-        browser: true,
-        es6: true
-    },
-    // Uses eslint default ruleset
-    extends: ["eslint:recommended", "prettier"],
-    plugins: [
-        // Keeps the default level to warn to avoid breaking the current
-        // CI build environment
-        "only-warn"
-    ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module"
-    },
-    rules: {
-    },
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
+  env: {
+    browser: true,
+    es6: true,
+  },
+  // Uses eslint default ruleset
+  extends: ["eslint:recommended", "prettier"],
+  plugins: [
+    // Keeps the default level to warn to avoid breaking the current
+    // CI build environment
+    "only-warn",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {},
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
 
-        '__dirname': false,
+    __dirname: false,
 
-        // Allow jest globals used in tests
-        jest: false,
-        expect: false,
-        it: false,
-        describe: false,
-        beforeEach: false,
-        afterEach: false,
-        beforeAll: false,
-        afterAll: false,
-    }
+    // Allow jest globals used in tests
+    jest: false,
+    expect: false,
+    it: false,
+    describe: false,
+    beforeEach: false,
+    afterEach: false,
+    beforeAll: false,
+    afterAll: false,
+  },
 };
--- war/.stylelintrc.js
+++ war/.stylelintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    indentation: null
+    indentation: null,
   },
   // Keeps the default level to warn to avoid breaking the current
   // CI build environment
-  defaultSeverity: "warning"
-}
+  defaultSeverity: "warning",
+};
