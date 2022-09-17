062b1dfa392b97d5b72fc8dbfd47b0443c223234
benebsiny <stu995106@hotmail.com.tw>
2022-09-17 12:16:48 +0800

    Show reset button only when value is different from default
    * Set the correct default value
    * Fix the indent

--- core/src/main/resources/hudson/PluginManager/_updateSite.js
+++ core/src/main/resources/hudson/PluginManager/_updateSite.js
@@ -1,6 +1,23 @@
+(function () {
+  showResetToDefaultButtonOrNot();
+})();
+
+function showResetToDefaultButtonOrNot() {
+  const resetButton = document.getElementById("reset-to-default");
+  const siteUrlInput = document.getElementById("update-site-url");
+  if (siteUrlInput.value === "https://updates.jenkins.io/update-center.json") {
+    resetButton.style.display = "none";
+  } else {
+    resetButton.style.display = "";
+  }
+}
+
+document.getElementById("update-site-url").onchange =
+  showResetToDefaultButtonOrNot;
+
 document.getElementById("reset-to-default").onclick = function (event) {
   event.preventDefault();
   const siteUrlInput = document.getElementById("update-site-url");
-  siteUrlInput.value = "https://updates.jenkins.io/current/update-center.json";
+  siteUrlInput.value = "https://updates.jenkins.io/update-center.json";
   siteUrlInput.dispatchEvent(new Event("change"));
 };
