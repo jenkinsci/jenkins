f41e7fdaa736ef3ceba4ecf548ee9c20fb6ef790
Vincent Latombe <vincent@latombe.net>
2022-09-23 09:30:58 +0200

    Add assertions for the symbol content used for testing

--- core/src/test/java/org/jenkins/ui/symbol/SymbolTest.java
+++ core/src/test/java/org/jenkins/ui/symbol/SymbolTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 public class SymbolTest {
 
+    public static final String SCIENCE_PATH = "<path d=\"M13,11.33L18,18H6l5-6.67V6h2 M15.96,4H8.04C7.62,4,7.39,4.48,7.65,4.81L9,6.5v4.17L3.2,18.4C2.71,19.06,3.18,20,4,20h16 c0.82,0,1.29-0.94,0.8-1.6L15,10.67V6.5l1.35-1.69C16.61,4.48,16.38,4,15.96,4L15.96,4z\"/>";
+
     @Test
     @DisplayName("Get symbol should build the symbol with given attributes")
     void getSymbol() {
@@ -20,6 +22,7 @@ public class SymbolTest {
                                            .withId("id")
                                            .build()
         );
+        assertThat(symbol, containsString(SCIENCE_PATH));
         assertThat(symbol, containsString("<span class=\"jenkins-visually-hidden\">Title</span>"));
         assertThat(symbol, containsString("tooltip=\"Tooltip\""));
         assertThat(symbol, containsString("class=\"class1 class2\""));
@@ -39,6 +42,7 @@ public class SymbolTest {
         );
         String symbol = Symbol.get(new SymbolRequest.Builder().withName("science").build());
 
+        assertThat(symbol, containsString(SCIENCE_PATH));
         assertThat(symbol, not(containsString("<span class=\"jenkins-visually-hidden\">Title</span>")));
         assertThat(symbol, not(containsString("tooltip=\"Tooltip\"")));
         assertThat(symbol, not(containsString("class=\"class1 class2\"")));
@@ -66,6 +70,7 @@ public class SymbolTest {
                                            .build()
         );
 
+        assertThat(symbol, containsString(SCIENCE_PATH));
         assertThat(symbol, not(containsString("<span class=\"jenkins-visually-hidden\">Title</span>")));
         assertThat(symbol, not(containsString("tooltip=\"Tooltip\"")));
         assertThat(symbol, not(containsString("class=\"class1 class2\"")));
@@ -91,6 +96,7 @@ public class SymbolTest {
                            .build()
         );
 
+        assertThat(symbol, containsString(SCIENCE_PATH));
         assertThat(symbol, not(containsString("tooltip")));
     }
 
@@ -101,6 +107,7 @@ public class SymbolTest {
                                            .withName("missing-icon")
                                            .build()
         );
+        assertThat(symbol, not(containsString(SCIENCE_PATH)));
         assertThat(symbol, containsString(Symbol.PLACEHOLDER_MATCHER));
     }
 
@@ -113,9 +120,13 @@ public class SymbolTest {
                 .withTooltip("With&Ampersand")
                 .withClasses("class1 class2")
                 .withId("id");
-        assertThat(Symbol.get(builder.build()), containsString("tooltip"));
+        String symbol = Symbol.get(builder.build());
+        assertThat(symbol, containsString(SCIENCE_PATH));
+        assertThat(symbol, containsString("tooltip"));
         // Remove tooltip
         builder.withTooltip(null);
-        assertThat(Symbol.get(builder.build()), not(containsString("tooltip")));
+        symbol = Symbol.get(builder.build());
+        assertThat(symbol, containsString(SCIENCE_PATH));
+        assertThat(symbol, not(containsString("tooltip")));
     }
 }
