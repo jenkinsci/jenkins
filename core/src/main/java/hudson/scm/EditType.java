2b9621cd14a5a2a9bf74ec5d684f2c80f4e11e05
Elton Alves <eltim.alves@gmail.com>
2022-10-07 13:44:05 -0300

    fix: covering every three states.

--- core/src/main/java/hudson/scm/EditType.java
+++ core/src/main/java/hudson/scm/EditType.java
@@ -56,7 +56,7 @@ public final class EditType implements CustomExportedBean {
 
     public static final EditType ADD = new EditType("add", "The file was added");
     public static final EditType EDIT = new EditType("edit", "The file was modified");
-    public static final EditType DELETE = new EditType("delete", "The file was removed");
+    public static final EditType DELETE = new EditType("trash", "The file was removed");
 
     public static final List<EditType> ALL = List.of(ADD, EDIT, DELETE);
 }
