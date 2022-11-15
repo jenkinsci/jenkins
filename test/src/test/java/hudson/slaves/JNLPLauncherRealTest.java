b1d06d25d2a8d147105ab4cecb7cc71f380631c0
Vincent Latombe <vincent@latombe.net>
2022-11-15 16:44:01 +0100

    Restore some initial assertions

--- test/src/test/java/hudson/slaves/JNLPLauncherRealTest.java
+++ test/src/test/java/hudson/slaves/JNLPLauncherRealTest.java
@@ -27,9 +27,12 @@ package hudson.slaves;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import hudson.ExtensionList;
+import hudson.PluginWrapper;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Slave;
+import hudson.util.FormValidation;
 import java.io.File;
 import jenkins.agents.WebSocketAgentsTest;
 import jenkins.slaves.JnlpSlaveAgentProtocol4;
@@ -91,6 +94,10 @@ public class JNLPLauncherRealTest {
 
         @Override
         public void run(JenkinsRule r) throws Throwable {
+            for (PluginWrapper plugin : r.jenkins.pluginManager.getPlugins()) {
+                System.err.println(plugin + " active=" + plugin.isActive() + " enabled=" + plugin.isEnabled());
+            }
+            assertThat(ExtensionList.lookupSingleton(JNLPLauncher.DescriptorImpl.class).doCheckWebSocket(webSocket, null).kind, is(FormValidation.Kind.OK));
             Slave agent = (Slave) r.jenkins.getNode(agentName);
             r.waitOnline(agent);
             FreeStyleProject p = r.createFreeStyleProject();
