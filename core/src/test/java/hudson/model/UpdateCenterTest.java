72152ef90442245a1ed6454694a6e6d1790992ad
Basil Crow <me@basilcrow.com>
2022-11-16 15:39:50 -0800

    Undo changes to UpdateCenterTest

--- core/src/test/java/hudson/model/UpdateCenterTest.java
+++ core/src/test/java/hudson/model/UpdateCenterTest.java
@@ -17,35 +17,35 @@ public class UpdateCenterTest {
     @Test
     public void toUpdateCenterCheckUrl_http_noQuery() throws Exception {
         assertThat(UpdateCenter.UpdateCenterConfiguration.toUpdateCenterCheckUrl(
-                "http://updates.jenkins-ci.org/update-center.json").toString(),
+                "http://updates.jenkins-ci.org/update-center.json").toExternalForm(),
                 is("http://updates.jenkins-ci.org/update-center.json?uctest"));
     }
 
     @Test
     public void toUpdateCenterCheckUrl_https_noQuery() throws Exception {
         assertThat(UpdateCenter.UpdateCenterConfiguration.toUpdateCenterCheckUrl(
-                "https://updates.jenkins-ci.org/update-center.json").toString(),
+                "https://updates.jenkins-ci.org/update-center.json").toExternalForm(),
                 is("https://updates.jenkins-ci.org/update-center.json?uctest"));
     }
 
     @Test
     public void toUpdateCenterCheckUrl_http_query() throws Exception {
         assertThat(UpdateCenter.UpdateCenterConfiguration.toUpdateCenterCheckUrl(
-                "http://updates.jenkins-ci.org/update-center.json?version=2.7").toString(),
+                "http://updates.jenkins-ci.org/update-center.json?version=2.7").toExternalForm(),
                 is("http://updates.jenkins-ci.org/update-center.json?version=2.7&uctest"));
     }
 
     @Test
     public void toUpdateCenterCheckUrl_https_query() throws Exception {
         assertThat(UpdateCenter.UpdateCenterConfiguration.toUpdateCenterCheckUrl(
-                "https://updates.jenkins-ci.org/update-center.json?version=2.7").toString(),
+                "https://updates.jenkins-ci.org/update-center.json?version=2.7").toExternalForm(),
                 is("https://updates.jenkins-ci.org/update-center.json?version=2.7&uctest"));
     }
 
     @Test
     public void toUpdateCenterCheckUrl_file() throws Exception {
         assertThat(UpdateCenter.UpdateCenterConfiguration.toUpdateCenterCheckUrl(
-                "file://./foo.jar!update-center.json").toString(),
+                "file://./foo.jar!update-center.json").toExternalForm(),
                 is("file://./foo.jar!update-center.json"));
     }
 
