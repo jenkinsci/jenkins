0a95e3dea175c98d5dbd77f102e429c9f7ff1955
Jesse Glick <jglick@cloudbees.com>
2022-11-17 07:16:06 -0500

    Apply suggestions from code review
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- ath.sh
+++ ath.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 cd $(dirname $0)
 
 # https://github.com/jenkinsci/acceptance-test-harness/releases
-ATH_VERSION=5458.v911b_2f0818ee
+export ATH_VERSION=5458.v911b_2f0818ee
 
 # TODO use Artifactory proxy?
 
@@ -13,10 +13,10 @@ mkdir -p target/ath-reports
 chmod a+rwx target/ath-reports
 
 docker run --rm \
-  -e ATH_VERSION=$ATH_VERSION \
+  --env ATH_VERSION \
   --shm-size 2g `# avoid selenium.WebDriverException exceptions like 'Failed to decode response from marionette' and webdriver closed` \
-  -v $(pwd)/war/target/jenkins.war:/jenkins.war \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $(pwd)/target/ath-reports:/reports \
-  jenkins/ath:$ATH_VERSION \
+  --volume "$(pwd)"/war/target/jenkins.war:/jenkins.war:ro \
+  --volume /var/run/docker.sock:/var/run/docker.sock:rw \
+  --volume "$(pwd)"/target/ath-reports:/reports:rw \
+  jenkins/ath:"$ATH_VERSION" \
   bash -c 'cd && eval $(vnc.sh) && env | sort && git clone -b $ATH_VERSION --depth 1 https://github.com/jenkinsci/acceptance-test-harness && cd acceptance-test-harness && run.sh firefox /jenkins.war -Dmaven.test.failure.ignore -DforkCount=1 -Dgroups=org.jenkinsci.test.acceptance.junit.SmokeTest && cp -v target/surefire-reports/TEST-*.xml /reports'
