787fc62073f0b8fa1be41f682a1a86d8a82b9dc4
Jesse Glick <jglick@cloudbees.com>
2022-11-29 03:00:47 -0500

    Missing `since` tag on `ErrorLoggingExecutorService` (#7456)

--- core/src/main/java/jenkins/util/ErrorLoggingExecutorService.java
+++ core/src/main/java/jenkins/util/ErrorLoggingExecutorService.java
@@ -34,6 +34,7 @@ import java.util.logging.Logger;
  * Executor service that logs unchecked exceptions / errors in {@link Runnable}.
  * Exceptions thrown from {@link Callable} are <em>not</em> not logged,
  * under the assumption that something is checking {@link Future#get()}.
+ * @since TODO
  */
 public class ErrorLoggingExecutorService extends InterceptingExecutorService {
 
