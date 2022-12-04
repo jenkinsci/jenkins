0e365445d7ed65c1ed3bfa6646c222ce52f7ff76
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-12-04 21:34:31 +0000

    Remove old import

--- war/src/main/js/components/dropdowns/index.js
+++ war/src/main/js/components/dropdowns/index.js
@@ -1,6 +1,5 @@
 import Jumplists from "@/components/dropdowns/jumplists";
 import InpageJumplist from "@/components/dropdowns/inpage-jumplist";
-import Overflow from "@/components/dropdowns/overflow";
 
 function init() {
   Jumplists.init();
