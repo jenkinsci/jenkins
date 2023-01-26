d2c6b03a12a42fbf0a2f665cda507f81744197fa
Vincent Latombe <vincent@latombe.net>
2023-01-26 10:23:56 +0100

    Computer links are incorrect in some cases
    https://github.com/jenkinsci/jenkins/pull/6907 computes computer urls based on
    privilege, which can cause inconsistent breadcrumb update as a result.
    
    $jenkinsUrl/manage/computer/ is typically the URL you access when
    browsing "Manage Jenkins > Configure Nodes and Clouds"
    
    vs.
    
    $jenkinsURl/computer/ which can be accessed by clicking on "Build
    Executor Status"

--- core/src/main/java/hudson/model/Computer.java
+++ core/src/main/java/hudson/model/Computer.java
@@ -774,10 +774,6 @@ public /*transient*/ abstract class Computer extends Actionable implements Acces
     }
 
     public String getUrl() {
-        if (Jenkins.get().hasAnyPermission(Jenkins.MANAGE, Jenkins.SYSTEM_READ)) {
-            return "manage/computer/" + Util.fullEncode(getName()) + "/";
-        }
-
         return "computer/" + Util.fullEncode(getName()) + "/";
     }
 
