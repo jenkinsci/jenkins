4605c6ac671c984d3f411db8abd6fc660d1f0e57
Brian P. Hinz <bphinz@users.sf.net>
2023-03-12 12:28:43 -0400

    Fix merge conflicts

--- core/src/main/java/hudson/model/DirectoryBrowserSupport.java
+++ core/src/main/java/hudson/model/DirectoryBrowserSupport.java
@@ -49,6 +49,7 @@ import java.util.Objects;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
@@ -379,11 +380,11 @@ public final class DirectoryBrowserSupport implements HttpResponse {
         if (view) {
             InputStream in;
             try {
-                in = baseFile.open(getNoFollowLinks());
+                in = baseFile.open(getOpenOptions());
                 byte[] sig = new byte[2];
                 int len = in.read(sig, 0, 2);
                 in.close();
-                in = baseFile.open(getNoFollowLinks());
+                in = baseFile.open(getOpenOptions());
                 if (len == 2 && (int) ((sig[0] & 0xff) | ((sig[1] << 8) & 0xff00)) == GZIPInputStream.GZIP_MAGIC) {
                     in = new GZIPInputStream(in);
                 }
