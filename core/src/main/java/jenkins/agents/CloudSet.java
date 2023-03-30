3dc86ed075dc0202f66c794d3341d8d603e881aa
Vincent Latombe <vincent@latombe.net>
2023-03-30 16:38:01 +0200

    Add license header, remove StaplerFallback

--- core/src/main/java/jenkins/agents/CloudSet.java
+++ core/src/main/java/jenkins/agents/CloudSet.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2023, CloudBees Inc, and other contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package jenkins.agents;
 
 import hudson.Extension;
@@ -34,7 +58,7 @@ import org.kohsuke.stapler.verb.POST;
 
 @ExportedBean
 @Restricted(NoExternalUse.class)
-public class CloudSet extends AbstractModelObject implements Describable<CloudSet>, StaplerFallback, ModelObjectWithChildren, RootAction {
+public class CloudSet extends AbstractModelObject implements Describable<CloudSet>, ModelObjectWithChildren, RootAction {
     @Override
     public Descriptor<CloudSet> getDescriptor() {
         return Jenkins.get().getDescriptorOrDie(CloudSet.class);
@@ -86,9 +110,8 @@ public class CloudSet extends AbstractModelObject implements Describable<CloudSe
         return m;
     }
 
-    @Override
-    public Object getStaplerFallback() {
-        return Jenkins.get();
+    public Cloud getDynamic(String name, StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        return Jenkins.get().clouds.getByName(name);
     }
 
     /**
@@ -163,7 +186,7 @@ public class CloudSet extends AbstractModelObject implements Describable<CloudSe
             Cloud result = (Cloud) Jenkins.XSTREAM.fromXML(xml);
             jenkins.clouds.add(result);
             // send the browser to the config page
-            rsp.sendRedirect2(result.getUrl() + "configure");
+            rsp.sendRedirect2("../../" + result.getUrl() + "configure");
         } else {
             // proceed to step 2
             if (mode == null) {
