70835cded9d85e023fab2e1cf27a4247a91c6ade
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-04-06 15:03:29 +0100

    Init

--- war/src/main/js/page-init.js
+++ war/src/main/js/page-init.js
@@ -1,37 +1,35 @@
-import $ from "jquery";
 import jsModules from "jenkins-js-modules";
 
 /*
  * Page initialisation tasks.
  */
-
-$(function () {
+document.addEventListener("DOMContentLoaded", () => {
   loadScripts();
   loadCSS();
 });
 
 function loadScripts() {
-  $(".jenkins-js-load").each(function () {
-    var scriptUrl = $(this).attr("data-src");
+  document.querySelectorAll(".jenkins-js-load").forEach((element) => {
+    const scriptUrl = element.dataset.src;
     if (scriptUrl) {
       // jsModules.addScript will ensure that the script is
       // loaded once and once only. So, this can be considered
       // analogous to a client-side adjunct.
       jsModules.addScript(scriptUrl);
-      $(this).remove();
+      element.remove();
     }
   });
 }
 
 function loadCSS() {
-  $(".jenkins-css-load").each(function () {
-    var cssUrl = $(this).attr("data-src");
+  document.querySelectorAll(".jenkins-css-load").forEach((element) => {
+    const cssUrl = element.dataset.src;
     if (cssUrl) {
       // jsModules.addCSSToPage will ensure that the CSS is
       // loaded once and once only. So, this can be considered
       // analogous to a client-side adjunct.
       jsModules.addCSSToPage(cssUrl);
-      $(this).remove();
+      element.remove();
     }
   });
 }
