e939c48391e5e2e54e11c7c2711ed470ead0054c
Yaroslav Afenkin <yaroslavafenkin@users.noreply.github.com>
2023-04-27 14:43:30 +0300

    [JENKINS-71035] CSP compatibility

---
+++ core/src/main/resources/hudson/model/Run/_delete-retry.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("delete-retry-show-error")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("delete-error").style.display = "block";
+  });
