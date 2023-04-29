eb975dfccd970f33ed684dcdaed08bd79c96fb59
Mark Waite <mark.earl.waite@gmail.com>
2023-04-29 07:28:23 -0600

    Test the Alpine end of life admin monitors

---
+++ core/src/test/java/jenkins/monitor/EndOfLifeAlpine316AdminMonitorTest.java
@@ -0,0 +1,76 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2023 Mark Waite.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.monitor;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.time.LocalDate;
+import jenkins.model.Jenkins;
+import org.junit.Test;
+
+public class EndOfLifeAlpine316AdminMonitorTest {
+
+    private final EndOfLifeAlpine316AdminMonitor monitor = new EndOfLifeAlpine316AdminMonitor();
+
+    public EndOfLifeAlpine316AdminMonitorTest() {
+    }
+
+    @Test
+    public void testGetIdentifier() {
+        assertThat(monitor.getIdentifier(), is("alpine_316"));
+    }
+
+    @Test
+    public void testGetDependencyName() {
+        assertThat(monitor.getDependencyName(), is("Alpine 3.16"));
+    }
+
+    @Test
+    public void testGetDisplayName() {
+        assertThat(monitor.getDisplayName(), is("Alpine 3.16 end of life"));
+    }
+
+    @Test
+    public void testGetBeginDisplayDate() {
+        assertThat(monitor.getBeginDisplayDate(), is("2024-02-23"));
+    }
+
+    @Test
+    public void testGetDocumentationURL() {
+        assertThat(monitor.getDocumentationURL(), is("https://www.jenkins.io/redirect/dependency-end-of-life"));
+    }
+
+    @Test
+    public void testIsUnsupported() {
+        assertThat(monitor.isUnsupported(), is(LocalDate.now().isAfter(LocalDate.of(2024, 5, 23))));
+    }
+
+    @Test
+    public void testGetRequiredPermission() {
+        assertThat(monitor.getRequiredPermission(), is(Jenkins.SYSTEM_READ));
+    }
+
+}
