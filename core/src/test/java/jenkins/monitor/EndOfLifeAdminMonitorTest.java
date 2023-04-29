52f68f13d10d4f9dab42a158143ed07d1e48d893
Mark Waite <mark.earl.waite@gmail.com>
2023-04-29 00:05:14 -0600

    Correct the copyright

--- core/src/test/java/jenkins/monitor/EndOfLifeAdminMonitorTest.java
+++ core/src/test/java/jenkins/monitor/EndOfLifeAdminMonitorTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2023 mwaite.
+ * Copyright 2023 Mark Waite.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
