21aba67c319e073de17995974734a0530e4681c6
Yaroslav Afenkin <yaroslavafenkin@users.noreply.github.com>
2023-05-01 13:18:31 +0300

    [JENKINS-71038] CSP compatibility: PluginManager/installed.jelly

---
+++ core/src/main/resources/hudson/PluginManager/_installed.js
@@ -0,0 +1,49 @@
+(function () {
+  const restartRequired = document
+    .querySelector("#is-restart-required-for-completion")
+    .getAttribute("data-is-restart-required");
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document
+      .querySelectorAll(".plugin-manager-toggle-switch")
+      .forEach(function (toggle) {
+        toggle.addEventListener("click", flip);
+      });
+  });
+
+  function flip(o) {
+    let btn = Event.element(o);
+
+    // trigger
+    fetch(
+      btn.getAttribute("url") +
+        "/make" +
+        (btn.checked ? "Enabled" : "Disabled"),
+      {
+        method: "post",
+        headers: crumb.wrap({}),
+      }
+    ).then((rsp) => {
+      if (!rsp.ok) {
+        $("needRestart").innerHTML = rsp.responseText;
+      }
+      updateMsg(btn);
+    });
+  }
+
+  function updateMsg() {
+    // has anything changed since its original state?
+    // eslint-disable-next-line no-undef
+    let e = $A(Form.getInputs("plugins", "checkbox")).find(function (e) {
+      return String(e.checked) !== e.getAttribute("original");
+    });
+
+    if (restartRequired === "true") {
+      e = true;
+    }
+
+    $("needRestart").style.display = e != null ? "block" : "none";
+  }
+
+  updateMsg(); // set the initial state
+})();
