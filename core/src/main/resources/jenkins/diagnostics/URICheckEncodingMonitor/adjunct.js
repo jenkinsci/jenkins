379945afc8663ce205b47c401d5c0ec1825af601
Basil Crow <me@basilcrow.com>
2023-05-05 14:07:19 -0700

    Fix URICheckEncodingMonitor

--- core/src/main/resources/jenkins/diagnostics/URICheckEncodingMonitor/adjunct.js
+++ core/src/main/resources/jenkins/diagnostics/URICheckEncodingMonitor/adjunct.js
@@ -4,10 +4,14 @@ Behaviour.specify(
   0,
   function (element) {
     var url = element.getAttribute("data-url");
-    var params = { value: "\u57f7\u4e8b" };
-    new Ajax.Updater("URICheckEncodingMonitor-message", url, {
-      method: "get",
-      parameters: params,
+    var params = new URLSearchParams({ value: "\u57f7\u4e8b" });
+    fetch(url + "?" + params).then((rsp) => {
+      rsp.text().then((responseText) => {
+        var message = document.getElementById(
+          "URICheckEncodingMonitor-message"
+        );
+        message.innerHTML = responseText;
+      });
     });
   }
 );
