ca8958094cda3d6b848c9828e5e207ff1b5d82a6
Basil Crow <me@basilcrow.com>
2023-05-04 21:21:33 -0700

    Remove Prototype from various form elements

--- core/src/main/resources/lib/form/select/select.js
+++ core/src/main/resources/lib/form/select/select.js
@@ -4,7 +4,7 @@ function updateListBox(listBox, url, config) {
   config = config || {};
   config = object(config);
   var originalOnSuccess = config.onSuccess;
-  var l = $(listBox);
+  var l = listBox;
 
   // Hacky function to retrofit compatibility with tables-to-divs
   // If the <select> tag parent is a <td> element we can consider it's following a
@@ -43,7 +43,7 @@ function updateListBox(listBox, url, config) {
     status.innerHTML = "";
   }
   config.onSuccess = function (rsp) {
-    l.removeClassName("select-ajax-pending");
+    l.classList.remove("select-ajax-pending");
     var currentSelection = l.value;
 
     // clear the contents
@@ -75,7 +75,7 @@ function updateListBox(listBox, url, config) {
     }
   };
   config.onFailure = function (rsp) {
-    l.removeClassName("select-ajax-pending");
+    l.classList.remove("select-ajax-pending");
     status.innerHTML = rsp.responseText;
     if (status.firstElementChild) {
       status.firstElementChild.setAttribute("data-select-ajax-error", "true");
@@ -91,7 +91,7 @@ function updateListBox(listBox, url, config) {
     }
   };
 
-  l.addClassName("select-ajax-pending");
+  l.classList.add("select-ajax-pending");
   new Ajax.Request(url, config);
 }
 
