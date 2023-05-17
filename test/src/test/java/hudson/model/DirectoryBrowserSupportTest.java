72d2cb48db2b89bd36ac999cd7d4efcbf434a71d
Jesse Glick <jglick@cloudbees.com>
2023-05-17 19:21:40 -0400

    `DirectoryBrowserSupportTest.junctionAndSymlink_outsideWorkspace_areNotAllowed_windowsJunction` to clean up junctions it created

--- test/src/test/java/hudson/model/DirectoryBrowserSupportTest.java
+++ test/src/test/java/hudson/model/DirectoryBrowserSupportTest.java
@@ -54,6 +54,7 @@ import hudson.slaves.WorkspaceList;
 import hudson.tasks.ArtifactArchiver;
 import hudson.tasks.BatchFile;
 import hudson.tasks.Shell;
+import hudson.util.StreamTaskListener;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -919,6 +920,7 @@ public class DirectoryBrowserSupportTest {
             List<String> entryNames = getListOfEntriesInDownloadedZip((UnexpectedPage) zipPage);
             assertThat(entryNames, contains("intermediateFolder/public2.key"));
         }
+        new Launcher.LocalLauncher(StreamTaskListener.fromStderr()).launch().cmds("cmd", "/c", "rmdir", "/s", "/q", j.jenkins.getRootDir().getAbsolutePath()).start().join();
     }
 
     private List<String> getListOfEntriesInDownloadedZip(UnexpectedPage zipPage) throws Exception {
