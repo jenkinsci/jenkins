a5c42a7a5be966223054980a8c70c809029f7775
Mark Waite <mark.earl.waite@gmail.com>
2023-05-20 06:56:09 -0600

    Test pretty name with sample os-release data files

--- core/src/test/java/jenkins/monitor/EndOfLifeOperatingSystemAdminMonitorTest.java
+++ core/src/test/java/jenkins/monitor/EndOfLifeOperatingSystemAdminMonitorTest.java
@@ -76,12 +76,85 @@ public class EndOfLifeOperatingSystemAdminMonitorTest {
     }
 
     @Test
-    public void testReadPrettyName() {
-        assertThat(monitor.readPrettyName(new File("/etc/os-release"), "Red .* 8"), is("Red Hat Enterprise Linux 8.8 (Ootpa)"));
+    public void testReadPrettyNameAlpine314() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-alpine-3.14").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Alpine Linux v3.14"), is("Alpine Linux v3.14"));
     }
 
     @Test
-    public void testReadPrettyNameBadFile() {
-        assertThat(monitor.readPrettyName(new File("/this/file/does/not/exist"), "Red .* 8"), is(""));
+    public void testReadPrettyNameAlpine315() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-alpine-3.15").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Alpine Linux v3.15"), is("Alpine Linux v3.15"));
+    }
+
+    @Test
+    public void testReadPrettyNameAlpine316() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-alpine-3.16").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Alpine Linux v3.16"), is("Alpine Linux v3.16"));
+    }
+
+    @Test
+    public void testReadPrettyNameAlpine317() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-alpine-3.17").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Alpine Linux v3.17"), is("Alpine Linux v3.17"));
+    }
+
+    @Test
+    public void testReadPrettyNameAlpine318() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-alpine-3.18").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Alpine Linux v3.18"), is("Alpine Linux v3.18"));
+    }
+
+    @Test
+    public void testReadPrettyNameCentOS7() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-centos-7").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "CentOS Linux.* 7"), is("CentOS Linux 7 (Core)"));
+    }
+
+    @Test
+    public void testReadPrettyNameDebian10() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-debian-10").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Debian.* 10"), is("Debian GNU/Linux 10 (buster)"));
+    }
+
+    @Test
+    public void testReadPrettyNameFedora36() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-fedora-36").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Fedora.* 36"), is("Fedora Linux 36 (Container Image)"));
+    }
+
+    @Test
+    public void testReadPrettyNameFedora37() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-fedora-37").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Fedora.* 37"), is("Fedora Linux 37 (Container Image)"));
+    }
+
+    @Test
+    public void testReadPrettyNameOracle7() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-oracle-7").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Oracle Linux.* 7"), is("Oracle Linux Server 7.9"));
+    }
+
+    @Test
+    public void testReadPrettyNameRedHat7() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-redhat-7").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Red Hat Enterprise Linux.* 7"), is("Red Hat Enterprise Linux Server 7.9 (Maipo)"));
+    }
+
+    @Test
+    public void testReadPrettyNameScientific7() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-scientific-7").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Scientific Linux.* 7"), is("Scientific Linux 7.9 (Nitrogen)"));
+    }
+
+    @Test
+    public void testReadPrettyNameUbuntu18() throws Exception {
+        File releaseFile = new File(this.getClass().getResource("os-release-ubuntu-18.04").toURI());
+        assertThat(monitor.readPrettyName(releaseFile, "Ubuntu.* 18"), is("Ubuntu 18.04.6 LTS"));
+    }
+
+    @Test
+    public void testReadPrettyNameMissingFile() {
+        assertThat(monitor.readPrettyName(new File("/this/file/does/not/exist"), ".*"), is(""));
     }
 }
