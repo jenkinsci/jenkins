3e29347151b4b0181db5b6e9c2f55d91a2c09ad1
Markus Winter <m.winter@sap.com>
2023-06-10 16:01:01 +0200

    rewrite without using class keyword
    check input

--- war/src/main/js/components/dialogs/index.js
+++ war/src/main/js/components/dialogs/index.js
@@ -1,170 +1,180 @@
 import { createElementFromHtml } from "@/util/dom";
 import { CLOSE } from "@/util/symbols";
 
-class Dialog {
-  dialog;
-  input = null;
-  ok = null;
-  dialogType;
+var _defaults = {
+  title: null,
+  message: null,
+  cancel: false,
+  okText: "OK",
+  cancelText: "Cancel",
+  maxWidth: "475px",
+  minWidth: "",
+  type: "default",
+  hideCloseButton: false,
+  allowEmpty: false,
+};
 
-  static #defaults = {
-    title: null,
-    message: null,
-    cancel: false,
-    okText: "OK",
-    cancelText: "Cancel",
-    maxWidth: "475px",
-    minWidth: "",
-    type: "default",
-    hideCloseButton: false,
-  };
+var _typeClassMap = {
+  default: "",
+  destructive: "jenkins-!-destructive-color",
+};
 
-  static #typeClassMap = {
-    default: "",
-    destructive: "jenkins-!-destructive-color",
-  };
+function Dialog(dialogType, options) {
+  this.dialogType = dialogType;
+  this.options = Object.assign({}, _defaults, options);
+  this.init();
+}
 
-  constructor(dialogType, options) {
-    this.dialogType = dialogType;
-    this.options = Object.assign({}, Dialog.#defaults, options);
+Dialog.prototype.init = function () {
+  this.dialog = document.createElement("dialog");
+  this.dialog.classList.add("jenkins-dialog");
+  this.dialog.style.maxWidth = this.options.maxWidth;
+  this.dialog.style.maxWidth = this.options.minWidth;
 
-    this.dialog = document.createElement("dialog");
-    this.dialog.classList.add("jenkins-dialog");
-    this.dialog.style.maxWidth = options.maxWidth;
-    this.dialog.style.maxWidth = options.minWidth;
+  let contentStyle = "jenkins-dialog__contents";
+  if (this.options.title != null) {
+    const title = createElementFromHtml(`<div class='jenkins-dialog__title'/>`);
+    this.dialog.appendChild(title);
+    title.innerText = this.options.title;
+    contentStyle = "jenkins-dialog__contents_title";
+  }
 
-    let contentStyle = "jenkins-dialog__contents";
-    if (options.title != null) {
-      const title = createElementFromHtml(
-        `<div class='jenkins-dialog__title'/>`
+  if (this.dialogType === "modal") {
+    if (this.options.content != null) {
+      const content = createElementFromHtml(`<div class='${contentStyle}'/>`);
+      content.appendChild(this.options.content);
+      this.dialog.appendChild(content);
+    }
+    if (this.options.hideCloseButton !== true) {
+      const closeButton = createElementFromHtml(`
+          <button class="jenkins-dialog__close-button jenkins-button">
+            <span class="jenkins-visually-hidden">Close</span>
+            ${CLOSE}
+          </button>
+        `);
+      this.dialog.appendChild(closeButton);
+      closeButton.addEventListener("click", () =>
+        this.dialog.dispatchEvent(new Event("cancel"))
       );
-      this.dialog.appendChild(title);
-      title.innerText = options.title;
-      contentStyle = "jenkins-dialog__contents_title";
+    }
+    this.dialog.addEventListener("click", function (e) {
+      if (e.target !== e.currentTarget) {
+        return;
+      }
+      this.dispatchEvent(new Event("cancel"));
+    });
+    this.ok = null;
+  } else {
+    if (this.options.message != null) {
+      const message = createElementFromHtml(`<div class='${contentStyle}'/>`);
+      this.dialog.appendChild(message);
+      message.innerText = this.options.message;
     }
 
-    if (this.dialogType === "modal") {
-      if (options.content != null) {
-        const content = createElementFromHtml(`<div class='${contentStyle}'/>`);
-        content.appendChild(options.content);
-        this.dialog.appendChild(content);
+    if (this.dialogType === "prompt") {
+      let inputDiv = createElementFromHtml(`<div class="jenkins-dialog__input">
+          <input data-id="input" type="text" class='jenkins-input'></div>`);
+      this.dialog.appendChild(inputDiv);
+      this.input = inputDiv.querySelector("[data-id=input]");
+      if (!this.options.allowEmpty) {
+        this.input.addEventListener("input", () => this.checkInput());
       }
-      if (options.hideCloseButton !== true) {
-        const closeButton = createElementFromHtml(`
-            <button class="jenkins-dialog__close-button jenkins-button">
-              <span class="jenkins-visually-hidden">Close</span>
-              ${CLOSE}
-            </button>
-          `);
-        this.dialog.appendChild(closeButton);
-        closeButton.addEventListener("click", () =>
-          this.dialog.dispatchEvent(new Event("cancel"))
-        );
-      }
-      this.dialog.addEventListener("click", function (e) {
-        if (e.target !== e.currentTarget) {
-          return;
-        }
-        this.dispatchEvent(new Event("cancel"));
-      });
-      this.ok = null;
-    } else {
-      if (options.message != null) {
-        const message = createElementFromHtml(`<div class='${contentStyle}'/>`);
-        this.dialog.appendChild(message);
-        message.innerText = options.message;
-      }
+    }
 
-      if (this.dialogType === "prompt") {
-        let inputDiv =
-          createElementFromHtml(`<div class="jenkins-dialog__input">
-            <input data-id="input" type="text" class='jenkins-input'></div>`);
-        this.dialog.appendChild(inputDiv);
-        this.input = inputDiv.querySelector("[data-id=input]");
-      }
+    this.appendButtons();
 
-      this.#appendButtons();
-
-      this.dialog.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-          e.preventDefault();
+    this.dialog.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        if (this.ok.disabled == false) {
           this.ok.dispatchEvent(new Event("click"));
         }
-        if (e.key === "Escape") {
-          e.preventDefault();
-          this.dialog.dispatchEvent(new Event("cancel"));
-        }
-      });
-    }
-    document.body.appendChild(this.dialog);
-  }
-
-  #appendButtons() {
-    const buttons = createElementFromHtml(`<div
-        class="jenkins-buttons-row jenkins-buttons-row--equal-width jenkins-dialog__buttons">
-        <button data-id="ok" class="jenkins-button jenkins-button--primary ${
-          Dialog.#typeClassMap[this.options.type]
-        }">${this.options.okText}</button>
-        <button data-id="cancel" class="jenkins-button">${
-          this.options.cancelText
-        }</button>
-      </div>`);
-
-    this.dialog.appendChild(buttons);
-
-    this.ok = buttons.querySelector("[data-id=ok]");
-    this.cancel = buttons.querySelector("[data-id=cancel]");
-    if (!this.options.cancel) {
-      this.cancel.style.display = "none";
-    } else {
-      this.cancel.addEventListener("click", (e) => {
+      }
+      if (e.key === "Escape") {
         e.preventDefault();
         this.dialog.dispatchEvent(new Event("cancel"));
-      });
-    }
+      }
+    });
   }
+  document.body.appendChild(this.dialog);
+};
 
-  show() {
-    return new Promise((resolve, cancel) => {
-      this.dialog.showModal();
-      this.dialog.addEventListener(
-        "cancel",
+Dialog.prototype.checkInput = function () {
+  if (this.input.value.trim()) {
+    this.ok.disabled = false;
+  } else {
+    this.ok.disabled = true;
+  }
+};
+
+Dialog.prototype.appendButtons = function () {
+  const buttons = createElementFromHtml(`<div
+      class="jenkins-buttons-row jenkins-buttons-row--equal-width jenkins-dialog__buttons">
+      <button data-id="ok" class="jenkins-button jenkins-button--primary ${
+        _typeClassMap[this.options.type]
+      }">${this.options.okText}</button>
+      <button data-id="cancel" class="jenkins-button">${
+        this.options.cancelText
+      }</button>
+    </div>`);
+
+  this.dialog.appendChild(buttons);
+
+  this.ok = buttons.querySelector("[data-id=ok]");
+  this.cancel = buttons.querySelector("[data-id=cancel]");
+  if (!this.options.cancel) {
+    this.cancel.style.display = "none";
+  } else {
+    this.cancel.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.dialog.dispatchEvent(new Event("cancel"));
+    });
+  }
+  if (this.dialogType === "prompt" && !this.options.allowEmpty) {
+    this.ok.disabled = true;
+  }
+};
+
+Dialog.prototype.show = function () {
+  return new Promise((resolve, cancel) => {
+    this.dialog.showModal();
+    this.dialog.addEventListener(
+      "cancel",
+      (e) => {
+        e.preventDefault();
+        this.dialog.addEventListener("webkitAnimationEnd", () => {
+          this.dialog.remove();
+          cancel();
+        });
+        this.dialog.classList.add("jenkins-dialog--hidden");
+      },
+      { once: true }
+    );
+    this.dialog.focus();
+    if (this.input != null) {
+      this.input.focus();
+    }
+    if (this.ok != null) {
+      this.ok.addEventListener(
+        "click",
         (e) => {
           e.preventDefault();
+
+          let value = true;
+          if (this.dialogType === "prompt") {
+            value = this.input.value;
+          }
           this.dialog.addEventListener("webkitAnimationEnd", () => {
             this.dialog.remove();
-            cancel();
+            resolve(value);
           });
           this.dialog.classList.add("jenkins-dialog--hidden");
         },
         { once: true }
       );
-      this.dialog.focus();
-      if (this.input != null) {
-        this.input.focus();
-      }
-      if (this.ok != null) {
-        this.ok.addEventListener(
-          "click",
-          (e) => {
-            e.preventDefault();
-
-            let value = true;
-            if (this.dialogType === "prompt") {
-              value = this.input.value;
-            }
-            this.dialog.addEventListener("webkitAnimationEnd", () => {
-              this.dialog.remove();
-              resolve(value);
-            });
-            this.dialog.classList.add("jenkins-dialog--hidden");
-          },
-          { once: true }
-        );
-      }
-    });
-  }
-}
+    }
+  });
+};
 
 function init() {
   window.dialog = {
