a94f1f6b59ccdae2b3a80c02b732525c215c4895
Basil Crow <me@basilcrow.com>
2023-06-29 02:21:24 -0700

    Clean up Stylelint configuration (#8195)

--- war/.stylelintrc.js
+++ war/.stylelintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     "selector-class-pattern": "[a-z]",
     "selector-id-pattern": "[a-z]",
     "custom-property-pattern": "[a-z]",
-    "function-name-case": "lower",
     "value-keyword-case": [
       "lower",
       {
@@ -15,7 +14,6 @@ module.exports = {
       },
     ],
     "property-no-vendor-prefix": null,
-    "at-rule-no-vendor-prefix": null,
     "at-rule-no-unknown": [
       true,
       {
@@ -34,12 +32,7 @@ module.exports = {
     "alpha-value-notation": "number",
     "number-max-precision": 5,
     "function-no-unknown": null,
-    "selector-type-no-unknown": null,
-    "font-family-no-missing-generic-family-keyword": null,
-    "declaration-block-single-line-max-declarations": 2,
-    "function-url-quotes": "always",
     "no-duplicate-selectors": null,
-    "no-invalid-position-at-import-rule": null,
     "hue-degree-notation": "number",
   },
 };
