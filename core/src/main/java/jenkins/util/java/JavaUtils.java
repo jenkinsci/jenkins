5ce290a1a3441989e1dea583dd08aa1869e0a379
Chrimle <krillestoffer1998@gmail.com>
2023-07-01 20:57:53 +0200

    Added Deprecated to JavaDocs
    Added deprecated tag to JavaDoc due to checkStyle fail in CI, along with short explanation for deprecation.

--- core/src/main/java/jenkins/util/java/JavaUtils.java
+++ core/src/main/java/jenkins/util/java/JavaUtils.java
@@ -43,6 +43,8 @@ public class JavaUtils {
     /**
      * Check whether the current JVM is running with Java 8 or below
      * @return {@code true} if it is Java 8 or older version
+     * @deprecated because the current version is at least Java 11,
+     * this method is redundant.
      */
     @Deprecated(since = "2023-06-16", forRemoval = true)
     public static boolean isRunningWithJava8OrBelow() {
@@ -52,6 +54,8 @@ public class JavaUtils {
     /**
      * Check whether the current JVM is running with Java 9 or above.
      * @return {@code true} if it is Java 9 or above
+     * @deprecated because the current version is at least Java 11,
+     * this method is redundant.
      */
     @Deprecated(since = "2023-06-16", forRemoval = true)
     public static boolean isRunningWithPostJava8() {
