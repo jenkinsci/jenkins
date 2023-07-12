b37bcdcb0e537a86bc3259067dce411c0dc1be44
Daniel Beck <daniel-beck@users.noreply.github.com>
2023-07-12 07:47:41 +0200

    Move test out of JenkinsTest, make parameterized for different contexts

--- test/src/test/java/jenkins/model/JenkinsTest.java
+++ test/src/test/java/jenkins/model/JenkinsTest.java
@@ -67,12 +67,10 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import jenkins.AgentProtocol;
-import jenkins.security.ResourceDomainConfiguration;
 import org.htmlunit.FailingHttpStatusCodeException;
 import org.htmlunit.HttpMethod;
 import org.htmlunit.Page;
@@ -80,7 +78,6 @@ import org.htmlunit.TextPage;
 import org.htmlunit.WebRequest;
 import org.htmlunit.WebResponse;
 import org.htmlunit.html.HtmlPage;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -91,7 +88,6 @@ import org.jvnet.hudson.test.MockAuthorizationStrategy;
 import org.jvnet.hudson.test.SmokeTest;
 import org.jvnet.hudson.test.TestExtension;
 import org.jvnet.hudson.test.recipes.WithPlugin;
-import org.kohsuke.stapler.Dispatcher;
 import org.kohsuke.stapler.HttpResponse;
 import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
@@ -822,137 +818,6 @@ public class JenkinsTest {
         @Override
         public String getUrlName() {
             return "login123";
-        }
-    }
-
-    @Test
-    @Issue("JENKINS-71087")
-    public void nice404ErrorPage() throws Exception {
-        try (WebClient wc = j.createWebClient()) {
-            Dispatcher.TRACE = false;
-
-            /* Start with no security realm configured */
-
-            { // basic error page
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("foo"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, not(containsString(j.contextPath + "/login?from=")));
-                assertThat(content, containsString("This page does not exist."));
-            }
-
-            { // paths are fine on error page even when nested
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("foo/bar/baz/"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, not(containsString(j.contextPath + "/login?from=")));
-                assertThat(content, containsString("This page does not exist."));
-            }
-
-            { // resource root action have custom (less) error message content
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("static-files/foo"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, not(containsString(j.contextPath + "/login?from=")));
-                assertThat(content, not(containsString("This page does not exist.")));
-                assertThat(content, not(containsString("This page may not exist, or you may not have permission to see it.")));
-            }
-
-            /* Set up security realm and request as anonymous, we expect login link and hedged response */
-            j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
-            j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy().grant(Jenkins.ADMINISTER).everywhere().toAuthenticated().grant(Jenkins.READ).everywhere().toEveryone());
-
-            { // basic error page
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("foo"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, containsString(j.contextPath + "/login?from=%2F"));
-                assertThat(content, not(containsString(j.contextPath + "/login?from=%2Ffoo")));
-                assertThat(content, containsString("This page may not exist, or you may not have permission to see it."));
-            }
-
-            { // paths are fine on error page even when nested
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("foo/bar/baz/"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, containsString(j.contextPath + "/login?from=%2F"));
-                assertThat(content, not(containsString(j.contextPath + "/login?from=%2Ffoo")));
-                assertThat(content, containsString("This page may not exist, or you may not have permission to see it."));
-            }
-
-            { // resource root action have custom (less) error message content
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("static-files/foo"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, containsString(j.contextPath + "/login?from=%2F"));
-                assertThat(content, not(containsString(j.contextPath + "/login?from=%2Ffoo")));
-                assertThat(content, not(containsString("This page does not exist.")));
-                assertThat(content, not(containsString("This page may not exist, or you may not have permission to see it.")));
-            }
-
-            /* With the security realm still set up, log in and expect the profile link to show */
-            wc.login("alice");
-
-            { // basic error page
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("foo"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, not(containsString(j.contextPath + "/login?from=")));
-                assertThat(content, containsString("user/alice"));
-                assertThat(content, containsString("This page may not exist, or you may not have permission to see it."));
-            }
-
-            { // paths are fine on error page even when nested
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("foo/bar/baz/"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, not(containsString(j.contextPath + "/login?from=")));
-                assertThat(content, containsString("user/alice"));
-                assertThat(content, containsString("This page may not exist, or you may not have permission to see it."));
-            }
-
-            { // resource root action have custom (less) error message content
-                final FailingHttpStatusCodeException ex = assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("static-files/foo"));
-                assertEquals(404, ex.getStatusCode());
-                final String content = ex.getResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(content, not(containsString(j.contextPath + "/login?from=")));
-                assertThat(content, containsString("user/alice"));
-                assertThat(content, not(containsString("This page does not exist.")));
-                assertThat(content, not(containsString("This page may not exist, or you may not have permission to see it.")));
-            }
-        } finally {
-            Dispatcher.TRACE = true;
-        }
-    }
-
-    @Test
-    @Issue("JENKINS-71087")
-    public void kindaNice404ErrorPageOnResourceDomain() throws Exception {
-
-        final String resourceRoot;
-        { // Setup stolen from ResourceDomainTest
-            URL root = j.getURL(); // which always will use "localhost", see JenkinsRule#getURL()
-            Assert.assertTrue(root.toString().contains("localhost")); // to be safe
-
-            resourceRoot = root.toString().replace("localhost", "127.0.0.1");
-            ResourceDomainConfiguration configuration = ExtensionList.lookupSingleton(ResourceDomainConfiguration.class);
-            configuration.setUrl(resourceRoot);
-        }
-
-        try (WebClient wc = j.createWebClient()) {
-            wc.setThrowExceptionOnFailingStatusCode(false);
-            @SuppressWarnings("deprecation") // we need to not access the usual Jenkins URLs
-            final Page page = wc.getPage(resourceRoot + "foo");
-            wc.setThrowExceptionOnFailingStatusCode(true);
-            assertEquals(404, page.getWebResponse().getStatusCode());
-            final String content = page.getWebResponse().getContentAsString(StandardCharsets.UTF_8);
-            assertThat(content, containsString("Back to Jenkins"));
-            if (page.isHtmlPage()) {
-                final HtmlPage htmlPage = (HtmlPage) page;
-                final Page nextPage = htmlPage.getAnchorByText("Back to Jenkins").click();
-                final String nextContent = nextPage.getWebResponse().getContentAsString(StandardCharsets.UTF_8);
-                assertThat(nextContent, containsString("Welcome to Jenkins"));
-            }
         }
     }
 }
