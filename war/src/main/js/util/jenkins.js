a89b68d782cec63ad8c8185f47ef7a9b59065595
Tim Jacomb <timjacomb1@gmail.com>
2023-08-03 08:46:18 +0100

    Remove prototype.js

--- war/src/main/js/util/jenkins.js
+++ war/src/main/js/util/jenkins.js
@@ -17,42 +17,6 @@ jenkins.baseUrl = function () {
   return u;
 };
 
-// awful hack to get around JSONifying things with Prototype taking over wrong. ugh. Prototype is the worst.
-jenkins.stringify = function (o) {
-  if (Array.prototype.toJSON) {
-    // Prototype f's this up something bad
-    var protoJSON = {
-      a: Array.prototype.toJSON,
-      o: Object.prototype.toJSON,
-      h: Hash.prototype.toJSON,
-      s: String.prototype.toJSON,
-    };
-    try {
-      delete Array.prototype.toJSON;
-      delete Object.prototype.toJSON;
-      delete Hash.prototype.toJSON;
-      delete String.prototype.toJSON;
-
-      return JSON.stringify(o);
-    } finally {
-      if (protoJSON.a) {
-        Array.prototype.toJSON = protoJSON.a;
-      }
-      if (protoJSON.o) {
-        Object.prototype.toJSON = protoJSON.o;
-      }
-      if (protoJSON.h) {
-        Hash.prototype.toJSON = protoJSON.h;
-      }
-      if (protoJSON.s) {
-        String.prototype.toJSON = protoJSON.s;
-      }
-    }
-  } else {
-    return JSON.stringify(o);
-  }
-};
-
 /**
  * redirect
  */
@@ -110,7 +74,7 @@ jenkins.post = function (url, data, success, options) {
       formBody = $.extend({}, formBody);
       formBody[crumb.fieldName] = crumb.value;
     }
-    formBody = jenkins.stringify(formBody);
+    formBody = JSON.stringify(formBody);
   }
 
   var args = {
