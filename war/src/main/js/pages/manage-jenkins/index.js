6aa40de88454b292e528af54881ec799d8af0e44
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2023-08-16 11:07:01 +0100

    Linting

--- war/src/main/js/pages/manage-jenkins/index.js
+++ war/src/main/js/pages/manage-jenkins/index.js
@@ -4,9 +4,8 @@ searchBarInput.suggestions = function () {
   return Array.from(document.querySelectorAll(".task-link"))
     .map((item) => ({
       url: item.href,
-      icon: item.querySelector(
-        ".task-icon-link svg, .task-icon-link img"
-      ).outerHTML,
+      icon: item.querySelector(".task-icon-link svg, .task-icon-link img")
+        .outerHTML,
       label: item.querySelector(".task-link-text")?.textContent,
     }))
     .filter((item) => !item.url.endsWith("#"));
