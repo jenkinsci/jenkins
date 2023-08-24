845c7f94781010c5f3abcc991407df17f076360a
Daniel Beck <daniel-beck@users.noreply.github.com>
2023-08-24 10:01:26 +0200

    Also override #init

--- core/src/main/java/jenkins/ErrorAttributeFilter.java
+++ core/src/main/java/jenkins/ErrorAttributeFilter.java
@@ -3,6 +3,7 @@ package jenkins;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -30,6 +31,11 @@ public class ErrorAttributeFilter implements Filter {
 
     @Override
     public void destroy() {
+        // Otherwise the PCT fails
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
         // Otherwise the PCT fails
     }
 }
