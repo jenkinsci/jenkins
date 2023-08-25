7bc3b36cdc1f1c1d3a14d6526957c5d46cd86e67
Daniel Beck <daniel-beck@users.noreply.github.com>
2023-08-25 17:46:44 +0200

    lint:fix

--- core/src/main/resources/hudson/model/BuildTimelineWidget/build-timeline-widget.js
+++ core/src/main/resources/hudson/model/BuildTimelineWidget/build-timeline-widget.js
@@ -3,52 +3,56 @@
 var targetDiv = document.querySelector("#build-timeline-div");
 // var tz = targetDiv.getAttribute("data-hour-local-timezone");
 // var tl = null;
-var interval = 24 * 60 * 60 * 1000;
+
+// var interval = 24 * 60 * 60 * 1000;
 
 function getData(timeline, current, min, max) {
-
   // if (!eventSource1.loaded[current]) {
   //   eventSource1.loaded[current] = true;
-    fetch("timeline/data", {
-      method: "POST",
-      headers: crumb.wrap({
-        "Content-Type": "application/x-www-form-urlencoded",
-      }),
-      body: new URLSearchParams({
-        min: min,
-        max: max,
-        utcOffset: new Date().getTimezoneOffset()
-      }),
-    }).then((t) => {
-      if (t.ok) {
-        t.json()
-          .then((json) => {
-            timeline.setData({ 'items': json.events });
-          })
-          .catch((err) => {
-            alert(err);
-          });
-      }
-    });
+  fetch("timeline/data", {
+    method: "POST",
+    headers: crumb.wrap({
+      "Content-Type": "application/x-www-form-urlencoded",
+    }),
+    body: new URLSearchParams({
+      min: min,
+      max: max,
+      utcOffset: new Date().getTimezoneOffset(),
+    }),
+  }).then((t) => {
+    if (t.ok) {
+      t.json()
+        .then((json) => {
+          timeline.setData({ items: json.events });
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  });
   // }
 }
 
 // Create a DataSet (allows two way data-binding)
-let items = new vis.DataSet([
-]);
+let items = new vis.DataSet([]);
 
 // Configuration for the Timeline
 let options = {
-  "horizontalScroll": true,
-  "zoomable": true,
-  "zoomMax": 1000*3600*24*7,
-  "height": "300px",
-  "max": Date.now() + 6*3600*1000
+  horizontalScroll: true,
+  zoomable: true,
+  zoomMax: 1000 * 3600 * 24 * 7,
+  height: "300px",
+  max: Date.now() + 6 * 3600 * 1000,
 };
 
 // Create a Timeline
 let timeline = new vis.Timeline(targetDiv, items, options);
-getData(timeline, 0, Date.parse(timeline.getItemRange().min), Date.parse(timeline.getItemRange().max));
+getData(
+  timeline,
+  0,
+  Date.parse(timeline.getItemRange().min),
+  Date.parse(timeline.getItemRange().max),
+);
 
 timeline.on("rangechanged", function (event) {
   console.log("NEw start: " + event.start + ", new end: " + event.end);
