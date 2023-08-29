18c1e043685b5c23cd3c87d506aebe1f39358996
Basil Crow <me@basilcrow.com>
2023-08-29 01:35:32 -0700

    Clear the queue before tearing down tests (#8428)
    Co-authored-by: Daniel Beck <1831569+daniel-beck@users.noreply.github.com>

--- test/src/test/java/hudson/model/ViewTest.java
+++ test/src/test/java/hudson/model/ViewTest.java
@@ -322,6 +322,12 @@ public class ViewTest {
         assertNotContainsItems(view1, notInView, inView2);
         assertContainsItems(view2, inView2, inBothViews);
         assertNotContainsItems(view2, notInView, inView1);
+
+        // Clear the queue
+        assertTrue(j.jenkins.getQueue().cancel(notInView));
+        assertTrue(j.jenkins.getQueue().cancel(inView1));
+        assertTrue(j.jenkins.getQueue().cancel(inView2));
+        assertTrue(j.jenkins.getQueue().cancel(inBothViews));
     }
 
     private void assertContainsItems(View view, Task... items) {
