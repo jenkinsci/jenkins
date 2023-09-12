8c3d6c65b758133a150095ef68f97bfd2c91eb21
Carroll Chiou <cchiou@cloudbees.com>
2023-09-12 11:40:05 -0600

    add name fields to config form data

--- core/src/main/java/hudson/slaves/Cloud.java
+++ core/src/main/java/hudson/slaves/Cloud.java
@@ -389,7 +389,12 @@ public abstract class Cloud extends Actionable implements ExtensionPoint, Descri
         if (cloud == null) {
             throw new ServletException("No such cloud " + this.name);
         }
-        Cloud result = cloud.reconfigure(req, req.getSubmittedForm());
+        JSONObject formData = req.getSubmittedForm();
+        // add the cloud name to the submitted form data
+        formData.put("name", name);
+        // multiple cloud implementations use cloudName instead of name.
+        formData.put("cloudName", name);
+        Cloud result = cloud.reconfigure(req, formData);
         if (!(result.name).equals(this.name)) {
             // Do not rename the cloud in the config page. Use doConfirmRename()
             result.name = this.name;
