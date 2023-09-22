e8fb99902421381df036904c2c0342eb19927b6f
James Nord <jtnord@users.noreply.github.com>
2023-09-22 16:20:22 +0100

    keep the checkstyle overlord happy

--- core/src/main/java/hudson/security/PasswordHashEncoder.java
+++ core/src/main/java/hudson/security/PasswordHashEncoder.java
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package hudson.security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
