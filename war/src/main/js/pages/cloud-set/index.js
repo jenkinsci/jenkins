002b773e8d52bbfee9de6522124bb0254109f7fb
Vincent Latombe <vincent@latombe.net>
2023-09-26 09:56:53 +0200

    Fix reviews

--- war/src/main/js/pages/cloud-set/index.js
+++ war/src/main/js/pages/cloud-set/index.js
@@ -3,10 +3,7 @@ import { registerSortableTableDragDrop } from "@/sortable-drag-drop";
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll("tbody").forEach((table) =>
     registerSortableTableDragDrop(table, function () {
-      YAHOO.util.Dom.removeClass(
-        document.getElementById("saveButton"),
-        "jenkins-hidden",
-      );
+      document.getElementById("saveButton").classList.remove("jenkins-hidden");
     }),
   );
 });
