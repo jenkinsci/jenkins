16debcb972c4d38c421667b633acebfc68855158
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2023-10-10 23:03:33 +0530

    [JENKINS-72028] at ` test/src/test/java/hudson/` replace deprecated `URL()` (#8569)
    * missing 'alt' attribute added
    
    * missing 'alt' attribute added
    
    * replaced 'new URL' to 'new URI' per Java 11 target codebase
    
    * Revert "replaced 'new URL' to 'new URI' per Java 11 target codebase"
    
    This reverts commit aeef4c1e4bb57a4832dbe368920aa0716a3f68e7.
    
    * at test classes replaced deprecated URL() with URI#.toURL()
    
    * Update test/src/test/java/hudson/model/UpdateSiteTest.java
    
    as per review
    
    Co-authored-by: James Nord <jtnord@users.noreply.github.com>
    
    * add missing quote
    
    * Spotless
    
    ---------
    
    Co-authored-by: James Nord <jtnord@users.noreply.github.com>
    Co-authored-by: Alexander Brandes <mc.cache@web.de>
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- test/src/test/java/hudson/model/ItemsTest.java
+++ test/src/test/java/hudson/model/ItemsTest.java
@@ -41,7 +41,7 @@ import hudson.security.ACL;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import jenkins.model.Jenkins;
@@ -228,7 +228,7 @@ public class ItemsTest {
                         // redirect perversely counts as a failure
                         .withRedirectEnabled(false)
                         .withThrowExceptionOnFailingStatusCode(false);
-                WebResponse webResponse = wc.getPage(new WebRequest(new URL(wc.getContextPath() + "createItem?name=" + target + "&mode=hudson.model.FreeStyleProject"), HttpMethod.POST)).getWebResponse();
+                WebResponse webResponse = wc.getPage(new WebRequest(new URI(wc.getContextPath() + "createItem?name=" + target + "&mode=hudson.model.FreeStyleProject").toURL(), HttpMethod.POST)).getWebResponse();
                 if (webResponse.getStatusCode() != HttpURLConnection.HTTP_MOVED_TEMP) {
                     throw new FailingHttpStatusCodeException(webResponse);
                 }
@@ -241,7 +241,7 @@ public class ItemsTest {
                 JenkinsRule.WebClient wc = wc(r)
                         .withRedirectEnabled(false)
                         .withThrowExceptionOnFailingStatusCode(false);
-                WebResponse webResponse = wc.getPage(new WebRequest(new URL(wc.getContextPath() + "createItem?name=" + target + "&mode=copy&from=dupe"), HttpMethod.POST)).getWebResponse();
+                WebResponse webResponse = wc.getPage(new WebRequest(new URI(wc.getContextPath() + "createItem?name=" + target + "&mode=copy&from=dupe").toURL(), HttpMethod.POST)).getWebResponse();
                 r.jenkins.getItem("dupe").delete();
                 if (webResponse.getStatusCode() != HttpURLConnection.HTTP_MOVED_TEMP) {
                     throw new FailingHttpStatusCodeException(webResponse);
@@ -252,7 +252,7 @@ public class ItemsTest {
         REST_CREATE {
             @Override void run(JenkinsRule r, String target) throws Exception {
                 JenkinsRule.WebClient wc = wc(r);
-                WebRequest req = new WebRequest(new URL(wc.getContextPath() + "createItem?name=" + target), HttpMethod.POST);
+                WebRequest req = new WebRequest(new URI(wc.getContextPath() + "createItem?name=" + target).toURL(), HttpMethod.POST);
                 req.setAdditionalHeader("Content-Type", "application/xml");
                 req.setRequestBody("<project/>");
                 wc.getPage(req);
@@ -265,7 +265,7 @@ public class ItemsTest {
                 JenkinsRule.WebClient wc = wc(r)
                         .withRedirectEnabled(false)
                         .withThrowExceptionOnFailingStatusCode(false);
-                WebResponse webResponse = wc.getPage(new WebRequest(new URL(wc.getContextPath() + "job/dupe/doRename?newName=" + target), HttpMethod.POST)).getWebResponse();
+                WebResponse webResponse = wc.getPage(new WebRequest(new URI(wc.getContextPath() + "job/dupe/doRename?newName=" + target).toURL(), HttpMethod.POST)).getWebResponse();
                 if (webResponse.getStatusCode() != HttpURLConnection.HTTP_MOVED_TEMP) {
                     r.jenkins.getItem("dupe").delete();
                     throw new FailingHttpStatusCodeException(webResponse);
