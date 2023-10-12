59926559a47ff6b9de4320d659fedfd8da6024cd
Markus Winter <m.winter@sap.com>
2023-10-12 21:33:18 +0200

    remove hetero-list.js again

--- core/src/main/resources/lib/form/hetero-list/hetero-list.js
+++
@@ -1,4 +0,0 @@
-/*
-  Intentionally empty. This file just exists so plugins that explicitly include the
-  hetero-list adjunct do not print a warning.
-*/
--- war/src/main/js/components/dropdowns/hetero-list.js
+++ war/src/main/js/components/dropdowns/hetero-list.js
@@ -22,20 +22,20 @@ function generateHandles() {
 }
 
 function convertInputsToButtons(e) {
-    let oldInputs = e.querySelectorAll("INPUT.hetero-list-add");
-    oldInputs.forEach((oldbtn) => {
-      let btn = document.createElement("button");
-      btn.setAttribute("type", "button");
-      btn.classList.add("hetero-list-add", "jenkins-button");
-      btn.innerText = oldbtn.getAttribute("value");
-      if (oldbtn.hasAttribute("suffix")) {
-        btn.setAttribute("suffix", oldbtn.getAttribute("suffix"));
-      }
-      let chevron = createElementFromHtml(Symbols.CHEVRON_DOWN);
-      btn.appendChild(chevron);
-      oldbtn.parentNode.appendChild(btn);
-      oldbtn.remove();
-    });
+  let oldInputs = e.querySelectorAll("INPUT.hetero-list-add");
+  oldInputs.forEach((oldbtn) => {
+    let btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.classList.add("hetero-list-add", "jenkins-button");
+    btn.innerText = oldbtn.getAttribute("value");
+    if (oldbtn.hasAttribute("suffix")) {
+      btn.setAttribute("suffix", oldbtn.getAttribute("suffix"));
+    }
+    let chevron = createElementFromHtml(Symbols.CHEVRON_DOWN);
+    btn.appendChild(chevron);
+    oldbtn.parentNode.appendChild(btn);
+    oldbtn.remove();
+  });
 }
 
 function generateButtons() {
