f53d34c4dbc7245ec8d23ce0b780c3288ea78fb6
Markus Winter <m.winter@sap.com>
2024-01-02 12:17:04 +0100

    set status url
    the parameters page also includes the buildCaption.jelly. But the js is
    using a relative url thus accessing `statusIcon` fails.
    Store status url in the div and read from there.

--- core/src/main/resources/lib/hudson/build-caption.js
+++ core/src/main/resources/lib/hudson/build-caption.js
@@ -1,36 +1,42 @@
 (function () {
   function updateBuildCaptionIcon() {
-    fetch("statusIcon").then((rsp) => {
-      let isBuilding = rsp.headers.get("X-Building");
-      if (isBuilding === "true") {
-        setTimeout(updateBuildCaptionIcon, 5000);
-        let progress = rsp.headers.get("X-Progress");
-        let runtime = rsp.headers.get("X-Executor-Runtime");
-        let remaining = rsp.headers.get("X-Executor-Remaining");
-        let progressBar = document.querySelector(".app-progress-bar");
-        let progressBarDone = document.querySelector(".app-progress-bar span");
-        if (progressBar) {
-          let tooltip = progressBar.dataset.tooltipTemplate;
-          tooltip = tooltip.replace("%0", runtime).replace("%1", remaining);
-          progressBar.setAttribute("tooltip", tooltip);
-          progressBar.setAttribute("title", tooltip);
-          Behaviour.applySubtree(progressBar, true);
+    const buildCaption = document.querySelector(".jenkins-build-caption");
+    const url = buildCaption.dataset.statusUrl;
+    fetch(url).then((rsp) => {
+      if (rsp.ok) {
+        let isBuilding = rsp.headers.get("X-Building");
+        if (isBuilding === "true") {
+          setTimeout(updateBuildCaptionIcon, 5000);
+          let progress = rsp.headers.get("X-Progress");
+          let runtime = rsp.headers.get("X-Executor-Runtime");
+          let remaining = rsp.headers.get("X-Executor-Remaining");
+          let progressBar = document.querySelector(".app-progress-bar");
+          let progressBarDone = document.querySelector(
+            ".app-progress-bar span",
+          );
+          if (progressBar) {
+            let tooltip = progressBar.dataset.tooltipTemplate;
+            tooltip = tooltip.replace("%0", runtime).replace("%1", remaining);
+            progressBar.setAttribute("tooltip", tooltip);
+            progressBar.setAttribute("title", tooltip);
+            Behaviour.applySubtree(progressBar, true);
+          }
+          if (progressBarDone) {
+            progressBarDone.style.width = `${progress}%`;
+          }
+        } else {
+          let progressBar = document.querySelector(
+            ".build-caption-progress-container",
+          );
+          if (progressBar) {
+            progressBar.style.display = "none";
+          }
         }
-        if (progressBarDone) {
-          progressBarDone.style.width = `${progress}%`;
-        }
-      } else {
-        let progressBar = document.querySelector(
-          ".build-caption-progress-container",
-        );
-        if (progressBar) {
-          progressBar.style.display = "none";
-        }
+        rsp.text().then((responseText) => {
+          document.querySelector(".jenkins-build-caption svg").outerHTML =
+            responseText;
+        });
       }
-      rsp.text().then((responseText) => {
-        document.querySelector(".jenkins-build-caption svg").outerHTML =
-          responseText;
-      });
     });
   }
 
