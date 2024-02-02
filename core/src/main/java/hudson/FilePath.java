3e6dc60edbdb2293ac35b2059ba1fe864b5f5242
Robert Sandell <rsandell@cloudbees.com>
2024-02-02 16:40:09 +0100

    Unrestrict FilePath.isDescendant (#8913)
    * Unrestrict FilePath.isDescendant
    
    I needed this and noticed that it was still restricted.
    
    * Fix spotless
    
    Signed-off-by: Alexander Brandes <mc.cache@web.de>
    
    ---------
    
    Signed-off-by: Alexander Brandes <mc.cache@web.de>
    Co-authored-by: Alexander Brandes <mc.cache@web.de>

--- core/src/main/java/hudson/FilePath.java
+++ core/src/main/java/hudson/FilePath.java
@@ -3766,10 +3766,7 @@ public final class FilePath implements SerializableOnlyOverRemoting {
 
     /**
      * Check if the relative child is really a descendant after symlink resolution if any.
-     *
-     * TODO un-restrict it in a weekly after the patch
      */
-    @Restricted(NoExternalUse.class)
     public boolean isDescendant(@NonNull String potentialChildRelativePath) throws IOException, InterruptedException {
         return act(new IsDescendant(potentialChildRelativePath));
     }
