715d36269c8ee93ce6c866866d4a89b8020d1a0a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-03-01 23:17:10 +0000

    More

--- core/src/main/java/jenkins/model/view/CheckFingerprintMenuItem.java
+++ core/src/main/java/jenkins/model/view/CheckFingerprintMenuItem.java
@@ -2,9 +2,10 @@ package jenkins.model.view;
 
 import hudson.Extension;
 import hudson.model.Action;
-import hudson.model.ListView;
+import hudson.model.View;
 import java.util.Collection;
 import java.util.Set;
+import jenkins.model.Jenkins;
 import jenkins.model.TransientActionFactory;
 import jenkins.model.menu.Group;
 import jenkins.model.menu.event.LinkAction;
@@ -28,19 +29,19 @@ public class CheckFingerprintMenuItem implements Action {
 
     @Override
     public jenkins.model.menu.event.Action getAction() {
-        return LinkAction.of("fingerprint");
+        return LinkAction.of(Jenkins.get().getRootUrl() + "fingerprint");
     }
 
     @Extension
-    public static class TransientActionFactoryImpl extends TransientActionFactory<ListView> {
+    public static class TransientActionFactoryImpl extends TransientActionFactory<View> {
 
         @Override
-        public Class<ListView> type() {
-            return ListView.class;
+        public Class<View> type() {
+            return View.class;
         }
 
         @Override
-        public Collection<? extends Action> createFor(ListView target) {
+        public Collection<? extends Action> createFor(View target) {
             return Set.of(new CheckFingerprintMenuItem());
         }
     }
