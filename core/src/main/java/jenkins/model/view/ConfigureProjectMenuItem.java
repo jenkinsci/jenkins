6bcf863a6806555c9d0279e588112ebd5dde341a
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-03-17 14:10:28 +0000

    More

---
+++ core/src/main/java/jenkins/model/view/ConfigureProjectMenuItem.java
@@ -0,0 +1,51 @@
+package jenkins.model.view;
+
+import hudson.Extension;
+import hudson.model.Action;
+import hudson.model.Job;
+import java.util.Collection;
+import java.util.Set;
+import jenkins.model.TransientActionFactory;
+import jenkins.model.menu.Group;
+import jenkins.model.menu.event.LinkAction;
+
+public class ConfigureProjectMenuItem implements Action {
+
+    @Override
+    public String getDisplayName() {
+        return "Configure";
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "symbol-settings";
+    }
+
+    @Override
+    public Group getGroup() {
+        return Group.IN_APP_BAR;
+    }
+
+    @Override
+    public jenkins.model.menu.event.Action getAction() {
+        return LinkAction.of("configure");
+    }
+
+    @Extension
+    public static class TransientActionFactoryImpl extends TransientActionFactory<Job> {
+
+        @Override
+        public Class<Job> type() {
+            return Job.class;
+        }
+
+        @Override
+        public Collection<? extends Action> createFor(Job target) {
+            if (!target.hasPermission(Job.CONFIGURE)) {
+                return Set.of();
+            }
+
+            return Set.of(new ConfigureProjectMenuItem());
+        }
+    }
+}
---
+++ core/src/main/java/jenkins/model/view/DeleteProjectMenuItem.java
@@ -0,0 +1,57 @@
+package jenkins.model.view;
+
+import hudson.Extension;
+import hudson.model.Action;
+import hudson.model.Job;
+import java.util.Collection;
+import java.util.Set;
+import jenkins.model.TransientActionFactory;
+import jenkins.model.menu.Group;
+import jenkins.model.menu.Semantic;
+import jenkins.model.menu.event.LinkAction;
+
+public class DeleteProjectMenuItem implements Action {
+
+    @Override
+    public String getDisplayName() {
+        return "Delete project";
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "symbol-trash";
+    }
+
+    @Override
+    public Group getGroup() {
+        return Group.LAST_IN_MENU;
+    }
+
+    @Override
+    public jenkins.model.menu.event.Action getAction() {
+        return LinkAction.of("delete");
+    }
+
+    @Override
+    public Semantic getSemantic() {
+        return Semantic.DESTRUCTIVE;
+    }
+
+    @Extension
+    public static class TransientActionFactoryImpl extends TransientActionFactory<Job> {
+
+        @Override
+        public Class<Job> type() {
+            return Job.class;
+        }
+
+        @Override
+        public Collection<? extends Action> createFor(Job target) {
+            if (!target.hasPermission(Job.DELETE)) {
+                return Set.of();
+            }
+
+            return Set.of(new DeleteProjectMenuItem());
+        }
+    }
+}
