8552ba362790ba17d00fe249b4f85c8f3fce9565
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-03-17 14:03:35 +0000

    Init

---
+++ core/src/main/java/jenkins/model/view/BuildActionFactory.java
@@ -0,0 +1,56 @@
+package jenkins.model.view;
+
+import hudson.Extension;
+import hudson.model.Action;
+import hudson.model.Job;
+import hudson.model.View;
+import jenkins.model.TransientActionFactory;
+import jenkins.model.menu.Group;
+import jenkins.model.menu.Semantic;
+import jenkins.model.menu.event.LinkAction;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Extension
+public class BuildActionFactory extends TransientActionFactory<Job> {
+
+    @Override
+    public Class<Job> type() {
+        return Job.class;
+    }
+
+    @Override
+    public Collection<? extends Action> createFor(Job target) {
+        if (!target.hasPermission(View.CREATE)) {
+            return Set.of();
+        }
+
+        return Set.of(new Action() {
+                @Override
+                public String getDisplayName() {
+                    return "Build";
+                }
+
+                @Override
+                public String getIconFileName() {
+                    return "symbol-play";
+                }
+
+            @Override
+            public Group getGroup() {
+                return Group.FIRST_IN_APP_BAR;
+            }
+
+            @Override
+            public Semantic getSemantic() {
+                return Semantic.BUILD;
+            }
+
+                @Override
+                public jenkins.model.menu.event.Action getAction() {
+                    return LinkAction.of("build");
+                }
+        });
+    }
+}
