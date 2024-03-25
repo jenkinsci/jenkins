b9d383af0c184de4cd3f5c1c01a8d52f951e4f40
jean pierre Lerbscher <5850581+jlerbsc@users.noreply.github.com>
2024-03-25 20:38:23 +0100

    Update CompoundEnumeration.java
    Remove the unnecessary import of NoSuchElementException

--- core/src/main/java/hudson/util/CompoundEnumeration.java
+++ core/src/main/java/hudson/util/CompoundEnumeration.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 /**
  * {@link Enumeration} that aggregates multiple {@link Enumeration}s.
