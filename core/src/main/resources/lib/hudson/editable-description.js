b84756b29552dc141dab96541153213a244bc039
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-04-05 20:03:22 +0100

    Push

--- core/src/main/resources/lib/hudson/editable-description.js
+++ core/src/main/resources/lib/hudson/editable-description.js
@@ -1,19 +1,12 @@
 /* global replaceDescription */
 (function () {
   document.addEventListener("DOMContentLoaded", function () {
-    const descriptionPane = document.querySelector("#description");
-    const dataUrl = descriptionPane.getAttribute("data-url");
-    const dataDescription = descriptionPane.getAttribute("data-description");
-
-    Behaviour.specify(
-      "#" + descriptionPane.dataset.buttonSetter,
-      "description-setter",
-      0,
-      function (e) {
-        e.addEventListener("click", () => {
-          return replaceDescription(dataDescription, dataUrl);
-        });
-      },
-    );
+    let descriptionLink = document.querySelector("#description-link");
+    descriptionLink.addEventListener("click", function (e) {
+      e.preventDefault();
+      let url = descriptionLink.getAttribute("data-url");
+      let description = descriptionLink.getAttribute("data-description");
+      return replaceDescription(description, url);
+    });
   });
 })();
