18c0224a5e2c63438b01c146316a058dc85c4bfc
Kris Stern <krisstern@outlook.com>
2024-04-09 23:29:05 +0800

    fix: spotless patch

--- core/src/main/java/jenkins/plugins/DetachedPluginsUtil.java
+++ core/src/main/java/jenkins/plugins/DetachedPluginsUtil.java
@@ -17,7 +17,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import jenkins.model.Jenkins;
 import org.apache.commons.io.IOUtils;
 import org.kohsuke.accmod.Restricted;
