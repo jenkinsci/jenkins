df1931ff55bb1b3ed46f4d1a5789a72ba3e11acf
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-04-09 10:08:42 +0100

    Lint

--- war/src/main/js/filter-build-history.js
+++ war/src/main/js/filter-build-history.js
@@ -1,37 +1,34 @@
 import debounce from "lodash/debounce";
 
 const buildHistoryPage = document.getElementById("buildHistoryPage");
-const pageSearch = buildHistoryPage.querySelector(
-  ".jenkins-search",
-);const pageSearchInput = buildHistoryPage.querySelector(
-  "input",
-);
+const pageSearch = buildHistoryPage.querySelector(".jenkins-search");
+const pageSearchInput = buildHistoryPage.querySelector("input");
 const ajaxUrl = buildHistoryPage.getAttribute("page-ajax");
 const card = document.querySelector("#jenkins-builds");
 const contents = card.querySelector("#jenkins-build-history");
 const container = card.querySelector(".app-builds-container");
-const noBuilds = card.querySelector("#no-builds")
+const noBuilds = card.querySelector("#no-builds");
 
 const up = document.querySelector("#up");
 const down = document.querySelector("#down");
 
-const updateBuildsRefreshInterval = 5000;
+// const updateBuildsRefreshInterval = 5000;
 
 function updatePageParams(dataTable) {
   const pageHasUp = dataTable.getAttribute("page-has-up");
   const pageHasDown = dataTable.getAttribute("page-has-down");
 
-  up.classList.toggle("app-builds-container__button--disabled", pageHasUp === "false");
-  down.classList.toggle("app-builds-container__button--disabled", pageHasDown === "false");
+  up.classList.toggle(
+    "app-builds-container__button--disabled",
+    pageHasUp === "false",
+  );
+  down.classList.toggle(
+    "app-builds-container__button--disabled",
+    pageHasDown === "false",
+  );
 
-  buildHistoryPage.setAttribute(
-    "page-has-up",
-    pageHasUp
-  );
-  buildHistoryPage.setAttribute(
-    "page-has-down",
-    pageHasDown
-  );
+  buildHistoryPage.setAttribute("page-has-up", pageHasUp);
+  buildHistoryPage.setAttribute("page-has-down", pageHasDown);
   buildHistoryPage.setAttribute(
     "page-entry-newest",
     dataTable.getAttribute("page-entry-newest"),
@@ -49,19 +46,19 @@ function getOldestEntryId() {
   return buildHistoryPage.getAttribute("page-entry-oldest");
 }
 
-up.addEventListener('click', () => {
+up.addEventListener("click", () => {
   loadPage({ "newer-than": getNewestEntryId() });
-})
+});
 
-down.addEventListener('click', () => {
+down.addEventListener("click", () => {
   // cancelRefreshTimeout();
   loadPage({ "older-than": getOldestEntryId() });
-})
+});
 
 function loadPage(options) {
   const params = {
     ...options,
-    search: pageSearchInput.value
+    search: pageSearchInput.value,
   };
 
   // console.log(getNewestEntryId())
