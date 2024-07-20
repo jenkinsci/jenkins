25b9cdaa9ec3dbbceb5760f6b0fa3b097cbc626e
Mark Waite <mark.earl.waite@gmail.com>
2024-07-20 07:34:47 -0600

    Spread test selection more widely
    Constant value tends to skip every test in a run

--- test/src/test/java/jenkins/SkipSomeTests.java
+++ test/src/test/java/jenkins/SkipSomeTests.java
@@ -13,8 +13,6 @@ public class SkipSomeTests {
             ? System.getenv("GIT_BRANCH")
             : "origin/master";
 
-    private static final long TEST_START_TIME = System.currentTimeMillis() / 1000L;
-
     public enum ReasonTestShouldRun {
         NEVER_FAILING_TEST, /* Test has not failed in a very long time, run it less often */
     }
@@ -31,12 +29,12 @@ public class SkipSomeTests {
                 if (GIT_BRANCH.endsWith("master")) {
                     /* Run 1 of 15 times on the master branch, typically at least once a week */
                     /* The master branch builds 10-15 times per week, with each build running 3 configurations */
-                    return (TEST_START_TIME % 15) == 0;
+                    return (System.currentTimeMillis() % 15) == 0;
                 } else {
                     /* Current ci.jenkins.io config runs 3 configurations. */
                     /* Run 1 of 3 times on non-master branch */
                     /* Tests are more likely to fail first on a pull request branch */
-                    return (TEST_START_TIME % 3) == 0;
+                    return (System.currentTimeMillis() % 3) == 0;
                 }
             }
             return true;
