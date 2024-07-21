940c54134fcbce372918cd5311a95fa8c14d71a0
Mark Waite <mark.earl.waite@gmail.com>
2024-07-21 07:45:34 -0600

    Only skip tests on ci.jenkins.io master branch
    Continues to run the tests on pull request branches, on developer
    environments, and on other CI servers like release.ci.jenkins.io and
    cert.ci.jenkins.io.
    
    Run the test approximately 1 of 8 times on the ci.jenkins.io master branch
    as well.  In the last 4 weeks, we've had 7.5 builds of the master branch
    completed each week.  Running the tests 1 of 8 times should provide about
    3 runs per week, since we run 3 configurations (Windows with Java 17,
    Linux with Java 17, and Linux with Java 21).

--- test/src/test/java/jenkins/SkipSomeTests.java
+++ test/src/test/java/jenkins/SkipSomeTests.java
@@ -5,13 +5,13 @@ package jenkins;
  */
 public class SkipSomeTests {
 
-    private static final boolean IS_CONTINUOUS_INTEGRATION = System.getenv("CI") != null
-            ? Boolean.parseBoolean(System.getenv("CI"))
-            : false;
+    private static final String JENKINS_URL = System.getenv("JENKINS_URL") != null
+            ? System.getenv("JENKINS_URL")
+            : "http://localhost:8080/";
 
     private static final String GIT_BRANCH = System.getenv("GIT_BRANCH") != null
             ? System.getenv("GIT_BRANCH")
-            : "origin/master";
+        : "GIT_BRANCH environment variable is null"; // intentionally not a typical branch name
 
     public enum ReasonTestShouldRun {
         NEVER_FAILING_TEST, /* Test has not failed in a very long time, run it less often */
@@ -25,16 +25,11 @@ public class SkipSomeTests {
     public static boolean runTestSometimes(ReasonTestShouldRun testReason) {
         switch (testReason) {
         case NEVER_FAILING_TEST:
-            if (IS_CONTINUOUS_INTEGRATION) {
-                if (GIT_BRANCH.endsWith("master")) {
-                    /* Run 1 of 15 times on the master branch, typically at least once a week */
-                    /* The master branch builds 10-15 times per week, with each build running 3 configurations */
-                    return (System.currentTimeMillis() % 15) == 0;
-                } else {
-                    /* Current ci.jenkins.io config runs 3 configurations. */
-                    /* Run 1 of 3 times on non-master branch */
-                    /* Tests are more likely to fail first on a pull request branch */
-                    return (System.currentTimeMillis() % 3) == 0;
+            if (JENKINS_URL.equals("https://ci.jenkins.io/")) {
+                if (GIT_BRANCH.equals("master") || GIT_BRANCH.equals("origin/master")) {
+                    /* Run 1 of 8 times on the master branch, typically once a week or more */
+                    /* The master branch builds complete 8 times per week (on average), with each build running 3 configurations */
+                    return (System.currentTimeMillis() % 8) == 0;
                 }
             }
             return true;
