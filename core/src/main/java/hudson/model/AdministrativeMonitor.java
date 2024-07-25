16922c70847d72b89accf91a83968e43bf4a87be
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-07-25 09:37:01 +0200

    Deprecate AdministrativeMonitor#getRequiredPermission

--- core/src/main/java/hudson/model/AdministrativeMonitor.java
+++ core/src/main/java/hudson/model/AdministrativeMonitor.java
@@ -200,7 +200,10 @@ public abstract class AdministrativeMonitor extends AbstractModelObject implemen
      *     Form UI elements that change system state, e.g. toggling a feature on or off, need to be hidden from users
      *     lacking Administer permission.
      * </p>
+     * @since 2.233
+     * @deprecated Callers should use {@link #checkRequiredPermission()} or {@link #hasRequiredPermission()}.
      */
+    @Deprecated
     public Permission getRequiredPermission() {
         return Jenkins.ADMINISTER;
     }
