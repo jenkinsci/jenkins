1ff79d558fa5e844ee9774420eadcc851e13074e
yen-cheng <lin0905580802@gmail.com>
2024-08-12 16:08:45 +0800

    fix: edit Build Information encountered an error

--- core/src/main/java/hudson/model/AbstractBuild.java
+++ core/src/main/java/hudson/model/AbstractBuild.java
@@ -150,7 +150,7 @@ public abstract class AbstractBuild<P extends AbstractProject<P, R>, R extends A
      *
      * @since 1.137
      */
-    private volatile Set<String> culprits;
+    private transient Set<String> culprits;
 
     /**
      * During the build this field remembers {@link hudson.tasks.BuildWrapper.Environment}s created by
