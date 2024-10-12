8c0ca4eda134367ff36cdafb7fd9cbcfe1c842fa
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-10-12 11:31:32 +0100

    Update configure-entries-resources.js

--- core/src/main/resources/hudson/model/ListView/configure-entries-resources.js
+++ core/src/main/resources/hudson/model/ListView/configure-entries-resources.js
@@ -1,6 +1,8 @@
 Behaviour.specify("#recurse", "ListView", 0, function (e) {
   // SPAN.nested is kept for backwards compatability
-  var nestedElements = document.querySelectorAll(".listview-jobs--nested, SPAN.nested");
+  var nestedElements = document.querySelectorAll(
+    ".listview-jobs--nested, SPAN.nested",
+  );
   e.onclick = function () {
     nestedElements.forEach(function (el) {
       el.style.display = e.checked ? "" : "none";
