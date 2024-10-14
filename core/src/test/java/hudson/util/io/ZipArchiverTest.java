67713a6f3f6b56bd8b06dbecd465a8c58cb2919b
Simeon L. <hi@simeon.lol>
2024-10-14 03:19:18 +0200

    chore: removed unused imports

--- core/src/test/java/hudson/util/io/ZipArchiverTest.java
+++ core/src/test/java/hudson/util/io/ZipArchiverTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import hudson.FilePath;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.nio.charset.StandardCharsets;
@@ -16,7 +15,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 import org.junit.Assume;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
