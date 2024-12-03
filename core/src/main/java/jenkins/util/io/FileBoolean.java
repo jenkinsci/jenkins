130c08fe2884fcf5d007af341f58d4a2ec703551
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2024-12-03 20:27:35 +0100

    Revert "Fixed spotbugs `PATH_TRAVERSAL_IN` issue in `FileBoolean` (#9638)"
    This reverts commit e994d293e0cf0d67988878cf63a8d5419f83df3c.

--- core/src/main/java/jenkins/util/io/FileBoolean.java
+++ core/src/main/java/jenkins/util/io/FileBoolean.java
@@ -8,7 +8,6 @@ import java.nio.file.InvalidPathException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jenkins.model.Jenkins;
-import org.apache.commons.io.FilenameUtils;
 
 /**
  * Uses a presence/absence of a file as a persisted boolean storage.
@@ -30,7 +29,7 @@ public class FileBoolean {
     }
 
     public FileBoolean(Class owner, String name) {
-        this(new File(Jenkins.get().getRootDir(), FilenameUtils.getName(owner.getName().replace('$', '.') + '/' + name)));
+        this(new File(Jenkins.get().getRootDir(), owner.getName().replace('$', '.') + '/' + name));
     }
 
     /**
