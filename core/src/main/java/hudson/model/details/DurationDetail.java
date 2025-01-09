991f787411e16d7c4184529a90c76bd1608257ce
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-01-09 16:11:20 +0000

    Update DurationDetail.java

--- core/src/main/java/hudson/model/details/DurationDetail.java
+++ core/src/main/java/hudson/model/details/DurationDetail.java
@@ -2,6 +2,8 @@ package hudson.model.details;
 
 import jenkins.model.Detail;
 
+// TODO - We need a way of passing the current run/object to this!
+
 /**
  * Displays the duration of the given build, or, if the build has completed, shows the total time it took to execute
  * @implNote This will render Jelly, hence the fields return null
