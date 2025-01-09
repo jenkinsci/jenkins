0da339b006c2089b0cd95f4990d26b4173d7a348
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-01-09 17:28:41 +0000

    Tidy up

--- core/src/main/java/hudson/model/details/DurationDetail.java
+++ core/src/main/java/hudson/model/details/DurationDetail.java
@@ -4,7 +4,7 @@ import hudson.model.Run;
 import jenkins.model.Detail;
 
 /**
- * Displays the duration of the given build, or, if the build has completed, shows the total time it took to execute
+ * Displays the duration of the given run, or, if the run has completed, shows the total time it took to execute
  * @implNote This will render Jelly, hence the fields return null
  */
 public class DurationDetail extends Detail {
--- core/src/main/java/hudson/model/details/TimestampDetail.java
+++ core/src/main/java/hudson/model/details/TimestampDetail.java
@@ -4,7 +4,7 @@ import hudson.model.Run;
 import jenkins.model.Detail;
 
 /**
- * Displays the start time of the given build
+ * Displays the start time of the given run
  * @implNote This will render Jelly, hence the fields return null
  */
 public class TimestampDetail extends Detail {
