5a0eee22634d61f774a96dbb8ab0bd798ef0a963
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-01-16 10:32:50 +0000

    WB

---
+++ src/main/js/components/header/index.js
@@ -0,0 +1,10 @@
+function init() {
+  window.addEventListener("scroll", () => {
+    const navigation = document.querySelector("#page-header");
+    navigation.style.setProperty("--background-opacity", Math.min(70, window.scrollY) + "%");
+    navigation.style.setProperty("--background-blur", Math.min(40, window.scrollY) + "px");
+    navigation.style.setProperty("--border-opacity", Math.min(10, window.scrollY) + "%");
+  });
+}
+
+export default { init };
