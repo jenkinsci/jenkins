7131e9a1aff7e9d3ee235792dfb48b6e03c00c4c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-01-19 21:34:22 +0000

    Add border to account image

--- src/main/js/components/header/index.js
+++ src/main/js/components/header/index.js
@@ -1,13 +1,14 @@
 function init() {
   window.addEventListener("scroll", () => {
     const navigation = document.querySelector("#page-header");
+    const scrollY = Math.max(0, window.scrollY);
     navigation.style.setProperty(
       "--background-opacity",
-      Math.min(70, window.scrollY) + "%",
+      Math.min(70, scrollY) + "%",
     );
     navigation.style.setProperty(
       "--background-blur",
-      Math.min(40, window.scrollY) + "px",
+      Math.min(40, scrollY) + "px",
     );
     if (
       !document.querySelector(".jenkins-search--app-bar") &&
@@ -15,7 +16,7 @@ function init() {
     ) {
       navigation.style.setProperty(
         "--border-opacity",
-        Math.min(10, window.scrollY) + "%",
+        Math.min(10, scrollY) + "%",
       );
     }
   });
