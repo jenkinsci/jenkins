7a7f056e5d885fec90d65270acb5f22e23250ef6
Vincent Latombe <vincent@latombe.net>
2025-01-31 09:28:19 +0100

    [JENKINS-58743] Allow to provide a custom path for master key

--- core/src/test/java/jenkins/security/DefaultConfidentialStoreTest.java
+++ core/src/test/java/jenkins/security/DefaultConfidentialStoreTest.java
@@ -7,10 +7,15 @@ import static org.junit.Assert.assertTrue;
 
 import hudson.FilePath;
 import hudson.Functions;
+import hudson.Util;
+import hudson.util.TextFile;
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.MalformedInputException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.security.SecureRandom;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -20,6 +25,8 @@ public class DefaultConfidentialStoreTest {
     @Rule
     public TemporaryFolder tmpRule = new TemporaryFolder();
 
+    private final SecureRandom sr = new SecureRandom();
+
     @Test
     public void roundtrip() throws Exception {
         File tmp = new File(tmpRule.getRoot(), "tmp"); // let ConfidentialStore create a directory
@@ -27,6 +34,17 @@ public class DefaultConfidentialStoreTest {
         DefaultConfidentialStore store = new DefaultConfidentialStore(tmp);
         ConfidentialKey key = new ConfidentialKey("test") {};
 
+        assertTrue(new File(tmp, "master.key").exists());
+        roundTrip(store, key, tmp);
+
+        // if the master key changes, we should gracefully fail to load the store
+        new File(tmp, "master.key").delete();
+        DefaultConfidentialStore store2 = new DefaultConfidentialStore(tmp);
+        assertTrue(new File(tmp, "master.key").exists()); // we should have a new key now
+        assertNull(store2.load(key));
+    }
+
+    private static void roundTrip(DefaultConfidentialStore store, ConfidentialKey key, File tmp) throws IOException, InterruptedException {
         // basic roundtrip
         String str = "Hello world!";
         store.store(key, str.getBytes(StandardCharsets.UTF_8));
@@ -34,19 +52,34 @@ public class DefaultConfidentialStoreTest {
 
         // data storage should have some stuff
         assertTrue(new File(tmp, "test").exists());
-        assertTrue(new File(tmp, "master.key").exists());
 
         assertThrows(MalformedInputException.class, () -> Files.readString(tmp.toPath().resolve("test"), StandardCharsets.UTF_8)); // the data shouldn't be a plain text, obviously
 
         if (!Functions.isWindows()) {
             assertEquals(0700, new FilePath(tmp).mode() & 0777); // should be read only
         }
+    }
 
-        // if the master key changes, we should gracefully fail to load the store
-        new File(tmp, "master.key").delete();
-        DefaultConfidentialStore store2 = new DefaultConfidentialStore(tmp);
-        assertTrue(new File(tmp, "master.key").exists()); // we should have a new key now
-        assertNull(store2.load(key));
+    @Test
+    public void masterKeyGeneratedBeforehand() throws IOException, InterruptedException {
+        File external = new File(tmpRule.getRoot(), "external");
+        File tmp = new File(tmpRule.getRoot(), "tmp");
+        var masterKeyFile = new File(external, "master.key");
+        new TextFile(masterKeyFile).write(Util.toHexString(randomBytes(128)));
+        System.setProperty(DefaultConfidentialStore.MASTER_KEY_FILE_SYSTEM_PROPERTY, masterKeyFile.getAbsolutePath());
+        System.setProperty(DefaultConfidentialStore.MASTER_KEY_READONLY_SYSTEM_PROPERTY_NAME, "true");
+        DefaultConfidentialStore store = new DefaultConfidentialStore(tmp);
+        ConfidentialKey key = new ConfidentialKey("test") {};
+        roundTrip(store, key, tmp);
+        // With this configuration, the master key file deletion is fatal
+        masterKeyFile.delete();
+        assertThrows(IOException.class, () -> new DefaultConfidentialStore(tmp));
+    }
+
+    private byte[] randomBytes(int size) {
+        byte[] random = new byte[size];
+        sr.nextBytes(random);
+        return random;
     }
 
 }
