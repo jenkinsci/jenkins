19b4f2713fc6bcff35a145d1e136612149b3d36e
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-02-01 12:39:34 +0100

    Revert "Update with HeaderAction"
    This reverts commit 2ea0b1f867c3f3ae446c81ae780a8eb8a28c3d88.

--- core/src/main/java/jenkins/views/FullHeader.java
+++ core/src/main/java/jenkins/views/FullHeader.java
@@ -2,8 +2,6 @@ package jenkins.views;
 
 import hudson.model.Action;
 import java.util.List;
-
-import hudson.model.RootAction;
 import jenkins.model.Jenkins;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
@@ -26,13 +24,11 @@ public abstract class FullHeader extends Header {
      * @return a list of {@link Action} to show in the header, defaults to {@link hudson.model.RootAction} extensions
      */
     @Restricted(NoExternalUse.class)
-    public List<HeaderAction> getActions() {
+    public List<Action> getActions() {
         return Jenkins.get()
                 .getActions()
                 .stream()
                 .filter(e -> e.getIconFileName() != null)
-                .map(e -> (RootAction) e)
-                .map(e -> new HeaderAction(e.getDisplayName(), e.getIconFileName(), e.getUrlName(), e.getBadge()))
                 .toList();
     }
 }
