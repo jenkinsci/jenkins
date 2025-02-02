ef847c995521f32bac3ef86b8355146b43008ff4
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-02-02 22:56:29 +0000

    Getting there 🚀

--- src/main/js/components/header/index.js
+++ src/main/js/components/header/index.js
@@ -1,3 +1,5 @@
+import Utils from "@/components/dropdowns/utils";
+
 function init() {
   window.addEventListener("scroll", () => {
     const navigation = document.querySelector("#page-header");
@@ -20,6 +22,40 @@ function init() {
       );
     }
   });
+
+  const breadcrumbsOverflow = document.querySelector("#button-breadcrumbs-overflow");
+  const breadcrumbs = [...document.querySelectorAll(".jenkins-header__breadcrumbs__list-item")].slice(2)
+
+  if (!breadcrumbsBarOverflows()) {
+    breadcrumbsOverflow.parentNode.remove();
+  }
+
+  const items = [];
+  while (breadcrumbsBarOverflows()) {
+    const item = breadcrumbs.shift();
+    items.push(item);
+    item.remove();
+
+    console.log("Overflowing?")
+    console.log(items)
+  }
+
+  Utils.generateDropdown(breadcrumbsOverflow, (instance) => {
+    const mappedItems = items.map(e => (
+      {
+        type: "link",
+        label: e.textContent,
+        url: e.querySelector("a")?.href,
+      }
+    ));
+
+    instance.setContent(Utils.generateDropdownItems(mappedItems));
+  });
+}
+
+function breadcrumbsBarOverflows() {
+  const breadcrumbsBar = document.querySelector("#breadcrumbBar");
+  return breadcrumbsBar.scrollWidth > breadcrumbsBar.offsetWidth;
 }
 
 export default { init };
