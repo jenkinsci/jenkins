11d1fa7b98e2a13952519e6e0b8a4346fbee672d
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-02-04 13:21:12 +0100

    Better document lib.hudson.widget-refresh

--- core/src/main/resources/lib/hudson/widget-refresh.js
+++ core/src/main/resources/lib/hudson/widget-refresh.js
@@ -1,10 +1,17 @@
+/* global Behaviour, refreshPart */
+/**
+ * Use the `widget-refresh-reference` class on an element with `data-id` and `data-url` attributes.
+ * The content from the URL will be used to replace the element with the specified ID.
+ * Usually the URL content is an element with the same ID as specified here, to allow continuous updates.
+ * This is primarily used for sidepanel widgets, but not exclusively.
+ */
 Behaviour.specify(
   ".widget-refresh-reference",
   "widget-refresh",
   0,
   function (e) {
-    var id = e.getAttribute("data-id");
-    var url = e.getAttribute("data-url");
+    let id = e.getAttribute("data-id");
+    let url = e.getAttribute("data-url");
     refreshPart(id, url);
   },
 );
