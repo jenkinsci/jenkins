aceaf366b0e998565b6554fd9edf34e09067cf65
Daniel Rivas <drivas8891@gmail.com>
2025-02-06 11:26:32 -0800

    Found correct directory for J-Unit Testing

---
+++ core/src/test/java/org/jenkins/mytests/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package org.jenkins.mytests;
+
+import static org.junit.Assert.assertTrue;  // SPACE BETWEEN IMPORTS HERE IS NECESSARY
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void helloWorld() {
+        assertTrue(true);
+    }
+}
