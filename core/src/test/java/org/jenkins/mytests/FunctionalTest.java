41c2eef1911aa6b326042ae9d3e5afa2f0b20ef4
Jennifer <jennifersun97@gmail.com>
2025-02-12 20:23:22 -0800

    FunctionalTest part 2 1 works yay

---
+++ core/src/test/java/org/jenkins/mytests/FunctionalTest.java
@@ -0,0 +1,22 @@
+package org.jenkins.mytests;
+
+// STATIC imports
+
+import hudson.security.HudsonPrivateSecurityRealm;
+import jenkins.model.Jenkins;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class FunctionalTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+    @Test
+    public void testGetAllUsers() {
+        HudsonPrivateSecurityRealm testRealm = new HudsonPrivateSecurityRealm(false, false, null);
+
+        Jenkins jenkins = j.jenkins;
+        j.jenkins.setSecurityRealm(testRealm);
+        System.out.println((testRealm.getAllUsers()).size());
+    }
+}
