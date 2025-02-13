be76a9558b73d9587d05d246263e0eaf62076e8d
Daniel Rivas <drivas8891@gmail.com>
2025-02-12 23:27:52 -0800

    Added functionality to delete users

--- core/src/test/java/org/jenkins/mytests/FunctionalTest.java
+++ core/src/test/java/org/jenkins/mytests/FunctionalTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 
 import hudson.model.TopLevelItem;
+import hudson.model.User;
 import hudson.security.HudsonPrivateSecurityRealm;
 import hudson.security.pages.SignupPage;
 import jenkins.model.Jenkins;
@@ -39,17 +40,54 @@ public class FunctionalTest {
 
     @Test
     public void createUser() throws Exception {
+        // Create a security realm to allow for user signup
         HudsonPrivateSecurityRealm securityRealm = new HudsonPrivateSecurityRealm(true, false, null);
         j.jenkins.setSecurityRealm(securityRealm);
         JenkinsRule.WebClient wc = j.createWebClient();
+
+        // Sign up the new user
         SignupPage signup = new SignupPage(wc.goTo("signup"));
         signup.enterUsername("test");
         signup.enterPassword("12345");
         signup.enterFullName("Test User");
+
+        // Submit the sign-up form
         HtmlPage success = signup.submit(j);
         assertThat(success.getElementById("main-panel").getTextContent(), containsString("Success"));
         assertThat(success.getAnchorByHref("/jenkins/user/test").getTextContent(), containsString("Test User"));
 
+        // Verify that the user was created successfully
         assertEquals("Test User", securityRealm.getUser("test").getDisplayName());
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        // Use a security realm that allows user signup
+        HudsonPrivateSecurityRealm securityRealm = new HudsonPrivateSecurityRealm(true, false, null);
+        j.jenkins.setSecurityRealm(securityRealm);
+        JenkinsRule.WebClient wc = j.createWebClient();
+
+        // First, sign up a new user
+        SignupPage signup = new SignupPage(wc.goTo("signup"));
+        signup.enterUsername("deleteTest");
+        signup.enterPassword("12345");
+        signup.enterFullName("Delete Test User");
+
+        // Submit the sign-up form
+        HtmlPage success = signup.submit(j);
+
+        // Verify that the user was created successfully by checking the main-panel and the user's display name
+        assertThat(success.getElementById("main-panel").getTextContent(), containsString("Success"));
+        assertEquals("Delete Test User", securityRealm.getUser("deleteTest").getDisplayName());
+
+        // Now, delete the user
+        User user = securityRealm.getUser("deleteTest");
+        user.delete();
+
+        // Verify that the user is no longer in the security realm (should return null)
+        assertEquals(null, securityRealm.getUser("deleteTest"));
+
+        // Also verify that the list of users is now empty
+        assertEquals(0, securityRealm.getAllUsers().size());
     }
 }
