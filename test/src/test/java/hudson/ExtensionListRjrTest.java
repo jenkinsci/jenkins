99b8be82e869505b4ccaccf6727b6cb97ad487d7
Devin Nusbaum <dnusbaum@cloudbees.com>
2025-02-20 12:36:40 -0500

    [JENKINS-75232] Update tests that were installing variant dynamically

--- test/src/test/java/hudson/ExtensionListRjrTest.java
+++ test/src/test/java/hudson/ExtensionListRjrTest.java
@@ -1,12 +1,20 @@
 package hudson;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Collection;
+import jenkins.plugins.dependee.Dependee;
+import jenkins.plugins.dependee.DependeeExtensionPoint;
 import jenkins.plugins.dynamic_extension_loading.CustomExtensionLoadedViaConstructor;
 import jenkins.plugins.dynamic_extension_loading.CustomExtensionLoadedViaListener;
 import jenkins.plugins.dynamic_extension_loading.CustomPeriodicWork;
+import jenkins.plugins.optional_depender.OptionalDepender;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.RealJenkinsRule;
 import org.jvnet.hudson.test.RealJenkinsRule.SyntheticPlugin;
@@ -21,6 +29,7 @@ public class ExtensionListRjrTest {
      * methods that load other extensions and which are called by an {@link ExtensionListListener}.
      */
     @Test
+    @Issue("JENKINS-75232")
     public void checkDynamicLoad_singleRegistration() throws Throwable {
         var pluginJpi = rjr.createSyntheticPlugin(new SyntheticPlugin(CustomPeriodicWork.class.getPackage())
                 .shortName("dynamic-extension-loading")
@@ -45,5 +54,27 @@ public class ExtensionListRjrTest {
             System.out.println("Duplicates are: " + list);
             throw t;
         }
+    }
+
+    @Test
+    @Issue({ "JENKINS-50336", "JENKINS-60449" })
+    public void installDependedOptionalPluginWithoutRestart() throws Throwable {
+        var optionalDependerJpi = rjr.createSyntheticPlugin(new SyntheticPlugin(OptionalDepender.class.getPackage())
+                .header("Plugin-Dependencies", "variant:0,dependee:0;resolution:=optional"));
+        var dependeeJpi = rjr.createSyntheticPlugin(new SyntheticPlugin(Dependee.class.getPackage()).shortName("dependee"));
+        var fqcn1 = OptionalDepender.class.getName();
+        var fqcn2 = DependeeExtensionPoint.class.getName();
+        rjr.then(r -> {
+            // Load optional-depender.
+            r.jenkins.pluginManager.dynamicLoad(optionalDependerJpi);
+            // JENKINS-60449: Extension depending on dependee class isn't loaded
+            assertThat((Collection<?>) r.jenkins.getExtensionList(fqcn1), empty());
+            // Load dependee.
+            r.jenkins.pluginManager.dynamicLoad(dependeeJpi);
+            // JENKINS-60449: Classes in depender are loaded prior to OptionalDepender being loaded.
+            assertThat((Collection<?>) r.jenkins.getExtensionList(fqcn1), hasSize(1));
+            // JENKINS-50336: Extension point from dependee now includes optional extension from optional-depender
+            assertThat((Collection<?>) r.jenkins.getExtensionList(fqcn2), hasSize(1));
+        });
     }
 }
