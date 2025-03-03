aea0d50b609ac578bffa482b3d386b89db5254ff
eazi201 <kfablionel@gmail.com>
2025-03-03 20:22:25 +0200

    implemented requested changes

--- core/src/main/java/jenkins/security/AgentSecretAction.java
+++ core/src/main/java/jenkins/security/AgentSecretAction.java
@@ -1,16 +1,19 @@
 package jenkins.security;
 
+import hudson.Extension;
 import hudson.model.Action;
 import hudson.model.Computer;
 import hudson.slaves.JNLPLauncher;
 import hudson.slaves.SlaveComputer;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jenkins.model.Jenkins;
+import jenkins.model.TransientActionFactory;
 import org.kohsuke.stapler.StaplerRequest2;
 import org.kohsuke.stapler.StaplerResponse2;
-import org.kohsuke.stapler.WebMethod;
 import org.kohsuke.stapler.verb.GET;
 
 public class AgentSecretAction implements Action {
@@ -37,25 +40,40 @@ public class AgentSecretAction implements Action {
         return null;
     }
 
-    @WebMethod(name = "")
     @GET
     public void doIndex(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException {
 
         computer.checkPermission(Computer.CONNECT);
 
-            if (!(computer.getLauncher() instanceof JNLPLauncher)) {
+        if (!(computer.getLauncher() instanceof JNLPLauncher)) {
                 throw new SecurityException("This API is only available for inbound agents.");
-            }
-            String secret = computer.getJnlpMac();
+        }
+        String secret = computer.getJnlpMac();
 
-            if (secret != null) {
-                rsp.setContentType("text/plain");
-                rsp.getWriter().write(secret);
-                LOGGER.log(Level.FINE, "Agent secret retrieved for node {0} by user {1}",
-                        new Object[]{computer.getName(), Jenkins.getAuthentication2().getName()});
-            } else {
-                throw new IOException("Secret not available for node: " + computer.getName());
-            }
+        if (secret != null) {
+            rsp.setContentType("text/plain");
+            rsp.getWriter().write(secret);
+            LOGGER.log(Level.FINE, "Agent secret retrieved for node {0} by user {1}",
+                    new Object[]{computer.getName(), Jenkins.getAuthentication2().getName()});
+        } else {
+            throw new IOException("Secret not available for node: " + computer.getName());
+        }
+    }
+
+        @Extension
+    public static class TransientAgentActionFactory extends TransientActionFactory<SlaveComputer> {
+
+        @Override
+        public Class<SlaveComputer> type() {
+            return SlaveComputer.class;
         }
 
+        @Override
+        public Collection<? extends Action> createFor(SlaveComputer target) {
+            if (!(target.getLauncher() instanceof hudson.slaves.JNLPLauncher)) {
+                return Collections.emptyList();
+          }
+            return Collections.singleton(new AgentSecretAction(target));
+        }
+    }
 }
--- test/src/test/java/jenkins/security/AgentSecretActionTest.java
+++ test/src/test/java/jenkins/security/AgentSecretActionTest.java
@@ -3,18 +3,16 @@ package jenkins.security;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import hudson.model.Computer;
-import hudson.model.User;
-import hudson.security.ACL;
-import hudson.security.ACLContext;
 import hudson.slaves.DumbSlave;
 import hudson.slaves.SlaveComputer;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import org.htmlunit.FailingHttpStatusCodeException;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,48 +30,48 @@ public class AgentSecretActionTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    private DumbSlave agent;
+
     private AgentSecretAction action;
     private StaplerRequest2 req;
     private StaplerResponse2 rsp;
     private StringWriter stringWriter;
     private PrintWriter writer;
 
+    private String agentUrl;
+
     @Before
     public void setUp() throws Exception {
-        SlaveComputer computer = mock(SlaveComputer.class);
+        agent = new DumbSlave("test-agent", "/home/jenkins", new hudson.slaves.JNLPLauncher());
+        j.jenkins.addNode(agent);
+        SlaveComputer computer = (SlaveComputer) agent.getComputer();
         action = new AgentSecretAction(computer);
         req = mock(StaplerRequest2.class);
         rsp = mock(StaplerResponse2.class);
         stringWriter = new StringWriter();
         writer = new PrintWriter(stringWriter);
         when(rsp.getWriter()).thenReturn(writer);
+        agentUrl = "computer/test-agent/agentSecret/";
     }
 
     @Test
     public void testGetSecretWithValidPermissions() throws Exception {
-        DumbSlave agent = j.createSlave("test-agent", null);
         String expectedSecret = ((SlaveComputer) agent.getComputer()).getJnlpMac();
 
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
         MockAuthorizationStrategy authStrategy = new MockAuthorizationStrategy()
                 .grant(Computer.CONNECT).everywhere().to("test-user");
-        j.jenkins.setAuthorizationStrategy(authStrategy);
+        JenkinsRule.WebClient webClient = j.createWebClient();
+        webClient.login("test-user");
+        String response = webClient.goTo(agentUrl, "text/plain")
+                .getWebResponse().getContentAsString();
 
-        User user = User.getById("test-user", true);
-            try (ACLContext ignored = ACL.as(user)) {
-                action.doIndex(req, rsp);
-                writer.flush();
-                verify(rsp).setContentType("text/plain");
-                assertEquals(expectedSecret, stringWriter.toString());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+        assertEquals(expectedSecret, response);
 
     }
 
     @Test
     public void testGetSecretViaWebClient() throws Exception {
-        DumbSlave agent = j.createSlave("test-agent", null);
         String expectedSecret = ((SlaveComputer) agent.getComputer()).getJnlpMac();
 
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
@@ -82,7 +80,7 @@ public class AgentSecretActionTest {
 
         JenkinsRule.WebClient webClient = j.createWebClient();
         webClient.login("test-user");
-        String response = webClient.goTo("computer/test-agent/agentSecret/", "text/plain")
+        String response = webClient.goTo(agentUrl, "text/plain")
                 .getWebResponse().getContentAsString();
 
         assertEquals(expectedSecret, response);
@@ -90,38 +88,17 @@ public class AgentSecretActionTest {
 
     @Test
     public void testGetSecretWithoutPermissions() throws Exception {
-        j.createSlave("test-agent", null);
-
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
         j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy());
 
         JenkinsRule.WebClient webClient = j.createWebClient();
         webClient.login("test-user");
-        thrown.expect(org.acegisecurity.AccessDeniedException.class);
-        action.doIndex(req, rsp);
-    }
-
-    @Test
-    public void testGetSecretWithInvalidNodeName() throws Exception {
-        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
-        j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy()
-                .grant(Computer.CONNECT).everywhere().to("test-user"));
-
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Node not found: non-existent-agent");
-        action.doIndex(req, rsp);
-    }
-
-    @Test
-    public void testGetSecretWithNullNodeName() throws Exception {
-        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
-        j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy()
-                .grant(Computer.CONNECT).everywhere().to("test-user"));
-
-
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Node name is required");
-        action.doIndex(req, rsp);
+        try {
+            webClient.goTo(agentUrl, "text/plain");
+            fail("Expected AccessDeniedException was not thrown.");
+        } catch (FailingHttpStatusCodeException e) {
+            assertEquals(403, e.getStatusCode());
+        }
     }
 
 }
