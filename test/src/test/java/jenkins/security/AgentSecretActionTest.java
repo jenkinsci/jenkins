f15db276a0d7da012aa4b97749d1acd61bc198a9
eazi201 <kfablionel@gmail.com>
2025-03-04 23:58:32 +0200

    implemented requested changes

--- test/src/test/java/jenkins/security/AgentSecretActionTest.java
+++ test/src/test/java/jenkins/security/AgentSecretActionTest.java
@@ -32,7 +32,6 @@ public class AgentSecretActionTest {
 
     private DumbSlave agent;
 
-    private AgentSecretAction action;
     private StaplerRequest2 req;
     private StaplerResponse2 rsp;
     private StringWriter stringWriter;
@@ -43,15 +42,12 @@ public class AgentSecretActionTest {
     @Before
     public void setUp() throws Exception {
         agent = new DumbSlave("test-agent", "/home/jenkins", new hudson.slaves.JNLPLauncher());
-        j.jenkins.addNode(agent);
-        SlaveComputer computer = (SlaveComputer) agent.getComputer();
-        action = new AgentSecretAction(computer);
         req = mock(StaplerRequest2.class);
         rsp = mock(StaplerResponse2.class);
         stringWriter = new StringWriter();
         writer = new PrintWriter(stringWriter);
         when(rsp.getWriter()).thenReturn(writer);
-        agentUrl = "computer/test-agent/agentSecret/";
+        agentUrl = "computer/test-agent/agent-secret/";
     }
 
     @Test
@@ -61,6 +57,7 @@ public class AgentSecretActionTest {
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
         MockAuthorizationStrategy authStrategy = new MockAuthorizationStrategy()
                 .grant(Computer.CONNECT).everywhere().to("test-user");
+        j.jenkins.setAuthorizationStrategy(authStrategy);
         JenkinsRule.WebClient webClient = j.createWebClient();
         webClient.login("test-user");
         String response = webClient.goTo(agentUrl, "text/plain")
@@ -68,22 +65,6 @@ public class AgentSecretActionTest {
 
         assertEquals(expectedSecret, response);
 
-    }
-
-    @Test
-    public void testGetSecretViaWebClient() throws Exception {
-        String expectedSecret = ((SlaveComputer) agent.getComputer()).getJnlpMac();
-
-        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
-        j.jenkins.setAuthorizationStrategy(new MockAuthorizationStrategy()
-                .grant(Computer.CONNECT).everywhere().to("test-user"));
-
-        JenkinsRule.WebClient webClient = j.createWebClient();
-        webClient.login("test-user");
-        String response = webClient.goTo(agentUrl, "text/plain")
-                .getWebResponse().getContentAsString();
-
-        assertEquals(expectedSecret, response);
     }
 
     @Test
