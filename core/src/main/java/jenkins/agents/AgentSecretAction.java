30c72fae1f266e54e647c9ea08dadbfb4295d972
eazi201 <kfablionel@gmail.com>
2025-03-08 15:25:11 +0200

    changing packages

--- core/src/main/java/jenkins/agents/AgentSecretAction.java
+++ core/src/main/java/jenkins/agents/AgentSecretAction.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2025 CloudBees, Inc.
+ * Copyright 2025 Kalinda Fabrice.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
