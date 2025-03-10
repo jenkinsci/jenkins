2979c40f5d9deb8d0456177db5a8fd1d128818e0
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-03-10 14:03:51 +0100

    [JENKINS-75388] Remove ?path and ?pattern support from DBS

--- test/src/test/java/jenkins/security/Security3501Test.java
+++ test/src/test/java/jenkins/security/Security3501Test.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.util.List;
+import jenkins.security.security3501Test.Security3501RootAction;
 import org.htmlunit.FailingHttpStatusCodeException;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -13,7 +14,7 @@ import org.jvnet.hudson.test.RealJenkinsRule;
 
 public class Security3501Test {
     @Rule
-    public RealJenkinsRule jj = new RealJenkinsRule();
+    public RealJenkinsRule jj = new RealJenkinsRule().addSyntheticPlugin(new RealJenkinsRule.SyntheticPlugin(Security3501RootAction.class.getPackage()).shortName("Security3501RootAction"));
 
     @Test
     public void testRedirects() throws Throwable {
@@ -24,9 +25,17 @@ public class Security3501Test {
         List<String> prohibitedPaths = List.of("%5C%5Cexample.org", "%5C/example.org", "/%5Cexample.org", "//example.org", "https://example.org", "\\example.org");
         for (String path : prohibitedPaths) {
             try (JenkinsRule.WebClient wc = j.createWebClient().withRedirectEnabled(false)) {
-                final FailingHttpStatusCodeException fhsce = Assert.assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("userContent?path=" + path));
+                final FailingHttpStatusCodeException fhsce = Assert.assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("redirects/content?path=" + path));
+                assertThat(fhsce.getStatusCode(), is(404));
+            }
+        }
+
+        List<String> allowedPaths = List.of("foo", "foo/bar");
+        for (String path : allowedPaths) {
+            try (JenkinsRule.WebClient wc = j.createWebClient().withRedirectEnabled(false)) {
+                final FailingHttpStatusCodeException fhsce = Assert.assertThrows(FailingHttpStatusCodeException.class, () -> wc.goTo("redirects/content?path=" + path));
                 assertThat(fhsce.getStatusCode(), is(302));
-                assertThat(fhsce.getResponse().getResponseHeaderValue("Location"), is(j.getURL().toExternalForm() + "userContent/"));
+                assertThat(fhsce.getResponse().getResponseHeaderValue("Location"), is(j.contextPath + "/redirects/" + path));
             }
         }
     }
