8b67b1ba42c357a10cd4adc7329d4f063668f5f0
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-03-18 09:57:27 +0000

    Move getActions to Header

--- core/src/main/java/jenkins/views/FullHeader.java
+++ core/src/main/java/jenkins/views/FullHeader.java
@@ -1,11 +1,5 @@
 package jenkins.views;
 
-import hudson.model.Action;
-import java.util.List;
-import jenkins.model.Jenkins;
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.NoExternalUse;
-
 /**
  * {@link Header} that provides its own resources as full replacement. It does not
  * depends on any core resource (images, CSS, JS, etc.)
@@ -18,17 +12,5 @@ public abstract class FullHeader extends Header {
 
     public boolean isCompatible() {
         return true;
-    }
-
-    /**
-     * @return a list of {@link Action} to show in the header, defaults to {@link hudson.model.RootAction} extensions
-     */
-    @Restricted(NoExternalUse.class)
-    public List<Action> getActions() {
-        return Jenkins.get()
-                .getActions()
-                .stream()
-                .filter(e -> e.getIconFileName() != null)
-                .toList();
     }
 }
