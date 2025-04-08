c769824fbac1a08260b445c5555c1b977d771fdf
Mark Waite <mark.earl.waite@gmail.com>
2025-04-08 01:32:14 -0600

    Revert "[JENKINS-75465] Delete RunIdMigrator as it has been 10 years since this migration" (#10517)
    * Revert "[JENKINS-75465] Delete RunIdMigrator as it has been 10 years since this migration"
    
    * Fast incremental build
    
    * Revert "Fast incremental build"
    
    Leave the commit as only the revert of the original change
    
    This reverts commit 2cf37a11df582b8c515b74eb8e7576de028993f6.
    
    ---------
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>

--- core/src/main/java/hudson/model/RunMap.java
+++ core/src/main/java/hudson/model/RunMap.java
@@ -45,8 +45,10 @@ import java.util.Objects;
 import java.util.SortedMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jenkins.model.RunIdMigrator;
 import jenkins.model.lazy.AbstractLazyLoadRunMap;
 import jenkins.model.lazy.BuildReference;
+import jenkins.model.lazy.LazyBuildMixIn;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 
@@ -69,6 +71,10 @@ public final class RunMap<R extends Run<?, R>> extends AbstractLazyLoadRunMap<R>
     private final @CheckForNull Job<?, ?> job;
 
     private Constructor<R> cons;
+
+    /** Normally overwritten by {@link LazyBuildMixIn#onLoad} or {@link LazyBuildMixIn#onCreatedFromScratch}, in turn created during {@link Job#onLoad}. */
+    @Restricted(NoExternalUse.class)
+    public RunIdMigrator runIdMigrator = new RunIdMigrator();
 
     // TODO: before first complete build
     // patch up next/previous build link
@@ -150,6 +156,7 @@ public final class RunMap<R extends Run<?, R>> extends AbstractLazyLoadRunMap<R>
     @Override
     public boolean removeValue(R run) {
         run.dropLinks();
+        runIdMigrator.delete(dir, run.getId());
         return super.removeValue(run);
     }
 
@@ -220,13 +227,14 @@ public final class RunMap<R extends Run<?, R>> extends AbstractLazyLoadRunMap<R>
         return super._put(r);
     }
 
-    @CheckForNull
     @Override public R getById(String id) {
+        int n;
         try {
-            return getByNumber(Integer.parseInt(id));
-        } catch (NumberFormatException e) { // see https://issues.jenkins.io/browse/JENKINS-75476
-            return null;
+            n = Integer.parseInt(id);
+        } catch (NumberFormatException x) {
+            n = runIdMigrator.findNumber(id);
         }
+        return getByNumber(n);
     }
 
     /**
