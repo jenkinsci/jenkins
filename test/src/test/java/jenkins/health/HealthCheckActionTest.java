158b7877be1c75f03de08643a9f99dd66afcebac
Vincent Latombe <vincent@latombe.net>
2025-04-08 17:58:55 +0200

    Checkstyle

--- test/src/test/java/jenkins/health/HealthCheckActionTest.java
+++ test/src/test/java/jenkins/health/HealthCheckActionTest.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
 package jenkins.health;
 
 import static org.hamcrest.MatcherAssert.assertThat;
