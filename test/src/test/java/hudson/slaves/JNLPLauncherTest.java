jenkinsci/jenkins

commit f45ba02af86d07caa3928010e384015babc2ccdb

[JENKINS-33704] Limit scope of `Jenkins#updateComputerList` to improve performance at scale (#10494)
* [JENKINS-33704] Limit scope of `Jenkins#updateComputerList`

Along the same line as #5882, but for computers

Adding/updating/removing a node to the system was causing all nodes retention strategies to be checked. If you have a lot of nodes, this quickly adds up to an extreme amount of checks.

This change makes the calls local, by only affecting the related nodes.

* Clarify variable name

* Fix logic

* Avoid dealing with `null`

core/src/main/java/jenkins/model/Nodes.java
@@ -166,8 +166,13 @@ public class Nodes implements PersistenceRoot {
                     @Override
                     public void run() {
                         nodes.compute(node.getNodeName(), (ignoredNodeName, ignoredNode) -> old);
-                        jenkins.updateComputerList();
-                        jenkins.trimLabels(node, old);
+                        if (old != null) {
+                            jenkins.updateComputers(node, old);
+                            jenkins.trimLabels(node, old);
+                        } else {
+                            jenkins.updateComputers(node);
+                            jenkins.trimLabels(node);
+                        }
                     }
                 });
                 throw e;
@@ -260,7 +265,7 @@ public class Nodes implements PersistenceRoot {
                 Util.deleteRecursive(new File(getRootDir(), oldOne.getNodeName()));
             }
             Queue.withLock(() -> {
-                jenkins.updateComputerList();
+                jenkins.updateComputers(oldOne, newOne);
                 jenkins.trimLabels(oldOne, newOne);
             });
             NodeListener.fireOnUpdated(oldOne, newOne);
@@ -297,7 +302,7 @@ public class Nodes implements PersistenceRoot {
             Util.deleteRecursive(new File(getRootDir(), node.getNodeName()));
 
             if (match.get()) {
-                jenkins.updateComputerList();
+                jenkins.updateComputers(node);
                 jenkins.trimLabels(node);
             }
             NodeListener.fireOnDeleted(node);
@@ -407,7 +412,7 @@ public class Nodes implements PersistenceRoot {
 
     public void load(File dir) throws IOException {
         Node n = load(dir, nodes);
-        jenkins.updateComputerList();
+        jenkins.updateComputers(n);
         jenkins.trimLabels(n);
     }
 
@@ -416,7 +421,7 @@ public class Nodes implements PersistenceRoot {
             AtomicBoolean match = new AtomicBoolean();
             Queue.withLock(() -> match.set(node == nodes.remove(node.getNodeName())));
             if (match.get()) {
-                jenkins.updateComputerList();
+                jenkins.updateComputers(node);
                 jenkins.trimLabels(node);
             }
         }

core/src/main/java/hudson/model/AbstractCIBase.java
@@ -33,6 +33,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.security.AccessControlled;
 import hudson.slaves.ComputerListener;
 import hudson.slaves.RetentionStrategy;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -226,48 +227,47 @@ public abstract class AbstractCIBase extends Node implements ItemGroup<TopLevelI
     }
 
     /**
-     * Updates Computers.
+     * Updates Computers for the specified nodes.
      *
      * <p>
      * This method tries to reuse existing {@link Computer} objects
      * so that we won't upset {@link Executor}s running in it.
+     *
+     * @param automaticAgentLaunch whether to automatically launch agents.
+     * @param nodes the nodes to update.
      */
-    protected void updateComputerList(final boolean automaticAgentLaunch) {
-        final ConcurrentMap<Node, Computer> computers = getComputerMap();
-        final Set<Computer> old = new HashSet<>(computers.size());
-        Queue.withLock(new Runnable() {
-            @Override
-            public void run() {
-                Map<String, Computer> byName = new HashMap<>();
-                for (Computer c : computers.values()) {
+    protected void updateComputerList(final boolean automaticAgentLaunch, @NonNull Collection<Node> nodes) {
+        final Set<Computer> old = new HashSet<>(nodes.size());
+        Queue.withLock(() -> {
+            Map<String, Computer> byName = new HashMap<>();
+            for (Computer c : getComputerMap().values()) {
+                Node node = c.getNode();
+                if (node == null) {
                     old.add(c);
-                    Node node = c.getNode();
-                    if (node == null)
-                        continue;   // this computer is gone
+                    // this computer is gone
+                } else if (nodes.contains(node)) {
+                    old.add(c);
                     byName.put(node.getNodeName(), c);
                 }
-
-                Set<Computer> used = new HashSet<>(old.size());
-
-                updateComputer(AbstractCIBase.this, byName, used, automaticAgentLaunch);
-                for (Node s : getNodes()) {
-                    long start = System.currentTimeMillis();
-                    updateComputer(s, byName, used, automaticAgentLaunch);
-                    if (LOG_STARTUP_PERFORMANCE && LOGGER.isLoggable(Level.FINE)) {
-                        LOGGER.fine(String.format("Took %dms to update node %s",
-                                System.currentTimeMillis() - start, s.getNodeName()));
-                    }
+            }
+            Set<Computer> used = new HashSet<>(old.size());
+            for (Node node : nodes) {
+                long start = System.currentTimeMillis();
+                updateComputer(node, byName, used, automaticAgentLaunch);
+                if (LOG_STARTUP_PERFORMANCE && LOGGER.isLoggable(Level.FINE)) {
+                    LOGGER.fine(String.format("Took %dms to update node %s",
+                            System.currentTimeMillis() - start, node.getNodeName()));
                 }
+            }
 
-                // find out what computers are removed, and kill off all executors.
-                // when all executors exit, it will be removed from the computers map.
-                // so don't remove too quickly
-                old.removeAll(used);
-                // we need to start the process of reducing the executors on all computers as distinct
-                // from the killing action which should not excessively use the Queue lock.
-                for (Computer c : old) {
-                    c.inflictMortalWound();
-                }
+            // find out what computers are removed, and kill off all executors.
+            // when all executors exit, it will be removed from the computers map.
+            // so don't remove too quickly
+            old.removeAll(used);
+            // we need to start the process of reducing the executors on all computers as distinct
+            // from the killing action which should not excessively use the Queue lock.
+            for (Computer c : old) {
+                c.inflictMortalWound();
             }
         });
         for (Computer c : old) {

core/src/main/java/hudson/model/Computer.java
@@ -89,6 +89,7 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Enumeration;
@@ -799,7 +800,7 @@ public /*transient*/ abstract class Computer extends Actionable implements Acces
     }
 
     /**
-     * Called by {@link Jenkins#updateComputerList()} to notify {@link Computer} that it will be discarded.
+     * Called by {@link Jenkins#updateComputerList(boolean, Collection)} to notify {@link Computer} that it will be discarded.
      *
      * <p>
      * Note that at this point {@link #getNode()} returns null.
@@ -814,7 +815,7 @@ public /*transient*/ abstract class Computer extends Actionable implements Acces
     }
 
     /**
-     * Called by {@link Jenkins#updateComputerList()} to notify {@link Computer} that it will be discarded.
+     * Called by {@link Jenkins#updateComputerList(boolean, Collection)} to notify {@link Computer} that it will be discarded.
      *
      * <p>
      * Note that at this point {@link #getNode()} returns null.

core/src/main/java/hudson/model/Node.java
@@ -59,6 +59,7 @@ import hudson.util.TagCloud;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -238,7 +239,7 @@ public abstract class Node extends AbstractModelObject implements Reconfigurable
     /**
      * Creates a new {@link Computer} object that acts as the UI peer of this {@link Node}.
      *
-     * Nobody but {@link Jenkins#updateComputerList()} should call this method.
+     * Nobody but {@link Jenkins#updateComputerList(boolean, Collection)} should call this method.
      * @return Created instance of the computer.
      *         Can be {@code null} if the {@link Node} implementation does not support it (e.g. {@link Cloud} agent).
      */

core/src/main/java/jenkins/model/Jenkins.java
@@ -1594,8 +1594,25 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
         updateNewComputer(n, AUTOMATIC_AGENT_LAUNCH);
     }
 
+    /**
+     * Update the list of computers that are running on this Jenkins instance.
+     * Consider {@link #updateComputers(Node...)} instead if you know what nodes needs to be updated.
+     * @see #updateComputers(Node...)
+     */
     protected void updateComputerList() {
-        updateComputerList(AUTOMATIC_AGENT_LAUNCH);
+        var allNodes = new HashSet<Node>();
+        allNodes.add(this);
+        allNodes.addAll(getNodes());
+        updateComputerList(AUTOMATIC_AGENT_LAUNCH, allNodes);
+    }
+
+    /**
+     * Update the computers for the given nodes.
+     */
+    protected void updateComputers(@NonNull Node... nodes) {
+        var nodeSet = new HashSet<Node>();
+        Collections.addAll(nodeSet, nodes);
+        updateComputerList(AUTOMATIC_AGENT_LAUNCH, nodeSet);
     }
 
     /** @deprecated Use {@link SCMListener#all} instead. */
@@ -2991,7 +3008,7 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
         }
         if (this.numExecutors != n) {
             this.numExecutors = n;
-            updateComputerList();
+            updateComputers(this);
             save();
         }
     }
@@ -4030,7 +4047,7 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
                 result &= configureDescriptor(req, json, d);
 
             save();
-            updateComputerList();
+            updateComputers(this);
             if (result)
                 FormApply.success(req.getContextPath() + '/').generateResponse(req, rsp, null);
             else
@@ -4083,7 +4100,7 @@ public class Jenkins extends AbstractCIBase implements DirectlyModifiableTopLeve
             bc.commit();
         }
 
-        updateComputerList();
+        updateComputers(this);
 
         FormApply.success(req.getContextPath() + '/' + toComputer().getUrl()).generateResponse(req, rsp, null);
     }

test/src/test/java/jenkins/model/NodesTest.java
@@ -29,6 +29,7 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.sameInstance;
@@ -49,6 +50,8 @@ import hudson.model.Slave;
 import hudson.model.listeners.SaveableListener;
 import hudson.slaves.ComputerLauncher;
 import hudson.slaves.DumbSlave;
+import hudson.slaves.RetentionStrategy;
+import hudson.slaves.SlaveComputer;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -166,6 +169,64 @@ public class NodesTest {
         r.jenkins.getNodesObject().replaceNode(oldNode, newNode);
         r.jenkins.getNodesObject().load();
         assertNotNull(r.jenkins.getNode("foo"));
+    }
+
+    @Test
+    @Issue("JENKINS-33704")
+    public void replacingSecondNodeIsLocal() throws Exception {
+        DumbSlave nodeA = r.createSlave("nodeA", "temp", null);
+        var retentionStrategyA = new MockRetentionStrategy();
+        nodeA.setRetentionStrategy(retentionStrategyA);
+        r.jenkins.addNode(nodeA);
+        assertThat(retentionStrategyA.checkCount, equalTo(0));
+        DumbSlave nodeB = r.createSlave("nodeB", "temp", null);
+        var retentionStrategyB = new MockRetentionStrategy();
+        nodeB.setRetentionStrategy(retentionStrategyB);
+        r.jenkins.addNode(nodeB);
+        assertThat(retentionStrategyA.checkCount, equalTo(0));
+        assertThat(retentionStrategyB.checkCount, equalTo(0));
+        Jenkins.get().getNodesObject().replaceNode(nodeA, nodeA);
+        assertThat(retentionStrategyB.checkCount, equalTo(0));
+    }
+
+    @Test
+    @Issue("JENKINS-33704")
+    public void removingSecondNodeIsLocal() throws Exception {
+        DumbSlave nodeA = r.createSlave("nodeA", "temp", null);
+        var retentionStrategyA = new MockRetentionStrategy();
+        nodeA.setRetentionStrategy(retentionStrategyA);
+        r.jenkins.addNode(nodeA);
+        assertThat(retentionStrategyA.checkCount, equalTo(0));
+        DumbSlave nodeB = r.createSlave("nodeB", "temp", null);
+        var retentionStrategyB = new MockRetentionStrategy();
+        nodeB.setRetentionStrategy(retentionStrategyB);
+        r.jenkins.addNode(nodeB);
+        assertThat(retentionStrategyA.checkCount, equalTo(0));
+        assertThat(retentionStrategyB.checkCount, equalTo(0));
+        r.jenkins.removeNode(nodeA);
+        assertThat(retentionStrategyB.checkCount, equalTo(0));
+    }
+
+    @Test
+    @Issue("JENKINS-33704")
+    public void changingBuiltInNodeDoesntChangeOtherNodes() throws Exception {
+        DumbSlave nodeA = r.createSlave("nodeA", "temp", null);
+        var retentionStrategyA = new MockRetentionStrategy();
+        nodeA.setRetentionStrategy(retentionStrategyA);
+        r.jenkins.addNode(nodeA);
+        assertThat(retentionStrategyA.checkCount, equalTo(0));
+        r.jenkins.setNumExecutors(1);
+        assertThat(retentionStrategyA.checkCount, equalTo(0));
+    }
+
+    public static class MockRetentionStrategy extends RetentionStrategy.Always {
+        private int checkCount = 0;
+
+        @Override
+        public long check(SlaveComputer c) {
+            checkCount++;
+            return super.check(c);
+        }
     }
 
     private static class InvalidNode extends Slave {

commit b640b69a665b1ce119d6638eb1e798818f66f1a7

[JENKINS-33704] Computer configuration not updated correctly after updating nodes
Several ATHs are failing since #10494

https://ci.jenkins.io/job/Core/job/acceptance-test-harness/job/PR-1956/7/testReport/

This is due to the call to `Jenkins#updateComputers(Node...)` being passed both the old and new object for a node when applying an update. This resulted into the old node configuration to be applied to the computer.
Changed the call to only pass the new node object.

The previous global call to `Jenkins#updateComputerList()` was looking up all nodes attached to the Jenkins node, so was not applying the logic to old node configurations.

Added a unit test catching the regression, as this was only caught in ATH.

core/src/main/java/jenkins/model/Nodes.java
@@ -166,11 +166,10 @@ public class Nodes implements PersistenceRoot {
                     @Override
                     public void run() {
                         nodes.compute(node.getNodeName(), (ignoredNodeName, ignoredNode) -> old);
+                        jenkins.updateComputers(node);
                         if (old != null) {
-                            jenkins.updateComputers(node, old);
                             jenkins.trimLabels(node, old);
                         } else {
-                            jenkins.updateComputers(node);
                             jenkins.trimLabels(node);
                         }
                     }
@@ -265,7 +264,7 @@ public class Nodes implements PersistenceRoot {
                 Util.deleteRecursive(new File(getRootDir(), oldOne.getNodeName()));
             }
             Queue.withLock(() -> {
-                jenkins.updateComputers(oldOne, newOne);
+                jenkins.updateComputers(newOne);
                 jenkins.trimLabels(oldOne, newOne);
             });
             NodeListener.fireOnUpdated(oldOne, newOne);

test/src/test/java/hudson/slaves/JNLPLauncherTest.java
@@ -48,6 +48,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
+import jenkins.model.Jenkins;
 import jenkins.security.SlaveToMasterCallable;
 import jenkins.slaves.RemotingWorkDirSettings;
 import org.htmlunit.Page;
@@ -59,6 +60,7 @@ import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.LoggerRule;
+import org.jvnet.hudson.test.SimpleCommandLauncher;
 import org.jvnet.hudson.test.SmokeTest;
 import org.jvnet.hudson.test.recipes.LocalData;
 
@@ -234,6 +236,18 @@ public class JNLPLauncherTest {
 
         Thread.sleep(500);
         assertTrue(c.isOffline());
+    }
+
+    @Test
+    public void changeLauncher() throws Exception {
+        Computer c = addTestAgent(false);
+        var name = c.getName();
+        var node = c.getNode();
+        assertThat(c.isLaunchSupported(), is(false));
+        var nodeCopy = (Slave) Jenkins.XSTREAM2.fromXML(Jenkins.XSTREAM2.toXML(node));
+        nodeCopy.setLauncher(new SimpleCommandLauncher("true"));
+        Jenkins.get().getNodesObject().replaceNode(node, nodeCopy);
+        assertThat(Jenkins.get().getComputer(name).isLaunchSupported(), is(true));
     }
 
     /**