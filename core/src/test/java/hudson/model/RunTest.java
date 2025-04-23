jenkinsci/jenkins

commit dd134ef84e642e851c1fcc8946bebef5cce840ca

Shortlog strip console notes (#4905)
* Do not render parts of serialized a ConsoleNote if output begins in the middle of it. Generalize finding pre/postamble

* Do not render parts of serialized a ConsoleNote while writing non-html output as well. Test sample script output.

* Add positive scenario test case. Fine-tune implementation

* Render progressive output with explicitly set no preBuffer. Ensure only the relevant lines get processed

* Revert pre-buffer functionality.
Keep tests in adjusted form.

* Push offset forward to start writing log on line beginning

* Move and fine-tune tests. Avoid rendering no output in case offset falls on last line

* Dont render whole log on offset on the last line. It might be problematic for really large logs

* Style adjustments

core/src/main/java/hudson/model/Run.java
@@ -65,6 +65,7 @@ import hudson.util.LogTaskListener;
 import hudson.util.ProcessTree;
 import hudson.util.XStream2;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -1538,7 +1539,19 @@ public abstract class Run <JobT extends Job<JobT,RunT>,RunT extends Run<JobT,Run
      * @since 1.349
      */
     public void writeLogTo(long offset, @NonNull XMLOutput out) throws IOException {
-        getLogText().writeHtmlTo(offset, out.asWriter());
+        long start = offset;
+        if (offset > 0) {
+            try (BufferedInputStream bufferedInputStream = new BufferedInputStream(getLogInputStream())) {
+                if (offset == bufferedInputStream.skip(offset)) {
+                    int r;
+                    do {
+                        r = bufferedInputStream.read();
+                        start = (r == -1)? 0 : start + 1;
+                    } while (r != -1 && r != '\n');
+                }
+            }
+        }
+        getLogText().writeHtmlTo(start, out.asWriter());
     }
 
     /**

core/src/test/java/hudson/model/RunTest.java
@@ -24,9 +24,8 @@
 
 package hudson.model;
 
-import java.io.IOException;
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
@@ -40,16 +39,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.console.AnnotatedLargeText;
 import jenkins.model.Jenkins;
+import org.apache.commons.jelly.XMLOutput;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
 import org.jvnet.localizer.LocaleProvider;
+import org.kohsuke.stapler.framework.io.ByteBuffer;
 import org.mockito.Mockito;
 
 
 public class RunTest {
+    private static final String SAMPLE_BUILD_OUTPUT = "Sample build output abc123.\n";
 
     @Rule public TemporaryFolder tmp = new TemporaryFolder();
 
@@ -95,7 +99,7 @@ public class RunTest {
             TimeZone.setDefault(origTZ);
         }
     }
-    
+
 
     private List<? extends Run<?, ?>.Artifact> createArtifactList(String... paths) throws Exception {
         Run r = new Run(new StubJob(), 0) {};
@@ -143,7 +147,7 @@ public class RunTest {
                 return Locale.ENGLISH;
             }
         });
-        
+
         Run r = new Run(new StubJob(), 0) {};
         assertEquals("Not started yet", r.getDurationString());
         r.onStartBuilding();
@@ -261,5 +265,51 @@ public class RunTest {
 
         assertTrue(r1.compareTo(r2) != 0);
         assertTrue(treeSet.size() == 2);
+    }
+
+    @Test
+    public void willTriggerLogToStartWithNextFullLine() throws Exception {
+        assertWriteLogToEquals(new String(new char[2]).replace("\0", SAMPLE_BUILD_OUTPUT) + "Finished: SUCCESS.\n", 2 * SAMPLE_BUILD_OUTPUT.length() + 10);
+    }
+
+    @Test
+    public void wontPushOffsetOnRenderingFromBeginning() throws Exception {
+        assertWriteLogToEquals(new String(new char[5]).replace("\0", SAMPLE_BUILD_OUTPUT) + "Finished: SUCCESS.\n", 0);
+    }
+
+    @Test
+    public void willRenderNothingIfOffsetSetOnLastLine() throws Exception {
+        assertWriteLogToEquals("", 5 * SAMPLE_BUILD_OUTPUT.length() + 6);
+    }
+
+    private void assertWriteLogToEquals(String expectedOutput, long offset) throws Exception {
+        try (
+            ByteBuffer buf = new ByteBuffer();
+            PrintStream ps = new PrintStream(buf, true);
+            StringWriter writer = new StringWriter()
+        ) {
+            for (int i = 0; i < 5; i++) {
+                ps.print(SAMPLE_BUILD_OUTPUT);
+            }
+            ps.print("Finished: SUCCESS.\n");
+
+            final Run<? extends Job<?, ?>, ? extends Run<?, ?>> r = new Run(Mockito.mock(Job.class)) {
+                @NonNull
+                @Override
+                public AnnotatedLargeText<?> getLogText() {
+                    return new AnnotatedLargeText<>(buf, StandardCharsets.UTF_8, true, null);
+                }
+
+                @NonNull
+                @Override
+                public InputStream getLogInputStream() throws IOException {
+                    return buf.newInputStream();
+                }
+            };
+            final XMLOutput xmlOutput = Mockito.mock(XMLOutput.class);
+            Mockito.when(xmlOutput.asWriter()).thenReturn(writer);
+            r.writeLogTo(offset, xmlOutput);
+            assertEquals(expectedOutput, writer.toString());
+        }
     }
 }

commit 9201bc824002d85c4b1002bd2279a9cb7cd510d0

Fixing `Run.writeLogTo` regressions in #4905

core/src/main/java/hudson/model/Run.java
@@ -72,7 +72,6 @@ import hudson.util.XStream2;
 import io.jenkins.servlet.ServletExceptionWrapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -1466,19 +1465,34 @@ public abstract class Run<JobT extends Job<JobT, RunT>, RunT extends Run<JobT, R
      */
     @SuppressFBWarnings(value = "RV_RETURN_VALUE_IGNORED", justification = "method signature does not permit plumbing through the return value")
     public void writeLogTo(long offset, @NonNull XMLOutput out) throws IOException {
-        long start = offset;
+        AnnotatedLargeText<?> logText = getLogText();
         if (offset > 0) {
-            try (BufferedInputStream bufferedInputStream = new BufferedInputStream(getLogInputStream())) {
-                if (offset == bufferedInputStream.skip(offset)) {
-                    int r;
-                    do {
-                        r = bufferedInputStream.read();
-                        start = r == -1 ? 0 : start + 1;
-                    } while (r != -1 && r != '\n');
-                }
+            long _offset = offset;
+            try {
+                logText.writeRawLogTo(offset - 1, new OutputStream() {
+                    long pos = _offset;
+                    @Override
+                    public void write(int b) throws IOException {
+                        if (b == '\n') {
+                            throw new Halt(pos);
+                        } else {
+                            pos++;
+                        }
+                    }
+                });
+            } catch (Halt halt) {
+                offset = halt.offset;
             }
         }
-        getLogText().writeHtmlTo(start, out.asWriter());
+        logText.writeHtmlTo(offset, out.asWriter());
+    }
+
+    private static final class Halt extends IOException {
+        final long offset;
+
+        Halt(long offset) {
+            this.offset = offset;
+        }
     }
 
     /**

core/src/test/java/hudson/model/RunTest.java
@@ -281,6 +281,11 @@ public class RunTest {
     }
 
     @Test
+    public void wontPushOffsetOnRenderingFromBeginningOfLine() throws Exception {
+        assertWriteLogToEquals(new String(new char[3]).replace("\0", SAMPLE_BUILD_OUTPUT) + "Finished: SUCCESS.\n", 2 * SAMPLE_BUILD_OUTPUT.length());
+    }
+
+    @Test
     public void willRenderNothingIfOffsetSetOnLastLine() throws Exception {
         assertWriteLogToEquals("", 5 * SAMPLE_BUILD_OUTPUT.length() + 6);
     }