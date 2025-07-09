d4b200582295ddb47933d22f498386289f4a4f23
Thorsten Scherler <scherler@gmail.com>
2025-07-09 21:28:26 +0200

    [JENKINS-75834_fix] limit the functionality to a specifi reference
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>
    
    (cherry picked from commit 67d2100f5c93521a922d295325c85c1a6d7eb12c)
    
    From pull request:
    
    * https://github.com/jenkinsci/jenkins/pull/10818

--- src/main/js/components/dropdowns/templates.js
+++ src/main/js/components/dropdowns/templates.js
@@ -36,15 +36,19 @@ function dropdown() {
     animation: "dropdown",
     duration: 250,
     onShow: (instance) => {
-      // Make sure only one instance is visible at all times
-      const dropdowns = document.querySelectorAll("[data-tippy-root]");
-      Array.from(dropdowns).forEach((element) => {
-        // Check if the Tippy.js instance exists
-        if (element && element._tippy) {
-          // To just hide the dropdown
-          element._tippy.hide();
-        }
-      });
+      console.log(instance)
+      // Make sure only one instance is visible at all times in case of breadcrumb
+      if(instance.reference.classList.contains('hoverable-model-link') || instance.reference.classList.contains('hoverable-cildren-model-link')) {
+        const dropdowns = document.querySelectorAll("[data-tippy-root]");
+        Array.from(dropdowns).forEach((element) => {
+          // Check if the Tippy.js instance exists
+          if (element && element._tippy) {
+            // To just hide the dropdown
+            element._tippy.hide();
+          }
+        });
+      }
+
       const referenceParent = instance.reference.parentNode;
 
       if (referenceParent.classList.contains("model-link")) {
