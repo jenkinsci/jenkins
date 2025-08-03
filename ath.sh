50262cbc453c90f83982abd62b2725a578b1f736
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-08-03 21:43:21 +0100

    Update ath.sh

--- ath.sh
+++ ath.sh
@@ -6,7 +6,7 @@ set -o xtrace
 cd "$(dirname "$0")"
 
 # https://github.com/jenkinsci/acceptance-test-harness/releases
-export ATH_VERSION=6300.v12732144c83f
+export ATH_VERSION=6319.v9da_e005f2fd2
 
 if [[ $# -eq 0 ]]; then
 	export JDK=17
