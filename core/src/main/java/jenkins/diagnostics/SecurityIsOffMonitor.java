ab63e836a122acc533313dcf5b8baa06d3090c52
Vincent Latombe <vincent@latombe.net>
2025-10-16 14:28:24 +0200

    Migrate SecurityIsOffMonitor

--- core/src/main/java/jenkins/diagnostics/SecurityIsOffMonitor.java
+++ core/src/main/java/jenkins/diagnostics/SecurityIsOffMonitor.java
@@ -2,12 +2,8 @@ package jenkins.diagnostics;
 
 import hudson.Extension;
 import hudson.model.AdministrativeMonitor;
-import java.io.IOException;
 import jenkins.model.Jenkins;
 import org.jenkinsci.Symbol;
-import org.kohsuke.stapler.StaplerRequest2;
-import org.kohsuke.stapler.StaplerResponse2;
-import org.kohsuke.stapler.interceptor.RequirePOST;
 
 /**
  * Unsecured Jenkins is, well, insecure.
@@ -34,18 +30,5 @@ public class SecurityIsOffMonitor extends AdministrativeMonitor {
     @Override
     public boolean isSecurity() {
         return true;
-    }
-
-    /**
-     * Depending on whether the user said "yes" or "no", send him to the right place.
-     */
-    @RequirePOST
-    public void doAct(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException {
-        if (req.hasParameter("no")) {
-            disable(true);
-            rsp.sendRedirect(req.getContextPath() + "/manage");
-        } else {
-            rsp.sendRedirect(req.getContextPath() + "/configureSecurity");
-        }
     }
 }
