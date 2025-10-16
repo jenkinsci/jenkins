95d6a5fe25eb60e8fe37d3e1decd04ddf0b7e6a5
Vincent Latombe <vincent@latombe.net>
2025-10-16 14:32:16 +0200

    Migrate ApiTokenPropertyEnabledNewLegacyAdministrativeMonitor

--- core/src/main/java/jenkins/security/apitoken/ApiTokenPropertyEnabledNewLegacyAdministrativeMonitor.java
+++ core/src/main/java/jenkins/security/apitoken/ApiTokenPropertyEnabledNewLegacyAdministrativeMonitor.java
@@ -27,12 +27,10 @@ package jenkins.security.apitoken;
 import hudson.Extension;
 import hudson.model.AdministrativeMonitor;
 import hudson.util.HttpResponses;
-import java.io.IOException;
 import org.jenkinsci.Symbol;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.HttpResponse;
-import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 /**
@@ -58,12 +56,8 @@ public class ApiTokenPropertyEnabledNewLegacyAdministrativeMonitor extends Admin
     }
 
     @RequirePOST
-    public HttpResponse doAct(@QueryParameter String no) throws IOException {
-        if (no == null) {
-            ApiTokenPropertyConfiguration.get().setCreationOfLegacyTokenEnabled(false);
-        } else {
-            disable(true);
-        }
+    public HttpResponse doDisableLegacyTokens() {
+        ApiTokenPropertyConfiguration.get().setCreationOfLegacyTokenEnabled(false);
         return HttpResponses.redirectViaContextPath("manage");
     }
 }
