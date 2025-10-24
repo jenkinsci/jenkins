352ae766c1d800585e075bd056bd984da97694f3
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-10-24 18:17:23 +0100

    Fix order and visibility of tabs for experimental UI

--- core/src/main/java/hudson/model/Run.java
+++ core/src/main/java/hudson/model/Run.java
@@ -2717,6 +2717,6 @@ public abstract class Run<JobT extends Job<JobT, RunT>, RunT extends Run<JobT, R
      */
     @Restricted(NoExternalUse.class)
     public List<Tab> getRunTabs() {
-        return getActions(Tab.class);
+        return getActions(Tab.class).stream().filter(e -> e.getIconFileName() != null).toList();
     }
 }
