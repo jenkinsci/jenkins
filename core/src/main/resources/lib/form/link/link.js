f3a626ef3541a9ff22f2eb61ccca955bc792ff16
somiljain2006 <somil16022006@gmail.com>
2026-01-02 16:38:29 +0530

    Fix race condition in pane collapse POST handling

--- core/src/main/resources/lib/form/link/link.js
+++ core/src/main/resources/lib/form/link/link.js
@@ -1,14 +1,12 @@
 Behaviour.specify("A.post", "link.post", 0, function (element) {
-  const pendingHref = element.getAttribute("data-post-href");
-  if (pendingHref) {
-    element.setAttribute("href", pendingHref);
-    element.removeAttribute("data-post-href");
-  }
-
   element.onclick = function () {
-    var form = document.createElement("form");
+    const url = element.getAttribute("data-post-href") || element.getAttribute("href");
+    if (!url) {
+      return false;
+    }
+    const form = document.createElement("form");
     form.setAttribute("method", "POST");
-    form.setAttribute("action", element.getAttribute("href"));
+    form.setAttribute("action", url);
     crumb.appendToForm(form);
     document.body.appendChild(form);
     form.submit();
