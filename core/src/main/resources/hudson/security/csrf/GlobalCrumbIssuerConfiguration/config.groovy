cbcfe181a7e295d2fabd91769319f5d42699106b
Amogh Parmar <amoghparmar30@gmail.com>
2026-01-03 11:24:46 -0800

    Hide CSRF configuration UI when only DefaultCrumbIssuer is available

--- core/src/main/resources/hudson/security/csrf/GlobalCrumbIssuerConfiguration/config.groovy
+++ core/src/main/resources/hudson/security/csrf/GlobalCrumbIssuerConfiguration/config.groovy
@@ -1,19 +1,31 @@
 package hudson.security.csrf.GlobalCrumbIssuerConfiguration
 
 import hudson.security.csrf.CrumbIssuer
+import hudson.security.csrf.DefaultCrumbIssuer
 
-def f=namespace(lib.FormTagLib)
+def f = namespace(lib.FormTagLib)
 def all = CrumbIssuer.all()
+def disableCsrf =
+    hudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION
 
-if (!all.isEmpty()) {
+def onlyDefaultIssuer =
+    all.size() == 1 && all[0].clazz == DefaultCrumbIssuer
+
+def showCsrfConfig = !onlyDefaultIssuer || disableCsrf
+
+if (showCsrfConfig) {
     f.section(title: _("CSRF Protection")) {
-        if (hudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION) {
+        if (disableCsrf) {
             f.entry {
                 p(raw(_('disabled')))
                 p(_('unsupported'))
             }
         } else {
-            f.dropdownDescriptorSelector(title: _("Crumb Issuer"), descriptors: all, field: 'crumbIssuer')
+            f.dropdownDescriptorSelector(
+                title: _("Crumb Issuer"),
+                descriptors: all,
+                field: 'crumbIssuer'
+            )
         }
     }
 }
