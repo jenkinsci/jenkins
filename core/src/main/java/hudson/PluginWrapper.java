54565473deff4854f879b1db136829620084ae89
somiljain2006 <somil16022006@gmail.com>
2026-01-30 13:01:29 +0530

    Fix formatting errors

--- core/src/main/java/hudson/PluginWrapper.java
+++ core/src/main/java/hudson/PluginWrapper.java
@@ -1435,7 +1435,7 @@ public class PluginWrapper implements Comparable<PluginWrapper>, ModelObject {
     /**
      * Returns the date when this plugin was installed.
      */
-		@SuppressWarnings("unused")
+    @SuppressWarnings("unused")
     @Exported
     public Date getInstallationDate() {
         if (archive == null) {
