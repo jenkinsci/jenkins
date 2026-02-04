ba2aafc49ea5e762515c0d362f5cd807ef71ced7
somiljain2006 <somil16022006@gmail.com>
2026-02-04 19:29:40 +0530

    Fied formatting errors

--- core/src/main/java/jenkins/model/lazy/LazyBuildMixIn.java
+++ core/src/main/java/jenkins/model/lazy/LazyBuildMixIn.java
@@ -229,7 +229,7 @@ public abstract class LazyBuildMixIn<JobT extends Job<JobT, RunT> & Queue.Task &
     /**
      * Suitable for {@link Job#getBuild}.
      */
-		@SuppressWarnings("deprecation")
+    @SuppressWarnings("deprecation")
     public final RunT getBuild(String id) {
         return builds.getById(id);
     }
