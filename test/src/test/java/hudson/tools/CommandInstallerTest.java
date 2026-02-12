5d1d9fa415beea2ad66a521c88004d85295b6052
Mark Waite <mark.earl.waite@gmail.com>
2026-02-12 09:13:42 -0700

    Test read only installation directory on ci.jenkins.io
    Accepts that the test will be skipped on Windows computers and on
    computers that do not have a read-only '/opt/jdk-25' directory.
    We've been without a test for this bug for a very long time.  A test
    that runs in a few common configurations is better than no test.

--- test/src/test/java/hudson/tools/CommandInstallerTest.java
+++ test/src/test/java/hudson/tools/CommandInstallerTest.java
@@ -1,7 +1,19 @@
 package hudson.tools;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import hudson.FilePath;
+import hudson.Functions;
+import hudson.model.JDK;
+import hudson.model.TaskListener;
+import java.io.File;
+import java.nio.file.Files;
+import jenkins.model.Jenkins;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
@@ -24,5 +36,20 @@ class CommandInstallerTest {
     void validateCommandInstallerCommandEOL() {
         CommandInstaller obj = new CommandInstaller("", "echo A\r\necho B\recho C", "");
         rule.assertStringContains(obj.getCommand(), "echo A\necho B\necho C");
+    }
+
+    @Issue("https://github.com/jenkinsci/jenkins/issues/13136")
+    @Test
+    void commandInstallerDoesNotRequireWritePermissionOnToolDir() throws Exception {
+        String javaHome = "/opt/jdk-25"; // ci.jenkins.io Java 25 directory
+        File javaHomeDir = new File(javaHome);
+        // Use a Unix installation dir that exists and is not writeable
+        assumeTrue(javaHomeDir.exists(), "No java home directory '" + javaHome + "'");
+        assumeTrue(!Functions.isWindows());
+        assumeTrue(!Files.isWritable(javaHomeDir.toPath()), "Java home directory '" + javaHome + "' is writable");
+        JDK jdk = new JDK("my-jdk", javaHome);
+        CommandInstaller installer = new CommandInstaller("", "echo 'Using " + javaHome + " as Java home'", javaHome);
+        FilePath filePath = installer.performInstallation(jdk, Jenkins.get(), TaskListener.NULL);
+        assertThat(filePath.getRemote(), is(javaHome));
     }
 }
