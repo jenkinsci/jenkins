jenkinsci/jenkins

commit 611294d365c578d35bfc8c6941b638104f7617d7

Introduce experimental API for adding actions to experimental Run UI (#11204)

src/main/js/components/dropdowns/templates.js
@@ -1,5 +1,7 @@
 import { createElementFromHtml } from "@/util/dom";
 import { xmlEscape } from "@/util/security";
+import behaviorShim from "@/util/behavior-shim";
+import Utils from "./utils";
 
 const hideOnPopperBlur = {
   name: "hideOnPopperBlur",
@@ -60,59 +62,234 @@ function dropdown() {
   };
 }
 
-function menuItem(options) {
+function kebabToCamelCase(str) {
+  return str.replace(/-([a-z])/g, function (match, char) {
+    return char.toUpperCase();
+  });
+}
+
+function loadScriptIfNotLoaded(url, item) {
+  // Check if the script element with the given URL already exists
+  const existingScript = document.querySelector(`script[src="${url}"]`);
+
+  if (!existingScript) {
+    const script = document.createElement("script");
+    script.src = url;
+
+    script.addEventListener("load", () => {
+      behaviorShim.applySubtree(item, true);
+    });
+
+    document.body.appendChild(script);
+  }
+}
+
+function optionalVal(key, val) {
+  if (!val) {
+    return "";
+  }
+
+  return `${key}="${xmlEscape(val)}"`;
+}
+
+function optionalVals(keyVals) {
+  return Object.keys(keyVals)
+    .map((key) => optionalVal(key, keyVals[key]))
+    .join(" ");
+}
+
+function icon(opt) {
+  if (!opt.icon) {
+    return "";
+  }
+
+  return `<div class="jenkins-dropdown__item__icon">${
+    opt.iconXml
+      ? opt.iconXml
+      : `<img alt="Icon" aria-hidden="true" src="${opt.icon}" />`
+  }</div>`;
+}
+
+function badge(opt) {
+  if (!opt.badge) {
+    return "";
+  }
+
+  let badgeText = xmlEscape(opt.badge.text);
+  let badgeTooltip = xmlEscape(opt.badge.tooltip);
+  let badgeSeverity = xmlEscape(opt.badge.severity);
+
+  return `<span class="jenkins-dropdown__item__badge jenkins-badge jenkins-!-${badgeSeverity}-color" tooltip="${badgeTooltip}">${badgeText}</span>`;
+}
+
+/**
+ * Generates the contents for the dropdown
+ * @param {DropdownItem}  dropdownItem
+ * @param {'jenkins-dropdown__item' | 'jenkins-button'}  type
+ * @param {string}  context
+ * @return {Element}
+ */
+function menuItem(dropdownItem, type = "jenkins-dropdown__item", context = "") {
+  /**
+   * @type {DropdownItem}
+   */
   const itemOptions = Object.assign(
     {
       type: "link",
     },
-    options,
+    dropdownItem,
   );
 
-  const label = xmlEscape(itemOptions.label);
-  let badgeText;
-  let badgeTooltip;
-  let badgeSeverity;
-  if (itemOptions.badge) {
-    badgeText = xmlEscape(itemOptions.badge.text);
-    badgeTooltip = xmlEscape(itemOptions.badge.tooltip);
-    badgeSeverity = xmlEscape(itemOptions.badge.severity);
+  const label = xmlEscape(itemOptions.displayName);
+
+  let clazz =
+    type +
+    " " +
+    itemOptions.clazz +
+    (itemOptions.semantic
+      ? " jenkins-!-" + itemOptions.semantic.toLowerCase() + "-color"
+      : "");
+
+  // If submenu
+  if (itemOptions.event && itemOptions.event.event) {
+    const wrapper = createElementFromHtml(
+      `<div class="jenkins-split-button"></div>`,
+    );
+    wrapper.appendChild(
+      menuItem(
+        Object.assign({}, dropdownItem, { event: dropdownItem.event.event }),
+        "jenkins-button",
+        context,
+      ),
+    );
+
+    const button = createElementFromHtml(
+      `<button class="${clazz}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M112 184l144 144 144-144"/></svg></button>`,
+    );
+    Utils.generateDropdown(
+      button,
+      (instance) => {
+        instance.setContent(
+          Utils.generateDropdownItems(dropdownItem.subMenu.items),
+        );
+        instance.loaded = true;
+      },
+      false,
+      {
+        appendTo: "parent",
+      },
+    );
+    wrapper.appendChild(button);
+
+    return wrapper;
   }
-  const tag = itemOptions.type === "link" ? "a" : "button";
+
+  const tag =
+    itemOptions.event && itemOptions.event.type === "GET" ? "a" : "button";
+  const url = tag === "a" ? context + xmlEscape(itemOptions.event.url) : null;
 
   const item = createElementFromHtml(`
-      <${tag} class="jenkins-dropdown__item ${itemOptions.clazz ? xmlEscape(itemOptions.clazz) : ""}"
-        ${itemOptions.url ? `href="${xmlEscape(itemOptions.url)}"` : ""} ${itemOptions.id ? `id="${xmlEscape(itemOptions.id)}"` : ""}
-        ${itemOptions.tooltip ? `data-html-tooltip="${xmlEscape(itemOptions.tooltip)}"` : ""}>
-          ${
-            itemOptions.icon
-              ? `<div class="jenkins-dropdown__item__icon">${
-                  itemOptions.iconXml
-                    ? itemOptions.iconXml
-                    : `<img alt="${label}" src="${itemOptions.icon}" />`
-                }</div>`
-              : ``
-          }
+      <${tag}
+        ${optionalVals({
+          class: clazz,
+          href: url,
+          id: itemOptions.id,
+          "data-html-tooltip": itemOptions.tooltip,
+        })}>
+          ${icon(itemOptions)}
           ${label}
-                    ${
-                      itemOptions.badge != null
-                        ? `<span class="jenkins-dropdown__item__badge jenkins-badge jenkins-!-${badgeSeverity}-color" tooltip="${badgeTooltip}">${badgeText}</span>`
-                        : ``
-                    }
+          ${badge(itemOptions)}
           ${
-            itemOptions.subMenu != null
+            itemOptions.event &&
+            itemOptions.event.actions &&
+            type === "jenkins-dropdown__item"
               ? `<span class="jenkins-dropdown__item__chevron"></span>`
               : ``
           }
       </${tag}>
     `);
 
-  if (options.onClick) {
-    item.addEventListener("click", (event) => options.onClick(event));
-  }
-  if (options.onKeyPress) {
-    item.onkeypress = options.onKeyPress;
-  }
+  // Handle special cases
+  tryOnClickEvent(item, dropdownItem);
+  tryLoadScripts(item, dropdownItem, context);
+  tryPost(item, dropdownItem, context);
+  tryConfirmationPost(item, dropdownItem, context);
+
   return item;
+}
+
+/**
+ * If the menu item has a custom onClick event, add it to the element
+ */
+function tryOnClickEvent(element, opt) {
+  if (!opt.onClick) {
+    return;
+  }
+
+  element.addEventListener("click", opt.onClick);
+}
+
+/**
+ * If scripts have been provided with the menu item, load them
+ */
+function tryLoadScripts(element, opt, context) {
+  if (!opt.event || !opt.event.attributes || !opt.event.javascriptUrl) {
+    return;
+  }
+
+  for (const key in opt.event.attributes) {
+    element.dataset[kebabToCamelCase(key)] =
+      opt.event.attributes[key].toString();
+  }
+  element.dataset["baseUrl"] = context;
+
+  loadScriptIfNotLoaded(opt.event.javascriptUrl, element);
+}
+
+/**
+ * If the menu item requires a POST, add a confirmation dialog and submit the form
+ */
+function tryConfirmationPost(element, opt, context) {
+  if (!opt.event || !opt.event.postTo) {
+    return;
+  }
+
+  element.addEventListener("click", () => {
+    dialog
+      .confirm(opt.event.title, {
+        message: opt.event.description,
+        type: opt.semantic.toLowerCase() ?? "default",
+      })
+      .then(
+        () => {
+          const form = document.createElement("form");
+          form.setAttribute("method", "POST");
+          form.setAttribute("action", context + xmlEscape(opt.event.postTo));
+          crumb.appendToForm(form);
+          document.body.appendChild(form);
+          form.submit();
+        },
+        () => {},
+      );
+  });
+}
+
+/**
+ * If the menu item requires a POST, do a POST rather than a GET
+ */
+function tryPost(element, opt, context) {
+  if (!opt.event || !opt.event.url || opt.event.type !== "POST") {
+    return;
+  }
+
+  element.addEventListener("click", () => {
+    const form = document.createElement("form");
+    form.setAttribute("method", "POST");
+    form.setAttribute("action", context + xmlEscape(opt.event.url));
+    crumb.appendToForm(form);
+    document.body.appendChild(form);
+    form.submit();
+  });
 }
 
 function heading(label) {

core/src/main/java/hudson/Functions.java
@@ -172,6 +172,7 @@ import jenkins.model.details.DetailFactory;
 import jenkins.model.details.DetailGroup;
 import jenkins.telemetry.impl.PasswordMasking;
 import jenkins.util.SystemProperties;
+import net.sf.json.JSONObject;
 import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.JellyTagException;
 import org.apache.commons.jelly.Script;
@@ -2658,6 +2659,21 @@ public class Functions {
     @Restricted(NoExternalUse.class)
     public static String generateItemId() {
         return String.valueOf(Math.floor(Math.random() * 3000));
+    }
+
+    /**
+     * Converts the given actions to a JSON object
+     */
+    @Restricted(NoExternalUse.class)
+    public static String convertActionsToJson(String baseUrl, List<Action> actions) {
+        ModelObjectWithContextMenu.ContextMenu contextMenu = new ModelObjectWithContextMenu.ContextMenu();
+        contextMenu.addAll(actions
+                .stream()
+                .filter(action -> action.getIconFileName() != null)
+                .toList());
+        JSONObject jsonObject = JSONObject.fromObject(contextMenu);
+        jsonObject.put("url", Util.ensureEndsWith(baseUrl, "/"));
+        return jsonObject.toString();
     }
 
     /**

core/src/main/java/hudson/model/Action.java
@@ -26,6 +26,10 @@ package hudson.model;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import hudson.Functions;
+import jenkins.model.menu.Group;
+import jenkins.model.menu.Semantic;
+import jenkins.model.menu.event.Event;
+import jenkins.model.menu.event.LinkEvent;
 
 /**
  * Object that contributes additional information, behaviors, and UIs to {@link ModelObject}
@@ -139,4 +143,42 @@ public interface Action extends ModelObject {
      * @see Functions#getActionUrl(String, Action)
      */
     @CheckForNull String getUrlName();
+
+    /**
+     * Returns the group that this item belongs to.
+     * The default implementation places the item in the menu group.
+     *
+     * @return the group of this item
+     */
+    default Group getGroup() {
+        return Group.IN_MENU;
+    }
+
+    /**
+     * Returns the event associated with this item.
+     * By default, this creates a link event pointing to the item's URL name.
+     *
+     * @return the event representing this item
+     */
+    default Event getEvent() {
+        return LinkEvent.of(getUrlName());
+    }
+
+    /**
+     * Returns the semantic information for this item.
+     *
+     * @return the semantic associated with this item, or {@code null} if none
+     */
+    default Semantic getSemantic() {
+        return null;
+    }
+
+    /**
+     * Indicates whether this item should be visible in a context menu for an {@link Actionable}.
+     *
+     * @return {@code true} if the item is shown in the context menu, {@code false} otherwise
+     */
+    default boolean isVisibleInContextMenu() {
+        return true;
+    }
 }

core/src/main/java/hudson/model/Actionable.java
@@ -30,14 +30,22 @@ import hudson.Util;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import jenkins.model.ModelObjectWithContextMenu;
+import jenkins.model.Tab;
 import jenkins.model.TransientActionFactory;
+import jenkins.model.menu.Group;
 import jenkins.security.stapler.StaplerNotDispatchable;
+import org.jenkins.ui.icon.IconSpec;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerRequest2;
 import org.kohsuke.stapler.StaplerResponse;
@@ -111,6 +119,39 @@ public abstract class Actionable extends AbstractModelObject implements ModelObj
             }
         }
         return Collections.unmodifiableList(_actions);
+    }
+
+    /**
+     * Collects and returns the list of actions to display in the application bar.
+     * <p>
+     * This method filters out actions that:
+     * <ul>
+     *   <li>are instances of {@link Tab}</li>
+     *   <li>lack a display name or an icon (either from {@link Action#getIconFileName()}
+     *       or, if available, {@link IconSpec#getIconClassName()})</li>
+     * </ul>
+     * <p>
+     * The resulting actions are sorted first by their {@link Group#getOrder()} value,
+     * then alphabetically by display name. The list is returned as an unmodifiable collection.
+     *
+     * @return an unmodifiable list of actions suitable for display in the app bar
+     */
+    @Restricted(NoExternalUse.class)
+    public List<Action> getAppBarActions() {
+        return getAllActions().stream()
+                .filter(e -> !(e instanceof Tab))
+                .filter(e -> {
+                    String icon = e.getIconFileName();
+
+                    if (e instanceof IconSpec iconSpec && iconSpec.getIconClassName() != null) {
+                        icon = iconSpec.getIconClassName();
+                    }
+
+                    return Util.fixEmptyAndTrim(e.getDisplayName()) != null || Util.fixEmptyAndTrim(icon) != null;
+                })
+                .sorted(Comparator.comparingInt((Action e) -> e.getGroup().getOrder())
+                        .thenComparing(e -> Objects.requireNonNullElse(e.getDisplayName(), "")))
+                .collect(Collectors.toUnmodifiableList());
     }
 
     private <T> Collection<? extends Action> createFor(TransientActionFactory<T> taf) {

core/src/main/java/hudson/model/Run.java
@@ -125,6 +125,7 @@ import jenkins.model.details.CauseDetail;
 import jenkins.model.details.Detail;
 import jenkins.model.details.DetailFactory;
 import jenkins.model.details.DurationDetail;
+import jenkins.model.details.KeptForeverDetail;
 import jenkins.model.details.TimestampDetail;
 import jenkins.model.lazy.BuildReference;
 import jenkins.model.lazy.LazyBuildMixIn;
@@ -2684,7 +2685,7 @@ public abstract class Run<JobT extends Job<JobT, RunT>, RunT extends Run<JobT, R
         }
 
         @NonNull @Override public List<? extends Detail> createFor(@NonNull Run target) {
-            return List.of(new CauseDetail(target), new TimestampDetail(target), new DurationDetail(target));
+            return List.of(new CauseDetail(target), new TimestampDetail(target), new DurationDetail(target), new KeptForeverDetail(target));
         }
     }
 

core/src/main/java/jenkins/model/ModelObjectWithContextMenu.java
@@ -5,11 +5,11 @@ import hudson.Functions;
 import hudson.Util;
 import hudson.model.Action;
 import hudson.model.Actionable;
-import hudson.model.BallColor;
 import hudson.model.Computer;
 import hudson.model.Job;
 import hudson.model.ModelObject;
 import hudson.model.Node;
+import hudson.model.Run;
 import hudson.slaves.Cloud;
 import jakarta.servlet.ServletException;
 import java.io.IOException;
@@ -19,6 +19,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import jenkins.management.Badge;
+import jenkins.model.menu.Group;
+import jenkins.model.menu.Semantic;
+import jenkins.model.menu.event.Event;
+import jenkins.model.menu.event.SplitButtonEvent;
 import jenkins.security.stapler.StaplerNotDispatchable;
 import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.JellyException;
@@ -27,6 +31,7 @@ import org.apache.commons.jelly.Script;
 import org.apache.commons.jelly.XMLOutput;
 import org.jenkins.ui.icon.Icon;
 import org.jenkins.ui.icon.IconSet;
+import org.jenkins.ui.icon.IconSpec;
 import org.jenkins.ui.symbol.Symbol;
 import org.jenkins.ui.symbol.SymbolRequest;
 import org.kohsuke.accmod.Restricted;
@@ -106,102 +111,61 @@ public interface ModelObjectWithContextMenu extends ModelObject {
         }
 
         public ContextMenu add(String url, String text) {
-            items.add(new MenuItem(url, null, text));
+            items.add(new MenuItem().withUrl(url).withIcon(null).withDisplayName(text));
             return this;
         }
 
         public ContextMenu addAll(Collection<? extends Action> actions) {
-            for (Action a : actions)
-                add(a);
+            for (Action a : actions) {
+                if (a.isVisibleInContextMenu()) {
+                    add(a);
+                }
+            }
+
             return this;
         }
 
         /**
          * @see ContextMenuVisibility
          */
-        public ContextMenu add(Action a) {
-            if (!Functions.isContextMenuVisible(a)) {
+        public ContextMenu add(Action action) {
+            MenuItem menuItem = new MenuItem()
+                    .withDisplayName(action.getDisplayName());
+
+            menuItem.semantic = action.getSemantic();
+            menuItem.group = action.getGroup();
+            menuItem.event = action.getEvent();
+
+            if (!Functions.isContextMenuVisible(action)) {
                 return this;
             }
-            StaplerRequest2 req = Stapler.getCurrentRequest2();
-            String text = a.getDisplayName();
-            String base = Functions.getIconFilePath(a);
-            if (base == null)     return this;
-            String url =  Functions.getActionUrl(req.findAncestor(ModelObject.class).getUrl(), a);
 
-            if (base.startsWith("symbol-")) {
-                Icon icon = Functions.tryGetIcon(base);
-                return add(url, icon.getClassSpec(), text);
+            if (action.getEvent() instanceof SplitButtonEvent splitButtonEvent) {
+                menuItem.subMenu = new ContextMenu();
+                menuItem.subMenu.addAll(splitButtonEvent.getActions());
+            }
+
+            // Set icon
+            String icon = action.getIconFileName();
+            if (action instanceof IconSpec iconSpec && iconSpec.getIconClassName() != null) {
+                icon = iconSpec.getIconClassName();
             } else {
-                String icon = Stapler.getCurrentRequest2().getContextPath() + (base.startsWith("images/") ? Functions.getResourcePath() : "") + '/' + base;
-                return add(url, icon, text);
+                Icon tmpIcon = Functions.tryGetIcon(icon);
+                if (tmpIcon != null) {
+                    icon = IconSet.tryTranslateTangoIconToSymbol(tmpIcon.getClassSpec());
+                }
             }
-        }
+            menuItem.icon = icon;
 
-        public ContextMenu add(String url, String icon, String text) {
-            if (text != null && icon != null && url != null)
-                items.add(new MenuItem(url, icon, text));
-            return this;
-        }
-
-        /** @since 1.504 */
-        public ContextMenu add(String url, String icon, String text, boolean post) {
-            if (text != null && icon != null && url != null) {
-                MenuItem item = new MenuItem(url, icon, text);
-                item.post = post;
-                items.add(item);
+            // If Symbol, set iconXml
+            if (icon != null && icon.startsWith("symbol-")) {
+                menuItem.iconXml = Symbol.get(new SymbolRequest.Builder()
+                        .withName(icon.split(" ")[0].substring(7))
+                        .withPluginName(Functions.extractPluginNameFromIconSrc(icon))
+                        .build());
             }
-            return this;
-        }
 
-        /** @since 1.512 */
-        public ContextMenu add(String url, String icon, String text, boolean post, boolean requiresConfirmation) {
-            if (text != null && icon != null && url != null) {
-                MenuItem item = new MenuItem(url, icon, text);
-                item.post = post;
-                item.requiresConfirmation = requiresConfirmation;
-                items.add(item);
-            }
-            return this;
-        }
-
-        /** @since 2.335 */
-        public ContextMenu add(String url, String icon, String iconXml, String text, boolean post, boolean requiresConfirmation) {
-            if (text != null && icon != null && url != null) {
-                MenuItem item = new MenuItem(url, icon, text);
-                item.iconXml = iconXml;
-                item.post = post;
-                item.requiresConfirmation = requiresConfirmation;
-                items.add(item);
-            }
-            return this;
-        }
-
-        /** @since 2.401 */
-        public ContextMenu add(String url, String icon, String iconXml, String text, boolean post, boolean requiresConfirmation, Badge badge) {
-            if (text != null && icon != null && url != null) {
-                MenuItem item = new MenuItem(url, icon, text);
-                item.iconXml = iconXml;
-                item.post = post;
-                item.requiresConfirmation = requiresConfirmation;
-                item.badge = badge;
-                items.add(item);
-            }
-            return this;
-        }
-
-        /** @since 2.415 */
-        public ContextMenu add(String url, String icon, String iconXml, String text, boolean post, boolean requiresConfirmation, Badge badge, String message) {
-            if (text != null && icon != null && url != null) {
-                MenuItem item = new MenuItem(url, icon, text);
-                item.iconXml = iconXml;
-                item.post = post;
-                item.requiresConfirmation = requiresConfirmation;
-                item.badge = badge;
-                item.message = message;
-                items.add(item);
-            }
-            return this;
+            return add(menuItem);
         }
 
         /**
@@ -245,9 +209,9 @@ public interface ModelObjectWithContextMenu extends ModelObject {
         public ContextMenu add(Node n) {
             Computer c = n.toComputer();
             return add(new MenuItem()
-                .withDisplayName(n.getDisplayName())
-                .withStockIcon(c == null ? "computer.svg" : c.getIcon())
-                .withContextRelativeUrl(n.getSearchUrl()));
+                    .withDisplayName(n.getDisplayName())
+                    .withStockIcon(c == null ? "computer.svg" : c.getIcon())
+                    .withContextRelativeUrl(n.getSearchUrl()));
         }
 
         /**
@@ -269,9 +233,9 @@ public interface ModelObjectWithContextMenu extends ModelObject {
          */
         public ContextMenu add(IComputer c) {
             return add(new MenuItem()
-                .withDisplayName(c.getDisplayName())
-                .withIconClass(c.getIconClassName())
-                .withContextRelativeUrl(c.getUrl()));
+                    .withDisplayName(c.getDisplayName())
+                    .withIconClass(c.getIconClassName())
+                    .withContextRelativeUrl(c.getUrl()));
         }
 
         public ContextMenu add(Cloud c) {
@@ -288,9 +252,23 @@ public interface ModelObjectWithContextMenu extends ModelObject {
          */
         public ContextMenu add(Job job) {
             return add(new MenuItem()
-                .withDisplayName(job.getDisplayName())
-                .withIcon(job.getIconColor())
-                .withUrl(job.getSearchUrl()));
+                    .withDisplayName(job.getDisplayName())
+                    .withIcon(job.getIconColor().getImage())
+                    .withUrl(job.getSearchUrl()));
+        }
+
+        // Used in Jelly! - task.jelly
+        public ContextMenu add(String url, String icon, String iconXml, String text, boolean post, boolean requiresConfirmation, Badge badge, String message) {
+            if (text != null && icon != null && url != null) {
+                MenuItem item = new MenuItem().withUrl(url).withIcon(icon).withDisplayName(text);
+                item.iconXml = iconXml;
+                item.post = post;
+                item.requiresConfirmation = requiresConfirmation;
+                item.badge = badge;
+                item.message = message;
+                items.add(item);
+            }
+            return this;
         }
 
         /**
@@ -311,11 +289,13 @@ public interface ModelObjectWithContextMenu extends ModelObject {
             return from(self, request, response, "sidepanel");
         }
 
-        public ContextMenu from(ModelObjectWithContextMenu self, StaplerRequest request, StaplerResponse response) throws JellyException, IOException {
-            return from(self, StaplerRequest.toStaplerRequest2(request), StaplerResponse.toStaplerResponse2(response), "sidepanel");
-        }
-
         public ContextMenu from(ModelObjectWithContextMenu self, StaplerRequest2 request, StaplerResponse2 response, String view) throws JellyException, IOException {
+            // Only Runs support getAppBarActions currently
+            if (self instanceof Run) {
+                this.addAll(((Actionable) self).getAppBarActions());
+                return this;
+            }
+
             WebApp webApp = WebApp.getCurrent();
             final Script s = webApp.getMetaClass(self).getTearOff(JellyClassTearOff.class).findScript(view);
             if (s != null) {
@@ -394,8 +374,13 @@ public interface ModelObjectWithContextMenu extends ModelObject {
         @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD", justification = "read by Stapler")
         public boolean requiresConfirmation;
 
-
         private Badge badge;
+
+        private Group group;
+
+        private Event event;
+
+        private Semantic semantic;
 
         private String message;
 
@@ -411,7 +396,6 @@ public interface ModelObjectWithContextMenu extends ModelObject {
          * If this is a submenu, definition of subitems.
          */
         @Exported(inline = true)
-        @SuppressFBWarnings(value = "URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD", justification = "read by Stapler")
         public ContextMenu subMenu;
 
         @Exported
@@ -428,13 +412,24 @@ public interface ModelObjectWithContextMenu extends ModelObject {
             return badge;
         }
 
+        @Exported(inline = true)
+        public Group getGroup() {
+            return group;
+        }
+
+        @Exported(inline = true)
+        public Event getEvent() {
+            return event;
+        }
+
+        @Exported
+        public Semantic getSemantic() {
+            return semantic;
+        }
+
         @Exported
         public String getMessage() {
             return message;
-        }
-
-        public MenuItem(String url, String icon, String displayName) {
-            withUrl(url).withIcon(icon).withDisplayName(displayName);
         }
 
         public MenuItem() {
@@ -463,8 +458,9 @@ public interface ModelObjectWithContextMenu extends ModelObject {
             return this;
         }
 
-        public MenuItem withIcon(BallColor color) {
-            return withStockIcon(color.getImage());
+        public MenuItem withIconXml(String icon) {
+            this.iconXml = icon;
+            return this;
         }
 
         /**

core/src/main/java/jenkins/model/details/KeptForeverDetail.java
@@ -1,0 +1,26 @@
+package jenkins.model.details;
+
+import hudson.model.Run;
+import org.jspecify.annotations.Nullable;
+
+/**
+ * Displays if the build is marked to be kept forever.
+ * Only shown if the build is kept forever.
+ */
+public class KeptForeverDetail extends Detail {
+
+    public KeptForeverDetail(Run<?, ?> run) {
+        super(run);
+    }
+
+    @Override
+    public @Nullable String getDisplayName() {
+        return Messages.KeptForeverDetail_DisplayName();
+    }
+
+    @Override
+    public @Nullable String getIconClassName() {
+        Run<?, ?> run = (Run<?, ?>) getObject();
+        return run.isKeepLog() ? "symbol-lock-closed" : null;
+    }
+}

commit 31a371b6111ea0efc865a30d6e3ebbf66cfb2f06

Fix #27112: Tab key not confirming highlighted dropdown suggestion
Pressing Tab to confirm a highlighted suggestion in autocomplete and
combobox dropdowns did nothing: the suggestion was not filled in and
focus left the field.

The menuItem() function in templates.js was not wiring the onKeyPress
property from dropdown items onto their DOM elements. autocomplete.js and
combo-box.js define an onKeyPress handler (which handles Tab), and
utils.js reads and invokes selectedItem.onkeypress(evt), but nothing
copied the handler onto the element. This was a regression from PR #11204.

Add tryOnKeyPressEvent(), mirroring the existing tryOnClickEvent(), and
call it from menuItem() so the handler is wired up.

src/main/js/components/dropdowns/templates.js
@@ -229,6 +229,7 @@ function menuItem(dropdownItem, type = "jenkins-dropdown__item", context = "") {
 
   // Handle special cases
   tryOnClickEvent(item, dropdownItem);
+  tryOnKeyPressEvent(item, dropdownItem);
   tryLoadScripts(item, dropdownItem, context);
   tryPost(item, dropdownItem, context);
   tryConfirmationPost(item, dropdownItem, context);
@@ -245,6 +246,17 @@ function tryOnClickEvent(element, opt) {
   }
 
   element.addEventListener("click", opt.onClick);
+}
+
+/**
+ * If the menu item has a custom onKeyPress event, add it to the element
+ */
+function tryOnKeyPressEvent(element, opt) {
+  if (!opt.onKeyPress) {
+    return;
+  }
+
+  element.onkeypress = opt.onKeyPress;
 }
 
 /**