08cc3f597e64e39dfdaa97796e679a22429f5888
Hardik Sharma <hardik2004s@gmail.com>
2026-08-06 11:42:54 +0530

    Handle missing denied artifact path

--- core/src/main/java/hudson/tasks/ArtifactArchiver.java
+++ core/src/main/java/hudson/tasks/ArtifactArchiver.java
@@ -45,6 +45,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.AccessDeniedException;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -288,7 +289,9 @@ public class ArtifactArchiver extends Recorder implements SimpleBuildStep {
                 }
             }
         } catch (AccessDeniedException e) {
-            String deniedPath = e.getFile() != null ? e.getFile() : e.toString();
+            String deniedPath = e.getFile() != null
+                    ? e.getFile()
+                    : Objects.toString(e.getMessage(), e.getClass().getName());
             String message = Messages.ArtifactArchiver_AccessDenied(deniedPath, ws.getRemote());
             LOG.log(Level.FINE, message, e);
             throw new AbortException(message);
