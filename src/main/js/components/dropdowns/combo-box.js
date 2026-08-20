62f7cabb4a408e2afc837591dc73d7139803664d
Adarsh Singh <anexus5919@gmail.com>
2026-08-21 02:43:45 +0530

    Fix dropdown suggestions requiring a double tap on touch devices (#26923)

--- src/main/js/components/dropdowns/combo-box.js
+++ src/main/js/components/dropdowns/combo-box.js
@@ -98,9 +98,16 @@ function init() {
 
           // otherwise menu won't hide on tab with nothing selected
           // needs delay as without that it blocks click selection of an item
-          e.addEventListener("focusout", () =>
-            setTimeout(() => e.dropdown.hide(), 200),
-          );
+          e.addEventListener("focusout", (event) => {
+            if (
+              event.relatedTarget &&
+              e.dropdown &&
+              e.dropdown.popper.contains(event.relatedTarget)
+            ) {
+              return;
+            }
+            setTimeout(() => e.dropdown && e.dropdown.hide(), 200);
+          });
 
           e.addEventListener(
             "input",
