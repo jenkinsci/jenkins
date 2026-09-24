0d0856943a7368c245d7930b49eb903be3d62536
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2026-09-24 09:13:58 +0100

    Update form-element-path.js

--- core/src/main/resources/jenkins/formelementpath/form-element-path.js
+++ core/src/main/resources/jenkins/formelementpath/form-element-path.js
@@ -202,12 +202,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  applyAll();
+  /* JavaScript sometimes re-arranges the DOM and doesn't call layout callback
+   * known cases: YUI buttons, CodeMirror.
+   * We run apply twice to work around this, once immediately so that most cases work and the tests don't need to wait,
+   * and once to catch the edge cases.
+   */
+  function hardenedApplyAll() {
+    applyAll();
 
-  Behaviour.addLoadEvent(applyAll);
+    setTimeout(function () {
+      applyAll();
+    }, 1000);
+  }
 
-  layoutUpdateCallback.add(applyAll);
+  hardenedApplyAll();
+
+  Behaviour.addLoadEvent(hardenedApplyAll);
+
+  layoutUpdateCallback.add(hardenedApplyAll);
 
   // expose this globally so that Selenium can call it
-  window.recomputeFormElementPath = applyAll;
+  window.recomputeFormElementPath = hardenedApplyAll;
 });
